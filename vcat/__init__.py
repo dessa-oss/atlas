5b2485f2f14251594d37eb80544097f1824d3338
Pippin Lee <pippinblee@gmail.com>
2018-05-16 15:57:17 -0400

    add provenance to pipeline arg filling behaviour

--- vcat/__init__.py
+++ vcat/__init__.py
@@ -185,13 +185,26 @@ class HyperparameterArgumentFill(object):
 class StageConnectorWrapperFill(object):
   def fill_arg_template(self, new_args, arg, kwargs):
     if isinstance(arg, StageConnectorWrapper):
-      new_args.append(arg.run_without_provenance(**kwargs))
+      new_args.append(arg.run(**kwargs))
       return True
     return False
     
   def fill_kwarg_template(self, new_kwargs, keyword, arg, kwargs):
     if isinstance(arg, StageConnectorWrapper):
-      new_kwargs[keyword] = arg.run_without_provenance(**kwargs)
+      new_kwargs[keyword] = arg.run(**kwargs)
+      return True
+    return False
+
+class HyperparameterArgumentNameFill(object):
+  def fill_arg_template(self, new_args, arg, kwargs):
+    if isinstance(arg, StageConnectorWrapper):
+      new_args.append("HYPE")
+      return True
+    return False
+    
+  def fill_kwarg_template(self, new_kwargs, keyword, arg, kwargs):
+    if isinstance(arg, StageConnectorWrapper):
+      new_kwargs[keyword] = "HYPE"
       return True
     return False
 
