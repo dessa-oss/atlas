6fbb1300d5b358be9eb29195c634f05f68f4fcf4
Thomas Rogers <tr@deeplearni.ng>
2018-05-17 13:45:28 -0400

    named jobs

--- deploy_spike.py
+++ deploy_spike.py
@@ -5,19 +5,17 @@ def print_it(self):
 
 pipe = pipeline | 'wonderful' | print_it
 
-def deploy(connector_wrapper, **kwargs):
+def deploy(job_name, connector_wrapper, **kwargs):
   import tarfile
   
   job = Job(pipe)
-  with open('job.bin', 'w+b') as file:
+  with open("job.bin", "w+b") as file:
     file.write(job.serialize())
 
-  tar = tarfile.open("job.tgz", "w:gz")
-
-  for name in ["job.bin", "run.sh", "main.py", "requirements.txt", "vcat"]:
-      tar.add(name, arcname="job/" + name)
-  tar.close()
+  with tarfile.open(job_name + ".tgz", "w:gz") as tar:
+    for name in ["job.bin", "run.sh", "main.py", "requirements.txt", "vcat"]:
+        tar.add(name, arcname=job_name + "/" + name)
   
   return job
 
-deploy(pipe)
+deploy('wonderful1', pipe)
