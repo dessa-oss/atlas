462c5d3b9f735e59792b43c4cf4eba84e5b1953e
Pippin Lee <pippinblee@gmail.com>
2018-05-17 10:56:43 -0400

    tar all files

--- deploy_spike.py
+++ deploy_spike.py
@@ -4,7 +4,19 @@ def print_it(self):
   print(self)
 
 pipe = pipeline | 'wonderful' | print_it
-job_123 = Job(pipe)
 
-with open('job.bin', 'w+b') as file:
-  file.write(job_123.serialize())
+def deploy(connector_wrapper, **kwargs):
+  import tarfile
+  
+  job = Job(pipe)
+  with open('job.bin', 'w+b') as file:
+    file.write(job.serialize())
+
+  tar = tarfile.open("job.tar", "w")
+  for name in ["job.bin", "main.sh", "main.py", "requirements.txt", "vcat"]:
+      tar.add(name)
+  tar.close()
+  
+  return job
+
+deploy(pipe)
