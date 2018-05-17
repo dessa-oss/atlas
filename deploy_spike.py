126c2ca9e9202b3db58b06cff69207c61b6c8d59
Thomas Rogers <tr@deeplearni.ng>
2018-05-17 11:20:34 -0400

    create a sub directory in the job archive

--- deploy_spike.py
+++ deploy_spike.py
@@ -12,9 +12,10 @@ def deploy(connector_wrapper, **kwargs):
   with open('job.bin', 'w+b') as file:
     file.write(job.serialize())
 
-  tar = tarfile.open("job.tar", "w")
-  for name in ["job.bin", "main.sh", "main.py", "requirements.txt", "vcat"]:
-      tar.add(name)
+  tar = tarfile.open("job.tgz", "w:gz")
+
+  for name in ["job.bin", "run.sh", "main.py", "requirements.txt", "vcat"]:
+      tar.add(name, arcname="job/" + name)
   tar.close()
   
   return job
