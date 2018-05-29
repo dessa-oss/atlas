9de87d548bc1703e4c795b7b2de87e8954fcb293
Thomas Rogers <tr@deeplearni.ng>
2018-05-29 16:43:38 -0400

    tweak deployment spike

--- test_job/deploy_spike.py
+++ test_job/deploy_spike.py
@@ -9,19 +9,19 @@ pipe.persist()
 job = Job(pipe)
 job_name = str(uuid.uuid4())
 
-bundle_name = str(uuid.uuid4())
-job_source_bundle = JobSourceBundle(bundle_name, '../')
-deployment = LocalShellJobDeployment(job_name, job, job_source_bundle)
-# deployment = GCPJobDeployment(job_name, job, job_source_bundle)
-# deployment.config()["result_savers"] = [
-#     GCPResultSaver,
-#     GCPBundledResultSaver
-# ]
-deployment.deploy()
-wait_for_deployment_to_complete(deployment)
-# raise_error_if_job_failed(deployment)
-result = deployment.fetch_job_results()
-print(result)
+# bundle_name = str(uuid.uuid4())
+# job_source_bundle = JobSourceBundle(bundle_name, '../')
+# deployment = LocalShellJobDeployment(job_name, job, job_source_bundle)
+# # deployment = GCPJobDeployment(job_name, job, job_source_bundle)
+# # deployment.config()["result_savers"] = [
+# #     GCPResultSaver,
+# #     GCPBundledResultSaver
+# # ]
+# deployment.deploy()
+# wait_for_deployment_to_complete(deployment)
+# # raise_error_if_job_failed(deployment)
+# result = deployment.fetch_job_results()
+# print(result)
 
 # pipe.run()
 # pipeline_context = pipe._pipeline_context
@@ -32,3 +32,16 @@ print(result)
 # bundler = JobBundler(job_name, {}, job)
 # bundler.bundle()
 # bundler.cleanup()
+
+pipeline_context.file_name = "test_job"
+pipe.run()
+pipeline_context.provenance.job_source_bundle = JobSourceBundle('test_job', '../')
+pipeline_context.provenance.job_source_bundle.bundle()
+pipeline_listing = GCPPipelineArchiveListing()
+with GCPPipelineArchive() as archive:
+# with LocalPipelineArchive() as archive:
+    archiver = PipelineArchiver(pipeline_context.file_name, pipeline_listing, archive, archive, archive, archive, archive, archive)
+    pipeline_context.save_to_archive(archiver)
+
+    pc = PipelineContext()
+    pc.load_from_archive(archiver)
