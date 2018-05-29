0d882cff6f9923a616fd8fb1c8519c5cf93b9a44
Thomas Rogers <tr@deeplearni.ng>
2018-05-29 13:20:38 -0400

    prepare to implement pipeline archiving

--- vcat_sdk/src/vcat/pipeline_context.py
+++ vcat_sdk/src/vcat/pipeline_context.py
@@ -6,11 +6,9 @@ class PipelineContext(object):
     def __init__(self):
         import uuid
 
-        self.predictions = {}
-        self.provenance = Provenance()
         self.file_name = str(uuid.uuid4()) + ".json"
+        self.provenance = Provenance()
         self.stage_contexts = {}
-
         self.global_stage_context = StageContext()
 
     def add_stage_context(self, stage_context):
@@ -21,6 +19,10 @@ class PipelineContext(object):
 
     def save(self, result_saver):
         result_saver.save(self.file_name, self._context())
+
+    def save_to_archive(self, archiver):
+        # archiver.append_miscellanous('pipeline_context', self.pipeline_context)
+        pass
 
     def _context(self):
         stringified_stage_contexts = {}
