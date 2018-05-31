9a21f2e137aba7e50ca3326d645a624e4ab3035d
Thomas Rogers <tr@deeplearni.ng>
2018-05-31 10:18:58 -0400

    use global cache

--- vcat_sdk/src/vcat/pipeline.py
+++ vcat_sdk/src/vcat/pipeline.py
@@ -4,7 +4,6 @@ from vcat.stage_smart_constructor import StageSmartConstructor
 from vcat.stage_connector_wrapper import StageConnectorWrapper
 from vcat.stage_context import StageContext
 from vcat.context_aware import ContextAware
-from vcat.null_cache import NullCache
 from vcat.utils import generate_uuid
 from vcat.utils import merged_uuids
 
@@ -13,7 +12,6 @@ class Pipeline(object):
 
     def __init__(self, pipeline_context):
         self.graph = StageGraph()
-        self.cache = NullCache()
         self.pipeline_context = pipeline_context
         self._stage_piping = StagePiping(self)
         self._uuid = generate_uuid('Humble beginnings...')
@@ -32,7 +30,7 @@ class Pipeline(object):
             self.uuid(), new_context, function, *args, **kwargs)
         stage_hierarchy = self.pipeline_context.provenance.stage_hierarchy
         stage_hierarchy.add_entry(current_stage, [self.uuid()])
-        return StageConnectorWrapper(self.graph.stage(self.cache, current_stage), self.pipeline_context, new_context)
+        return StageConnectorWrapper(self.graph.stage(current_stage), self.pipeline_context, new_context)
 
     def join(self, upstream_connector_wrappers, function, *args, **kwargs):
         upstream_connectors = [
@@ -48,7 +46,7 @@ class Pipeline(object):
             current_uuid, new_context, function, *args, **kwargs)
         stage_hierarchy = self.pipeline_context.provenance.stage_hierarchy
         stage_hierarchy.add_entry(current_stage, parent_uuids)
-        return StageConnectorWrapper(self.graph.join(self.cache, current_stage, upstream_connectors), self.pipeline_context, new_context)
+        return StageConnectorWrapper(self.graph.join(current_stage, upstream_connectors), self.pipeline_context, new_context)
 
     def __or__(self, stage_args):
         return self._stage_piping.pipe(stage_args)
