b81842c0ae73fdaa722de313cfd03d4f255622e8
Thomas Rogers <tr@deeplearni.ng>
2018-05-31 10:06:51 -0400

    code formatting

--- vcat_sdk/src/vcat/resources/main.py
+++ vcat_sdk/src/vcat/resources/main.py
@@ -6,39 +6,40 @@ import time
 import sys
 import dill as pickle
 
+
 def main():
-  job_source_bundle = JobSourceBundle('job', './')
+    job_source_bundle = JobSourceBundle('job', './')
 
-  config = config_manager.config
+    config = config_manager.config
 
-  job_name = config.get('job_name', 'job')
-  job_binary_path = job_name + '.bin'
+    job_name = config.get('job_name', 'job')
+    job_binary_path = job_name + '.bin'
 
-  with open(job_binary_path, 'rb') as file:
-    job = Job.deserialize(file.read())
+    with open(job_binary_path, 'rb') as file:
+        job = Job.deserialize(file.read())
 
-  pipeline_context = job.pipeline_context()
-  global_stage_context = pipeline_context.global_stage_context
-  pipeline_context.provenance.config.update(config)
-  config = pipeline_context.provenance.config
+    pipeline_context = job.pipeline_context()
+    global_stage_context = pipeline_context.global_stage_context
+    pipeline_context.provenance.config.update(config)
+    config = pipeline_context.provenance.config
 
-  pipeline_context.provenance.job_source_bundle = job_source_bundle
-  
-  def execute_job():
-    try:
-      job.run()
-      return None
-    except Exception as error:
-      exception_info = sys.exc_info()
-      global_stage_context.add_error_information(exception_info)
-      return exception_info
-  exception_info = global_stage_context.time_callback(execute_job)
+    pipeline_context.provenance.job_source_bundle = job_source_bundle
 
-  with open('results.pkl', 'w+b') as file:
-    pickle.dump(pipeline_context._context(), file)
+    def execute_job():
+        try:
+            job.run()
+            return None
+        except Exception as error:
+            exception_info = sys.exc_info()
+            global_stage_context.add_error_information(exception_info)
+            return exception_info
+    exception_info = global_stage_context.time_callback(execute_job)
 
-  if global_stage_context.error_information is not None:
-    raise exception_info[0], exception_info[1], exception_info[2]
+    with open('results.pkl', 'w+b') as file:
+        pickle.dump(pipeline_context._context(), file)
+
+    if global_stage_context.error_information is not None:
+        raise exception_info[0], exception_info[1], exception_info[2]
 
 if __name__ == "__main__":
-  main()
+    main()
