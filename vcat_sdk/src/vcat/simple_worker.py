027ac232361e90e1f22cad14ce93fc54c903933e
Thomas Rogers <tr@deeplearni.ng>
2018-06-04 13:36:19 -0400

    extract and cleanup job archive

--- vcat_sdk/src/vcat/simple_worker.py
+++ vcat_sdk/src/vcat/simple_worker.py
@@ -1,3 +1,4 @@
+from vcat.utils import tgz_archive_without_extension
 from vcat.local_file_system_bucket import LocalFileSystemBucket
 
 
@@ -19,7 +20,29 @@ class SimpleWorker(object):
             for path in self._code_bucket.list_files('*.tgz'):
                 if not path in started_jobs:
                     started_jobs.add(path)
-                    self._log.info('Running job %s', path)
-                    self._log.info('Job %s is complete', path)
+                    self._run_job(path)
 
             sleep(0.5)
+
+    def _run_job(self, archive_path):
+        try:
+            self._log.info('Running job %s', archive_path)
+            self._extract_archive(archive_path)
+            self._log.info('Job %s is complete', archive_path)
+
+        finally:
+            self._remove_job_directory(archive_path)
+
+    def _remove_job_directory(self, archive_path):
+        from shutil import rmtree
+        from os.path import basename
+        
+        archive_name = basename(archive_path)
+        job_path = tgz_archive_without_extension(archive_name)
+        rmtree(job_path)
+
+    def _extract_archive(self, archive_path):
+        import tarfile
+
+        with tarfile.open(archive_path, 'r:gz') as tar:
+            tar.extractall()
