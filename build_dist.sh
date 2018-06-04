24f44d207cf3cde747dd20e1d7914cc4ba7fa8b8
Jinnah Ali-Clarke <ja@deeplearni.ng>
2018-06-04 16:30:41 -0400

    stupid mistakes fixed

--- build_dist.sh
+++ build_dist.sh
@@ -11,21 +11,21 @@ elif [[ "${python_version}" = "python3" ]]
 then
     wheel_suffix="py3"
 else
-    echo "Usage: ./build_dist (python2|python3)"
+    echo "Usage: ./build_dist.sh (python2|python3)"
     exit 1
 fi
 
 cd vcat_sdk/ && \
-    ./build.sh && \
+    ${python_version} setup.py sdist bdist_wheel
     cd ../ && \
     ${python_version} -m pip install -U vcat_sdk/dist/vcat-0.0.1-${wheel_suffix}-none-any.whl
 
 cd gcp_utils/ && \
-    ./build.sh && \
+    ${python_version} setup.py sdist bdist_wheel
     cd ../ && \
     ${python_version} -m pip install -U gcp_utils/dist/vcat_gcp-0.0.1-${wheel_suffix}-none-any.whl
 
 cd ssh_utils/ && \
-    ./build.sh && \
+    ${python_version} setup.py sdist bdist_wheel
     cd ../ && \
     ${python_version} -m pip install -U ssh_utils/dist/vcat_ssh-0.0.1-${wheel_suffix}-none-any.whl
