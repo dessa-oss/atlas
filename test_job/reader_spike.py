31a739ff650d4a16ade510d368154457dc53ee30
Thomas Rogers <tr@deeplearni.ng>
2018-06-06 10:42:51 -0400

    remove reader spike

--- test_job/reader_spike.py
+++
@@ -1,9 +0,0 @@
-import cProfile
-from vcat import *
-
-
-def run_it(archive_listing, archive):
-    with JobPersister.load_archiver_fetch() as pipeline_archiver_fetch:
-        return ResultReader(pipeline_archiver_fetch)
-
-reader = run_it(GCPPipelineArchiveListing(), GCPPipelineArchive())
