c24b4dbc9cafe21099bce0b854f340a673ccf15d
Thomas Rogers <tr@deeplearni.ng>
2018-06-06 14:35:05 -0400

    finish gcp bucket implementation

--- gcp_utils/src/vcat_gcp/gcp_bucket.py
+++ gcp_utils/src/vcat_gcp/gcp_bucket.py
@@ -4,22 +4,22 @@ class GCPBucket(object):
         from google.cloud.storage import Client
 
         self._connection = Client()
-        self._result_bucket_connection = self._connection.get_bucket(name)
+        self._bucket = self._connection.get_bucket(name)
 
     def upload_from_string(self, name, data):
-        pass
+        self._blob(name).upload_from_string(data)
 
     def upload_from_file(self, name, input_file):
-        pass
+        self._blob(name).upload_from_file(input_file)
 
     def exists(self, name):
-        pass
+        return self._blob(name).exists()
 
     def download_as_string(self, name):
-        pass
+        return self._blob(name).download_as_string()
 
     def download_to_file(self, name, output_file):
-        pass
+        return self._blob(name).download_to_file(output_file)
 
     def list_files(self, pathname):
         from os.path import dirname
@@ -29,8 +29,11 @@ class GCPBucket(object):
         directory = dirname(pathname)
         path_filter = basename(pathname)
 
-        objects = self._result_bucket_connection.list_blobs(
+        objects = self._bucket.list_blobs(
             prefix=directory + '/', delimiter='/')
         object_names = [bucket_object.name for bucket_object in objects]
         object_file_names = [basename(path) for path in object_names]
         return filter(lambda path: fnmatch(path, path_filter), object_file_names)
+
+    def _blob(self, name):  
+        return self._bucket.blob(name)
