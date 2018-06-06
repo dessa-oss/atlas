c7237fbcfc43602845432047d549d4c9cf2c81cc
Thomas Rogers <tr@deeplearni.ng>
2018-06-06 13:00:53 -0400

    update spikes

--- test_job/ssh_bucket_spike.py
+++ test_job/ssh_bucket_spike.py
@@ -2,8 +2,12 @@ from vcat import *
 from vcat_ssh import *
 
 config_manager.config()['log_level'] = 'DEBUG'
+config_manager.config()['shell_command'] = '/bin/bash'
+config_manager.config()['remote_user'] = 'thomas'
+config_manager.config()['remote_host'] = 'localhost'
+config_manager.config()['key_path'] = '/home/thomas/.ssh/id_local'
 
-bucket = SSHFileSystemBucket('/bin/bash', 'thomas', 'localhost', '/home/thomas/.ssh/id_local', '/home/thomas/Dev/Spiking/vcat-results/tmp/archives')
+bucket = SSHFileSystemBucket('/home/thomas/Dev/Spiking/vcat-results/tmp/archives')
 
 files = bucket.list_files('*.tracker')
 print(files)
