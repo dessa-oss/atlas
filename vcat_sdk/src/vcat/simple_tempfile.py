e63408533efdd58a16399ad13ef6f67d7bb3806d
Thomas Rogers <tr@deeplearni.ng>
2018-06-06 12:37:57 -0400

    add more convenience functionality to SimpleTempfile

--- vcat_sdk/src/vcat/simple_tempfile.py
+++ vcat_sdk/src/vcat/simple_tempfile.py
@@ -3,7 +3,21 @@ class SimpleTempfile(object):
     def __init__(self, mode):
         self.path = None
         self.file = None
+        self.name = None
         self._mode = mode
+
+    def write(self, *args, **kwargs):
+        return self.file.write(*args, **kwargs)
+
+    def read(self, *args, **kwargs):
+        return self.file.read(*args, **kwargs)
+
+    def flush(self, *args, **kwargs):
+        return self.file.flush(*args, **kwargs)
+
+    def write_and_flush(self, data):
+        self.write(data)
+        self.flush()
 
     def __enter__(self):
         import tempfile
@@ -12,6 +26,7 @@ class SimpleTempfile(object):
         file_descriptor, self.path = tempfile.mkstemp()
         self.file = os.fdopen(file_descriptor, self._mode)
         self.file.__enter__()
+        self.name = self.file.name
 
         return self
 
@@ -19,7 +34,8 @@ class SimpleTempfile(object):
         import os
 
         try:
+            self.name = None
             self.file.__exit__()
+            self.file = None
         finally:
             os.remove(self.path)
-
