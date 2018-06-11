1c2710acf6e08cf6819a8ff1bb6c0ab63e0fb011
Pippin Lee <pippinblee@gmail.com>
2018-06-11 12:13:07 -0400

    fix virtualenv on windows

--- vcat_sdk/src/vcat/resources/run.sh
+++ vcat_sdk/src/vcat/resources/run.sh
@@ -5,8 +5,17 @@ BASEDIR=$(dirname "$0")
 cd $BASEDIR && \
   tar -xvf job.tgz && \
   python -m pip install virtualenv && \
-  virtualenv venv && \
-  . venv/bin/activate && \
+  virtualenv venv
+
+stat $BASEDIR/venv/bin/activate
+if [ $? -eq 0 ]; then 
+  activate_path=venv/bin/activate
+else
+  activate_path=venv/Scripts/activate
+fi
+
+cd $BASEDIR && \
+  . $activate_path && \
   python -m pip install -r requirements.txt && \
   python main.py
   
