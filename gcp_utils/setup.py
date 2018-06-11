2ef170cd9cbc54b903b48646f98e1b5b5c8afffd
Pippin Lee <pippinblee@gmail.com>
2018-06-11 09:52:20 -0400

    add license header

--- gcp_utils/setup.py
+++ gcp_utils/setup.py
@@ -1,8 +1,8 @@
-"""A setuptools based setup module.
-
-See:
-https://packaging.python.org/en/latest/distributing.html
-https://github.com/pypa/sampleproject
+"""
+Copyright (C) DeepLearning Financial Technologies Inc. - All Rights Reserved
+Unauthorized copying, distribution, reproduction, publication, use of this file, via any medium is strictly prohibited
+Proprietary and confidential
+Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 """
 
 # Always prefer setuptools over distutils
@@ -28,5 +28,6 @@ setup(
     package_dir={'':'src'},
     package_data={
         'vcat_gcp': ['resources/*'],
-    }
+    },
+    data_files = [("", ["LICENSE.txt"])]
 )
