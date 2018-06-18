cc3f1138849f8f04cd48c503032d03b8f68640c9
Jinnah Ali-Clarke <ja@deeplearni.ng>
2018-06-18 16:47:32 -0400

    added some headers + tests

--- tests/test_remote_exception.py
+++ tests/test_remote_exception.py
@@ -4,13 +4,3 @@ Unauthorized copying, distribution, reproduction, publication, use of this file,
 Proprietary and confidential
 Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 """
-
-import unittest
-
-def suite_factory():
-    loader = unittest.TestLoader()
-    return loader.discover(".", pattern="test*.py")
-
-if __name__ == "__main__":
-    runner = unittest.TextTestRunner()
-    runner.run(suite_factory())
