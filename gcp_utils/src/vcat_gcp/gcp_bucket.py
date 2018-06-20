56ca9ee6f01c974dac695f68fee91dda7a751b19
Thomas Rogers <tr@deeplearni.ng>
2018-06-20 16:25:32 -0400

    add copyright headers to gcp project

--- gcp_utils/src/vcat_gcp/gcp_bucket.py
+++ gcp_utils/src/vcat_gcp/gcp_bucket.py
@@ -1,3 +1,11 @@
+"""
+Copyright (C) DeepLearning Financial Technologies Inc. - All Rights Reserved
+Unauthorized copying, distribution, reproduction, publication, use of this file, via any medium is strictly prohibited
+Proprietary and confidential
+Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
+"""
+
+
 class GCPBucket(object):
 
     def __init__(self, name):
@@ -38,5 +46,5 @@ class GCPBucket(object):
         object_file_names = [basename(path) for path in object_names]
         return filter(lambda path: fnmatch(path, path_filter), object_file_names)
 
-    def _blob(self, name):  
+    def _blob(self, name):
         return self._bucket.blob(name)
--- gcp_utils/src/vcat_gcp/gcp_pipeline_archive_listing.py
+++ gcp_utils/src/vcat_gcp/gcp_pipeline_archive_listing.py
@@ -1,13 +1,23 @@
+"""
+Copyright (C) DeepLearning Financial Technologies Inc. - All Rights Reserved
+Unauthorized copying, distribution, reproduction, publication, use of this file, via any medium is strictly prohibited
+Proprietary and confidential
+Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
+"""
+
+
 class GCPPipelineArchiveListing(object):
 
     def __init__(self, bucket):
         from vcat_gcp.gcp_bucket import GCPBucket
         from vcat.prefixed_bucket import PrefixedBucket
 
-        self._gcp_bucket = PrefixedBucket('pipeline_archives', GCPBucket, bucket)
+        self._gcp_bucket = PrefixedBucket(
+            'pipeline_archives', GCPBucket, bucket)
 
     def track_pipeline(self, pipeline_name):
-        self._gcp_bucket.upload_from_string(pipeline_name + '.tracker', pipeline_name)
+        self._gcp_bucket.upload_from_string(
+            pipeline_name + '.tracker', pipeline_name)
 
     def get_pipeline_names(self):
         file_paths = self._gcp_bucket.list_files('*.tracker')
