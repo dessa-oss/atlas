b8c0b542b06242cb2b947ea172d0518e73e196a1
Thomas Rogers <tr@deeplearni.ng>
2018-06-20 16:42:26 -0400

    refactor ssh listing to use the ssh file system bucket

--- ssh_utils/src/vcat_ssh/ssh_listing.py
+++ ssh_utils/src/vcat_ssh/ssh_listing.py
@@ -5,42 +5,28 @@ Proprietary and confidential
 Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 """
 
-from vcat.utils import tgz_archive_without_extension
-from vcat_ssh.ssh_utils import SSHUtils
-
 
 class SSHListing(object):
 
     def __init__(self, path_filter='*.tgz'):
         from vcat.global_state import config_manager
+        from vcat_ssh.ssh_file_system_bucket import SSHFileSystemBucket
+
         self._config = config_manager.config()
-        self._ssh_utils = SSHUtils(self._config)
         self._path_filter = path_filter
+        self._bucket = SSHFileSystemBucket(self._result_path())
 
     def track_pipeline(self, pipeline_name):
         pass
 
     def get_pipeline_names(self):
+        from vcat.utils import tgz_archive_without_extension
+
         archives = self._list_archive_names()
         return [tgz_archive_without_extension(archive_name) for archive_name in archives]
 
     def _list_archive_names(self):
-        from os.path import basename
-
-        archives = self._list_archives()
-        return [basename(path) for path in archives]
-
-    def _list_archives(self):
-        from subprocess import Popen
-        from subprocess import PIPE
-        from vcat.utils import split_process_output
-
-        command = self._list_archives_command()
-        stdout, _, _ = self._ssh_utils.execute_command(command)
-        return split_process_output(stdout)
-
-    def _list_archives_command(self):
-        return self._ssh_utils.command_in_ssh_command('ls -1 ' + self._result_path() + '/' + self._path_filter)
+        return self._bucket.list_files(self._path_filter)
 
     def _result_path(self):
         return self._config['result_path']
