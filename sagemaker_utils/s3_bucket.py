63d47111ebbe04f7c5a94ca2acfde947674a5b6b
Jinnah Ali-Clarke <ja@deeplearni.ng>
2018-06-21 15:51:59 -0400

    got s3 bucket working

---
+++ sagemaker_utils/s3_bucket.py
@@ -0,0 +1,43 @@
+# assumes python 3
+
+class S3Bucket(object):
+    def __init__(self, bucket_name):
+        import boto3
+        self._bucket = boto3.resource('s3').Bucket(bucket_name)
+
+    def upload_from_string(self, name, data):
+        self._bucket.put_object(Key=name, Body=byte_string(data))
+
+    def upload_from_file(self, name, input_file):
+        self._bucket.upload_file(input_file, name)
+
+    def exists(self, name):
+        objs = self._objs_with_prefix(name)
+        
+        for obj in objs:
+            if obj.key == name:
+                return True
+
+        return False
+
+    def download_as_string(self, name):
+        objs = self._objs_with_prefix(name)
+        for obj in objs:
+            if obj.key == name:
+                return byte_string(obj.get()['Body'].read())
+
+    def download_to_file(self, name, output_file):
+        self._bucket.download_file(name, output_file)
+
+    def list_files(self, pathname):
+        objs = self._objs_with_prefix(pathname)
+        return [obj.key for obj in objs]
+
+    def _objs_with_prefix(self, prefix):
+        return self._bucket.objects.filter(Prefix=prefix)
+
+def byte_string(string):
+    if isinstance(string, bytes):
+        return string
+    else:
+        return bytes(string.encode('utf-8', 'ignore'))
