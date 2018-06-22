4d4fc0b543f067edeb32a49493cf55dd2dd917ec
Thomas Rogers <tr@deeplearni.ng>
2018-06-22 10:22:16 -0400

    create a bucket job deployment to refactor deployments that use similar behaviour; add missing copyright headers

--- ssh_utils/src/vcat_ssh/sftp_job_deployment.py
+++ ssh_utils/src/vcat_ssh/sftp_job_deployment.py
@@ -9,71 +9,36 @@ Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 class SFTPJobDeployment(object):
 
     def __init__(self, job_name, job, job_source_bundle):
-        from vcat.job_bundler import JobBundler
-        from vcat.global_state import config_manager
+        from vcat.bucket_job_deployment import BucketJobDeployment
+        from vcat_ssh.sftp_bucket import SFTPBucket
 
-        self._config = {}
-        self._config.update(config_manager.config())
-
-        self._job_name = job_name
-        self._job = job
-        self._job_bundler = JobBundler(
-            self._job_name, self._config, self._job, job_source_bundle)
-        self._code_bucket = None
-        self._result_bucket = None
+        self._deployment = BucketJobDeployment(
+            job_name,
+            job,
+            job_source_bundle,
+            SFTPBucket(self._code_path()),
+            SFTPBucket(self._result_path())
+        )
 
     def config(self):
-        return self._config
+        return self._deployment.config()
 
     def job_name(self):
-        return self._job_name
+        return self._deployment.job_name()
 
     def deploy(self):
-        self._job_bundler.bundle()
-        try:
-            with open(self._job_bundler.job_archive(), 'rb') as file:
-                self._get_code_bucket().upload_from_file(self._job_bundler.job_archive_name(), file)
-        finally:
-            self._job_bundler.cleanup()
+        return self._deployment.deploy()
 
     def is_job_complete(self):
-        return self._get_result_bucket().exists(self._job_bundler.job_archive_name())
+        return self._deployment.is_job_complete()
 
     def fetch_job_results(self):
-        from os.path import basename
-        import tarfile
-        from vcat.simple_tempfile import SimpleTempfile
-        from vcat.serializer import deserialize_from_file
-
-        result = None
-        with SimpleTempfile('w+b') as temp_file:
-            self._get_result_bucket().download_to_file(
-                self._job_bundler.job_archive_name(), temp_file)
-            with tarfile.open(temp_file.name, 'r:gz') as tar:
-                for tarinfo in tar:
-                    if basename(tarinfo.name) == "results.pkl":
-                        file = tar.extractfile(tarinfo)
-                        result = deserialize_from_file(file)
-                        file.close()
-
-        return result
-
-    def _get_code_bucket(self):
-        from vcat_ssh.sftp_bucket import SFTPBucket
-
-        if self._code_bucket is None:
-            self._code_bucket = SFTPBucket(self._code_path())
-        return self._code_bucket
-
-    def _get_result_bucket(self):
-        from vcat_ssh.sftp_bucket import SFTPBucket
-
-        if self._result_bucket is None:
-            self._result_bucket = SFTPBucket(self._result_path())
-        return self._result_bucket
+        return self._deployment.fetch_job_results()
 
     def _code_path(self):
-        return self._config['code_path']
+        from vcat.global_state import config_manager
+        return config_manager['code_path']
 
     def _result_path(self):
-        return self._config['result_path']
+        from vcat.global_state import config_manager
+        return config_manager['result_path']
--- vcat_sdk/note.py
+++ vcat_sdk/note.py
@@ -1,3 +1,9 @@
+"""
+Copyright (C) DeepLearning Financial Technologies Inc. - All Rights Reserved
+Unauthorized copying, distribution, reproduction, publication, use of this file, via any medium is strictly prohibited
+Proprietary and confidential
+Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
+"""
 from vcat import *
 from pandas import DataFrame
 
@@ -5,9 +11,11 @@ from pandas import DataFrame
 def create_data_frame():
     return DataFrame([[0]], columns=["hello"])
 
+
 def print_it(thing):
-  print(thing)
-  return thing
+    print(thing)
+    return thing
+
 
 data_frame = pipeline | create_data_frame
 something = data_frame["hello"].count() | print_it
