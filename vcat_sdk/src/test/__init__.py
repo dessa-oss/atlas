971d61dd670056e2aa663e81cea5e67689fbf92e
Thomas Rogers <tr@deeplearni.ng>
2018-06-26 14:18:32 -0400

    include copyright headers

--- vcat_sdk/src/test/__init__.py
+++ vcat_sdk/src/test/__init__.py
@@ -1,2 +1,9 @@
+"""
+Copyright (C) DeepLearning Financial Technologies Inc. - All Rights Reserved
+Unauthorized copying, distribution, reproduction, publication, use of this file, via any medium is strictly prohibited
+Proprietary and confidential
+Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
+"""
+
 from .test_staged_module_loader import TestStagedModuleLoader
 from .test_staged_meta_finder import TestStagedMetaFinder
