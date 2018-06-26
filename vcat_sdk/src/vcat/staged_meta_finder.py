58824440db8efc46b9fcc48eca77aaaf01c4942b
Thomas Rogers <tr@deeplearni.ng>
2018-06-26 14:11:01 -0400

    refactor StageMetaFinder

--- vcat_sdk/src/vcat/staged_meta_finder.py
+++ vcat_sdk/src/vcat/staged_meta_finder.py
@@ -2,16 +2,46 @@ from importlib.abc import MetaPathFinder
 
 
 class StagedMetaFinder(MetaPathFinder):
+    """Used to create define specs for loading staged modules in importing modules
+    """
+
     STAGED_PREFIX = 'staged_'
+    STAGED_PREFIX_LENGTH = len(STAGED_PREFIX)
 
     def find_spec(self, fullname, path, target=None):
-      from vcat.staged_module_loader import StagedModuleLoader
-      from importlib import import_module
-      from importlib.util import spec_from_file_location
+        """Find a module spec for loading
 
-      if fullname.startswith(StagedMetaFinder.STAGED_PREFIX):
-            module_name = fullname[len(StagedMetaFinder.STAGED_PREFIX):]
-            inner_module = import_module(module_name)
-            return spec_from_file_location(fullname, loader=StagedModuleLoader(inner_module))
+        Arguments:
+          fullname {str} -- Name of the module
+          path {str} -- Unused
 
-      return None
+        Keyword Arguments:
+          target {str} -- Unused (default: {None})
+
+        Returns:
+          importlib._bootstrap.ModuleSpec -- [description]
+        """
+
+        if self._is_staged_module(fullname):
+            return self._load_spec(fullname)
+
+        return None
+
+    def _load_spec(self, staged_name):
+        from vcat.staged_module_loader import StagedModuleLoader
+        from importlib.util import spec_from_file_location
+
+        inner_module = self._find_module(staged_name)
+        return spec_from_file_location(staged_name, loader=StagedModuleLoader(inner_module))
+
+    def _find_module(self, staged_named):
+        from importlib import import_module
+
+        module_name = self._module_without_staged_prefix(staged_named)
+        return import_module(module_name)
+
+    def _module_without_staged_prefix(self, fullname):
+        return fullname[StagedMetaFinder.STAGED_PREFIX_LENGTH:]
+
+    def _is_staged_module(self, fullname):
+        return fullname.startswith(StagedMetaFinder.STAGED_PREFIX)
