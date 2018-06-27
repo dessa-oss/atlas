c876587e2733600d9628aa85b130bf092666f370
Thomas Rogers <tr@deeplearni.ng>
2018-06-27 16:07:02 -0400

    add missing copyright headers

--- vcat_sdk/src/acceptance/fixtures/stages.py
+++ vcat_sdk/src/acceptance/fixtures/stages.py
@@ -1,7 +1,16 @@
+"""
+Copyright (C) DeepLearning Financial Technologies Inc. - All Rights Reserved
+Unauthorized copying, distribution, reproduction, publication, use of this file, via any medium is strictly prohibited
+Proprietary and confidential
+Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
+"""
+
 import random
+
 
 def bundle_value(value):
     return value
+
 
 def add(a, b):
     return a + b
