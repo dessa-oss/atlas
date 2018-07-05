19e10197f755fbdf5785cbd1f7fd233398788c84
Thomas Rogers <tr@deeplearni.ng>
2018-07-05 14:27:48 -0400

    properly configure job listing

--- examples/config/__init__.py
+++ examples/config/__init__.py
@@ -5,10 +5,27 @@ Proprietary and confidential
 Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 """
 
-from vcat import config_manager, LocalFileSystemCacheBackend
+from vcat import config_manager, LocalFileSystemCacheBackend, LocalFileSystemPipelineListing, LocalFileSystemPipelineArchive
 from uuid import uuid4
 
+# configure caching layer
 config_manager['cache_implementation'] = {
     'cache_type': LocalFileSystemCacheBackend,
     'constructor_arguments': ['/tmp/vcat_example_' + str(uuid4())],
 }
+
+# configure all archive types to use the sample implementation
+archive_implementation = {
+    'archive_type': LocalFileSystemPipelineArchive,
+    'constructor_arguments': ['tmp/archives'],
+}
+config_manager['archive_listing_implementation'] = {
+    'archive_listing_type': LocalFileSystemPipelineListing,
+    'constructor_arguments': ['tmp/archives'],
+}
+config_manager['stage_log_archive_implementation'] = archive_implementation
+config_manager['persisted_data_archive_implementation'] = archive_implementation
+config_manager['provenance_archive_implementation'] = archive_implementation
+config_manager['job_source_archive_implementation'] = archive_implementation
+config_manager['artifact_archive_implementation'] = archive_implementation
+config_manager['miscellaneous_archive_implementation'] = archive_implementation
