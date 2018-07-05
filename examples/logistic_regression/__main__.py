c6e770a8050ea1cfdc4ade1ad695d521899b9c22
Thomas Rogers <tr@deeplearni.ng>
2018-07-05 10:41:54 -0400

    refactor logistic regression example into large stages and smaller execution

--- examples/logistic_regression/__main__.py
+++ examples/logistic_regression/__main__.py
@@ -7,62 +7,35 @@ Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 
 import vcat
 import config
-from staged_common.data import load_titanic
-from staged_common.prep import get_mode, fillna, one_hot_encode, encode, assign_columns, union, train_imputer, train_one_hot_encoder, drop_non_numeric
-from staged_common.models import train_logistic_regression, get_metrics
-from staged_sklearn.model_selection import train_test_split
-from staged_common.one_hot_encoder import OneHotEncoder
-from staged_sklearn.preprocessing import Imputer
-from staged_common.logging import log_data, log_formatted
+from staged_common.prep import union
+from staged_common.models import train_logistic_regression
+from staged_common.logging import log_formatted
+from staged_titanic.etl import load_data, fill_categorical_nulls, split_inputs_and_targets, split_training_and_validation, impute, one_hot_encode, drop_non_numeric_columns, get_metrics
+
 
 def main():
-    # load the data set
-    data = load_titanic()
+    # data prep
+    data = load_data()
+    data = fill_categorical_nulls(data)
+    inputs, targets = split_inputs_and_targets(data).splice(2)
 
-    # replace null category values with real ones
-    data = fillna(data, 'Cabin', 'NULLCABIN')
-    data = fillna(data, 'Embarked', 'NULLEMBARKED')
+    # feature engineering
+    x_train, x_valid, y_train, y_valid = split_training_and_validation(
+        inputs, targets).splice(4)
+    x_train, x_valid = impute(x_train, x_valid).splice(2)
+    x_train, x_valid = one_hot_encode(x_train, x_valid).splice(2)
+    x_train, x_valid = drop_non_numeric_columns(x_train, x_valid).splice(2)
 
-    # input/target split
-    inputs = data[['Age', 'SibSp', 'Parch', 'Fare',
-                   'Pclass', 'Sex', 'Cabin', 'Embarked']]
-    targets = data[['Survived']]
-
-    # train/validation split
-    x_train, x_valid, y_train, y_valid = train_test_split(
-        inputs, targets, test_size=0.2, random_state=42, stratify=targets.as_matrix()).splice(4)
-
-    # impute numeric columns using mean
-    encoder = train_imputer(x_train, ['Age', 'Fare'])
-    x_train = encoder.transform(x_train)
-    x_valid = encoder.transform(x_valid)
-
-    # one hot encode categorical columns
-    encoder = train_one_hot_encoder(x_train, ['Pclass', 'Sex', 'Cabin', 'Embarked'])
-    x_train = encoder.transform(x_train)
-    x_valid = encoder.transform(x_valid)
-
-    # remove non numerical columns
-    x_train = drop_non_numeric(x_train)
-    x_valid = drop_non_numeric(x_valid)
-
-    # train the model
+    # model training and scoring
     model = train_logistic_regression(x_train, y_train)
-
-    # retrieve training predictions and metrics
-    y_train_predications, train_score = get_metrics(model, x_train, y_train).splice(2)
-    y_train = assign_columns(y_train, 'PredictedSurvived', y_train_predications)
-    y_train = assign_columns(y_train, 'Type', 'Training')
-
-    # retrieve validation predictions and metrics
-    y_valid_predications, valid_score = get_metrics(model, x_valid, y_valid).splice(2)
-    y_valid = assign_columns(y_valid, 'PredictedSurvived', y_valid_predications)
-    y_train = assign_columns(y_valid, 'Type', 'Validation')
-
+    y_train, train_score = get_metrics(model, x_train, y_train).splice(2)
+    y_valid, valid_score = get_metrics(model, x_valid, y_valid).splice(2)
     results = union(y_train, y_valid)
 
     # print out the results
-    log_formatted('\nData: {}\nTraining score was {}\nValidation score was {}', results, train_score, valid_score).run_same_process()
+    log_formatted('\nData: {}\nTraining score was {}\nValidation score was {}',
+                  results, train_score, valid_score).run()
+
 
 if __name__ == '__main__':
     main()
---
+++ examples/titanic/etl.py
@@ -0,0 +1,68 @@
+"""
+Copyright (C) DeepLearning Financial Technologies Inc. - All Rights Reserved
+Unauthorized copying, distribution, reproduction, publication, use of this file, via any medium is strictly prohibited
+Proprietary and confidential
+Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
+"""
+
+from common.data import load_titanic
+from common.prep import fillna, train_imputer, train_one_hot_encoder, drop_non_numeric
+from common.models import get_metrics_internal
+from sklearn.model_selection import train_test_split
+
+
+def load_data():
+    return load_titanic()
+
+
+def fill_categorical_nulls(data):
+    data = fillna(data, 'Cabin', 'NULLCABIN')
+    data = fillna(data, 'Embarked', 'NULLEMBARKED')
+    return data
+
+
+def split_inputs_and_targets(data):
+    inputs = data[['Age', 'SibSp', 'Parch', 'Fare',
+                   'Pclass', 'Sex', 'Cabin', 'Embarked']]
+    targets = data[['Survived']]
+    return [inputs, targets]
+
+
+def split_training_and_validation(inputs, targets):
+    return train_test_split(inputs, targets, test_size=0.2, random_state=42, stratify=targets.as_matrix())
+
+
+def create_imputer(data):
+    return train_imputer(data, ['Age', 'Fare'])
+
+
+def impute(x_train, x_valid):
+    encoder = create_imputer(x_train)
+    x_train = encoder.transform(x_train)
+    x_valid = encoder.transform(x_valid)
+    return [x_train, x_valid]
+
+
+def create_one_hot_encoder(data):
+    return train_one_hot_encoder(data, ['Pclass', 'Sex', 'Cabin', 'Embarked'])
+
+
+def one_hot_encode(x_train, x_valid):
+    encoder = create_one_hot_encoder(x_train)
+    x_train = encoder.transform(x_train)
+    x_valid = encoder.transform(x_valid)
+    return [x_train, x_valid]
+
+
+def drop_non_numeric_columns(x_train, x_valid):
+    x_train = drop_non_numeric(x_train)
+    x_valid = drop_non_numeric(x_valid)
+    return [x_train, x_valid]
+
+
+def get_metrics(model, inputs, targets):
+    targets_predications, train_score = get_metrics_internal(
+        model, inputs, targets)
+    targets['PredictedSurvived'] = targets_predications
+    targets['Type'] = 'Training'
+    return [targets, train_score]
