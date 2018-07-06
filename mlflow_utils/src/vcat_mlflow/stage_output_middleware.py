164d26e4b7b4803511b62d360e21a889b708cd6f
Thomas Rogers <tr@deeplearni.ng>
2018-07-06 11:42:39 -0400

    create docs for stage output middleware

--- mlflow_utils/src/vcat_mlflow/stage_output_middleware.py
+++ mlflow_utils/src/vcat_mlflow/stage_output_middleware.py
@@ -7,12 +7,35 @@ Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 
 
 class StageOutputMiddleware(object):
+    """MLFlow integration for creating artifacts from stage outputs that also
+    supports saving csv files to make easier viewing on the dashboard
+
+    Arguments:
+        pipeline_context {PipelineContext} -- Unused
+        stage_config {StageConfig} -- Unused
+        stage_context {StageContext} -- Used to determine if and what to store as an artifact
+        stage {Stage} -- Stage to fetch meta data from
+    """
 
     def __init__(self, pipeline_context, stage_config, stage_context, stage):
         self._stage_context = stage_context
         self._stage = stage
 
     def call(self, upstream_result_callback, filler_builder, filler_kwargs, args, kwargs, callback):
+        """Executes the middleware
+
+        Arguments:
+            upstream_result_callback {function} -- Unused
+            filler_builder {[type]} -- Unused
+            filler_kwargs {[type]} -- Unused
+            args {[type]} -- Passed to callback
+            kwargs {[type]} -- Passed to callback
+            callback {function} -- Callback to call before storing the output
+
+        Returns:
+            Object -- The result of calling callback
+        """
+
         from vcat.simple_tempfile import SimpleTempfile
         from vcat.serializer import serialize_to_file
         from pandas import DataFrame
