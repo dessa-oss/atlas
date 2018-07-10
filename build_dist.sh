094c105b3dffe18794d17867b7d92f160d508ea3
Jinnah Ali-Clarke <ja@deeplearni.ng>
2018-07-10 12:15:24 -0400

    Revert "there we go"
    This reverts commit 041a532e37ce6bb2f5c832e9af27dad300b4ceaa.

--- build_dist.sh
+++ build_dist.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
-wheel_suffix=`python -c "import sys; print(sys.version_info.major)"`
+python_version=$1
+
+wheel_suffix=""
+
+if [[ "${python_version}" = "python2" ]]
+then
+    wheel_suffix="py2"
+elif [[ "${python_version}" = "python3" ]]
+then
+    wheel_suffix="py3"
+else
+    echo "Usage: ./build_dist.sh (python2|python3)"
+    exit 1
+fi
 
 cd vcat_sdk/ && \
-    python setup.py sdist bdist_wheel && \
+    ${python_version} setup.py sdist bdist_wheel && \
     cd ../ && \
-    python -m pip install -U vcat_sdk/dist/vcat-0.0.1-py${wheel_suffix}-none-any.whl && \
+    ${python_version} -m pip install -U vcat_sdk/dist/vcat-0.0.1-${wheel_suffix}-none-any.whl && \
     cd gcp_utils/ && \
-    python setup.py sdist bdist_wheel && \
+    ${python_version} setup.py sdist bdist_wheel && \
     cd ../ && \
-    python -m pip install -U gcp_utils/dist/vcat_gcp-0.0.1-py${wheel_suffix}-none-any.whl && \
+    ${python_version} -m pip install -U gcp_utils/dist/vcat_gcp-0.0.1-${wheel_suffix}-none-any.whl && \
     cd ssh_utils/ && \
-    python setup.py sdist bdist_wheel &&\
+    ${python_version} setup.py sdist bdist_wheel &&\
     cd ../ && \
-    python -m pip install -U ssh_utils/dist/vcat_ssh-0.0.1-py${wheel_suffix}-none-any.whl
+    ${python_version} -m pip install -U ssh_utils/dist/vcat_ssh-0.0.1-${wheel_suffix}-none-any.whl
