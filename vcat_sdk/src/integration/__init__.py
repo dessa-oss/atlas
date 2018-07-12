50d00128e4b6fe8d7bd388d413d1dd4d48081c69
Thomas Rogers <tr@deeplearni.ng>
2018-07-12 09:15:24 -0400

    refactor configuration for integration tests

--- vcat_sdk/src/integration/__init__.py
+++ vcat_sdk/src/integration/__init__.py
@@ -7,12 +7,9 @@ Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 
 from uuid import uuid4
 import vcat
+import integration.config
 from integration.test_pipeline_interface import TestPipelineInterface
 from integration.test_run_stages import TestRunStages
 from integration.test_placeholder_parameters import TestPlaceHolderParameters
 from integration.test_caching import TestCaching
-
-vcat.config_manager['cache_implementation'] = {
-    'cache_type': vcat.LocalFileSystemCacheBackend,
-    'constructor_arguments': ['/tmp/vcat_example_' + str(uuid4())],
-}
+from integration.test_result_reader import TestResultReader
---
+++ vcat_sdk/src/integration/config.py
@@ -0,0 +1,14 @@
+"""
+Copyright (C) DeepLearning Financial Technologies Inc. - All Rights Reserved
+Unauthorized copying, distribution, reproduction, publication, use of this file, via any medium is strictly prohibited
+Proprietary and confidential
+Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
+"""
+
+from uuid import uuid4
+from vcat import config_manager, LocalFileSystemCacheBackend
+
+config_manager['cache_implementation'] = {
+    'cache_type': LocalFileSystemCacheBackend,
+    'constructor_arguments': ['/tmp/vcat_example_' + str(uuid4())],
+}
