560a9cfda37f981e248d2a658ab48b3939a93300
Thomas Rogers <tr@deeplearni.ng>
2018-07-12 10:12:10 -0400

    create integration tests for creating and fetching job information

--- vcat_sdk/src/integration/config.py
+++ vcat_sdk/src/integration/config.py
@@ -5,10 +5,38 @@ Proprietary and confidential
 Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 """
 
-from uuid import uuid4
-from vcat import config_manager, LocalFileSystemCacheBackend
 
-config_manager['cache_implementation'] = {
-    'cache_type': LocalFileSystemCacheBackend,
-    'constructor_arguments': ['/tmp/vcat_example_' + str(uuid4())],
-}
+def _config():
+    from uuid import uuid4
+    from os import getcwd
+    from vcat import config_manager, LocalFileSystemCacheBackend, LocalFileSystemPipelineArchive, LocalFileSystemPipelineListing
+
+    # separates test runs
+    test_uuid = uuid4()
+
+    # below is used to ensure we get a different cache for every run
+    config_manager['cache_implementation'] = {
+        'cache_type': LocalFileSystemCacheBackend,
+        'constructor_arguments': ['/tmp/vcat_example_{}'.format(test_uuid)],
+    }
+
+    # below is used to create archives for all different types
+    archive_root = getcwd() + '/tmp/archives_{}'.format(test_uuid)
+
+    archive_implementation = {
+        'archive_type': LocalFileSystemPipelineArchive,
+        'constructor_arguments': [archive_root],
+    }
+    config_manager['archive_listing_implementation'] = {
+        'archive_listing_type': LocalFileSystemPipelineListing,
+        'constructor_arguments': [archive_root],
+    }
+    config_manager['stage_log_archive_implementation'] = archive_implementation
+    config_manager['persisted_data_archive_implementation'] = archive_implementation
+    config_manager['provenance_archive_implementation'] = archive_implementation
+    config_manager['job_source_archive_implementation'] = archive_implementation
+    config_manager['artifact_archive_implementation'] = archive_implementation
+    config_manager['miscellaneous_archive_implementation'] = archive_implementation
+
+
+_config()
---
+++ vcat_sdk/src/integration/test_result_reader.py
@@ -0,0 +1,81 @@
+"""
+Copyright (C) DeepLearning Financial Technologies Inc. - All Rights Reserved
+Unauthorized copying, distribution, reproduction, publication, use of this file, via any medium is strictly prohibited
+Proprietary and confidential
+Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
+"""
+
+import unittest
+from vcat import Pipeline, PipelineContext, Job, JobPersister, ResultReader
+
+
+class TestResultReader(unittest.TestCase):
+
+    def test_creates_job_information(self):
+        def method():
+            pass
+
+        stage = self._make_pipeline().stage(method)
+        self._run_and_persist(stage)
+
+        job_information = self._create_reader_and_get_job_information(stage)
+        self.assertFalse(job_information.empty)
+
+    def test_stores_hierarchy(self):
+        def method():
+            pass
+
+        pipeline = self._make_pipeline()
+        stage = pipeline.stage(method)
+        self._run_and_persist(stage)
+
+        job_information = self._create_reader_and_get_job_information(stage)
+        parent_list = job_information['parent_ids'].iloc[0]
+        self.assertIn(pipeline.uuid(), parent_list)
+
+    def test_stores_stage_name(self):
+        def method():
+            pass
+
+        stage = self._make_pipeline().stage(method)
+        self._run_and_persist(stage)
+
+        job_information = self._create_reader_and_get_job_information(stage)
+        stage_name = job_information['stage_name'].iloc[0]
+        self.assertEqual('method', stage_name)
+
+    def test_stores_stage_uuid(self):
+        def method():
+            pass
+
+        stage = self._make_pipeline().stage(method)
+        self._run_and_persist(stage)
+
+        job_information = self._create_reader_and_get_job_information(stage)
+        stage_uuid = job_information['stage_id'].iloc[0]
+        self.assertEqual(stage.uuid(), stage_uuid)
+
+    def _create_reader_and_get_job_information(self, stage):
+        with JobPersister.load_archiver_fetch() as archiver:
+            reader = ResultReader(archiver)
+            return self._get_job_information(reader, stage)
+
+    def _run_and_persist(self, stage):
+        stage.run_same_process()
+        self._persist_stage(stage)
+
+    def _get_job_information(self, reader, stage):
+        job_name = stage.pipeline_context().file_name
+        job_information = reader.get_job_information()
+        return job_information[job_information['pipeline_name'] == job_name]
+
+    def _make_pipeline(self):
+        return Pipeline(PipelineContext())
+
+    def _persist_stage(self, stage):
+        persister = self._make_persister(stage)
+        persister.persist()
+
+    def _make_persister(self, stage, **kwargs):
+        job = Job(stage, **kwargs)
+        return JobPersister(job)
