64356e55c8820780744255af9631eefa3bc09618
Thomas Rogers <tr@deeplearni.ng>
2018-07-16 10:04:29 -0400

    create acceptance test for duplicating jobs

--- vcat_sdk/src/acceptance/__init__.py
+++ vcat_sdk/src/acceptance/__init__.py
@@ -10,3 +10,4 @@ import vcat
 import acceptance.config
 from acceptance.test_pipeline_interface import TestPipelineInterface
 from acceptance.test_run_job import TestRunJob
+from acceptance.test_duplicate_job_source import TestDuplicateJobSource
