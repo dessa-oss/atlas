732ab79871fc88fe75db1d37e3dedd50c260589e
Pippin Lee <pippinblee@gmail.com>
2018-07-25 17:15:15 -0400

    more get logger test

--- foundations_sdk/src/test/test_log_manager.py
+++ foundations_sdk/src/test/test_log_manager.py
@@ -13,10 +13,15 @@ class TestLogManager(unittest.TestCase):
 
     def test_get_logger_if_string_value(self):
         log_manager = LogManager()
-        logging = log_manager.get_logger('test once').info('Adding current directory to source bundle')
-        self.assertEqual(None , logging)
+        logging = log_manager.get_logger('plato').info('Adding current directory to source bundle')
+        self.assertEqual(None, logging)
 
     def test_get_logger_if_none_value(self):
         log_manager = LogManager()
-        logging = log_manager.get_logger(None).info('None')
-        self.assertEqual(None , logging)
+        logging = log_manager.get_logger(None).info('Tests with none value')
+        self.assertEqual(None, logging)
+
+    def test_get_logger_with_template_string(self):
+        log_manager = LogManager()
+        test_value = 'a thing'
+        logging = log_manager.get_logger('plato').info('Adding current directory to source bundle %s', test_value)
