cb81054954e233f37a9da7b2540078cc7c976568
Thomas Rogers <tr@deeplearni.ng>
2018-07-30 11:42:50 -0400

    FOUND-89 hard code path for expected job source content to avoid issues in python2

--- foundations_sdk/src/acceptance/test_duplicate_job_source.py
+++ foundations_sdk/src/acceptance/test_duplicate_job_source.py
@@ -13,6 +13,8 @@ class TestDuplicateJobSource(unittest.TestCase):
     def setUp(self):
         from acceptance.cleanup import cleanup
         cleanup()
+        with open('acceptance/test_duplicate_job_source.py', 'r') as file:
+            self.file_content = file.read()
 
     def test_can_duplicate_job_source(self):
         import acceptance.fixtures.stages as stages
@@ -30,10 +32,7 @@ class TestDuplicateJobSource(unittest.TestCase):
             duplicate_path = 'tmp/duplicate_jobs_{}'.format(TEST_UUID)
             reader.create_working_copy(deployment.job_name(), duplicate_path)
 
-            with open(__file__, 'r') as file:
-                expected_content = file.read()
-
             with open('{}/acceptance/test_duplicate_job_source.py'.format(duplicate_path), 'r') as file:
                 result_content = file.read()
 
-            self.assertEqual(expected_content, result_content)
+            self.assertEqual(self.file_content, result_content)
