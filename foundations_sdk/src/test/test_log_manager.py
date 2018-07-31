ccb8cf520e7185c67e042d5674dac52dd6e9ee85
Pippin Lee <pippinblee@gmail.com>
2018-07-31 14:23:06 -0400

    log manager tests for stdout, namespaces, log level

--- foundations_sdk/src/test/test_log_manager.py
+++ foundations_sdk/src/test/test_log_manager.py
@@ -10,32 +10,108 @@ from foundations.log_manager import LogManager
 
 
 class TestLogManager(unittest.TestCase):
-  
-    def test_logger_with_empty_config(self):
-        from foundations.config_manager import ConfigManager
 
-        config_manager = ConfigManager()
+    def setUp(self):
+        from foundations.config_manager import ConfigManager
+        self.config_manager = ConfigManager()
         
-        log_manager = LogManager(config_manager)
-        self.assertEqual({}, config_manager.config())
+  
+    def test_logger_return_logging_type(self):
+        import logging
+        
+        log_manager = LogManager(self.config_manager)
+        result_logger = log_manager.get_logger('namespaced_log_levels')
+        self.assertTrue(isinstance(result_logger, logging.Logger))
     
 
-    def test_logger_returns_string(self):
+    def test_logger_returns_log_level_info(self):
+        import logging
+
+        log_manager = LogManager(self.config_manager)
+        result_logger = log_manager.get_logger('namespaced_log_levels')
+        self.assertEqual(logging.INFO, result_logger.level)
+    
+
+    def test_logger_return_log_level_override(self):
+        import logging
+
+        self.config_manager['log_level'] = 'DEBUG'
+        log_manager = LogManager(self.config_manager)
+        result_logger = log_manager.get_logger('namespaced_log_levels')
+        self.assertEqual(logging.DEBUG, result_logger.level)
+
+    def test_logger_return_namespace_override(self):
+        import logging
+
+        self.config_manager['namespaced_log_levels'] = {'a': 'DEBUG'}
+        log_manager = LogManager(self.config_manager)
+        result_logger = log_manager.get_logger('a')
+        self.assertEqual(logging.DEBUG, result_logger.level)
+
+    def test_logger_return_namespace_override_does_not_match(self):
+        import logging
+
+        self.config_manager['namespaced_log_levels'] = {'a': 'DEBUG'}
+        log_manager = LogManager(self.config_manager)
+        result_logger = log_manager.get_logger('b')
+        self.assertEqual(logging.INFO, result_logger.level)
+
+    def test_logger_return_namespace_override_matches_with_longer_value(self):
+        import logging
+
+        self.config_manager['namespaced_log_levels'] = {'foundations': 'DEBUG'}
+        log_manager = LogManager(self.config_manager)
+        result_logger = log_manager.get_logger('foundations.config_manager')
+        self.assertEqual(logging.DEBUG, result_logger.level)
+    
+    def test_logger_return_namespace_override_matches_with_longest_match(self):
+        import logging
+
+        self.config_manager['namespaced_log_levels'] = {'foundations': 'DEBUG', 'foundations.config_manager': 'ERROR'}
+        log_manager = LogManager(self.config_manager)
+        result_logger = log_manager.get_logger('foundations.config_manager')
+        self.assertEqual(logging.ERROR, result_logger.level)
+
+    def test_logger_return_when_clears_default_handlers(self):
+        import logging
+
+        log_manager = LogManager(self.config_manager)
+        log_manager.get_logger('foundations.config_manager')
+        root_logger = logging.getLogger()
+        self.assertEquals(1, len(root_logger.handlers))
+
+    def test_log_handler_return_format(self):
+        import logging
+
+        log_manager = LogManager(self.config_manager)
+        log_manager.get_logger('foundations.config_manager')
+        root_logger = logging.getLogger()
+        log_handler = root_logger.handlers[0]
+        formatter = log_handler.formatter
+        self.assertEquals('%(asctime)s - %(name)s - %(levelname)s - %(message)s', formatter._fmt)
+
+    def test_log_handler_return_stdout(self):
+        import logging
+        from sys import stdout
+
+        log_manager = LogManager(self.config_manager)
+        log_manager.get_logger('foundations.config_manager')
+        root_logger = logging.getLogger()
+        log_handler = root_logger.handlers[0]
+        self.assertEquals(stdout, log_handler.stream)
+
+    def test_logger_return_cached_logger(self):
+        import logging
         
-        from foundations.config_manager import ConfigManager
-
-        config_manager = ConfigManager()
-        config_manager.config()['foo'] = 'bar'
-        log_manager = LogManager(config_manager)
-        log_manager.get_logger('namespaced_log_levels')
-        self.assertEqual({'foo': 'bar'}, config_manager.config())
-
-    def test_logger_with_dictionary(self):
+        log_manager = LogManager(self.config_manager)
+        first_logger = log_manager.get_logger('namespaced_log_levels')
+        second_logger = log_manager.get_logger('namespaced_log_levels')
+        self.assertEqual(id(first_logger), id(second_logger))
+    
+    def test_logger_return_different_loggers(self):
+        import logging
         
-        from foundations.config_manager import ConfigManager
-
-        config_manager = ConfigManager()
-        log_manager = LogManager(config_manager)
-        config_manager['namespaced_log_levels'] = { 'foundations_gcp.gcp_bucket': 'DEBUG' }
-        log_manager.get_logger('namespaced_log_levels')
-        self.assertEqual({'foundations_gcp.gcp_bucket': 'DEBUG'}, config_manager.config()['namespaced_log_levels'])
+        log_manager = LogManager(self.config_manager)
+        first_logger = log_manager.get_logger('namespaced_log_levels')
+        second_logger = log_manager.get_logger('namespaced_log_levels_two')
+        self.assertNotEqual(first_logger, second_logger)
