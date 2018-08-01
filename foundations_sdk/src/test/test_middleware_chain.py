ba4c11010d666687616effbeab3144fbadb54165
Thomas Rogers <tr@deeplearni.ng>
2018-08-01 11:30:34 -0400

    FOUND-95 add tests for executing middleware recursively

--- foundations_sdk/src/test/test_middleware_chain.py
+++ foundations_sdk/src/test/test_middleware_chain.py
@@ -12,10 +12,24 @@ from foundations.middleware_chain import MiddlewareChain
 
 class TestMiddlewareChain(unittest.TestCase):
 
+    class MockData(object):
+        pass
+
     class MockMiddleware(object):
 
+        def __init__(self):
+            self.called = False
+            self.upstream_result_callback = None
+            self.filler_builder = None
+            self.filler_kwargs = None
+
         def call(self, upstream_result_callback, filler_builder, filler_kwargs, args, kwargs, callback):
-            pass
+            self.called = True
+            self.upstream_result_callback = upstream_result_callback
+            self.filler_builder = filler_builder
+            self.filler_kwargs = filler_kwargs
+
+            return callback(args, kwargs)
 
     def setUp(self):
         from uuid import uuid4
@@ -24,24 +38,78 @@ class TestMiddlewareChain(unittest.TestCase):
         self._callback_kwargs = None
         self._callback_result = uuid4()
 
+        self._middleware_chain = MiddlewareChain()
+        self._middleware = self.MockMiddleware()
+        self._middleware_chain.append_middleware(self._middleware)
+
     def test_call_returns_callback_result(self):
-        chain = MiddlewareChain()
-        result = chain.call(None, None, None, None, None, self._callback)
+        result = self._middleware_chain.call(
+            None, None, None, None, None, self._callback)
         self.assertEqual(self._callback_result, result)
 
     def test_call_returns_callback_result_with_args(self):
-        chain = MiddlewareChain()
         args = self._random_args()
-        chain.call(None, None, None, args, None, self._callback)
+        self._middleware_chain.call(
+            None, None, None, args, None, self._callback)
 
         self.assertEqual(args, self._callback_args)
 
     def test_call_returns_callback_result_with_kwargs(self):
-        chain = MiddlewareChain()
         kwargs = self._random_kwargs()
-        chain.call(None, None, None, None, kwargs, self._callback)
+        self._middleware_chain.call(
+            None, None, None, None, kwargs, self._callback)
 
         self.assertEqual(kwargs, self._callback_kwargs)
+
+    def test_call_calls_middleware(self):
+        self._middleware_chain.call(
+            None, None, None, None, None, self._callback)
+        self.assertTrue(self._middleware.called)
+
+    def test_call_calls_multiple_middleware(self):
+        middleware_two = self.MockMiddleware()
+        self._middleware_chain.append_middleware(middleware_two)
+
+        self._middleware_chain.call(
+            None, None, None, None, None, self._callback)
+        self.assertTrue(self._middleware.called and middleware_two.called)
+
+    def test_call_calls_multiple_middleware_with_extend(self):
+        middleware_two = self.MockMiddleware()
+        middleware_three = self.MockMiddleware()
+        self._middleware_chain.extend([middleware_two, middleware_three])
+
+        self._middleware_chain.call(
+            None, None, None, None, None, self._callback)
+        self.assertTrue(self._middleware.called and middleware_two.called and middleware_three.called)
+
+    def test_call_calls_middleware_with_upstream_result(self):
+        expected_result = self.MockData()
+        self._middleware_chain.call(
+            expected_result, None, None, None, None, self._callback)
+        self.assertEqual(expected_result, self._middleware.upstream_result_callback)
+
+    def test_call_calls_middleware_with_filler_builder(self):
+        expected_result = self.MockData()
+        self._middleware_chain.call(
+            None, expected_result, None, None, None, self._callback)
+        self.assertEqual(expected_result, self._middleware.filler_builder)
+
+    def test_call_calls_middleware_with_filler_kwargs(self):
+        expected_result = self.MockData()
+        self._middleware_chain.call(
+            None, None, expected_result, None, None, self._callback)
+        self.assertEqual(expected_result, self._middleware.filler_kwargs)
+
+    def test_chain_returns_chain(self):
+        expected_result = [self._middleware]
+        self.assertEqual(expected_result, self._middleware_chain.chain())
+
+    def test_chain_returns_chain_multiple_items(self):
+        middleware_two = self.MockMiddleware()
+        self._middleware_chain.append_middleware(middleware_two)
+        expected_result = [self._middleware, middleware_two]
+        self.assertEqual(expected_result, self._middleware_chain.chain())
 
     def _random_args(self):
         import random
