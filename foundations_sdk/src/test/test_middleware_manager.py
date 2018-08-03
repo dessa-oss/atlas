9442a298079f0b1770d3b5899e9972e117210424
Thomas Rogers <tr@deeplearni.ng>
2018-08-03 10:53:38 -0400

    FOUND-95 add additional test for middleware attributes

--- foundations_sdk/src/test/test_middleware_manager.py
+++ foundations_sdk/src/test/test_middleware_manager.py
@@ -8,17 +8,16 @@ Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 import unittest
 
 from foundations.middleware_manager import MiddlewareManager
+from foundations.basic_stage_middleware import BasicStageMiddleware
 
 
 class TestMiddlewareManager(unittest.TestCase):
 
-    class MockMiddleware(object):
-        def __init__(self, pipeline_context, stage_config, stage_context, stage):
-            pass
+    class MockMiddleware(BasicStageMiddleware):
+        pass
 
-    class MockMiddlewareTwo(object):
-        def __init__(self, pipeline_context, stage_config, stage_context, stage):
-            pass
+    class MockMiddlewareTwo(BasicStageMiddleware):
+        pass
 
     def setUp(self):
         from foundations.pipeline_context import PipelineContext
@@ -29,9 +28,9 @@ class TestMiddlewareManager(unittest.TestCase):
 
         from uuid import uuid4
 
-        self._pipeline_context = None
-        self._stage_config = None
-        self._stage_context = None
+        self._pipeline_context = PipelineContext
+        self._stage_config = StageConfig
+        self._stage_context = StageContext
 
         self._stage_uuid = str(uuid4())
         self._stage = Stage(None, self._stage_uuid,
@@ -43,25 +42,43 @@ class TestMiddlewareManager(unittest.TestCase):
         from foundations.redundant_execution_middleware import RedundantExecutionMiddleware
         self._test_has_middleware('Redundant', RedundantExecutionMiddleware)
 
+    def test_has_redundant_middleware_configured(self):
+        self._test_constructor_attributes('Redundant')
+
     def test_has_error_middleware(self):
         from foundations.error_middleware import ErrorMiddleware
         self._test_has_middleware('Error', ErrorMiddleware)
+
+    def test_has_error_middleware_configured(self):
+        self._test_constructor_attributes('Error')
 
     def test_has_stage_output_middleware(self):
         from foundations.stage_output_middleware import StageOutputMiddleware
         self._test_has_middleware('StageOutput', StageOutputMiddleware)
 
+    def test_has_stage_output_middleware_configured(self):
+        self._test_constructor_attributes('StageOutput')
+
     def test_has_stage_log_middleware(self):
         from foundations.stage_log_middleware import StageLogMiddleware
         self._test_has_middleware('StageLog', StageLogMiddleware)
+
+    def test_has_stage_log_middleware_configured(self):
+        self._test_constructor_attributes('StageLog')
 
     def test_has_argument_filler_middleware(self):
         from foundations.argument_filler_middleware import ArgumentFillerMiddleware
         self._test_has_middleware('ArgumentFiller', ArgumentFillerMiddleware)
 
+    def test_has_argument_filler_middleware_configured(self):
+        self._test_constructor_attributes('ArgumentFiller')
+
     def test_has_cache_middleware(self):
         from foundations.cache_middleware import CacheMiddleware
         self._test_has_middleware('Cache', CacheMiddleware)
+
+    def test_has_cache_middleware_configured(self):
+        self._test_constructor_attributes('Cache')
 
     def test_has_upstream_result_middleware(self):
         from foundations.upstream_result_middleware import UpstreamResultMiddleware
@@ -71,29 +88,54 @@ class TestMiddlewareManager(unittest.TestCase):
         from foundations.context_aware_middleware import ContextAwareMiddleware
         self._test_has_middleware('ContextAware', ContextAwareMiddleware)
 
+    def test_has_context_aware_middleware_configured(self):
+        self._test_constructor_attributes('ContextAware')
+
     def test_has_time_stage_middleware(self):
         from foundations.time_stage_middleware import TimeStageMiddleware
         self._test_has_middleware('TimeStage', TimeStageMiddleware)
+
+    def test_has_time_stage_middleware_configured(self):
+        self._test_constructor_attributes('TimeStage')
 
     def test_has_stage_logging_middleware(self):
         from foundations.stage_logging_middleware import StageLoggingMiddleware
         self._test_has_middleware('StageLogging', StageLoggingMiddleware)
 
-    def test_has_configured_middleware(self):
-        from foundations.stage_logging_middleware import StageLoggingMiddleware
+    def test_has_stage_logging_middleware_configured(self):
+        self._test_constructor_attributes('StageLogging')
 
+    def test_has_configured_middleware(self):
         self._config_manager['stage_middleware'] = [
             {'name': 'Mock', 'constructor': self.MockMiddleware}
         ]
         self._test_has_middleware('Mock', self.MockMiddleware)
 
-    def test_has_configured_middleware_different_middleware(self):
-        from foundations.stage_logging_middleware import StageLoggingMiddleware
+    def test_has_configured_middleware_configured(self):
+        self._config_manager['stage_middleware'] = [
+            {'name': 'Mock', 'constructor': self.MockMiddleware}
+        ]
+        self._test_constructor_attributes('Mock')
 
+    def test_has_configured_middleware_different_middleware(self):
         self._config_manager['stage_middleware'] = [
             {'name': 'MockTwo', 'constructor': self.MockMiddlewareTwo}
         ]
         self._test_has_middleware('MockTwo', self.MockMiddlewareTwo)
+
+    def _test_constructor_attributes(self, name):
+        middleware_manager = MiddlewareManager(self._config_manager)
+        middleware = self._construct_middleware(middleware_manager, name)
+
+        # hack for ensuring construction is working
+        has_any = False
+        for attribute in ['_pipeline_context', '_stage_config', '_stage_context', '_stage']:
+            if hasattr(middleware, attribute):
+                has_any = True
+                expected_result = getattr(self, attribute)
+                result = getattr(middleware, attribute)
+                self.assertEqual(expected_result, result)
+        self.assertTrue(has_any)
 
     def _test_has_middleware(self, name, middleware_type):
         middleware_manager = MiddlewareManager(self._config_manager)
