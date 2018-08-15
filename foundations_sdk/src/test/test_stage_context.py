e8b2c5b133b3b2f39fc48b2f1d1136cd155c1bd8
pippinlee <pippinblee@gmail.com>
2018-08-15 09:17:03 -0400

    FOUND-98 add stage context test for loading from archive and persisted data, creat mock archive

--- foundations_sdk/src/test/test_stage_context.py
+++ foundations_sdk/src/test/test_stage_context.py
@@ -11,6 +11,32 @@ from foundations.stage_context import StageContext
 
 class TestStageContext(unittest.TestCase):
 
+    class MockArchive(object):
+
+        def __init__(self):
+            self.archive_provenance = None
+
+        def append_stage_log(self, uuid, stage_log):
+            pass
+
+        def append_stage_persisted_data(self, uuid, stage_output):
+            pass
+
+        def append_stage_model_data(self, uuid, model_data):
+            pass
+
+        def append_stage_miscellaneous(self, uuid, stage_context, archive_stage_context):
+            pass
+
+        def fetch_stage_log(self, uuid):
+            return '90s8d'
+
+        def fetch_stage_persisted_data(self, stage_uuid_string):
+            return '190uh23'
+
+        def fetch_stage_model_data(self, stage_uuid_string):
+            return '0912h1'
+
     def test_set_stage_output(self):
         stage_context = StageContext()
         stage_context.set_stage_output('some stage output')
@@ -30,3 +56,42 @@ class TestStageContext(unittest.TestCase):
         stage_context.add_error_information(mock_exception)
         self.assertEqual({'type': None, 'exception': None,
                           'traceback': []}, stage_context.error_information)
+
+    def test_save_to_archive(self):
+        stage_context = StageContext()
+        mock_archive = self.MockArchive()
+
+        stage_context.save_to_archive(mock_archive)
+
+    def test_load_stage_log_from_archive(self):
+        stage_context = StageContext()
+        mock_archive = self.MockArchive()
+
+        stage_context.load_stage_log_from_archive(mock_archive)
+        self.assertEqual('90s8d', stage_context.stage_log)
+
+    def test_load_persisted_data_from_archive(self):
+        stage_context = StageContext()
+        mock_archive = self.MockArchive()
+
+        stage_context.load_persisted_data_from_archive(mock_archive)
+        self.assertEqual('190uh23', stage_context.stage_output)
+        self.assertEqual('0912h1', stage_context.model_data)
+
+    def test_load_provenance_from_archive(self):
+        stage_context = StageContext()
+        mock_archive = self.MockArchive()
+
+        stage_context.load_provenance_from_archive(mock_archive)
+
+    def test_load_job_source_from_archive(self):
+        stage_context = StageContext()
+        mock_archive = self.MockArchive()
+
+        stage_context.load_job_source_from_archive(mock_archive)
+
+    def test_load_artifact_from_archive(self):
+        stage_context = StageContext()
+        mock_archive = self.MockArchive()
+
+        stage_context.load_artifact_from_archive(mock_archive)
