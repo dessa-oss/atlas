0d2d2033ff0d9d49aec98f9b1feb40f20ef2d6e6
Thomas Rogers <tr@deeplearni.ng>
2018-08-20 15:27:05 -0400

    implement DynamicParameter

---
+++ foundations_sdk/src/foundations/dynamic_parameter.py
@@ -0,0 +1,24 @@
+"""
+Copyright (C) DeepLearning Financial Technologies Inc. - All Rights Reserved
+Unauthorized copying, distribution, reproduction, publication, use of this file, via any medium is strictly prohibited
+Proprietary and confidential
+Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
+"""
+
+
+class DynamicParameter(object):
+
+    def __init__(self, hyper_parameter):
+        self._hyper_parameter = hyper_parameter
+
+    def compute_value(self, runtime_data):
+        return runtime_data[self._hyper_parameter.name]
+
+    def hash(self, runtime_data):
+        from foundations.utils import generate_uuid
+
+        value = self.compute_value(runtime_data)
+        return generate_uuid(str(value))
+
+    def enable_caching(self):
+        pass
