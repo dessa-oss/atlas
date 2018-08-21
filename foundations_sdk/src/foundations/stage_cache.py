14be5adf8ef9055de3cb792171370af59e3c0e27
Thomas Rogers <tr@deeplearni.ng>
2018-08-21 13:50:31 -0400

    implement StageCache

---
+++ foundations_sdk/src/foundations/stage_cache.py
@@ -0,0 +1,38 @@
+"""
+Copyright (C) DeepLearning Financial Technologies Inc. - All Rights Reserved
+Unauthorized copying, distribution, reproduction, publication, use of this file, via any medium is strictly prohibited
+Proprietary and confidential
+Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
+"""
+
+
+class StageCache(object):
+
+    def __init__(self, stage, stage_config, live_arguments):
+        from foundations.cache_name_generator import CacheNameGenerator
+
+        self._stage_config = stage_config
+        self._cache_name = CacheNameGenerator(stage, live_arguments).hash()
+
+    def cache_name(self):
+        return self._cache_name
+
+    def fetch_option(self):
+        from foundations.global_state import cache_manager
+        from foundations.nothing import Nothing
+
+        if self._allow_caching():
+            return cache_manager.cache().get_option(self._cache_name)
+
+        return Nothing()
+
+    def submit(self, value):
+        from foundations.global_state import cache_manager
+
+        if self._allow_caching():
+            return cache_manager.cache().set(self._cache_name, value)
+
+        return value
+
+    def _allow_caching(self):
+        return self._stage_config.allow_caching()
