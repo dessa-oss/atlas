f533d5e2b3c9088360c16099b284085daae4c83e
Thomas Rogers <tr@deeplearni.ng>
2018-08-22 14:45:27 -0400

    implement StageLoggingContext#change_logger

--- foundations_sdk/src/foundations/stage_logging_context.py
+++ foundations_sdk/src/foundations/stage_logging_context.py
@@ -7,9 +7,26 @@ Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 
 
 class StageLoggingContext(object):
-    
+
+    class ChangeLogger(object):
+
+        def __init__(self, context, logger):
+            self._context = context
+            self._logger = logger
+            self._previous_logger = None
+
+        def __enter__(self):
+            self._previous_logger = self._context._logger
+            self._context._logger = self._logger
+
+        def __exit__(self, exception_type, exception_value, traceback):
+            self._context._logger = self._previous_logger
+
     def __init__(self, logger):
         self._logger = logger
 
     def log_metric(self, key, value):
         self._logger.log_metric(key, value)
+
+    def change_logger(self, new_logger):
+        return self.ChangeLogger(self, new_logger)
--- foundations_sdk/src/test/test_stage_logging_context.py
+++ foundations_sdk/src/test/test_stage_logging_context.py
@@ -21,33 +21,51 @@ class TestStageLoggingContext(unittest.TestCase):
         def log_metric(self, key, value):
             self.key = key
             self.value = value
-    
+
     def test_log_metric_logs_key(self):
         logger = self.MockLogger()
         context = StageLoggingContext(logger)
-        
+
         context.log_metric('loss', 0.554)
         self.assertEqual('loss', logger.key)
-    
+
     def test_log_metric_logs_key_different_key(self):
         logger = self.MockLogger()
         context = StageLoggingContext(logger)
-        
+
         context.log_metric('accuracy', 0.554)
         self.assertEqual('accuracy', logger.key)
-    
+
     def test_log_metric_logs_value(self):
         logger = self.MockLogger()
         context = StageLoggingContext(logger)
-        
+
         context.log_metric('loss', 0.554)
         self.assertEqual(0.554, logger.value)
-    
+
     def test_log_metric_logs_value_different_value(self):
         logger = self.MockLogger()
         context = StageLoggingContext(logger)
-        
+
         context.log_metric('loss', 0.1554)
         self.assertEqual(0.1554, logger.value)
 
-    
+    def test_change_logger_changes_logger(self):
+        logger = self.MockLogger()
+        logger_two = self.MockLogger()
+        context = StageLoggingContext(logger)
+
+        with context.change_logger(logger_two):
+            context.log_metric('loss', 0.1554)
+            self.assertEqual(0.1554, logger_two.value)
+
+    def test_change_logger_resets_logger(self):
+        logger = self.MockLogger()
+        logger_two = self.MockLogger()
+        context = StageLoggingContext(logger)
+
+        with context.change_logger(logger_two):
+            pass
+
+        context.log_metric('loss', 0.1554)
+        self.assertEqual(0.1554, logger.value)
