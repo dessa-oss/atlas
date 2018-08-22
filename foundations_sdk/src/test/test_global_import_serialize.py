b5643008cb5aba9aadf214688bc66eb73f7402d0
pippinlee <pippinblee@gmail.com>
2018-08-22 16:09:11 -0400

    FOUND-174 test to cover that our serialize command uses recurse=True

--- foundations_sdk/src/test/test_global_import_serialize.py
+++ foundations_sdk/src/test/test_global_import_serialize.py
@@ -20,3 +20,12 @@ class TestGlobalImportSerialization(unittest.TestCase):
         item = 'some data to serialize'
         serialize(item)
         mock_dumps.assert_called_with('some data to serialize', protocol=2, recurse=True)
+
+    
+    @patch.object(dill, 'dumps')
+    def test_calls_dumps_with_recurse_true_with_different_data(self, mock_dumps):
+        from foundations.serializer import serialize
+
+        item = 'lou data'
+        serialize(item)
+        mock_dumps.assert_called_with('lou data', protocol=2, recurse=True)
