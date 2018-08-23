c50f415fe75c3f1ca6f3a2bcf40a72d02cc06265
pippinlee <pippinblee@gmail.com>
2018-08-23 11:15:42 -0400

    add docstring to state_logger

--- foundations_sdk/src/foundations/stage_logger.py
+++ foundations_sdk/src/foundations/stage_logger.py
@@ -7,6 +7,9 @@ Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 
 
 class StageLogger(object):
+    """Stage logging
+    """
+
 
     def __init__(self, pipeline_context, stage, stage_config, stage_context):
         self._pipeline_context = pipeline_context
@@ -15,6 +18,13 @@ class StageLogger(object):
         self._stage_context = stage_context
 
     def log_metric(self, key, value):
+        """Saves metrics
+        
+        Arguments:
+            key {string}
+            value {int}
+        """
+
         if key in self._stage_context.stage_log:
             previous_value = self._stage_context.stage_log[key]
             if isinstance(previous_value, list):
@@ -25,13 +35,37 @@ class StageLogger(object):
             self._stage_context.stage_log[key] = value
 
     def pipeline_context(self):
+        """Logs pipeline context
+        
+        Returns:
+            [object]
+        """
+
         return self._pipeline_context
 
     def stage(self):
+        """Logs stage
+        
+        Returns:
+            [object]
+        """
+
         return self._stage
 
     def stage_config(self):
+        """Logs stage config
+        
+        Returns:
+            [object]
+        """
+
         return self._stage_config
 
     def stage_context(self):
+        """Logs stage context
+        
+        Returns:
+            [object]
+        """
+
         return self._stage_context
