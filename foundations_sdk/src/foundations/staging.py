58e05cfb5a02c1cb6fbd0e325e3120630211eca6
Thomas Rogers <tr@deeplearni.ng>
2018-08-23 16:54:12 -0400

    add more docs

--- foundations_sdk/src/foundations/staging.py
+++ foundations_sdk/src/foundations/staging.py
@@ -6,6 +6,16 @@ Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 """
 
 def create_stage(function):
+    """Take in a function and returns a method capable of generating stages
+    
+    Arguments:
+        function {callable} -- Function to wrap
+    
+    Returns:
+        callable -- Stage generator
+    """
+
+
     def stage(*args, **kwargs):
         from foundations.global_state import foundations_context
         return foundations_context.pipeline().stage(function, *args, **kwargs)
