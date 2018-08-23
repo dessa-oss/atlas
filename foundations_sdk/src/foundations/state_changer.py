ec6ee25159078f82bff172d35c27826edade119c
pippinlee <pippinblee@gmail.com>
2018-08-23 10:30:32 -0400

    add docstrings to state_changer.py

--- foundations_sdk/src/foundations/state_changer.py
+++ foundations_sdk/src/foundations/state_changer.py
@@ -6,6 +6,10 @@ Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 """
 
 class StateChanger(object):
+    """
+    Allows for setting, and getting of state of modules
+    """
+    
     
     def __init__(self, module_name, state_to_change, value):
         self._module = self._load_module(module_name)
@@ -14,11 +18,23 @@ class StateChanger(object):
         self._previous_value = None
 
     def __enter__(self):
+        """
+        Gets and sets stores attributes for state of modules
+        """
         self._previous_value = getattr(self._module, self._state_to_change)
         setattr(self._module, self._state_to_change, self._value)
 
     def __exit__(self, exception_type, exception_value, traceback):
+        """
+        Sets attributes for state for state of modules
+
+        Arguments:
+            exception_type: Error type
+            exception_value: Error value
+            traceback: Stack trace from error
+        """
         setattr(self._module, self._state_to_change, self._previous_value)
+
         
     def _load_module(self, module_name):
         module = __import__(module_name)
