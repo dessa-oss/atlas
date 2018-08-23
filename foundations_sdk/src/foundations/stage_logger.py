9523b14be0b11688c4353bddaefa49fc0bf41879
Thomas Rogers <tr@deeplearni.ng>
2018-08-23 10:31:57 -0400

    add documentation

--- foundations_sdk/src/foundations/stage_logger.py
+++ foundations_sdk/src/foundations/stage_logger.py
@@ -15,17 +15,49 @@ class StageLogger(object):
         self._stage_context = stage_context
 
     def log_metric(self, key, value):
+        """Logs a metric to the stage log
+
+        Arguments:
+            key {string} -- The name of the metric to log
+            value {object} -- The value to store
+        """
+
         from time import time
-        self._stage_context.stage_log.append({'key': key, 'value': value, 'timestamp': time()})
+        self._stage_context.stage_log.append(
+            {'key': key, 'value': value, 'timestamp': time()})
 
     def pipeline_context(self):
+        """The PipelineContext associated with the Stage
+
+        Returns:
+            PipelineContext -- As above
+        """
+
         return self._pipeline_context
 
     def stage(self):
+        """The Stage itself
+
+        Returns:
+            Stage -- As above
+        """
+
         return self._stage
 
     def stage_config(self):
+        """The StageConfig associated with the Stage
+
+        Returns:
+            StageConfig -- As above
+        """
+
         return self._stage_config
 
     def stage_context(self):
+        """The StageContext associated with the Stage
+
+        Returns:
+            StageContext -- As above
+        """
+
         return self._stage_context
