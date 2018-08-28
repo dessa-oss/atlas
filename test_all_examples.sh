35a4828baaf2db05fe5cdf5dbad91320fde34716
Jinnah Ali-Clarke <ja@deeplearni.ng>
2018-08-28 12:42:36 -0400

    script is done

--- test_all_examples.sh
+++ test_all_examples.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-rm -rf /tmp/archives
-
-python -m virtualenv --system-site-packages /tmp/examples-venv
-source /tmp/examples-venv/bin/activate
-python -m pip install -r requirements.txt
-
-bash build_dist.sh
-
-cd examples
+deactivate 2> /dev/null
 
 # make sure to add a space after module name
 modules=""
@@ -23,16 +15,39 @@ modules+="replacing_nulls "
 modules+="fetch_job_information "
 modules+="fetch_results "
 
-for module in ${modules}
+cleanup () {
+    deactivate
+    rm -rf /tmp/examples-venv
+    rm -rf /tmp/foundations_example*
+}
+
+cleanup_and_exit () {
+    cleanup
+    exit 1
+}
+
+trap 'cleanup_and_exit' SIGINT SIGTERM
+
+for python_version in python python3
 do
-    python -Wi -m ${module}
+    rm -rf /tmp/archives
 
-    if [ $? -ne 0 ]
-    then
-        break
-    fi
+    python -m virtualenv -p ${python_version} /tmp/examples-venv
+    source /tmp/examples-venv/bin/activate
+    python -m pip install -r requirements.txt
+
+    bash build_dist.sh
+
+    cd examples
+
+    python -m pip install -r requirements.txt
+
+    for module in ${modules}
+    do
+        python -Wi -m ${module} || cleanup_and_exit
+    done
+
+    cleanup
+
+    cd ..
 done
-
-deactivate
-rm -rf /tmp/examples-venv
-rm -rf /tmp/foundations_example*
