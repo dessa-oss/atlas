b4ae2cab04b1361370d226e66177da0b908f3f2e
Thomas Rogers <tr@deeplearni.ng>
2018-09-18 10:51:26 -0400

    add documentation to Response class

--- foundations_rest_api/src/foundations_rest_api/response.py
+++ foundations_rest_api/src/foundations_rest_api/response.py
@@ -7,12 +7,29 @@ Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 
 
 class Response(object):
+    """Common response object used across the project for running
+    post processing against data to be sent to front end applications.
+
+    Arguments:
+        resource_name {str} -- Name of the resource type being acted upon
+        action_callback {function} -- Method to call when evaluating
+
+    Keyword Arguments:
+        parent {Response} -- Parent response to evaluate (default: {None})
+    """
 
     def __init__(self, resource_name, action_callback, parent=None):
         self._action_callback = action_callback
         self._parent = parent
 
     def evaluate(self):
+        """Calls the action callback and returns the result. 
+        Also ensures that parent responses are evaluated.
+
+        Returns:
+            object -- The return value of the evaluated callback
+        """
+
         if self._parent is not None:
             self._parent.evaluate()
         return self._action_callback()
