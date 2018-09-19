4e513cacc7717b3337437d6e8e2f0108f3e1e486
Thomas Rogers <tr@deeplearni.ng>
2018-09-19 17:24:42 -0400

    Project.all now includes information about queued jobs

--- foundations_rest_api/src/foundations_rest_api/v1/models/project.py
+++ foundations_rest_api/src/foundations_rest_api/v1/models/project.py
@@ -17,6 +17,7 @@ class Project(PropertyModel):
 
     name = PropertyModel.define_property()
     completed_jobs = PropertyModel.define_property()
+    queued_jobs = PropertyModel.define_property()
 
     @staticmethod
     def new(name):
@@ -42,9 +43,11 @@ class Project(PropertyModel):
 
         def callback():
             from foundations_rest_api.v1.models.completed_job import CompletedJob
+            from foundations_rest_api.v1.models.queued_job import QueuedJob
 
             project = Project(name=name)
             project.completed_jobs = CompletedJob.all()
+            project.queued_jobs = QueuedJob.all()
             return project
 
         return Response(None, callback)
