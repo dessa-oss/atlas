e23b82674265baee2064e81e97ff1934305638a1
pippinlee <pippinblee@gmail.com>
2018-09-19 15:54:24 -0400

    formatting

--- foundations_rest_api/src/foundations_rest_api/utils/api_resource.py
+++ foundations_rest_api/src/foundations_rest_api/utils/api_resource.py
@@ -16,9 +16,11 @@ api = Api(app)
 
 def api_resource(klass):
     from flask_restful import Resource
+
     if hasattr(klass, 'index'):
         def _get(self):
             return klass().index()
-        resource_class = type('blah', (Resource,), {'get': _get})
+        resource_class = type('', (Resource,), {'get': _get})
         api.add_resource(resource_class, '/lou')
+
     return klass
