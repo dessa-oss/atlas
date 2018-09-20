134356123957f7a1aaa69d2b16fa8b06503d8f09
Thomas Rogers <tr@deeplearni.ng>
2018-09-20 16:59:48 -0400

    add documentation

--- foundations_sdk/src/foundations/null_scheduler_backend.py
+++ foundations_sdk/src/foundations/null_scheduler_backend.py
@@ -11,18 +11,21 @@ from foundations.scheduler_legacy_backend import LegacyBackend
 class NullSchedulerBackend(LegacyBackend):
 
     def __init__(self):
+        """Default implementation for scheduler job information for use when no backend is supported
+        """
+
         pass
 
     def get_paginated(self, start_index, number_to_get, status):
         """Default implementation for scheduler job information for use when no backend is supported
 
         Arguments:
-          start_index {int} -- Unused
-          number_to_get {int} -- Unused
-          status {str} -- Unused
+            start_index {int} -- Unused
+            number_to_get {int} -- Unused
+            status {str} -- Unused
 
         Returns:
-          generator -- An iterable containing the jobs as specified by the arguments.
+            generator -- An iterable containing the jobs as specified by the arguments.
         """
 
         return []
