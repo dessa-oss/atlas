9a206c885a10ec1f6fefb837be78e462cd6ba3e0
pippinlee <pippinblee@gmail.com>
2018-09-20 15:47:52 -0400

    refactor .api_resource

--- foundations_rest_api/src/foundations_rest_api/utils/api_resource.py
+++ foundations_rest_api/src/foundations_rest_api/utils/api_resource.py
@@ -10,27 +10,35 @@ def api_resource(base_path):
     def _make_api_resource(klass):
         """Decorator for defining resource for controllers
         """
-        from flask_restful import Resource
-        from foundations_rest_api.global_state import app_manager
-
-        import random
-
-        if hasattr(klass, 'index'):
-            def _get(self, **kwargs):
-                from flask import request
-
-                instance = klass()
-                instance.params = dict(kwargs)
-                dict_args = dict(request.args)
-                for key, value in dict_args.items():
-                    instance.params[key] = value if len(
-                        value) > 1 else value[0]
-
-                return instance.index().as_json()
-
-            class_name = '_%08x' % random.getrandbits(32)
-            resource_class = type(class_name, (Resource,), {'get': _get})
-            app_manager.api().add_resource(resource_class, base_path)
-
+        _create_index_route(klass, base_path)
         return klass
+        
     return _make_api_resource
+
+def _create_index_route(klass, base_path):
+    from flask_restful import Resource
+    from foundations_rest_api.global_state import app_manager
+    import random
+
+    if hasattr(klass, 'index'):
+        class_name = '_%08x' % random.getrandbits(32)
+        resource_class = type(class_name, (Resource,), {'get': _get_api_index(klass)})
+        app_manager.api().add_resource(resource_class, base_path)
+
+def _get_api_index(klass):
+    def _get(self, **kwargs):
+        instance = klass()
+        instance.params = _api_params(kwargs)
+
+        return instance.index().as_json()
+    return _get
+
+def _api_params(kwargs):
+    from flask import request
+
+    params = dict(kwargs)
+    dict_args = dict(request.args)
+    for key, value in dict_args.items():
+        params[key] = value if len(
+            value) > 1 else value[0]
+    return params
