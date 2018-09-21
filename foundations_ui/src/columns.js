64e951c5c7d998921521cda58336edc1d4fdb8ea
pippinlee <pippinblee@gmail.com>
2018-09-21 10:42:48 -0400

    foundation ui initial

---
+++ foundations_ui/src/columns.js
@@ -0,0 +1,18 @@
+const columns = [{
+    Header: 'Start Time',
+    accessor: 'start_time'
+  }, {
+    Header: 'Status',
+    accessor: 'status'
+  }, {
+    Header: 'JobId',
+    accessor: 'job_id'
+  }, {
+    Header: 'Duration',
+    accessor: 'start_time'
+  }, {
+    Header: 'Name',
+    accessor: 'name'
+}]
+
+module.exports.columns = columns;
