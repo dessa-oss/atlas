d5caf0014360e6e7795345b281fa363dbb2f7298
Thomas Rogers <tr@deeplearni.ng>
2018-09-25 13:58:26 -0400

    formatting

--- foundations_sdk/src/test/test_projects.py
+++ foundations_sdk/src/test/test_projects.py
@@ -9,30 +9,35 @@ import unittest
 
 from foundations.projects import set_project_name
 
+
 class TestProjects(unittest.TestCase):
 
     def test_set_project_name_sets_project_name(self):
         from foundations.global_state import foundations_context
 
-        set_project_name('some project')      
-        self.assertEqual('some project', foundations_context.pipeline_context().provenance.project_name)
+        set_project_name('some project')
+        self.assertEqual(
+            'some project', foundations_context.pipeline_context().provenance.project_name)
 
     def test_set_project_name_sets_project_name_different_name(self):
         from foundations.global_state import foundations_context
 
-        set_project_name('some different project name')      
-        self.assertEqual('some different project name', foundations_context.pipeline_context().provenance.project_name)
-  
+        set_project_name('some different project name')
+        self.assertEqual('some different project name',
+                         foundations_context.pipeline_context().provenance.project_name)
+
     def test_set_project_name_is_global(self):
         from foundations.global_state import foundations_context
         import foundations
 
-        foundations.set_project_name('some project')      
-        self.assertEqual('some project', foundations_context.pipeline_context().provenance.project_name)
-  
+        foundations.set_project_name('some project')
+        self.assertEqual(
+            'some project', foundations_context.pipeline_context().provenance.project_name)
+
     def test_set_project_name_is_global(self):
         from foundations.global_state import foundations_context
         import foundations
 
-        foundations.set_project_name('some different project name')      
-        self.assertEqual('some different project name', foundations_context.pipeline_context().provenance.project_name)
+        foundations.set_project_name('some different project name')
+        self.assertEqual('some different project name',
+                         foundations_context.pipeline_context().provenance.project_name)
