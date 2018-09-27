b0216294a0de3ca720084571a1ed8eebb44a98b5
Thomas Rogers <tr@deeplearni.ng>
2018-09-27 10:58:45 -0400

    support dependency loading methods

--- foundations_sdk/src/foundations/helpers/wrap_class_with_dependency.py
+++ foundations_sdk/src/foundations/helpers/wrap_class_with_dependency.py
@@ -7,11 +7,20 @@ Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 
 
 class Wrapping(object):
-    def __init__(self, klass, wrapped_klass):
+    def __init__(self, klass, wrapped_klass, dependency_functions):
         self._klass = klass
         self._wrapped_klass = wrapped_klass
+        self._dependency_functions = dependency_functions
 
     def wrap(self):
+        for dependency_method in self._dependency_functions:
+            def _inner(method_name):
+                def _call_dependency_method(wrapper_instance, *args, **kwargs):
+                    method = getattr(wrapper_instance._wrapped, method_name)
+                    return method(wrapper_instance._dependency, *args, **kwargs)
+                setattr(self._klass, method_name, _call_dependency_method)
+            _inner(dependency_method)
+
         setattr(self._klass, '__getattr__', self._getattr())
         setattr(self._klass, '__init__', self._init())
         return self._klass
@@ -27,12 +36,13 @@ class Wrapping(object):
         return "'{}' object has no attribute '{}'".format(self._klass.__name__, name)
 
     def _init(self):
-        def _inner(wrapper_instance):
+        def _inner(wrapper_instance, dependency):
+            wrapper_instance._dependency = dependency
             wrapper_instance._wrapped = self._wrapped_klass()
         return _inner
 
 
 def wrap_class_with_dependency(wrapped_klass, *dependency_functions):
     def _wrap(klass):
-        return Wrapping(klass, wrapped_klass).wrap()
+        return Wrapping(klass, wrapped_klass, dependency_functions).wrap()
     return _wrap
--- foundations_sdk/src/test/helpers/test_wrap_class_with_dependency.py
+++ foundations_sdk/src/test/helpers/test_wrap_class_with_dependency.py
@@ -19,41 +19,95 @@ class TestWrapClassWithDependency(unittest.TestCase):
 
         def another_arb_method(self):
             self.another_flag = True
-        
-    @wrap_class_with_dependency(MockWrappedClass)
+
+        def load(self, dependency):
+            self.dependency = dependency
+
+        def another_load(self, another_dependency):
+            self.another_dependency = another_dependency
+
+        def fn_that_returns(self, dependency):
+            return dependency*2
+
+        def load_with_params(self, dependency, arg, kwarg):
+            self.arg = arg
+            self.kwarg = kwarg
+
+    @wrap_class_with_dependency(MockWrappedClass, 'load', 'another_load', 'fn_that_returns', 'load_with_params')
     class MockClassWrapper(object):
 
         def wrapped(self):
             return self._wrapped
 
-    @wrap_class_with_dependency(MockWrappedClass)
+    @wrap_class_with_dependency(MockWrappedClass, 'load')
     class MockClassWrapper_2(object):
         pass
 
     def test_stores_wrapped_class(self):
-        mock = self.MockClassWrapper()
+        mock = self.MockClassWrapper(None)
         self.assertTrue(isinstance(mock.wrapped(), self.MockWrappedClass))
 
     def test_calls_wrapped_class_method(self):
-        mock = self.MockClassWrapper()
+        mock = self.MockClassWrapper(None)
         mock.arb_method()
         self.assertTrue(mock.wrapped().flag)
 
     def test_calls_wrapped_class_different_method(self):
-        mock = self.MockClassWrapper()
+        mock = self.MockClassWrapper(None)
         mock.another_arb_method()
         self.assertTrue(mock.wrapped().another_flag)
-    
+
     def test_calls_invalid_method(self):
-        mock = self.MockClassWrapper()
+        mock = self.MockClassWrapper(None)
         with self.assertRaises(AttributeError) as context:
             mock.bad_method()
-            
-        self.assertIn("'MockClassWrapper' object has no attribute 'bad_method'", context.exception.args)
-    
+
+        self.assertIn(
+            "'MockClassWrapper' object has no attribute 'bad_method'", context.exception.args)
+
     def test_calls_invalid_method_different_class_different_method(self):
-        mock = self.MockClassWrapper_2()
+        mock = self.MockClassWrapper_2(None)
         with self.assertRaises(AttributeError) as context:
             mock.different_bad_method()
-            
-        self.assertIn("'MockClassWrapper_2' object has no attribute 'different_bad_method'", context.exception.args)
+
+        self.assertIn(
+            "'MockClassWrapper_2' object has no attribute 'different_bad_method'", context.exception.args)
+
+    def test_load_dependency(self):
+        dependency = object()
+        mock = self.MockClassWrapper(dependency)
+        mock.load()
+        self.assertEquals(dependency, mock.wrapped().dependency)
+
+    def test_load_dependency_with_different_method(self):
+        another_dependency = object()
+        mock = self.MockClassWrapper(another_dependency)
+        mock.another_load()
+        self.assertEquals(another_dependency,
+                          mock.wrapped().another_dependency)
+
+    def test_load_dependency_invalid_dependency(self):
+        dependency = object()
+        mock = self.MockClassWrapper_2(dependency)
+        with self.assertRaises(TypeError) as context:
+            mock.another_load()
+
+        self.assertIn(
+            "another_load() missing 1 required positional argument: 'another_dependency'", context.exception.args)
+
+    def test_load_dependency_with_return_value(self):
+        dependency = 4
+        mock = self.MockClassWrapper(dependency)
+        self.assertEquals(8, mock.fn_that_returns())
+
+    def test_load_dependency_with_different_return_value(self):
+        dependency = 6
+        mock = self.MockClassWrapper(dependency)
+        self.assertEquals(12, mock.fn_that_returns())
+
+    def test_load_with_arguments(self):
+        another_dependency = object()
+        mock = self.MockClassWrapper(another_dependency)
+        mock.load_with_params('hello', kwarg=4)
+        self.assertEquals('hello', mock.wrapped().arg)
+        self.assertEquals(4, mock.wrapped().kwarg)
