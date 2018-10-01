f603abc64b9beac2568786f6bbd8a23dd651d593
pippinlee <pippinblee@gmail.com>
2018-10-01 15:42:25 -0400

    add import statement in controller to allow for projects

--- foundations_ui/src/Home.js
+++ foundations_ui/src/Home.js
@@ -22,7 +22,7 @@ class Home extends Component {
   }
 
   componentDidMount() {
-    fetch("http://localhost:37722/api/v1/projects/asdf/jobs/queued")
+    fetch("http://localhost:37722/api/v1/projects")
       .then(res => res.json())
       .then(
         (result) => {
@@ -45,18 +45,30 @@ class Home extends Component {
     var projects;
     projects = result;
 
+    const List = props => <ul className="project-list">{props.children}</ul>;
+    const ListItem = props  => <a href={"/" + props.text}><li className="project-items">{props.text}</li></a>;
+
     if (error && result[0]) {
       return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
+    } else if (!isLoaded) { 
       return <div>Loading...</div>;
+    } else if (result && result[0]) {
+      return (
+        <BrowserRouter>
+          <div>
+            <h2>Projects Listing</h2>
+            <h1 className="project-name">Project name: </h1>
+            <h3 className="project-source">Source: not known</h3>
+            <List items={result} />
+          </div>
+        </BrowserRouter>
+      );
     } else {
       return (
         <BrowserRouter>
           <div>
-            <h2>Completed Jobs</h2>
-            <h1 className="project-name">Project name: </h1>
-            <h3 className="project-source">Source: not known</h3>
-            <List items={result} />
+            <h2>Project Listing</h2>
+            <p>No project have been created––you should create one!</p>
           </div>
         </BrowserRouter>
       );
