9803a8ab13ef93d2333934d3226303b3216d25d2
Thomas Rogers <tr@deeplearni.ng>
2018-10-01 11:41:27 -0400

    refactor get_metrics_for_all_jobs

--- foundations_sdk/src/foundations/projects.py
+++ foundations_sdk/src/foundations/projects.py
@@ -18,43 +18,58 @@ def get_metrics_for_all_jobs(project_name):
 
 
 def _flattened_job_metrics(project_name):
-    from foundations.models.completed_job_data_listing import CompletedJobDataListing
-
     stage_uuids = []
+
+    for job_data in _project_job_data(project_name):
+        _update_job_data(job_data, stage_uuids)
+        yield job_data
+
+def _update_job_data(job_data, stage_uuids):
+    output_metrics = job_data['output_metrics']
+    del job_data['output_metrics']
+
+    _fill_job_parameters(job_data, stage_uuids)
+
+    job_data.update(output_metrics)
+
+def _fill_job_parameters(job_data, stage_uuids):
+    job_parameters = job_data['job_parameters']
+    del job_data['job_parameters']
+
+    input_params = job_data['input_params']
+    del job_data['input_params']
+
+    _update_uuid_list(input_params, stage_uuids)
+
+    for param in input_params:
+        stage_name = _parameter_name(param, stage_uuids)
+        stage_value = _stage_value(param, job_parameters)
+
+        job_data[stage_name] = stage_value
+
+def _parameter_name(parameter, stage_uuids):
+    stage_index = stage_uuids.index(parameter['stage_uuid'])
+    return '{}-{}'.format(parameter['name'], stage_index)
+
+def _stage_value(parameter, job_parameters):
+    if parameter['value']['type'] == 'stage':
+        return parameter['value']['stage_name']
+
+    if parameter['value']['type'] == 'dynamic':
+        stage_value_key = parameter['value']['name']
+        return job_parameters[stage_value_key]
+
+    return parameter['value']['value']
+
+def _project_job_data(project_name):
+    from foundations.models.completed_job_data_listing import CompletedJobDataListing
 
     for job_data in CompletedJobDataListing.completed_job_data():
         if project_name == job_data['project_name']:
-            job_parameters = job_data['job_parameters']
-            del job_data['job_parameters']
-
-            input_params = job_data['input_params']
-            del job_data['input_params']
-
-            output_metrics = job_data['output_metrics']
-            del job_data['output_metrics']
-
-            _uuid_list(input_params, stage_uuids)
-
-            for param in input_params:
-                stage_uuid = param['stage_uuid']
-                stage_index = stage_uuids.index(stage_uuid)
-
-                stage_name = '{}-{}'.format(param['name'], stage_index)
-                if param['value']['type'] == 'stage':
-                    stage_value = param['value']['stage_name']
-                elif param['value']['type'] == 'dynamic':
-                    stage_value_key = param['value']['name']
-                    stage_value = job_parameters[stage_value_key]
-                else:
-                    stage_value = param['value']['value']
-
-                job_data[stage_name] = stage_value
-
-            job_data.update(output_metrics)
-
             yield job_data
 
-def _uuid_list(input_params, stage_uuids):
+
+def _update_uuid_list(input_params, stage_uuids):
     for param in input_params:
         if not param['stage_uuid'] in stage_uuids:
             stage_uuids.append(param['stage_uuid'])
