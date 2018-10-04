813843d7cf5d715a9094e75d94b979dc3e11d1ea
kbanc <katherine.bancroft@gmail.com>
2018-10-04 13:26:48 -0400

    more comments

--- foundations_ui/start_foundations_ui.sh
+++ foundations_ui/start_foundations_ui.sh
@@ -10,7 +10,7 @@ function trap_ctrlc ()
     exit 2
 }
 
-# initialise trap to call trap_ctrlc function when signal 2 (SIGINT) is received
+# initialise trap to call trap_ctrlc function when signal 2 (SIGINT) or 15 (SIGTERM) is received
 trap "trap_ctrlc" 2 15 
 
 npm install 
