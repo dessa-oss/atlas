e54f39e871b288216b6ab42da060efcc6efb850d
Ashwin Jiwane <ajiwane@Ashwins-MacBook-Pro.local>
2018-10-04 16:00:43 -0400

    Use stage_creation

--- documentation/sample_code/driver.py
+++ documentation/sample_code/driver.py
@@ -1,14 +1,17 @@
 import foundations
 from model import incr_by_10, mult
 
+incr_by_10 = foundations.create_stage(incr_by_10)
+mult = foundations.create_stage(mult)
+
 # input to model
 x = 20
 
 # build step1 of model
-incr_value = foundations.create_stage(incr_by_10(x))
+incr_value = incr_by_10(x)
 
 # build step2 of model
-result = foundations.create_stage(mult(x, incr_value))
+result = mult(x, incr_value)
 
 # run the model
 result.run()
