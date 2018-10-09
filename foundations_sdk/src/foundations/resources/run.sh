489ea8f9593b6b09869319aa2bba8467f60c4d12
Jinnah Ali-Clarke <ja@deeplearni.ng>
2018-10-09 16:22:40 -0400

    set default log level and make more portable

--- foundations_sdk/src/foundations/resources/run.sh
+++ foundations_sdk/src/foundations/resources/run.sh
@@ -6,25 +6,21 @@ cd $BASEDIR && \
   stat run.env> /dev/null 2>&1 && \
   . ./run.env
 
-current_tty=$(tty)
-
-if [ "${log_level}" = "DEBUG" ]; then
-  debug_log=$current_tty
-  info_log=$current_tty
-  error_log=$current_tty
-elif [ "${log_level}" = "INFO" ]; then
-  debug_log=/dev/null
-  info_log=$current_tty
-  error_log=$current_tty
-elif [ "${log_level}" = "ERROR" ]; then
-  debug_log=/dev/null
-  info_log=/dev/null
-  error_log=$current_tty
-else
-  debug_log=/dev/null
-  info_log=/dev/null
-  error_log=/dev/null
+if [ "${log_level}" = "" ]; then
+  log_level=INFO
 fi
+
+with_output_redirect () {
+  if [ "${log_level}" = "DEBUG" ]; then
+    "$@"
+  elif [ "${log_level}" = "INFO" ]; then
+    "$@" > /dev/null
+  elif [ "${log_level}" = "ERROR" ]; then
+    "$@" > /dev/null
+  else
+    "$@" > /dev/null 2> /dev/null
+  fi
+}
 
 if [[ -z "${python_path}" ]]; then
   python_path=$(which python || which python3)
@@ -32,10 +28,10 @@ fi
 
 cd $BASEDIR && \
   tar -xf job.tgz && \
-  $python_path -m pip install virtualenv 2>$error_log >$debug_log && \
-  $python_path -m virtualenv --system-site-packages venv 2>$error_log >$debug_log
+  with_output_redirect $python_path -m pip install virtualenv && \
+  with_output_redirect $python_path -m virtualenv --system-site-packages venv
 
-stat $BASEDIR/venv/bin/activate >$debug_log 2>$error_log
+with_output_redirect stat $BASEDIR/venv/bin/activate
 if [ $? -eq 0 ]; then 
   activate_path=venv/bin/activate
 else
@@ -44,9 +40,9 @@ fi
 
 cd $BASEDIR && \
   . $activate_path && \
-  echo Running python version `${python_path} --version` located at ${python_path} >$debug_log && \
+  with_output_redirect echo Running python version `${python_path} --version` located at ${python_path} && \
   touch requirements.txt && \
-  python -m pip install -r requirements.txt >$debug_log 2>$error_log && \
+  with_output_redirect python -m pip install -r requirements.txt && \
   python main.py
   
 status=$?
