bab2bf7ecd005d5c0902bb3b48d8ea24fdd8cdef
Katherine Bancroft <katherine.bancroft@gmail.com>
2018-10-18 16:00:14 -0400

    [FOUND-379][FOUND-380] Changed some log levels to INFO (#156)
    * logging changes for FOUND-379
    
    * FOUND-380 added project_listing_type to info log
    
    * made info log less verbose
    
    * increased log level for deployment type instead
    
    * added logging tests to stage_connector_wrapper
    
    * added logging tests to config_manager
    
    * added test for stage_logging_middleware
    
    * removed unnecessary 'any_order' in assert_has_calls
    
    * added test with different values to test logging on stage_logging_middleware

--- foundations_sdk/src/test/test_stage_connector_wrapper.py
+++ foundations_sdk/src/test/test_stage_connector_wrapper.py
@@ -7,6 +7,7 @@ Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 
 import unittest
 from foundations.stage_connector_wrapper import StageConnectorWrapper
+from mock import patch, call
 
 
 class TestStageConnectorWrapper(unittest.TestCase):
@@ -93,3 +94,22 @@ class TestStageConnectorWrapper(unittest.TestCase):
         self._stage.enable_caching()
         self.assertTrue(argument.cache_enabled)
         self.assertTrue(argument_two.cache_enabled)
+    
+    class MockDeploymentWrapper(object):
+        def __init__(self, deployment):
+            self._deployment = deployment
+            self._job_name = 'potato'
+        
+        def job_name(self):
+            return self._job_name
+    
+    @patch('foundations.deployment_wrapper.DeploymentWrapper', MockDeploymentWrapper)
+    @patch('foundations.deployment_manager.simple_deploy')
+    @patch('logging.Logger.info')
+    def test_run_logging(self, logger_mock, deployment_mock):
+        deployment_mock.return_value = 'something'
+        self._stage.run()
+        message1 = call("Deploying job...")
+        message2 = call("Job 'potato' deployed.")
+
+        logger_mock.assert_has_calls([message1, message2])
