828f44e19dda09d76516b15bd984c8d692b049dd
Thomas Rogers <thomasrogers@Macs-MacBook-Air.local>
2018-11-07 12:26:51 -0500

    create ScriptEnvironment class to wrap functionality of exporting the run script environment for the JobBundler

---
+++ foundations_sdk/src/foundations/job_bundling/script_environment.py
@@ -0,0 +1,22 @@
+"""
+Copyright (C) DeepLearning Financial Technologies Inc. - All Rights Reserved
+Unauthorized copying, distribution, reproduction, publication, use of this file, via any medium is strictly prohibited
+Proprietary and confidential
+Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
+"""
+
+class ScriptEnvironment(object):
+
+    def __init__(self, config):
+        self._config = config
+
+    def write_environment(self, file):
+        from pipes import quote
+
+        for name, value in self._run_script_environment().items():
+            file.write('export {}={}\n'.format(quote(name), quote(value)))
+        file.flush()
+        file.seek(0)
+
+    def _run_script_environment(self):
+        return self._config.get('run_script_environment', {})
