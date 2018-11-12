e67c7fd3754c0f2830dbd6d6517846bd72c1bd41
Thomas Rogers <tr@deeplearni.ng>
2018-11-12 16:59:31 -0500

    add documentation

--- foundations_sdk/src/foundations/consumers/jobs/queued/project_listing.py
+++ foundations_sdk/src/foundations/consumers/jobs/queued/project_listing.py
@@ -5,11 +5,23 @@ Proprietary and confidential
 Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 """
 
-
 class ProjectListing(object):
-
+    """Saves the job to a list of queued jobs for a project in redis
+    
+    Arguments:
+        redis {redis.Redis} -- A Redis connection object
+    """
+    
     def __init__(self, redis):
         self._redis = redis
 
     def call(self, message, timestamp, meta_data):
+        """See above
+        
+        Arguments:
+            message {dict} -- Event attributes
+            timestamp {int} -- The time the event was created
+            meta_data {dict} -- Additional data about the event
+        """
+
         self._redis.sadd('project:{}:jobs:queued'.format(message['project_name']), message['job_id'])
