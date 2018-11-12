c5c1d5491e09463c476054a69e91c38355cf8592
Thomas Rogers <tr@deeplearni.ng>
2018-11-12 15:55:00 -0500

    refactor input parameters consumer

--- foundations_sdk/src/foundations/consumers/jobs/queued/input_parameters.py
+++ foundations_sdk/src/foundations/consumers/jobs/queued/input_parameters.py
@@ -5,7 +5,9 @@ Proprietary and confidential
 Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 """
 
-class InputParameters(object):
+from foundations.consumers.jobs.queued.mixins.serialized_parameter import SerializedParameter
+
+class InputParameters(SerializedParameter):
     """Stores information about the input parameters used to create each
     parameter for a Foundations stage
     
@@ -13,18 +15,8 @@ class InputParameters(object):
         redis {redis.Redis} -- A Redis connection object
     """
     
-    def __init__(self, redis, serializer):
-        self._redis = redis
-        self._serializer = serializer
+    def _get_attribute(self, message):
+        return message['input_parameters']
 
-    def call(self, message, timestamp, meta_data):
-        """See above
-        
-        Arguments:
-            message {dict} -- Event attributes
-            timestamp {int} -- The time the event was created
-            meta_data {dict} -- Additional data about the event
-        """
-
-        serialized_run_data = self._serializer.dumps(message['input_parameters'])
-        self._redis.set('jobs:{}:input_parameters'.format(message['job_id']), serialized_run_data)
+    def _get_attribute_key(self):
+        return 'input_parameters'
