1312390108b2dd2f1705e9c3cee022020ff9d95b
kbanc <katherine.bancroft@gmail.com>
2018-11-12 17:03:15 -0500

    added documentation

--- foundations_sdk/src/foundations/consumers/job_metric_consumer.py
+++ foundations_sdk/src/foundations/consumers/job_metric_consumer.py
@@ -6,11 +6,25 @@ Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 """
 
 class JobMetricConsumer(object):
+    """
+    Class to consume the 'stage_log_middleware' channel. 
+    Passes the metric and value to a Redis list based on job_id
+    Arguments:
+        redis {redis.Redis} -- A Redis connection object
+    """
     
     def __init__(self, redis):
         self._redis = redis
 
     def call(self, message, timestamp, meta_data):
+        """
+        Adds metric names to redis list
+
+        Arguments:
+            message {dict} -- Event attributes
+            timestamp {int} -- The time the event was created
+            meta_data {dict} -- Additional data about the event
+        """
         from foundations.fast_serializer import serialize
         key = 'job:'+str(message['job_id'])
         value = (timestamp, message['key'], message['value'])
--- foundations_sdk/src/foundations/consumers/job_metric_name_consumer.py
+++ foundations_sdk/src/foundations/consumers/job_metric_name_consumer.py
@@ -6,11 +6,25 @@ Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 """
 
 class JobMetricNameConsumer(object):
+    """
+    Class to consume the 'stage_log_middleware' channel. 
+    Passes the name of job metrics into a list based on project name. 
+    Arguments:
+        redis {redis.Redis} -- A Redis connection object
+    """
     
     def __init__(self, redis):
         self._redis = redis
 
     def call(self, message, timestamp, meta_data):
+        """
+        Adds metric names to redis list
+
+        Arguments:
+            message {dict} -- Event attributes
+            timestamp {int} -- The time the event was created
+            meta_data {dict} -- Additional data about the event
+        """
         key = 'project:' + str(message['project_name'])
         value = message['key']
         self._redis.lpush(key, value)
