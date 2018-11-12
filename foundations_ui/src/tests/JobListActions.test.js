bf2005dfb4084b2f34dd0368b1c1ce33742a3c74
Chris Elcombe <chris.elcombe@ellefsontech.com>
2018-11-12 16:58:02 -0500

    CE - added unit tests for this PR

---
+++ foundations_ui/src/tests/JobListActions.test.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow, mount } from 'enzyme';
+import configureTests from './setupTests';
+import JobActions from '../js/actions/JobListActions';
+
+configureTests();
+
+it('getDateDiff', () => {
+  const now = Date.now();
+  const day1 = now;
+  const day2 = now + 1000;
+  const diff = JobActions.getDateDiff(day1, day2);
+  expect(diff).toBe(1000);
+});
+
+it('getDateDiff no 2nd time', () => {
+  const now = Date.now();
+  const day1 = now;
+  const day2 = null;
+  const diff = JobActions.getDateDiff(day1, day2);
+  expect(diff).toBeGreaterThanOrEqual(0);
+});
+
+it('getFormatedDate no date', () => {
+  const date = null;
+  const formatedDate = JobActions.getFormatedDate(date);
+  expect(formatedDate).toBe('');
+});
+
+it('getFormatedDate has date', () => {
+  const date = '2018-08-23T09:30:00';
+  const formatedDate = JobActions.getFormatedDate(date);
+  expect(formatedDate).toBe('2018/08/23');
+});
+
+it('getFormatedTime no date', () => {
+  const date = null;
+  const formatedTime = JobActions.getFormatedTime(date);
+  expect(formatedTime).toBe('');
+});
+
+it('getFormatedTime has date', () => {
+  const date = '2018-08-23T09:30:00';
+  const formatedTime = JobActions.getFormatedTime(date);
+  expect(formatedTime).toBe('09:30:00');
+});
+
+it('getDurationDays', () => {
+  const time = '86400500';
+  const duration = JobActions.getDurationDays(time);
+  expect(duration).toBe(1);
+});
+
+it('getDurationHours', () => {
+  const time = '7205000';
+  const duration = JobActions.getDurationHours(time);
+  expect(duration).toBe(2);
+});
+
+it('getDurationMinutes', () => {
+  const time = '183000';
+  const duration = JobActions.getDurationMinutes(time);
+  expect(duration).toBe(3);
+});
+
+it('getDurationSeconds', () => {
+  const time = '17500';
+  const duration = JobActions.getDurationSeconds(time);
+  expect(duration).toBe(17);
+});
