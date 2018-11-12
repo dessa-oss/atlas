eb8d6fc0fd0827e60a60fb4cc5a90a5dbe8d187b
Thomas Rogers <tr@deeplearni.ng>
2018-11-12 16:59:38 -0500

    add documentation

--- foundations_sdk/src/foundations/consumers/jobs/queued/project_name.py
+++ foundations_sdk/src/foundations/consumers/jobs/queued/project_name.py
@@ -6,9 +6,22 @@ Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 """
 
 class ProjectName(object):
+    """Saves the name of the project associated with the job to redis
+    
+    Arguments:
+        redis {redis.Redis} -- A Redis connection object
+    """
     
     def __init__(self, redis):
         self._redis = redis
 
     def call(self, message, timestamp, meta_data):
+        """See above
+        
+        Arguments:
+            message {dict} -- Event attributes
+            timestamp {int} -- The time the event was created
+            meta_data {dict} -- Additional data about the event
+        """
+
         self._redis.set('jobs:{}:project'.format(message['job_id']), message['project_name'])
