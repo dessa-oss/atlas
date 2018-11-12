3466f0bd1fca09c9530fbad50fdb3e0d427cdb81
Thomas Rogers <tr@deeplearni.ng>
2018-11-12 16:12:04 -0500

    refactor input parameter keys consumer

--- foundations_sdk/src/foundations/consumers/jobs/queued/input_parameter_keys.py
+++ foundations_sdk/src/foundations/consumers/jobs/queued/input_parameter_keys.py
@@ -5,25 +5,18 @@ Proprietary and confidential
 Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 """
 
-class InputParameterKeys(object):
+from foundations.consumers.jobs.queued.mixins.attribute_key_list import AttributeKeyList
+
+
+class InputParameterKeys(AttributeKeyList):
     """Stores a list of all common stage input parameter keys for a project in redis
-    
+
     Arguments:
         redis {redis.Redis} -- A Redis connection object
     """
 
-    
-    def __init__(self, redis):
-        self._redis = redis
+    def _get_attribute(self, message):
+        return message['input_parameters']
 
-    def call(self, message, timestamp, meta_data):
-        """See above
-        
-        Arguments:
-            message {dict} -- Event attributes
-            timestamp {int} -- The time the event was created
-            meta_data {dict} -- Additional data about the event
-        """
-
-        for key in message['input_parameters'].keys():
-            self._redis.sadd('projects:{}:input_parameter_names'.format(message['project_name']), key)
+    def _get_attribute_key(self):
+        return 'input_parameter_names'
