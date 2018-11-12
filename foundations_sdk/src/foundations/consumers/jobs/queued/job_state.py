3827aa39eebab9c18ecafd3fcbc4ff7b91a6399e
Thomas Rogers <tr@deeplearni.ng>
2018-11-12 16:59:25 -0500

    add documentation

--- foundations_sdk/src/foundations/consumers/jobs/queued/job_state.py
+++ foundations_sdk/src/foundations/consumers/jobs/queued/job_state.py
@@ -6,9 +6,22 @@ Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 """
 
 class JobState(object):
+    """Sets the state of a job to queued in redis
+    
+    Arguments:
+        redis {redis.Redis} -- A Redis connection object
+    """
     
     def __init__(self, redis):
         self._redis = redis
 
     def call(self, message, timestamp, meta_data):
+        """See above
+        
+        Arguments:
+            message {dict} -- Event attributes
+            timestamp {int} -- The time the event was created
+            meta_data {dict} -- Additional data about the event
+        """
+
         self._redis.set('jobs:{}:state'.format(message['job_id']), 'qeueud')
