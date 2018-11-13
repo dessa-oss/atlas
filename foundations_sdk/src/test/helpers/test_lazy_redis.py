cc09968f3cdd6ca8915227a88950be7aff841f5d
kbanc <katherine.bancroft@gmail.com>
2018-11-13 13:38:18 -0500

    added further tests to lazy_redis

--- foundations_sdk/src/test/helpers/test_lazy_redis.py
+++ foundations_sdk/src/test/helpers/test_lazy_redis.py
@@ -9,25 +9,38 @@ import unittest
 from mock import Mock
 from foundations.helpers.lazy_redis import LazyRedis
 
+
 class TestLazyRedis(unittest.TestCase):
 
     class MockObject(object):
         def __init__(self):
             self.value = 5
-    
+            self.name = 'mock'
+
     def setUp(self):
         pass
-    
-    def test_get_attr_returns_attribute(self):
+
+    def test_get_attr_returns_attribute_value(self):
         lazy_redis = LazyRedis(self._callback)
         self.assertEqual(lazy_redis.value, 5)
-    
+
+    def test_get_attr_returns_attribute_name(self):
+        lazy_redis = LazyRedis(self._callback)
+        self.assertEqual(lazy_redis.name, 'mock')
+
     def test_get_attr_raises_attribute_error(self):
         lazy_redis = LazyRedis(self._callback)
         with self.assertRaises(AttributeError) as context:
             lazy_redis.redis
-        self.assertIn("'MockObject' object has no attribute 'redis'", context.exception.args)
+        self.assertIn("'MockObject' object has no attribute 'redis'",
+                      context.exception.args)
 
-    
+    def test_get_attr_raises_attribute_error_different_attribute(self):
+        lazy_redis = LazyRedis(self._callback)
+        with self.assertRaises(AttributeError) as context:
+            lazy_redis.potato
+        self.assertIn("'MockObject' object has no attribute 'potato'",
+                      context.exception.args)
+
     def _callback(self):
         return self.MockObject()
