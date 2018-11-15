8a5ad8723bc407483d0c5d9b752738c28ccf5634
trogers03 <tr@deeplearni.ng>
2018-11-15 13:17:40 -0500

    FOUND-456 Tr clean consumer tests (#209)
    * Update README.md
    
    * add template for pull request
    
    * Update to note backwards compatibility (#173)
    
    * update readme to include release links
    
    * create consumer for removing queued jobs
    
    * prepare for refactoring
    
    * create consumer for listing running jobs
    
    * refactor running job project listing to use mixin
    
    * refactor queued job project listing to use mixin
    
    * create consumer for setting the start time of a job
    
    * refactor job start time setter to use time setter
    
    * refactor job creation time setter to use time setter
    
    * create running job state consumer
    
    * refactor property setting functionality
    
    * refactor running job state to use property setter
    
    * refactor queued job state to use property setter
    
    * create completed time consumer
    
    * create consumer for completed job state
    
    * fix queued project listing
    
    * fix queued job state
    
    * uniquify tests names
    
    * create consumer for failed job state
    
    * create consumer for job error information
    
    * cleanup formatting
    
    * register queued job event consumers
    
    * register running job event consumers
    
    * register completed job event consumers
    
    * register failed job event consumers
    
    * create consumer for setting job users
    
    * add user setter to list of queued consumers
    
    * fix documentation
    
    * create test requirements file
    
    * move redis connection to global state
    
    * import consumers to get them in use
    
    * add integration tests for queued job event
    
    * create integration test for run job event
    
    * create integration test for complete job event
    
    * fix key for error information on a failed job
    
    * add integration tests for failed job event
    
    * added metric test
    
    * moved position of my test in file
    
    * install test requirements file during CI
    
    * removed unnecessary import statement
    
    * added fakeredis to jenkins
    
    * add docs
    
    * put the set user consumer back
    
    * changed the time mock
    
    * start refactoring for visual cleanup
    
    * make 'r clean, once again

--- foundations_sdk/src/integration/test_consumers.py
+++ foundations_sdk/src/integration/test_consumers.py
@@ -28,7 +28,8 @@ class TestConsumers(unittest.TestCase):
         job_id = self._str_random_uuid()
         expected_job_parameters = {'random_job_data': self._str_random_uuid()}
         expected_input_parameters = {
-            'random_input_data': self._str_random_uuid()}
+            'random_input_data': self._str_random_uuid()
+        }
         user = self._random_name()
 
         message = {
@@ -41,39 +42,45 @@ class TestConsumers(unittest.TestCase):
         message_router.push_message('queue_job', message)
         current_time = time()
 
-        job_parameter_names = self._redis.smembers(
-            'projects:{}:job_parameter_names'.format(project_name))
+        parameter_key = 'projects:{}:job_parameter_names'.format(project_name)
+        job_parameter_names = self._redis.smembers(parameter_key)
         self.assertEqual(set([b'random_job_data']), job_parameter_names)
 
-        input_parameter_names = self._redis.smembers(
-            'projects:{}:input_parameter_names'.format(project_name))
+        input_parameter_key = self._input_parameter_key(project_name)
+        input_parameter_names = self._redis.smembers(input_parameter_key)
         self.assertEqual(set([b'random_input_data']), input_parameter_names)
 
-        queued_jobs = self._redis.smembers(
-            'project:{}:jobs:queued'.format(project_name))
+        queued_job_key = 'project:{}:jobs:queued'.format(project_name)
+        queued_jobs = self._redis.smembers(queued_job_key)
         self.assertEqual(set([byte_string(job_id)]), queued_jobs)
 
-        job_parameters = self._get_and_deserialize(
-            'jobs:{}:parameters'.format(job_id))
+        job_parameters_key = 'jobs:{}:parameters'.format(job_id)
+        job_parameters = self._get_and_deserialize(job_parameters_key)
         self.assertEqual(expected_job_parameters, job_parameters)
 
-        state = self._redis.get('jobs:{}:state'.format(job_id))
+        job_state_key = 'jobs:{}:state'.format(job_id)
+        state = self._redis.get(job_state_key)
         self.assertEqual(b'queued', state)
 
-        job_project_name = self._redis.get('jobs:{}:project'.format(job_id))
+        job_project_key = 'jobs:{}:project'.format(job_id)
+        job_project_name = self._redis.get(job_project_key)
         self.assertEqual(byte_string(project_name), job_project_name)
 
-        job_user = self._redis.get('jobs:{}:user'.format(job_id))
+        job_user_key = 'jobs:{}:user'.format(job_id)
+        job_user = self._redis.get(job_user_key)
         self.assertEqual(byte_string(user), job_user)
 
-        string_creation_time = self._redis.get(
-            'jobs:{}:creation_time'.format(job_id))
+        creation_time_key = 'jobs:{}:creation_time'.format(job_id)
+        string_creation_time = self._redis.get(creation_time_key)
         creation_time = float(string_creation_time.decode())
         self.assertTrue(current_time - creation_time < 0.1)
 
-        input_parameters = self._get_and_deserialize(
-            'jobs:{}:input_parameters'.format(job_id))
+        input_parameters_key = 'jobs:{}:input_parameters'.format(job_id)
+        input_parameters = self._get_and_deserialize(input_parameters_key)
         self.assertEqual(expected_input_parameters, input_parameters)
+
+    def _input_parameter_key(self, project_name):
+        return 'projects:{}:input_parameter_names'.format(project_name)
 
     def test_running_job_consumers(self):
         from foundations.global_state import message_router
@@ -90,16 +97,17 @@ class TestConsumers(unittest.TestCase):
         message_router.push_message('run_job', message)
         current_time = time()
 
-        running_and_completed_jobs = self._redis.smembers(
-            'project:{}:jobs:running'.format(project_name))
-        self.assertEqual(set([byte_string(job_id)]),
-                         running_and_completed_jobs)
+        running_jobs_key = 'project:{}:jobs:running'.format(project_name)
+        running_and_completed_jobs = self._redis.smembers(running_jobs_key)
+        expected_jobs = set([byte_string(job_id)])
+        self.assertEqual(expected_jobs, running_and_completed_jobs)
 
-        state = self._redis.get('jobs:{}:state'.format(job_id))
+        job_state_key = 'jobs:{}:state'.format(job_id)
+        state = self._redis.get(job_state_key)
         self.assertEqual(b'running', state)
 
-        string_start_time = self._redis.get(
-            'jobs:{}:start_time'.format(job_id))
+        start_time_key = 'jobs:{}:start_time'.format(job_id)
+        string_start_time = self._redis.get(start_time_key)
         start_time = float(string_start_time.decode())
         self.assertTrue(current_time - start_time < 0.1)
 
@@ -120,8 +128,8 @@ class TestConsumers(unittest.TestCase):
         state = self._redis.get('jobs:{}:state'.format(job_id))
         self.assertEqual(b'completed', state)
 
-        string_completed_time = self._redis.get(
-            'jobs:{}:completed_time'.format(job_id))
+        completed_time_key = 'jobs:{}:completed_time'.format(job_id)
+        string_completed_time = self._redis.get(completed_time_key)
         completed_time = float(string_completed_time.decode())
         self.assertTrue(current_time - completed_time < 0.1)
 
@@ -144,12 +152,12 @@ class TestConsumers(unittest.TestCase):
         state = self._redis.get('jobs:{}:state'.format(job_id))
         self.assertEqual(b'Error', state)
 
-        state = self._get_and_deserialize(
-            'jobs:{}:error_information'.format(job_id))
+        error_information_key = 'jobs:{}:error_information'.format(job_id)
+        state = self._get_and_deserialize(error_information_key)
         self.assertEqual(expected_input_parameters, state)
 
-        string_completed_time = self._redis.get(
-            'jobs:{}:completed_time'.format(job_id))
+        completed_time_key = 'jobs:{}:completed_time'.format(job_id)
+        string_completed_time = self._redis.get(completed_time_key)
         completed_time = float(string_completed_time.decode())
         self.assertTrue(current_time - completed_time < 0.1)
 
@@ -174,15 +182,16 @@ class TestConsumers(unittest.TestCase):
         message_router.push_message('job_metrics', message)
         current_time = time()
 
-        job_metrics = self._redis.get('job:{}:metrics'.format(job_id))
+        job_metrics_key = 'job:{}:metrics'.format(job_id)
+        job_metrics = self._redis.get(job_metrics_key)
         job_metrics = deserialize(job_metrics)
 
         self.assertTrue(current_time - job_metrics[0] < 0.1)
         self.assertEqual(key, job_metrics[1])
         self.assertEqual(value, job_metrics[2])
 
-        project_metric_name = self._redis.smembers(
-            'project:{}:metrics'.format(project_name))
+        project_metrics_key = 'project:{}:metrics'.format(project_name)
+        project_metric_name = self._redis.smembers(project_metrics_key)
         self.assertEqual(project_metric_name, set([byte_string(key)]))
 
     def _get_and_deserialize(self, key):
