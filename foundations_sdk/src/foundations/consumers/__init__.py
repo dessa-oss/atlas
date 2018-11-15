06a6eb744c6940ac83657b1fcf1a2633181e89ef
trogers03 <tr@deeplearni.ng>
2018-11-15 11:16:18 -0500

    FOUND-465 Tr consumers tie together (#197)
    * create consumer for removing queued jobs
    
    * prepare for refactoring
    
    * create consumer for listing running jobs
    
    * refactor running job project listing to use mixin
    
    * refactor queued job project listing to use mixin
    
    * create consumer for setting the start time of a job
    
    * refactor job start time setter to use time setter
    
    * refactor job creation time setter to use time setter
    
    * create running job state consumer
    
    * refactor property setting functionality
    
    * refactor running job state to use property setter
    
    * refactor queued job state to use property setter
    
    * create completed time consumer
    
    * create consumer for completed job state
    
    * fix queued project listing
    
    * fix queued job state
    
    * uniquify tests names
    
    * create consumer for failed job state
    
    * create consumer for job error information
    
    * cleanup formatting
    
    * register queued job event consumers
    
    * register running job event consumers
    
    * register completed job event consumers
    
    * register failed job event consumers
    
    * fix documentation
    
    * fix key for error information on a failed job

--- foundations_sdk/src/foundations/consumers/__init__.py
+++ foundations_sdk/src/foundations/consumers/__init__.py
@@ -6,19 +6,80 @@ Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 """
 
 from foundations.global_state import message_router
-from foundations.consumers.job_metric_consumer import JobMetricConsumer
-from foundations.consumers.job_metric_name_consumer import JobMetricNameConsumer
+
+_add_listener = message_router.add_listener
+
+
+def _add_consumers_for_stage_log_middleware(redis):
+    from foundations.consumers.job_metric_consumer import JobMetricConsumer
+    from foundations.consumers.job_metric_name_consumer import JobMetricNameConsumer
+
+    _add_listener(JobMetricConsumer(redis), 'job_metrics')
+    _add_listener(JobMetricNameConsumer(redis), 'job_metrics')
+
+
+def _add_consumers_for_queue_job(redis):
+    from foundations.consumers.jobs.queued.creation_time import CreationTime
+    from foundations.consumers.jobs.queued.input_parameter_keys import InputParameterKeys
+    from foundations.consumers.jobs.queued.input_parameters import InputParameters
+    from foundations.consumers.jobs.queued.job_state import JobState
+    from foundations.consumers.jobs.queued.project_listing import ProjectListing
+    from foundations.consumers.jobs.queued.project_name import ProjectName
+    from foundations.consumers.jobs.queued.run_data_keys import RunDataKeys
+    from foundations.consumers.jobs.queued.run_data import RunData
+
+    import json
+
+    _add_listener(CreationTime(redis), 'queue_job')
+    _add_listener(InputParameterKeys(redis), 'queue_job')
+    _add_listener(InputParameters(redis, json), 'queue_job')
+    _add_listener(JobState(redis), 'queue_job')
+    _add_listener(ProjectListing(redis), 'queue_job')
+    _add_listener(ProjectName(redis), 'queue_job')
+    _add_listener(RunDataKeys(redis), 'queue_job')
+    _add_listener(RunData(redis, json), 'queue_job')
+
+
+def _add_consumers_for_run_job(redis):
+    from foundations.consumers.jobs.running.job_state import JobState
+    from foundations.consumers.jobs.running.project_listing import ProjectListing
+    from foundations.consumers.jobs.running.remove_queued_job import RemoveQueuedJob
+    from foundations.consumers.jobs.running.start_time import StartTime
+
+    _add_listener(JobState(redis), 'run_job')
+    _add_listener(ProjectListing(redis), 'run_job')
+    _add_listener(RemoveQueuedJob(redis), 'run_job')
+    _add_listener(StartTime(redis), 'run_job')
+
+
+def _add_consumers_for_complete_job(redis):
+    from foundations.consumers.jobs.completed.completed_time import CompletedTime
+    from foundations.consumers.jobs.completed.job_state import JobState
+
+    _add_listener(CompletedTime(redis), 'complete_job')
+    _add_listener(JobState(redis), 'complete_job')
+
+
+def _add_consumers_for_fail_job(redis):
+    from foundations.consumers.jobs.completed.completed_time import CompletedTime
+    from foundations.consumers.jobs.failed.error_data import ErrorData
+    from foundations.consumers.jobs.failed.job_state import JobState
+
+    _add_listener(CompletedTime(redis), 'fail_job')
+    _add_listener(CompletedTime(redis), 'fail_job')
+    _add_listener(JobState(redis), 'fail_job')
 
 
 def _create_redis_instance_and_add_consumers():
     import redis
     from foundations.helpers.lazy_redis import LazyRedis
-    _redis = LazyRedis(redis.Redis)
 
-    message_router.add_listener(
-        JobMetricConsumer(_redis), 'job_metrics')
-    message_router.add_listener(
-        JobMetricNameConsumer(_redis), 'job_metrics')
+    redis = LazyRedis(redis.Redis)
+    _add_consumers_for_stage_log_middleware(redis)
+    _add_consumers_for_queue_job(redis)
+    _add_consumers_for_run_job(redis)
+    _add_consumers_for_complete_job(redis)
+    _add_consumers_for_fail_job(redis)
 
 
 _create_redis_instance_and_add_consumers()
