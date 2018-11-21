a4541089a6f523c08d39fc085e9df8e3f5ad561e
Thomas Rogers <thomasrogers@Macs-MacBook-Air.local>
2018-11-21 11:00:08 -0500

    create configurable connection wrapper for redis

--- foundations_sdk/src/test/helpers/__init__.py
+++ foundations_sdk/src/test/helpers/__init__.py
@@ -8,3 +8,4 @@ Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 from test.helpers.test_wrap_class_with_dependency import TestWrapClassWithDependency
 from test.helpers.test_argument_namer import TestArgumentNamer
 from test.helpers.test_lazy_redis import TestLazyRedis
+from test.helpers.test_redis_connector import TestRedisConnector
