8cdeca5950944ccf91173cb2da52eeebb40b14c5
Thomas Rogers <thomasrogers@Macs-MacBook-Air.local>
2018-11-21 11:52:21 -0500

    update input parameter consumer to properly handle producer message

--- foundations_sdk/src/test/consumers/jobs/queued/test_input_parameter_keys.py
+++ foundations_sdk/src/test/consumers/jobs/queued/test_input_parameter_keys.py
@@ -17,18 +17,25 @@ class TestInputParameterKeys(unittest.TestCase):
         self._consumer = InputParameterKeys(self._redis)
 
     def test_call_saved_run_data_keys(self):
-        self._consumer.call({'project_name': 'here be dragons', 'input_parameters': {'number_of_neurons': 3434}}, None, None)
+        input_parameters = [{'argument': {'name': 'number_of_neurons', 'value': 3434}, 'stage_uuid': 'stage1'}]
+        self._consumer.call({'project_name': 'here be dragons', 'input_parameters': input_parameters}, None, None)
         self._redis.sadd.assert_called_with('projects:here be dragons:input_parameter_names', 'number_of_neurons')
 
     def test_call_saved_run_data_keys_different_keys(self):
-        self._consumer.call({'project_name': 'here be dragons', 'input_parameters': {'hidden_layers': 7777}}, None, None)
+        input_parameters = [{'argument': {'name': 'hidden_layers', 'value': 7777}, 'stage_uuid': 'stage1'}]
+        self._consumer.call({'project_name': 'here be dragons', 'input_parameters': input_parameters}, None, None)
         self._redis.sadd.assert_called_with('projects:here be dragons:input_parameter_names', 'hidden_layers')
 
     def test_call_saved_run_data_keys_multiple_keys(self):
-        self._consumer.call({'project_name': 'here be dragons', 'input_parameters': {'shown_layers': 7777, 'neurons': 33}}, None, None)
+        input_parameters = [
+            {'argument': {'name': 'shown_layers', 'value': 7777}, 'stage_uuid': 'stage1'},
+            {'argument': {'name': 'neurons', 'value': 33}, 'stage_uuid': 'stage1'},
+        ]
+        self._consumer.call({'project_name': 'here be dragons', 'input_parameters': input_parameters}, None, None)
         self._redis.sadd.assert_any_call('projects:here be dragons:input_parameter_names', 'shown_layers')
         self._redis.sadd.assert_any_call('projects:here be dragons:input_parameter_names', 'neurons')
 
     def test_call_saved_run_data_keys_different_project_name(self):
-        self._consumer.call({'project_name': 'here be sheep', 'input_parameters': {'hidden_layers': 7777}}, None, None)
+        input_parameters = [{'argument': {'name': 'hidden_layers', 'value': 7777}, 'stage_uuid': 'stage1'}]
+        self._consumer.call({'project_name': 'here be sheep', 'input_parameters': input_parameters}, None, None)
         self._redis.sadd.assert_called_with('projects:here be sheep:input_parameter_names', 'hidden_layers')
