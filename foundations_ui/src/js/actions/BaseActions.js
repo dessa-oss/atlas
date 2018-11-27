e6198e5a55b4b62378c76c0900e4517c842d6432
Chris Elcombe <chris.elcombe@ellefsontech.com>
2018-11-27 14:24:20 -0500

    CE - removed double _

--- foundations_ui/src/js/actions/BaseActions.js
+++ foundations_ui/src/js/actions/BaseActions.js
@@ -1,6 +1,6 @@
 const BaseActions = {
   baseURL: process.env.REACT_APP_API_URL || 'http://private-83924-dessa.apiary-mock.com/api/v1/',
-  baseBetaURL: process.env.REACT_APP__BETA_API_URL || 'http://private-83924-dessa.apiary-mock.com/api/v2beta/',
+  baseBetaURL: process.env.REACT_APP_BETA_API_URL || 'http://private-83924-dessa.apiary-mock.com/api/v2beta/',
   getFromAPI(url) {
     const fullURL = this.baseURL.concat(url);
     return fetch(fullURL)
