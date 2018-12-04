dba3e8debcccd3b9f4455a70ac5a5b3b10c1f232
Jinnah Ali-Clarke <GRX01@hotmail.com>
2018-12-04 16:03:21 -0500

    FOUND-650 preliminary dockerization of gui (#253)
    * dockerized rest api
    
    * move stuff
    
    * dockerized gui
    
    * build both images and also create startup script
    
    * consistency

--- docker/run_api_server.py
+++ docker/run_api_server.py
@@ -1,8 +1,12 @@
+import os
+
 from foundations_rest_api.global_state import app_manager
 from foundations.global_state import deployment_manager, config_manager
+
+config_manager.config()["redis_url"] = os.environ["REDIS_URL"]
 
 for key, value in config_manager.config().items():
     print(key, value)
 
 app = app_manager.app()
-app.run(port=37722)
+app.run(host="0.0.0.0", port=37722)
