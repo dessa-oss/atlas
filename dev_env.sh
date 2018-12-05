438839e2a42909672f6b31eb4538f19d7f4dafeb
thomasrogers03 <thomasrogers03@gmail.com>
2018-12-05 09:31:04 -0500

    modify dev environment script to support contrib and internal modules

--- dev_env.sh
+++ dev_env.sh
@@ -6,6 +6,8 @@ add_path () {
 
 cwd=`pwd`
 add_path "$cwd/foundations_internal/src" && \
+    add_path "$cwd/foundations_contrib/src" && \
+    add_path "$cwd/foundations_sdk/src" && \
     add_path "$cwd/ssh_utils/src" && \
     add_path "$cwd/gcp_utils/src" && \
     add_path "$cwd/foundations_rest_api/src"
