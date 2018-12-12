b045e315f61ae0bec2c9a108bb928cbc0d670eaa
Dariem Perez <d.perez@dessa.com>
2018-12-12 14:25:57 -0500

    Fix bool parser

--- foundations_rest_api/src/foundations_rest_api/filters/parsers/bool_parser.py
+++ foundations_rest_api/src/foundations_rest_api/filters/parsers/bool_parser.py
@@ -9,4 +9,10 @@ Written by Dariem Perez <d.perez@dessa.com>, 12 2018
 class BoolParser(object):
 
     def parse(self, value):
-        return bool(value)
+        return True if self._is_true(value) else False if self._is_false(value) else None
+
+    def _is_true(self, value):
+        return str(value).title() == 'True'
+
+    def _is_false(self, value):
+        return str(value).title() == 'False'
