d4c1efe9965bab13dd9159a244cb1fe3602f8538
Dariem Perez <d.perez@dessa.com>
2018-12-13 18:24:06 -0500

    I actually need three jobs

--- foundations_rest_api/src/acceptance/v2beta/test_job_listing_parametrics.py
+++ foundations_rest_api/src/acceptance/v2beta/test_job_listing_parametrics.py
@@ -20,6 +20,7 @@ class TestJobListingParametrics(APIAcceptanceTestCaseBase):
         klass._project_name = 'hanna'
         klass._first_job_name = 'test job 1'
         klass._second_job_name = 'test job 2'
+        klass._third_job_name = 'test job 3'
         klass._run_stages()
 
     @classmethod
@@ -27,7 +28,7 @@ class TestJobListingParametrics(APIAcceptanceTestCaseBase):
         from foundations.global_state import redis_connection as redis
 
         keys = []
-        for name in (klass._project_name, klass._first_job_name, klass._second_job_name):
+        for name in (klass._project_name, klass._first_job_name, klass._second_job_name, klass._third_job_name):
             keys += redis.keys('*{}*'.format(name))
         redis.delete(*keys)
 
@@ -60,36 +61,76 @@ class TestJobListingParametrics(APIAcceptanceTestCaseBase):
             value4 = stage1(20, float('nan'))
             stage2(value3, value4, 8.3).run(job_name=klass._second_job_name)
 
+        def run_third_job():
+            set_project_name(klass._project_name)
+            value3 = stage0()
+            value4 = stage1(30, 'some string')
+            stage2(value3, value4, 3.14).run(job_name=klass._third_job_name)
+
         from multiprocessing import Process
 
+        # Fork job run due to bug not allowing Foundation to create more than one job in the same process
         p = Process(target=run_first_job)
         p.start()
         p.join()
         p = Process(target=run_second_job)
         p.start()
         p.join()
+        p = Process(target=run_third_job)
+        p.start()
+        p.join()
 
     def test_get_route(self):
         data = super(TestJobListingParametrics, self).test_get_route()
-        self.assertEqual(data['jobs'][0]['job_id'], self._second_job_name)
-        self.assertEqual(data['jobs'][0]['input_params'][0]['value'], 20)
-        self.assertEqual(data['jobs'][0]['input_params'][1]['value'], None)
+
+        self.assertEqual(data['jobs'][0]['job_id'], self._third_job_name)
+
+        self.assertEqual(data['jobs'][0]['input_params'][0]['name'], 'value1-5')
+        self.assertEqual(data['jobs'][0]['input_params'][1]['name'], 'value2-5')
+        self.assertEqual(data['jobs'][0]['input_params'][2]['name'], 'value1-6')
+        self.assertEqual(data['jobs'][0]['input_params'][3]['name'], 'value2-6')
+        self.assertEqual(data['jobs'][0]['input_params'][4]['name'], 'value3-6')
+        self.assertEqual(data['jobs'][0]['input_params'][0]['value'], 30)
+        self.assertEqual(data['jobs'][0]['input_params'][1]['value'], 'some string')
         self.assertEqual(data['jobs'][0]['input_params'][2]['value'], 'stage0-2')
-        self.assertEqual(data['jobs'][0]['input_params'][3]['value'], 'stage1-3')
-        self.assertEqual(data['jobs'][0]['input_params'][4]['value'], 8.3)
+        self.assertEqual(data['jobs'][0]['input_params'][3]['value'], 'stage1-5')
+        self.assertEqual(data['jobs'][0]['input_params'][4]['value'], 3.14)
 
         self.assertIsNone(data['jobs'][0]['output_metrics'][0]['value'])
         self.assertIsNone(data['jobs'][0]['output_metrics'][1]['value'])
-        self.assertEqual(data['jobs'][0]['output_metrics'][2]['value'], 8.3)
+        self.assertEqual(data['jobs'][0]['output_metrics'][2]['value'], 3.14)
 
-        self.assertEqual(data['jobs'][1]['job_id'], self._first_job_name)
-        self.assertEqual(data['jobs'][1]['input_params'][0]['value'], 10)
-        # This "NoneType" must be changed to None when bug is addressed in FOUND-715
-        self.assertEqual(data['jobs'][1]['input_params'][1]['value'], 'NoneType')
+        self.assertEqual(data['jobs'][1]['job_id'], self._second_job_name)
+
+        self.assertEqual(data['jobs'][1]['input_params'][0]['name'], 'value1-3')
+        self.assertEqual(data['jobs'][1]['input_params'][1]['name'], 'value2-3')
+        self.assertEqual(data['jobs'][1]['input_params'][2]['name'], 'value1-4')
+        self.assertEqual(data['jobs'][1]['input_params'][3]['name'], 'value2-4')
+        self.assertEqual(data['jobs'][1]['input_params'][4]['name'], 'value3-4')
+        self.assertEqual(data['jobs'][1]['input_params'][0]['value'], 20)
+        self.assertEqual(data['jobs'][1]['input_params'][1]['value'], None)
         self.assertEqual(data['jobs'][1]['input_params'][2]['value'], 'stage0-2')
-        self.assertEqual(data['jobs'][1]['input_params'][3]['value'], 'stage1-0')
-        self.assertEqual(data['jobs'][1]['input_params'][4]['value'], 5)
+        self.assertEqual(data['jobs'][1]['input_params'][3]['value'], 'stage1-3')
+        self.assertEqual(data['jobs'][1]['input_params'][4]['value'], 8.3)
 
         self.assertIsNone(data['jobs'][1]['output_metrics'][0]['value'])
         self.assertIsNone(data['jobs'][1]['output_metrics'][1]['value'])
-        self.assertEqual(data['jobs'][1]['output_metrics'][2]['value'], 5)
+        self.assertEqual(data['jobs'][1]['output_metrics'][2]['value'], 8.3)
+
+        self.assertEqual(data['jobs'][2]['job_id'], self._first_job_name)
+
+        self.assertEqual(data['jobs'][2]['input_params'][0]['name'], 'value1-0')
+        self.assertEqual(data['jobs'][2]['input_params'][1]['name'], 'value2-0')
+        self.assertEqual(data['jobs'][2]['input_params'][2]['name'], 'value1-1')
+        self.assertEqual(data['jobs'][2]['input_params'][3]['name'], 'value2-1')
+        self.assertEqual(data['jobs'][2]['input_params'][4]['name'], 'value3-1')
+        self.assertEqual(data['jobs'][2]['input_params'][0]['value'], 10)
+        # This "NoneType" must be changed to None when bug is addressed in FOUND-715
+        self.assertEqual(data['jobs'][2]['input_params'][1]['value'], 'NoneType')
+        self.assertEqual(data['jobs'][2]['input_params'][2]['value'], 'stage0-2')
+        self.assertEqual(data['jobs'][2]['input_params'][3]['value'], 'stage1-0')
+        self.assertEqual(data['jobs'][2]['input_params'][4]['value'], 5)
+
+        self.assertIsNone(data['jobs'][2]['output_metrics'][0]['value'])
+        self.assertIsNone(data['jobs'][2]['output_metrics'][1]['value'])
+        self.assertEqual(data['jobs'][2]['output_metrics'][2]['value'], 5)
