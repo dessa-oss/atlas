26db14624c29f5a51c2e8cbaaffc42b8784e51e4
Dariem Perez <d.perez@dessa.com>
2018-12-13 13:27:05 -0500

    Add unit tests for status parser

--- foundations_rest_api/src/test/filters/parsers/__init__.py
+++ foundations_rest_api/src/test/filters/parsers/__init__.py
@@ -8,3 +8,4 @@ from test.filters.parsers.test_datetime_parser import TestDateTimeParser
 from test.filters.parsers.test_elapsed_time_parser import TestElapsedTimeParser
 from test.filters.parsers.test_number_parser import TestNumberParser
 from test.filters.parsers.test_bool_parser import TestBoolParser
+from test.filters.parsers.test_status_parser import TestStatusParser
