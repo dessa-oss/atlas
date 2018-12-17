98132dacb614bbd6832214f9a432109ba0bdccd9
Pippin Lee <pippinblee@gmail.com>
2018-12-17 13:43:50 -0500

    FOUND-667 remove split_at from results (#278)
    * FOUND-667 check for split at in input params to filter them out
    
    * FOUND-667 remove test lines
    
    * remove extra line
    
    * FOUND-667 break out into remove_split_at function
    
    * FOUND-667 move logic for split_at filtering to input_parameter_formatter to reduce logic split between REST API and resultreader
    
    * remove extra line
    
    * FOUND-667 add test to check that removing split_at stages
    
    * remove comments
    
    * formatting
    
    * FOUND-667 create contains_split_at function to keep logic cleaner
    
    * refactoring
    
    * refactor

--- foundations_contrib/src/foundations_contrib/input_parameter_formatter.py
+++ foundations_contrib/src/foundations_contrib/input_parameter_formatter.py
@@ -5,55 +5,63 @@ Proprietary and confidential
 Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 """
 
+
 class InputParameterFormatter(object):
-    
+
     def __init__(self, input_parameters, job_parameters, stage_rank):
         self._stage_rank = stage_rank
         self._input_parameters = input_parameters
         self._job_parameters = job_parameters
-    
+
     def format_input_parameters(self):
         formatted_input_parameters = []
 
         for param in self._input_parameters:
-            self._append_parameter_info(param, formatted_input_parameters, self._stage_rank)
+            self._append_parameter_info(
+                param, formatted_input_parameters, self._stage_rank)
 
         return formatted_input_parameters
-    
+
     def _append_parameter_info(self, param, formatted_input_parameters, stage_rank):
         from foundations_rest_api.v2beta.models.extract_type import extract_type
 
         param_name = self._index_input_param(param, stage_rank)
-        param_value, param_source = self._evaluate_input_param_value(param, stage_rank)
+        param_value, param_source = self._evaluate_input_param_value(
+            param, stage_rank)
 
         value_type = extract_type(param_value)
         if 'unknown' in value_type:
             value_type = 'string'
             param_value = type(param_value).__name__
-        
-        formatted_input_parameters.append(
-            {
-                'name': param_name,
-                'value': param_value,
-                'type': value_type,
-                'source': param_source
-            }
-        )
-        
+
+        input_param = {
+            'name': param_name,
+            'value': param_value,
+            'type': value_type,
+            'source': param_source
+        }
+
+        if not self._contains_split_at(input_param):
+            formatted_input_parameters.append(input_param)
+
+    def _contains_split_at(self, input_param):
+        if input_param['type'] == 'string':
+            return 'split_at' in input_param['value']
+
+        return False
+
     def _index_input_param(self, param, stage_ranks):
         if param['stage_uuid'] not in stage_ranks:
             self._update_stage_rank(param['stage_uuid'], stage_ranks)
-            
+
         stage_rank = stage_ranks[param['stage_uuid']]
         name = '{}-{}'.format(param['argument']['name'], str(stage_rank))
         return name
 
     def _update_stage_rank(self, stage_uuid, stage_ranks):
-        index = 1
-        if stage_ranks.values():
-            index = max(stage_ranks.values()) + 1
-        stage_ranks.update({stage_uuid: index})
-                
+        index = max(stage_ranks.values(), default=0) + 1
+        stage_ranks[stage_uuid] = index
+
     def _evaluate_input_param_value(self, param, stage_rank):
         argument_value = param['argument']['value']
         if argument_value['type'] == 'stage':
@@ -63,7 +71,8 @@ class InputParameterFormatter(object):
                 self._update_stage_rank(stage_uuid, stage_rank)
 
             input_stage_rank = stage_rank[stage_uuid]
-            value = '{}-{}'.format(argument_value['stage_name'], input_stage_rank)
+            value = '{}-{}'.format(argument_value['stage_name'],
+                                   input_stage_rank)
             source = 'stage'
         elif argument_value['type'] == 'dynamic':
             value = self._job_parameters[argument_value['name']]
--- foundations_contrib/src/test/test_input_parameter_formatter.py
+++ foundations_contrib/src/test/test_input_parameter_formatter.py
@@ -5,180 +5,250 @@ Proprietary and confidential
 Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 """
 
-import unittest, fakeredis, json
+import unittest
+import fakeredis
+import json
 from mock import MagicMock
 
 from foundations_contrib.input_parameter_formatter import InputParameterFormatter
 
+
 class TestInputParameterFormatter(unittest.TestCase):
-    
+
     def test_format_input_parameters_one_constant_parameter(self):
         stage_uuid = 'gorilla'
         stage_rank = {'gorilla': 1}
-        input_param = [{'argument':{'name': 'owl', 'value': {'value':'red', 'type': 'constant'}}, 'stage_uuid': stage_uuid}]
+        input_param = [{'argument': {'name': 'owl', 'value': {
+            'value': 'red', 'type': 'constant'}}, 'stage_uuid': stage_uuid}]
         job_param = {}
         expected = [{'name': 'owl-1',
-                    'value': 'red',
-                    'type': 'string',
-                    'source': 'constant'}]
-        result = InputParameterFormatter(input_param, job_param,  stage_rank).format_input_parameters()
+                     'value': 'red',
+                     'type': 'string',
+                     'source': 'constant'}]
+        result = InputParameterFormatter(
+            input_param, job_param,  stage_rank).format_input_parameters()
         self.assertDictEqual(expected[0], result[0])
-    
+
     def test_format_input_parameters_one_constant_parameter_different_type(self):
         stage_uuid = 'gorilla'
         stage_rank = {'gorilla': 1}
-        input_param = [{'argument':{'name': 'ab', 'value': {'value': 123, 'type': 'constant'}}, 'stage_uuid': stage_uuid}]
+        input_param = [{'argument': {'name': 'ab', 'value': {
+            'value': 123, 'type': 'constant'}}, 'stage_uuid': stage_uuid}]
         job_param = {}
         expected = [{'name': 'ab-1',
-                    'value': 123,
-                    'type': 'number',
-                    'source': 'constant'}]
-        result = InputParameterFormatter(input_param, job_param,  stage_rank).format_input_parameters()
+                     'value': 123,
+                     'type': 'number',
+                     'source': 'constant'}]
+        result = InputParameterFormatter(
+            input_param, job_param,  stage_rank).format_input_parameters()
         self.assertDictEqual(expected[0], result[0])
-    
+
     def test_format_input_parameters_one_dynamic_parameter(self):
         stage_uuid = 'gorilla'
         stage_rank = {'gorilla': 1}
-        input_param = [{'argument':{'name': 'ab', 'value': {'name':'hi', 'type': 'dynamic'}}, 'stage_uuid': stage_uuid}]
+        input_param = [{'argument': {'name': 'ab', 'value': {
+            'name': 'hi', 'type': 'dynamic'}}, 'stage_uuid': stage_uuid}]
         job_param = {'hi': 'bye'}
         expected = [{'name': 'ab-1',
-                    'value': 'bye',
-                    'type': 'string',
-                    'source': 'placeholder'}]
-        result = InputParameterFormatter(input_param, job_param, stage_rank).format_input_parameters()
+                     'value': 'bye',
+                     'type': 'string',
+                     'source': 'placeholder'}]
+        result = InputParameterFormatter(
+            input_param, job_param, stage_rank).format_input_parameters()
         self.assertDictEqual(expected[0], result[0])
 
     def test_format_input_parameters_one_stage_parameter(self):
         stage_uuid = 'gorilla'
         stage_rank = {'gorilla': 1}
-        input_param = [{'argument':{'name': 'ab', 'value': {'stage_name':'hi', 'type': 'stage', 'stage_uuid': stage_uuid}}, 'stage_uuid': stage_uuid}]
+        input_param = [{'argument': {'name': 'ab', 'value': {
+            'stage_name': 'hi', 'type': 'stage', 'stage_uuid': stage_uuid}}, 'stage_uuid': stage_uuid}]
         expected = [{'name': 'ab-1',
-                    'value': 'hi-1',
-                    'type': 'string',
-                    'source': 'stage'}]
-        result = InputParameterFormatter(input_param,{},  stage_rank).format_input_parameters()
+                     'value': 'hi-1',
+                     'type': 'string',
+                     'source': 'stage'}]
+        result = InputParameterFormatter(
+            input_param, {},  stage_rank).format_input_parameters()
         self.assertDictEqual(expected[0], result[0])
-    
+
     def test_format_input_parameters_two_stages(self):
         stage_uuid_1 = 'gorilla'
         stage_uuid_2 = 'ape'
         stage_rank = {stage_uuid_2: 1, stage_uuid_1: 2}
 
-        input_param = [{'argument':{'name': 'ab', 'value': {'value':'hi', 'type': 'constant'}}, 'stage_uuid': stage_uuid_1},
-                        {'argument':{'name': 'ab', 'value': {'value':'bye', 'type': 'constant'}}, 'stage_uuid': stage_uuid_2}]
+        input_param = [{'argument': {'name': 'ab', 'value': {'value': 'hi', 'type': 'constant'}}, 'stage_uuid': stage_uuid_1},
+                       {'argument': {'name': 'ab', 'value': {'value': 'bye', 'type': 'constant'}}, 'stage_uuid': stage_uuid_2}]
         expected = [{'name': 'ab-2',
-                    'value': 'hi',
-                    'type': 'string',
-                    'source': 'constant'},
+                     'value': 'hi',
+                     'type': 'string',
+                     'source': 'constant'},
                     {'name': 'ab-1',
-                    'value': 'bye',
-                    'type': 'string',
-                    'source': 'constant'},
+                     'value': 'bye',
+                     'type': 'string',
+                     'source': 'constant'},
                     ]
-        result = InputParameterFormatter(input_param, {}, stage_rank).format_input_parameters()
+        result = InputParameterFormatter(
+            input_param, {}, stage_rank).format_input_parameters()
         self.assertListEqual(expected, result)
-    
-    
+
     def test_format_input_parameter_adds_stages_when_stage_time_empty(self):
         stage_uuid_1 = 'gorilla'
         stage_uuid_2 = 'ape'
 
-        input_param = [{'argument':{'name': 'ab', 'value': {'value':'hi', 'type': 'constant'}}, 'stage_uuid': stage_uuid_1}]
+        input_param = [{'argument': {'name': 'ab', 'value': {
+            'value': 'hi', 'type': 'constant'}}, 'stage_uuid': stage_uuid_1}]
         expected = [{'name': 'ab-1',
-                    'value': 'hi',
-                    'type': 'string',
-                    'source': 'constant'}
+                     'value': 'hi',
+                     'type': 'string',
+                     'source': 'constant'}
                     ]
-        result = InputParameterFormatter(input_param, {}, {}).format_input_parameters()
+        result = InputParameterFormatter(
+            input_param, {}, {}).format_input_parameters()
         self.assertListEqual(expected, result)
-    
+
     def test_format_input_parameter_adds_stages_when_stage_time_empty_but_stage_time(self):
         stage_uuid_1 = 'gorilla'
         stage_uuid_2 = 'ape'
         stage_rank = {stage_uuid_1: 1}
 
-        input_param = [{'argument':{'name': 'ab', 'value': {'value':'hi', 'type': 'constant'}}, 'stage_uuid': stage_uuid_2}]
+        input_param = [{'argument': {'name': 'ab', 'value': {
+            'value': 'hi', 'type': 'constant'}}, 'stage_uuid': stage_uuid_2}]
         expected = [{'name': 'ab-2',
-                    'value': 'hi',
-                    'type': 'string',
-                    'source': 'constant'}
+                     'value': 'hi',
+                     'type': 'string',
+                     'source': 'constant'}
                     ]
-        result = InputParameterFormatter(input_param, {}, stage_rank).format_input_parameters()
+        result = InputParameterFormatter(
+            input_param, {}, stage_rank).format_input_parameters()
         self.assertListEqual(expected, result)
-    
+
     def test_format_input_parameter_adds_stages_when_one_empty_argument_one_stage_time_argument(self):
         stage_uuid_1 = 'gorilla'
         stage_uuid_2 = 'ape'
 
         stage_rank = {stage_uuid_1: 1}
 
-        input_param = [{'argument':{'name': 'ab', 'value': {'value':'hi', 'type': 'constant'}}, 'stage_uuid': stage_uuid_1},
-                        {'argument':{'name': 'ab', 'value': {'value':'bye', 'type': 'constant'}}, 'stage_uuid': stage_uuid_2}]
+        input_param = [{'argument': {'name': 'ab', 'value': {'value': 'hi', 'type': 'constant'}}, 'stage_uuid': stage_uuid_1},
+                       {'argument': {'name': 'ab', 'value': {'value': 'bye', 'type': 'constant'}}, 'stage_uuid': stage_uuid_2}]
         expected = [{'name': 'ab-1',
-                    'value': 'hi',
-                    'type': 'string',
-                    'source': 'constant'},
+                     'value': 'hi',
+                     'type': 'string',
+                     'source': 'constant'},
                     {'name': 'ab-2',
-                    'value': 'bye',
-                    'type': 'string',
-                    'source': 'constant'},
+                     'value': 'bye',
+                     'type': 'string',
+                     'source': 'constant'},
                     ]
-        result = InputParameterFormatter(input_param, {}, stage_rank).format_input_parameters()
+        result = InputParameterFormatter(
+            input_param, {}, stage_rank).format_input_parameters()
         self.assertListEqual(expected, result)
-
 
     def test_format_input_parameters_one_unknown_type(self):
         stage_uuid = 'gorilla'
         stage_rank = {'gorilla': 1}
-        input_param = [{'argument':{'name': 'ab', 'value': {'value':{}, 'type': 'constant'}}, 'stage_uuid': stage_uuid}]
+        input_param = [{'argument': {'name': 'ab', 'value': {
+            'value': {}, 'type': 'constant'}}, 'stage_uuid': stage_uuid}]
         expected = [{'name': 'ab-1',
-                    'value': 'dict',
-                    'type': 'string',
-                    'source': 'constant'}]
-        result = InputParameterFormatter(input_param,{},  stage_rank).format_input_parameters()
+                     'value': 'dict',
+                     'type': 'string',
+                     'source': 'constant'}]
+        result = InputParameterFormatter(
+            input_param, {},  stage_rank).format_input_parameters()
         self.assertDictEqual(expected[0], result[0])
 
     def test_format_input_parameters_one_unknown_type_different_type(self):
         stage_uuid = 'gorilla'
         stage_rank = {'gorilla': 1}
-        input_param = [{'argument':{'name': 'ab', 'value': {'value':[{}], 'type': 'constant'}}, 'stage_uuid': stage_uuid}]
+        input_param = [{'argument': {'name': 'ab', 'value': {
+            'value': [{}], 'type': 'constant'}}, 'stage_uuid': stage_uuid}]
         expected = [{'name': 'ab-1',
-                    'value': 'list',
-                    'type': 'string',
-                    'source': 'constant'}]
-        result = InputParameterFormatter(input_param,{},  stage_rank).format_input_parameters()
+                     'value': 'list',
+                     'type': 'string',
+                     'source': 'constant'}]
+        result = InputParameterFormatter(
+            input_param, {},  stage_rank).format_input_parameters()
         self.assertListEqual(expected, result)
-
 
     def test_format_input_parameters_input_parameter_array(self):
         stage_uuid = 'gorilla'
         stage_rank = {'gorilla': 1}
-        input_param = [{'argument':{'name': 'ab', 'value': {'value':['some_data'], 'type': 'constant'}}, 'stage_uuid': stage_uuid}]
+        input_param = [{'argument': {'name': 'ab', 'value': {
+            'value': ['some_data'], 'type': 'constant'}}, 'stage_uuid': stage_uuid}]
         expected = [{'name': 'ab-1',
-                    'value': ['some_data'],
-                    'type': 'array string',
-                    'source': 'constant'}]
-        result = InputParameterFormatter(input_param,{},  stage_rank).format_input_parameters()
+                     'value': ['some_data'],
+                     'type': 'array string',
+                     'source': 'constant'}]
+        result = InputParameterFormatter(
+            input_param, {},  stage_rank).format_input_parameters()
         self.assertListEqual(expected, result)
 
     def test_format_input_parameters_input_parameter_array_number(self):
         stage_uuid = 'gorilla'
         stage_rank = {'gorilla': 1}
-        input_param = [{'argument':{'name': 'ab', 'value': {'value':[5], 'type': 'constant'}}, 'stage_uuid': stage_uuid}]
+        input_param = [{'argument': {'name': 'ab', 'value': {
+            'value': [5], 'type': 'constant'}}, 'stage_uuid': stage_uuid}]
         expected = [{'name': 'ab-1',
-                    'value': [5],
-                    'type': 'array number',
-                    'source': 'constant'}]
-        result = InputParameterFormatter(input_param,{},  stage_rank).format_input_parameters()
+                     'value': [5],
+                     'type': 'array number',
+                     'source': 'constant'}]
+        result = InputParameterFormatter(
+            input_param, {},  stage_rank).format_input_parameters()
         self.assertListEqual(expected, result)
 
     def test_format_input_parameters_input_parameter_array_bool(self):
         stage_uuid = 'gorilla'
         stage_rank = {'gorilla': 1}
-        input_param = [{'argument':{'name': 'ab', 'value': {'value':True, 'type': 'constant'}}, 'stage_uuid': stage_uuid}]
+        input_param = [{'argument': {'name': 'ab', 'value': {
+            'value': True, 'type': 'constant'}}, 'stage_uuid': stage_uuid}]
         expected = [{'name': 'ab-1',
-                    'value': True,
-                    'type': 'bool',
-                    'source': 'constant'}]
-        result = InputParameterFormatter(input_param,{},  stage_rank).format_input_parameters()
+                     'value': True,
+                     'type': 'bool',
+                     'source': 'constant'}]
+        result = InputParameterFormatter(
+            input_param, {},  stage_rank).format_input_parameters()
+        self.assertListEqual(expected, result)
+
+    def test_format_input_parameters_two_stages_filter_split_at_stage(self):
+        stage_uuid_1 = 'gorilla'
+        stage_uuid_2 = 'ape'
+        data_1 = json.dumps({'stage_uuid': stage_uuid_1, 'time': 1234})
+        data_2 = json.dumps({'stage_uuid': stage_uuid_2, 'time': 1300})
+        data_2_5 = json.dumps(
+            {'parameter_name': 'blah', 'stage_uuid': stage_uuid_2, 'time': 1200})
+        stage_rank = {'gorilla': 1, 'ape': 2}
+        input_param = [
+            {
+                'argument': {
+                    'name': 'ab',
+                    'value': {
+                        'value': 'split_at-1',
+                        'type': 'constant'
+                    }
+                },
+                'stage_uuid': stage_uuid_1
+            },
+            {
+                'argument': {
+                    'name': 'ab',
+                    'value': {
+                        'value': 'bye',
+                        'type': 'constant'
+                    }
+                },
+                'stage_uuid': stage_uuid_2
+            }
+        ]
+        expected = [
+            {
+                'name': 'ab-2',
+                'value': 'bye',
+                'type': 'string',
+                'source': 'constant'
+            }
+        ]
+
+        result = InputParameterFormatter(
+            input_param,
+            {},
+            stage_rank
+        ).format_input_parameters()
         self.assertListEqual(expected, result)
