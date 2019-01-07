7784f1c3c9254b95dce4e2596ce32273455a53dd
Thomas Rogers <thomasrogers@Macs-MacBook-Air.local>
2019-01-07 15:44:09 -0500

    fix row count and refactor callbacks for highlighting

--- foundations_ui/src/scss/jquery/rowHovers.js
+++ foundations_ui/src/scss/jquery/rowHovers.js
@@ -5,26 +5,28 @@ window.jQuery = require('jquery');
 window.$ = window.jQuery;
 
 class hoverActions {
-  static hover() {
-    for (let row = 0; row < 100; row += 1) {
-      const rowClass = `.row-${row}`;
-      // Tried to refactor the callbacks into their own functions
-      // however: https://stackoverflow.com/a/37893975
-      $('body').on('mouseenter', rowClass, () => {
-        if ($(rowClass).hasClass('error')) {
-          $(rowClass).css({ background: '#DCDCDC', 'background-color': '#DCDCDC' });
-        } else {
-          $(rowClass).css({ background: '#DCDCDC' });
-        }
-      });
+  static onMouseEnter(rowClass, row) {
+    if ($(rowClass).hasClass('error')) {
+      $(rowClass).css({ background: '#DCDCDC', 'background-color': '#DCDCDC' });
+    } else {
+      $(rowClass).css({ background: '#DCDCDC' });
+    }
+  }
 
-      $('body').on('mouseleave', rowClass, () => {
-        if ($(rowClass).hasClass('error')) {
-          $(rowClass).css({ color: '#FB3D42', 'background-color': '#FDF6F5' });
-        } else {
-          $(rowClass).css({ background: 'white' });
-        }
-      });
+  static onMouseLeave(rowClass, row) {
+    if ($(rowClass).hasClass('error')) {
+      $(rowClass).css({ color: '#FB3D42', 'background-color': '#FDF6F5' });
+    } else {
+      $(rowClass).css({ background: 'white' });
+    }
+  }
+
+  static hover() {
+    for (let row = 0; row < $('.job-table-row').length / 3; row += 1) {
+      const rowClass = `.row-${row}`;
+      console.log(row);
+      $('body').on('mouseenter', rowClass, () => hoverActions.onMouseEnter(rowClass, row));
+      $('body').on('mouseleave', rowClass, () => hoverActions.onMouseLeave(rowClass, row));
     }
   }
 }
