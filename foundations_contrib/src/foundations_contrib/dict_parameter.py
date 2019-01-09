321d2734a0588c8cb4b4f122f5a7d14bb0af4db0
Thomas Rogers <thomasrogers@Macs-MacBook-Air.local>
2019-01-09 11:55:01 -0500

    add documentation

--- foundations_contrib/src/foundations_contrib/dict_parameter.py
+++ foundations_contrib/src/foundations_contrib/dict_parameter.py
@@ -7,17 +7,38 @@ Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 
 
 class DictParameter(object):
+    """Represents a python dictionary where the values are themselves parameters.
+    This is used to properly deference any internal parameters that are non-constant
+    
+    Arguments:
+        dict_of_parameters {dict} -- A dictionary mapping keys to parameters
+    """
 
     def __init__(self, dict_of_parameters):
         self._dict_of_parameters = dict_of_parameters
 
     def compute_value(self, runtime_data):
+        """Computes the resolves value of the parameter
+        
+        Arguments:
+            runtime_data {dict} -- Contains the runtime values defined when calling #run on a stage
+        
+        Returns:
+            object -- The result of evaluating the paremter with the given runtime data
+        """
+
         result = {}
         for key, parameter in self._dict_of_parameters.items():
             result[key] = parameter.compute_value(runtime_data)
         return result
 
     def provenance(self):
+        """Provides information about what was used to create this parameter when a job was run
+        
+        Returns:
+            dict -- Contains the information above
+        """
+
         return {'type': 'dict', 'parameters': self._dict_provenance()}
 
     def _dict_provenance(self):
@@ -27,6 +48,15 @@ class DictParameter(object):
         return result
 
     def hash(self, runtime_data):
+        """Computes a reproducable hashing value for this parameter
+        
+        Arguments:
+            runtime_data {dict} -- Contains the runtime values defined when calling #run on a stage
+        
+        Returns:
+            [type] -- [description]
+        """
+
         from foundations.utils import merged_uuids
         return merged_uuids(self._dict_hashes(runtime_data))
 
@@ -34,10 +64,19 @@ class DictParameter(object):
         return [parameter.hash(runtime_data) for parameter in self._dict_of_parameters.values()]
 
     def enable_caching(self):
+        """Enables caching for this parameter so we don't have to evaluate it multiple times
+        """
+
         for parameter in self._dict_of_parameters.values():
             parameter.enable_caching()
 
     def __str__(self):
+        """String representation of the parameter
+        
+        Returns:
+            str -- As above
+        """
+
         return self._dict_of_strs().__str__()
 
     def _dict_of_strs(self):
--- foundations_contrib/src/foundations_contrib/list_parameter.py
+++ foundations_contrib/src/foundations_contrib/list_parameter.py
@@ -7,20 +7,50 @@ Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 
 
 class ListParameter(object):
+    """Represents a python list where the values are themselves parameters.
+    This is used to properly deference any internal parameters that are non-constant
+    
+    Arguments:
+        list_of_parameters {dict} -- A list of parameters
+    """
 
     def __init__(self, list_of_parameters):
         self._list_of_parameters = list_of_parameters
 
     def compute_value(self, runtime_data):
+        """Computes the resolves value of the parameter
+        
+        Arguments:
+            runtime_data {dict} -- Contains the runtime values defined when calling #run on a stage
+        
+        Returns:
+            object -- The result of evaluating the paremter with the given runtime data
+        """
+
         return [parameter.compute_value(runtime_data) for parameter in self._list_of_parameters]
 
     def provenance(self):
+        """Provides information about what was used to create this parameter when a job was run
+        
+        Returns:
+            dict -- Contains the information above
+        """
+
         return {'type': 'list', 'parameters': self._list_provenance()}
 
     def _list_provenance(self):
         return [parameter.provenance() for parameter in self._list_of_parameters]
 
     def hash(self, runtime_data):
+        """Computes a reproducable hashing value for this parameter
+        
+        Arguments:
+            runtime_data {dict} -- Contains the runtime values defined when calling #run on a stage
+        
+        Returns:
+            [type] -- [description]
+        """
+
         from foundations.utils import merged_uuids
         return merged_uuids(self._list_hashes(runtime_data))
 
@@ -28,10 +58,19 @@ class ListParameter(object):
         return [parameter.hash(runtime_data) for parameter in self._list_of_parameters]
 
     def enable_caching(self):
+        """Enables caching for this parameter so we don't have to evaluate it multiple times
+        """
+
         for parameter in self._list_of_parameters:
             parameter.enable_caching()
 
     def __str__(self):
+        """String representation of the parameter
+        
+        Returns:
+            str -- As above
+        """
+
         return self._list_of_strs().__str__()
 
     def _list_of_strs(self):
