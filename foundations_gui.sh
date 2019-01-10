b0761c39ee0c100ef32b9b1aa746c403e72edfbb
Thomas Rogers <thomasrogers@Macs-MacBook-Air.local>
2019-01-10 12:12:32 -0500

    respect proper nginx port

--- foundations_gui.sh
+++ foundations_gui.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-node_ip="192.168.0.21"
-redis_url="redis://192.168.0.21:6379"
+node_ip="10.1.8.61"
+redis_url="redis://10.1.8.61:6379"
 
 action="$1"
 
@@ -26,12 +26,12 @@ start_ui () {
     docker run -d --rm \
         --name foundations-gui \
         -e FOUNDATIONS_REST_API="${node_ip}:37722" \
-        -p 3000:3000 \
+        -p 8011:8011 \
         foundations-gui:${image_tag} \
         > /dev/null \
         && \
 
-    echo "Foundations UI listening on port 3000."
+    echo "Foundations UI listening on port 8011."
 }
 
 stop_ui () {
