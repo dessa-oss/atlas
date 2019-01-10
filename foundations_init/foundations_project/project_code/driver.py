e1f9311cf53a0e70d7cb862d69c8828f6b8d2c68
Pippin Lee <pippinblee@gmail.com>
2019-01-09 23:12:07 -0500

    remove directory creation code, add custom filename for directory

--- foundations_init/foundations_project/project_code/driver.py
+++
@@ -1,5 +0,0 @@
-import foundations
-
-foundations.config_manager.add_config_path('../config/default.local.yaml')
-
-foundations.set_project_name('my-foundations-project')
--- foundations_init/index.py
+++ foundations_init/index.py
@@ -2,8 +2,8 @@ from distutils.dir_util import copy_tree
 from pathlib import Path
 import sys
 
-def initialize():
-    copy_tree('./template', './foundations_project')
+def initialize(root_directory):
+    copy_tree('./template', './' + root_directory)
 
 try:
 	root_directory = sys.argv[1]
@@ -11,67 +11,6 @@ try:
 	if directory_path.is_dir():
 		print('Error: directory already exists\n')
 	else:
-		initialize()
+		initialize(root_directory)
 except IndexError:
 	print('Error: provide a project directory. \n\nExample: \n\n foundations init my_foundations_project\n')
-
-
-# from pathlib import Path
-# import sys
-
-# readme_content = """
-# 		# Foundations project
-
-# 		This project template was generated with `foundations init`. In
-# 		this project you'll find the following structure:
-
-# 		/config
-# 			default.config.yaml
-# 		/project_code
-# 			driver.py
-# 			README.md
-# 		/post_processing
-# 			results.py
-		
-# 		"""
-
-# def create_directories(list_of_directories):
-# 	for directory in list_of_directories:
-# 		Path(root_directory + '/' + directory).mkdir(parents=True, exist_ok=True)
-
-# def open_and_write(file, content):
-# 	with file.open('w') as wf:
-# 		wf.write(content)
-
-# def add_file_to_path(directory_path, filename):
-# 	return Path(root_directory + '/' + directory_path).joinpath(filename)
-
-# def initialize():
-# 	directory_list = ['config', 'project_code', 'post_processing', 'data']
-# 	create_directories(directory_list)
-
-# 	root_readme = add_file_to_path('', 'README.md')
-# 	open_and_write(root_readme, readme_content)
-
-# 	config_yaml = add_file_to_path('config', 'default.local.yaml')
-# 	open_and_write(config_yaml, 'some config stuff')
-
-# 	driver_file = add_file_to_path('project_code', 'driver.py')
-# 	open_and_write(driver_file, 'driver code blah')
-
-# 	project_readme = add_file_to_path('project_code', 'README.md')
-# 	open_and_write(project_readme, 'this is the readme for the project')
-
-# 	results_file = add_file_to_path('post_processing', "results.py")
-# 	open_and_write(results_file, 'some code to check results')
-
-# try:
-# 	root_directory = sys.argv[1]
-# 	directory_path = Path(root_directory)
-# 	if directory_path.is_dir():
-# 		print('Error: directory already exists\n')
-# 	else:
-# 		initialize()
-	
-# except IndexError:
-# 	print('Error: provide a project directory. \n\nExample: \n\n foundations init my_foundations_project\n')
