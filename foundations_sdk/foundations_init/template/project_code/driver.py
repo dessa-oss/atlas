e4741a16eb749b2cc6fb2313fb3f8e799403dcfc
pippinlee <>
2019-01-10 17:18:39 -0500

    update template

--- foundations_sdk/foundations_init/template/project_code/driver.py
+++ foundations_sdk/foundations_init/template/project_code/driver.py
@@ -1,5 +1,28 @@
+"""
+This file is used for your model code, and for organizing and executing Foundations stages.
+
+Below we import foundations, and then tell it how to run by giving foundations a path to a configuration file.
+
+We then use `set_project_name` to give the project a name.
+
+
+...expand
+
+"""
+
 import foundations
+from model import dummy_function
+
+dummy_function1 = foundations.create_stage(dummy_function1)
 
 foundations.config_manager.add_config_path('../config/default.local.yaml')
 foundations.set_project_name('my-foundations-project')
 
+# input to model
+x = 20
+
+# build step1 of model
+result = dummy_function1(x)
+
+# run the model
+result.run()
---
+++ foundations_sdk/foundations_init/template/project_code/model.py
@@ -0,0 +1,10 @@
+"""
+Each function here can be considered as a step (stage) towards building a model.
+
+... what does each line do
+"""
+
+import foundations
+
+def dummy_function(x):
+	return x + 10
