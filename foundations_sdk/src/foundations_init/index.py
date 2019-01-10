0b6717de17972692fcf4554973582c81b3ace708
pippinlee <>
2019-01-10 10:18:54 -0500

    remove unnecessary import

--- foundations_sdk/src/foundations_init/index.py
+++ foundations_sdk/src/foundations_init/index.py
@@ -1,13 +1,18 @@
+"""
+Copyright (C) DeepLearning Financial Technologies Inc. - All Rights Reserved
+Unauthorized copying, distribution, reproduction, publication, use of this file, via any medium is strictly prohibited
+Proprietary and confidential
+Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
+"""
+
 from distutils.dir_util import copy_tree
 from pathlib import Path
 import sys
-import os
 
 def scaffold(new_directory):
 	copy_from = Path(__file__).parents[0].joinpath('template')
 	copy_to = Path().absolute().joinpath(new_directory)
 	copy_tree(copy_from, str(copy_to))
-
 
 def initialize():
 	try:
