1a064b0e751f860dd611227e7331b4dfb6bc9ddd
pippinlee <>
2019-01-15 16:07:37 -0500

    docstring formatting

--- foundations_sdk/foundations_init/scaffold.py
+++ foundations_sdk/foundations_init/scaffold.py
@@ -15,8 +15,9 @@ class Scaffold(object):
         self.command = None
 
     def setup(self):
-    		"""This method is triggered via main.py and kicks off the scaffolding process.
-				"""
+        """This method is triggered via main.py and kicks off the scaffolding process.
+        """
+
         try:
             self.get_command()
             self.arg_controller()
@@ -24,15 +25,15 @@ class Scaffold(object):
             print('Error: no command given. To start a project, try: \n\n python -m foundations_init init <project_name>\n')
 
     def get_command(self):
-    		"""Sets the given command by the user.
-				"""
+        """Sets the given command by the user.
+        """
 
         import sys
         self.command = sys.argv[1]
 
     def arg_controller(self):
-    		"""Checks and handles which arguments are used when running the module.
-				"""
+        """Checks and handles which arguments are used when running the module.
+        """
 
         if self.command == 'init':
             self.foundations_init()
@@ -40,32 +41,32 @@ class Scaffold(object):
             print('Error: incorrect command. To start a project, try: \n\n python -m foundations_init init <project_name>\n')
 
     def get_project_name(self):
-    		"""Gets the project name a user provides.
-				
-				Returns:
-					string -- name of project user provides.
-				"""
+        """Gets the project name a user provides.
+
+        Returns:
+                string -- name of project user provides.
+        """
 
         import sys
         project_name = sys.argv[2]
         return project_name
 
     def directory_exists(self, project_name):
-    		"""Checks if directory already exists.
-				
-				Arguments:
-					project_name {string} -- name that user provides for project.
-				
-				Returns:
-					Boolean -- true if directory exists.
-				"""
+        """Checks if directory already exists.
+
+        Arguments:
+                project_name {string} -- name that user provides for project.
+
+        Returns:
+                Boolean -- true if directory exists.
+        """
 
         directory_path_exist = Path(project_name).is_dir()
         return directory_path_exist
 
     def foundations_init(self):
-    		""" Wrapping logic to check that user gave command and to start scaffolding if directory is unique and does not exist.
-				"""
+        """ Wrapping logic to check that user gave command and to start scaffolding if directory is unique and does not exist.
+        """
 
         try:
             project_name = self.get_project_name()
@@ -83,11 +84,11 @@ class Scaffold(object):
             print('Error: incorrect command. To start a project, try: \n\n python -m foundations_init init <project_name>\n')
 
     def scaffold_project(self, new_directory):
-    		"""Copies files from /template directory to new user defined location path.
-				
-				Arguments:
-					new_directory {string} -- directory for content to be copied to.
-				"""
+        """Copies files from /template directory to new user defined location path.
+
+        Arguments:
+                new_directory {string} -- directory for content to be copied to.
+        """
 
         from distutils.dir_util import copy_tree
         copy_from = self.source_template_path()
@@ -95,23 +96,23 @@ class Scaffold(object):
         copy_tree(copy_from, str(copy_to))
 
     def source_template_path(self):
-    		"""Method that helps find the absolute path to the template directory.
-				
-				Returns:
-					object -- Posix file object from pathlib.
-				"""
+        """Method that helps find the absolute path to the template directory.
+
+        Returns:
+                object -- Posix file object from pathlib.
+        """
 
         copy_from_path = Path(__file__).parents[0].joinpath('template')
         return copy_from_path
 
     def destination_template_path(self, new_directory):
-    		"""Method that helps define the absolute path to the destination directory.
-				
+        """Method that helps define the absolute path to the destination directory.
+
 				Arguments:
-					new_directory {string} -- directory for content to be copied to.
-				
+								new_directory {string} -- directory for content to be copied to.
+
 				Returns:
-					object -- Posix file object from pathlib.
+								object -- Posix file object from pathlib.
 				"""
 
         copy_to_path = Path().absolute().joinpath(new_directory)
