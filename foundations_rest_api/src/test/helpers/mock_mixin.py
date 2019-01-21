9c355b1d1a626804f6ab53584239131d3ec417e3
Thomas Rogers <thomasrogers@Macs-MacBook-Air.local>
2019-01-21 15:51:30 -0500

    some fixes for the mock mixin

--- foundations_rest_api/src/test/helpers/mock_mixin.py
+++ foundations_rest_api/src/test/helpers/mock_mixin.py
@@ -8,17 +8,19 @@ Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 
 class MockMixin(object):
 
-    def setUp(self):
-        self._mocks = []
-
-    def testDown(self):
-        for mock in self._mocks:
+    def tearDown(self):
+        for mock in self._get_mocks():
             mock.stop()
 
     def patch(self, *args, **kwargs):
         from mock import patch
 
         mock = patch(*args, **kwargs)
-        self._mocks.append(mock)
+        self._get_mocks().append(mock)
 
         return mock.start()
+
+    def _get_mocks(self):
+        if getattr(self, '_mocks', None) is None:
+            self._mocks = []
+        return self._mocks
