3096064fac6b0691b30c215fe5c64f87038a9e86
Thomas Rogers <thomasrogers@Macs-MacBook-Air.local>
2019-01-29 14:36:23 -0500

    create configurable config translator

--- foundations_internal/src/foundations_internal/config/config_translator.py
+++ foundations_internal/src/foundations_internal/config/config_translator.py
@@ -8,10 +8,17 @@ Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 class ConfigTranslator(object):
     
     def __init__(self):
-        pass
+        self._translators = {}
+
+    def add_translator(self, name, translator):
+        self._translators[name] = translator
         
     def translate(self, source_config):
-        deployment_type = source_config['job_deployment_env']
+        deployment_type = source_config.get('job_deployment_env')
+
+        if deployment_type in self._translators:
+            return self._translators[deployment_type].translate(source_config)
+
         raise self._invalid_environment_error(source_config, deployment_type)
 
     def _invalid_environment_error(self, source_config, deployment_type):
--- foundations_internal/src/test/config/test_config_translator.py
+++ foundations_internal/src/test/config/test_config_translator.py
@@ -26,3 +26,61 @@ class TestConfigTranslator(unittest.TestCase):
             self._translator.translate({'job_deployment_env': 'azure'})
 
         self.assertIn('Got invalid deployment environment `azure`', error_context.exception.args)        
+    
+    def test_raises_error_when_no_environment_provided(self):
+        with self.assertRaises(ValueError) as error_context:
+            self._translator.translate({})
+
+        self.assertIn('Got invalid deployment environment `None`', error_context.exception.args)        
+    
+    def test_calls_translate_with_existing_environment(self):
+        azure = Mock()
+        azure.translate.return_value = 'some configured azure environment'
+        self._translator.add_translator('azure', azure)
+
+        translation = self._translator.translate({'job_deployment_env': 'azure'})
+        self.assertEqual('some configured azure environment', translation)
+    
+    def test_calls_translate_with_existing_environment_different_environment(self):
+        gcp = Mock()
+        gcp.translate.return_value = 'gcp environment with configured data'
+        self._translator.add_translator('gcp', gcp)
+
+        translation = self._translator.translate({'job_deployment_env': 'gcp'})
+        self.assertEqual('gcp environment with configured data', translation)
+    
+    def test_calls_translate_with_correct_params(self):
+        azure = Mock()
+        self._translator.add_translator('azure', azure)
+
+        self._translator.translate({'job_deployment_env': 'azure'})
+        azure.translate.assert_called_with({'job_deployment_env': 'azure'})
+    
+    def test_calls_translate_with_correct_params_different_params(self):
+        azure = Mock()
+        self._translator.add_translator('azure', azure)
+
+        self._translator.translate({'job_deployment_env': 'azure', 'ip_address': '11.22.33.44'})
+        azure.translate.assert_called_with({'job_deployment_env': 'azure', 'ip_address': '11.22.33.44'})
+    
+    def test_calls_translate_with_multiple_environments(self):
+        gcp = Mock()
+        gcp.translate.return_value = 'gcp environment with configured data'
+        self._translator.add_translator('gcp', gcp)
+
+        azure = Mock()
+        azure.translate.return_value = 'some configured azure environment'
+        self._translator.add_translator('azure', azure)
+
+        translation = self._translator.translate({'job_deployment_env': 'gcp'})
+        self.assertEqual('gcp environment with configured data', translation)
+    
+    def test_raises_error_when_invalid_environment_requested(self):
+        gcp = Mock()
+        gcp.translate.return_value = 'gcp environment with configured data'
+        self._translator.add_translator('gcp', gcp)
+
+        with self.assertRaises(ValueError) as error_context:
+            self._translator.translate({'job_deployment_env': 'potato'})
+
+        self.assertIn('Got invalid deployment environment `potato`', error_context.exception.args)        
