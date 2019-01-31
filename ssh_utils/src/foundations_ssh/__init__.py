9c59b3ce360030439ea1b6b28f5c78a12a8190af
Thomas Rogers <thomasrogers@Macs-MacBook-Air.local>
2019-01-31 16:08:25 -0500

    ensure that ssh config translate is added automatically

--- ssh_utils/src/foundations_ssh/__init__.py
+++ ssh_utils/src/foundations_ssh/__init__.py
@@ -4,6 +4,7 @@ Unauthorized copying, distribution, reproduction, publication, use of this file,
 Proprietary and confidential
 Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 """
+
 from foundations_ssh.ssh_job_deployment import SSHJobDeployment
 from foundations_ssh.sftp_job_deployment import SFTPJobDeployment
 from foundations_ssh.ssh_listing import SSHListing
@@ -13,6 +14,8 @@ from foundations_ssh.multi_sftp_bundled_pipeline_archive import MultiSFTPBundled
 from foundations_ssh.sftp_bucket import SFTPBucket
 from foundations_ssh.versioning import __version__
 
+import foundations_ssh.config
+
 
 def _append_module():
     import sys
@@ -21,3 +24,13 @@ def _append_module():
 
 
 _append_module()
+
+
+def _inject_config_translate():
+    from foundations_internal.global_state import config_translator
+    import foundations_ssh.config.ssh_config_translate as translator
+
+    config_translator.add_translator('ssh', translator)
+
+
+_inject_config_translate()
