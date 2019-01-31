0de2f13e80163cb7e8d16610491cf32c2e2c204b
Thomas Rogers <thomasrogers@Macs-MacBook-Air.local>
2019-01-31 11:45:44 -0500

    fix typo

--- foundations_internal/src/foundations_internal/global_state.py
+++ foundations_internal/src/foundations_internal/global_state.py
@@ -6,7 +6,7 @@ Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 """
 
 from foundations_internal.module_manager import ModuleManager
-from foundations_internal.config_translator import ConfigTranslator
+from foundations_internal.config.config_translator import ConfigTranslator
 
 module_manager = ModuleManager()
 config_translator = ConfigTranslator()
