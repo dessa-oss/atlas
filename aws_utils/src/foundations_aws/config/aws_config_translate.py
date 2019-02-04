6792fa0d64fdb89f0441d1979f8a6f308a4aaf5d
Thomas Rogers <thomasrogers@Macs-MacBook-Air.local>
2019-02-04 16:57:38 -0500

    update for typo of forked branch

--- aws_utils/src/foundations_aws/config/aws_config_translate.py
+++ aws_utils/src/foundations_aws/config/aws_config_translate.py
@@ -55,7 +55,7 @@ def _project_listing_implementation(result_end_point):
 
     project_path = join(result_end_point, 'projects')
     return {
-        'archive_type': AWSPipelineArchiveListing,
+        'project_listing_type': AWSPipelineArchiveListing,
         'constructor_arguments': [project_path]
     }
 
@@ -70,7 +70,7 @@ def _archive_listing_implementation(result_end_point):
 
     archive_path = join(result_end_point, 'archive')
     return {
-        'archive_type': AWSPipelineArchiveListing,
+        'archive_listing_type': AWSPipelineArchiveListing,
         'constructor_arguments': [archive_path]
     }
 
