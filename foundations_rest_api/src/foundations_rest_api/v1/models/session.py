5cb8be452fae48ca76a6b3d12329439be9ab1317
kbanc <katherine.bancroft@gmail.com>
2019-02-12 16:27:10 -0500

    Update environment variable name in rest_api

--- foundations_rest_api/src/foundations_rest_api/v1/models/session.py
+++ foundations_rest_api/src/foundations_rest_api/v1/models/session.py
@@ -8,13 +8,17 @@ Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 
 class Session(object):
 
-    def __init__(self):
-        pass
-
     @staticmethod
     def auth(password):
+        """
+        Checks if password matches environment variable.
+
+        Input: Password to check
+
+        Returns: Status code
+        """
         import os
-        if password == os.environ.get('GUIPASSWORD', None):
+        if password == os.environ.get('FOUNDATIONS_GUI_PASSWORD', None):
             return 200
         else:
             return 401
--- foundations_rest_api/src/test/v1/models/test_session.py
+++ foundations_rest_api/src/test/v1/models/test_session.py
@@ -15,17 +15,17 @@ class TestSession(unittest.TestCase):
     def setUp(self):
         pass
     
-    @patch.dict('os.environ', {'PASSWORD': 'camel'})
+    @patch.dict('os.environ', {'FOUNDATIONS_GUI_PASSWORD': 'camel'})
     def test_auth_returns_401_if_incorrect_password(self):
         password ='hippo'
         self.assertEqual(Session.auth(password), 401)
     
-    @patch.dict('os.environ',{'PASSWORD': 'camel'} )
+    @patch.dict('os.environ',{'FOUNDATIONS_GUI_PASSWORD': 'camel'} )
     def test_auth_returns_200_if_correct_password(self):
         password ='camel'
         self.assertEqual(Session.auth(password), 200)
     
-    @patch.dict('os.environ',{'PASSWORD': 'rhino'} )
+    @patch.dict('os.environ',{'FOUNDATIONS_GUI_PASSWORD': 'rhino'} )
     def test_auth_returns_200_if_correct_password_different_password(self):
         password ='rhino'
         self.assertEqual(Session.auth(password), 200)
