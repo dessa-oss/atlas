cd18869c4eed520a3608287e3498b069669e024f
pippinlee <>
2019-02-15 11:46:41 -0500

    [TR/PL/KB] add test for credentials in header

--- foundations_ui/src/js/actions/BaseActions.js
+++ foundations_ui/src/js/actions/BaseActions.js
@@ -57,4 +57,5 @@ const BaseActions = {
       );
   },
 };
+
 export default BaseActions;
--- foundations_ui/src/tests/actions/BaseActions.test.js
+++ foundations_ui/src/tests/actions/BaseActions.test.js
@@ -1,7 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { shallow, mount } from 'enzyme';
-import { Redirect } from 'react-router-dom';
 import configureTests from '../setupTests';
 import BaseActions from '../../js/actions/BaseActions';
 
@@ -11,12 +7,12 @@ function mockFetch(data, status) {
   return jest.fn().mockImplementation(() =>
     Promise.resolve({
       status: status,
-      json: () => data
+      json: () => data,
     })
   );
 }
 
-it('Gets Result and Status from API', async () => {
+it('Gets Result and Status from getAPI', async () => {
   fetch = mockFetch('data', 404);
   const [status, result] = await BaseActions.getFromAPI('some_query_url');
   expect(status).toEqual(404);
@@ -24,12 +20,35 @@ it('Gets Result and Status from API', async () => {
   expect(fetch).toHaveBeenCalledTimes(1);
 });
 
-it('Gets Result and Status from BetaAPI', async () => {
+it('Calls getAPI with Correct Params', async () => {
+  fetch = mockFetch('other_data', 500);
+  const [status, result] = await BaseActions.getFromAPI('some_query_url');
+  expect(status).toEqual(500);
+  expect(result).toEqual('other_data');
+  expect(fetch).toBeCalledWith('http://private-83924-dessa.apiary-mock.com/api/v1/some_query_url',
+  {
+    credentials: 'include',
+  });
+});
+
+it('Gets Result and Status from getBetaAPI', async () => {
   fetch = mockFetch('other_data', 500);
   const [status, result] = await BaseActions.getBetaFromAPI('some_query_url');
   expect(status).toEqual(500);
   expect(result).toEqual('other_data');
   expect(fetch).toHaveBeenCalledTimes(1);
+});
+
+it('Calls getBetaAPI with Correct Params', async () => {
+  fetch = mockFetch('other_data', 500);
+  const [status, result] = await BaseActions.getBetaFromAPI('some_query_url');
+  expect(status).toEqual(500);
+  expect(result).toEqual('other_data');
+  expect(fetch).toBeCalledWith('http://private-83924-dessa.apiary-mock.com/api/v2beta/some_query_url',
+    {
+      credentials: 'include',
+    }
+  );
 });
 
 it('Posts Results to API', async () => {
@@ -42,9 +61,10 @@ it('Posts Results to API', async () => {
     'http://private-83924-dessa.apiary-mock.com/api/v1/login', 
     {
       method: 'POST',
-      body: 'some body'
-    }
-  )
+      body: 'some body',
+      credentials: 'include',
+    },
+  );
 });
 
 
@@ -58,7 +78,8 @@ it('Posts Results to API Different Data', async () => {
     'http://private-83924-dessa.apiary-mock.com/api/v1/login', 
     {
       method: 'POST',
-      body: 'some body else'
-    }
-  )
+      body: 'some body else',
+      credentials: 'include',
+    },
+  );
 });
