5c99783bd8b88d883e5770c6868f41e3aaef7ac7
kbanc <katherine.bancroft@gmail.com>
2019-02-15 09:52:49 -0500

    [TR KB] Refactor tests

--- foundations_rest_api/src/test/utils/test_api_resource.py
+++ foundations_rest_api/src/test/utils/test_api_resource.py
@@ -26,6 +26,10 @@ class TestAPIResource(Spec):
         return '{}={}'.format(self.random_cookie_key, self.random_cookie_value)
 
     @let
+    def cookie_header(self):
+        return {'Cookie': 'auth_token={}'.format(self.random_cookie_value)}
+
+    @let
     def random_cookie_key(self):
         return self.faker.name()
 
@@ -37,6 +41,7 @@ class TestAPIResource(Spec):
     def faker(self):
         from faker import Faker
         return Faker()
+        
 
     authorization_mock = let_patch_mock('foundations_rest_api.v1.models.session.Session.is_authorized')
 
@@ -45,170 +50,136 @@ class TestAPIResource(Spec):
         self.assertEqual(klass, APIResourceMocks.Mock)
 
     def test_get_returns_index(self):
-        from foundations_rest_api.global_state import app_manager
-
         klass = api_resource('/path/to/resource')(APIResourceMocks.MockWithIndex)
-        with app_manager.app().test_client() as client:
+        with self._test_client() as client:
             response = client.get('/path/to/resource')
             self.assertEqual(self._json_response(response), 'some data')
     
     def test_post_returns_post(self):
-        from foundations_rest_api.global_state import app_manager
-
         klass = api_resource('/path/to/resource')(APIResourceMocks.MockWithPost)
-        with app_manager.app().test_client() as client:
+        with self._test_client() as client:
             response = client.post('/path/to/resource')
             self.assertEqual(self._json_response(response), 'some data')
         
     
     def test_post_sets_params(self):
-        from foundations_rest_api.global_state import app_manager
-
         def _callback(mock_instance):
             return mock_instance.params
 
         mock_klass = self._mock_resource('post', _callback)
 
         klass = api_resource('/path/to/resource/with/query/params')(mock_klass)
-        with app_manager.app().test_client() as client:
+        with self._test_client() as client:
             response = client.post('/path/to/resource/with/query/params', data={'password': 'world'})
             self.assertEqual(self._json_response(response), {'password': 'world'})
 
     def test_post_sets_params_different_params(self):
-        from foundations_rest_api.global_state import app_manager
-
         def _callback(mock_instance):
             return mock_instance.params
 
         mock_klass = self._mock_resource('post', _callback)
 
         klass = api_resource('/path/to/different/resource/with/query/params')(mock_klass)
-        with app_manager.app().test_client() as client:
+        with self._test_client() as client:
             response = client.post('/path/to/different/resource/with/query/params', data={'password': 'world', 'cat': 'dog'})
             self.assertEqual(self._json_response(response), {'password': 'world', 'cat': 'dog'})
 
     def test_get_returns_index_different_data(self):
-        from foundations_rest_api.global_state import app_manager
-
         klass = api_resource('/path/to/different/resource')(APIResourceMocks.DifferentMockWithIndex)
-        with app_manager.app().test_client() as client:
+        with self._test_client() as client:
             response = client.get('/path/to/different/resource')
             self.assertEqual(self._json_response(response), 'some different data')
     
     def test_get_and_post_returns_index_and_post(self):
-        from foundations_rest_api.global_state import app_manager
-
         klass = api_resource('/path/to/another/resource')(APIResourceMocks.MockWithIndexAndPost)
-        with app_manager.app().test_client() as client:
+        with self._test_client() as client:
             post_response = client.post('/path/to/another/resource')
             get_response = client.get('/path/to/another/resource')
             self.assertEqual(self._json_response(post_response), 'some post data')
             self.assertEqual(self._json_response(get_response), 'some index data')
 
     def test_get_returns_empty_params(self):
-        from foundations_rest_api.global_state import app_manager
-
         klass = api_resource('/path/to/resource/with/params')(APIResourceMocks.ParamsMockWithIndex)
-        with app_manager.app().test_client() as client:
+        with self._test_client() as client:
             response = client.get('/path/to/resource/with/params')
             self.assertEqual(self._json_response(response), {})
 
     def test_get_has_status_code(self):
-        from foundations_rest_api.global_state import app_manager
-
         klass = api_resource('/path/to/resource/with/params')(APIResourceMocks.ParamsMockWithIndex)
-        with app_manager.app().test_client() as client:
+        with self._test_client() as client:
             response = client.get('/path/to/resource/with/params')
             self.assertEqual(response.status_code, 200)
 
     def test_get_has_status_code_different_code(self):
-        from foundations_rest_api.global_state import app_manager
-
         klass = api_resource('/path/to/resource/with/params/and/status/code')(APIResourceMocks.ParamsMockWithIndexAndStatus)
-        with app_manager.app().test_client() as client:
+        with self._test_client() as client:
             response = client.get('/path/to/resource/with/params/and/status/code')
             self.assertEqual(response.status_code, 403)
 
     def test_post_has_status_code(self):
-        from foundations_rest_api.global_state import app_manager
-
         klass = api_resource('/path/to/another/resource')(APIResourceMocks.MockWithIndexAndPost)
-        with app_manager.app().test_client() as client:
+        with self._test_client() as client:
             response = client.post('/path/to/another/resource')
             self.assertEqual(response.status_code, 200)
 
     def test_post_has_status_code_different_code(self):
-        from foundations_rest_api.global_state import app_manager
-
         klass = api_resource('/posty/path/to/resource/with/params/and/status/code')(APIResourceMocks.ParamsMockWithPostAndStatus)
-        with app_manager.app().test_client() as client:
+        with self._test_client() as client:
             response = client.post('/posty/path/to/resource/with/params/and/status/code')
             self.assertEqual(response.status_code, 403)
 
     def test_post_has_cookie(self):
-        from foundations_rest_api.global_state import app_manager
-
         def _callback(mock_instance):
             return mock_instance.params
 
         mock_klass = self._mock_resource('post', _callback, cookie=self.random_cookie, )
         klass = api_resource('/path/to/post/resource/with/a/cookie/yum')(mock_klass)
-        with app_manager.app().test_client() as client:
+        with self._test_client() as client:
             response = client.post('/path/to/post/resource/with/a/cookie/yum', data={'password': 'world'})
             self.assertEqual(self.cookie_string, response.headers.get('Set-Cookie'))
 
     def test_get_returns_path_param(self):
-        from foundations_rest_api.global_state import app_manager
-
         klass = api_resource('/path/to/resource/with/<string:project_name>/params')(APIResourceMocks.ParamsMockWithIndex)
-        with app_manager.app().test_client() as client:
+        with self._test_client() as client:
             response = client.get('/path/to/resource/with/value/params')
             self.assertEqual(self._json_response(response), {'project_name': 'value'})
 
     def test_get_returns_path_with_query_params(self):
-        from foundations_rest_api.global_state import app_manager
-
         klass = api_resource('/path/to/resource/with/query/params')(APIResourceMocks.ParamsMockWithIndex)
-        with app_manager.app().test_client() as client:
+        with self._test_client() as client:
             response = client.get('/path/to/resource/with/query/params?hello=world')
             self.assertEqual(self._json_response(response), {'hello': 'world'})
 
     def test_get_returns_path_with_query_list_params(self):
-        from foundations_rest_api.global_state import app_manager
-
         klass = api_resource('/path/to/resource/with/query_list/params')(APIResourceMocks.ParamsMockWithIndex)
-        with app_manager.app().test_client() as client:
+        with self._test_client() as client:
             response = client.get('/path/to/resource/with/query_list/params?hello=world&hello=lou')
             self.assertEqual(self._json_response(response), {'hello': ['world', 'lou']})
 
     def test_get_returns_unauthorized_when_not_authenticated(self):
-        from foundations_rest_api.global_state import app_manager
-
-        def _callback(mock_instance):
-            return ''
-
-        mock_klass = self._mock_resource('index', _callback)
+        mock_klass = self._mock_resource('index', self._empty_callback)
         klass = api_resource('/path/to/resource/that/is/unauthorized')(mock_klass)
 
         self.authorization_mock.return_value = False
         headers = {'Cookie': 'auth_token={}'.format(self.random_cookie_value)}
-        with app_manager.app().test_client() as client:
-            response = client.get('/path/to/resource/that/is/unauthorized', headers=headers)
+        with self._test_client() as client:
+            response = client.get('/path/to/resource/that/is/unauthorized', headers=self.cookie_header)
             self.assertEqual(self._json_response(response), 'Unauthorized')
     
     def test_get_calls_session_is_authorized_with_correct_parameters(self):
-        from foundations_rest_api.global_state import app_manager
-
-        def _callback(mock_instance):
-            return ''
-
-        mock_klass = self._mock_resource('index', _callback)
+        mock_klass = self._mock_resource('index', self._empty_callback)
         klass = api_resource('/path/to/resource/that/takes/cookie')(mock_klass)
 
-        headers = {'Cookie': 'auth_token={}'.format(self.random_cookie_value)}
-        with app_manager.app().test_client() as client:
-            response = client.get('/path/to/resource/that/takes/cookie', headers=headers)
+        with self._test_client() as client:
+            response = client.get('/path/to/resource/that/takes/cookie', headers=self.cookie_header)
             self.authorization_mock.assert_called_with({'auth_token': self.random_cookie_value})
+
+    def _empty_callback(self, mock_instance):
+        return ''
+
+    def _test_client(self):
+        from foundations_rest_api.global_state import app_manager
+        return app_manager.app().test_client()
 
     def _mock_resource(self, method, callback, status=200, cookie=None):
         from foundations_rest_api.lazy_result import LazyResult
