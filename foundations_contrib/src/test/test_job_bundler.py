f5c939fbd207ac0b04fe392dc085fdd78a5f94b4
Dariem Perez <d.perez@dessa.com>
2019-02-20 10:21:37 -0500

    [DP KB] added more tests to JobBundler

--- foundations_contrib/src/test/test_job_bundler.py
+++ foundations_contrib/src/test/test_job_bundler.py
@@ -18,11 +18,19 @@ class TestJobBundler(Spec):
     mock_os_remove = let_patch_mock('os.remove')
     mock_tarfile_open = let_patch_mock('tarfile.open')
     mock_builtins_open = let_patch_mock('builtins.open')
-    
+
+    class MockFileContextManager(Mock):
+
+        def __enter__(self):
+            return self
+
+        def __exit__(self, exc_type, exc_val, exc_tb):
+            return self
+
     def test_job_name_method_returns_job_name(self):
        job_bundler = JobBundler('fake_name', {}, None, None)
        self.assertEqual(job_bundler.job_name(), 'fake_name')
-    
+
     def test_job_archive_name_method_returns_job_archive_name(self):
        job_bundler = JobBundler('fake_name', {}, None, None)
        self.assertEqual(job_bundler.job_archive_name(), 'fake_name.tgz')
@@ -30,7 +38,7 @@ class TestJobBundler(Spec):
     def test_job_archive_method_returns_job_archive(self):
        job_bundler = JobBundler('fake_name', {}, None, None)
        self.assertEqual(job_bundler.job_archive(), '../fake_name.tgz')
-    
+
     def test_cleanup_removes_correct_files(self):
         job_bundler = JobBundler('fake_name', {}, None, None)
         job_bundler.cleanup()
@@ -38,18 +46,31 @@ class TestJobBundler(Spec):
         remove_bin = call('fake_name.bin')
         remove_config = call('fake_name.config.yaml')
         self.mock_os_remove.assert_has_calls([remove_archive, remove_bin, remove_config])
-    
+
     def test_unbundle_opens_correct_file(self):
         job_bundler = JobBundler('fake_name', {}, None, None)
         job_bundler.unbundle()
         self.mock_tarfile_open.assert_called_with('../fake_name.tgz', 'r:gz')
-    
+
     def test_unbundle_extracts_from_tarfile(self):
-        return_object = Mock()
-        return_object.__enter__ = lambda obj: obj
-        return_object.__exit__ = lambda obj, exc_type, exc_val, exc_tb: obj
+        return_object = self.MockFileContextManager()
         self.mock_tarfile_open.return_value = return_object
         job_bundler = JobBundler('fake_name', {}, None, None)
         job_bundler.unbundle()
         return_object.extractall.assert_called()
- 
+
+    def test_save_job_opens_file(self):
+        mock_job = Mock()
+        mock_job.serialize = lambda: 'something'
+        job_bundler = JobBundler('fake_name', {}, mock_job, None)
+        job_bundler._save_job()
+        self.mock_builtins_open.assert_called_with('fake_name.bin', 'w+b')
+
+    def test_save_job_writes_to_file(self):
+        mock_job = Mock()
+        mock_job.serialize = lambda: 'something'
+        return_object = self.MockFileContextManager()
+        self.mock_builtins_open.return_value = return_object
+        job_bundler = JobBundler('fake_name', {}, mock_job, None)
+        job_bundler._save_job()
+        return_object.write.assert_called_with('something')
