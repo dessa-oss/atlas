1e3f04d82d27d7c3cf990d5a0d2e8c46ac1db99d
kbanc <katherine.bancroft@gmail.com>
2019-02-20 14:08:39 -0500

    [TR KB] Refactor

--- gcp_utils/src/test/test_connection_manager.py
+++ gcp_utils/src/test/test_connection_manager.py
@@ -11,12 +11,16 @@ from mock import Mock
 from foundations_internal.testing.helpers.spec import Spec
 from foundations_internal.testing.helpers.conditional_return import ConditionalReturn
 from foundations_internal.testing.helpers import set_up, let, let_mock, let_now, let_patch_mock
-from foundations_gcp.connection_manager import ConnectionManager
 
 class TestConnectionManager(Spec):
 
     mock_authorization_instance = let_mock()
     mock_connection_instance = let_mock()
+
+    @let
+    def connection_manager(self):
+        from foundations_gcp.connection_manager import ConnectionManager
+        return ConnectionManager()
 
     @set_up
     def set_up(self):
@@ -31,10 +35,9 @@ class TestConnectionManager(Spec):
         self.mock_connection.return_when(self.mock_connection_instance, _http=self.mock_authorization_instance)
     
     def test_bucket_connection_creates_connection(self):
-        self.assertEqual(ConnectionManager().bucket_connection(), self.mock_connection_instance)
+        self.assertEqual(self.connection_manager.bucket_connection(), self.mock_connection_instance)
     
     def test_bucket_connection_reuses_connection(self):
-        connection_manager = ConnectionManager()
-        connection_manager.bucket_connection()
-        connection_manager.bucket_connection()
+        self.connection_manager.bucket_connection()
+        self.connection_manager.bucket_connection()
         self.mock_connection.assert_called_once()
