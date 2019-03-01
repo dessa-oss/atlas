d3ad20661125aa10d7bc1259a1e72e3a573589e6
Pairing Station Hinton <t.rogers@dessa.com>
2019-03-01 11:52:32 -0500

    [TR] Refactor job notifiers

--- foundations_contrib/src/foundations_contrib/consumers/jobs/failed/job_notifier.py
+++ foundations_contrib/src/foundations_contrib/consumers/jobs/failed/job_notifier.py
@@ -5,35 +5,15 @@ Proprietary and confidential
 Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 """
 
-class JobNotifier(object):
+from foundations_contrib.consumers.jobs.mixins.job_event_notifier import JobEventNotifier
+
+class JobNotifier(JobEventNotifier):
     """Sends a notification message when a job is failed
     
     Arguments:
         job_notifier {JobNofitier} -- A JobNotifier for sending out the messages
     """
     
-    def __init__(self, job_notifier):
-        self._job_notifier = job_notifier
-
-    def call(self, message, timestamp, meta_data):
-        """See above
-        
-        Arguments:
-            message {dict} -- Event attributes
-            timestamp {int} -- The time the event was created
-            meta_data {dict} -- Additional data about the event
-        """
-
-        self._job_notifier.send_message(
-            """
-Job Failed
-Job Id: {}
-Timestamp: {}
-Project Name: {}
-            """.format(message['job_id'], self._readable_timestamp(timestamp), message['project_name'])
-        )
-
     @staticmethod
-    def _readable_timestamp(timestamp):
-        from datetime import datetime
-        return datetime.fromtimestamp(timestamp)
+    def _state():
+        return 'Failed'
