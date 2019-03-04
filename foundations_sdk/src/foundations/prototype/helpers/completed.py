267c0c13465e633cd9cd9a31a435f5c1a083f7f2
Pairing Station Hinton <t.rogers@dessa.com>
2019-03-04 14:30:04 -0500

    [TR] Add support for removing completed jobs

--- foundations_sdk/src/foundations/prototype/helpers/completed.py
+++ foundations_sdk/src/foundations/prototype/helpers/completed.py
@@ -7,3 +7,7 @@ Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 
 def list_jobs(redis):
     return {job_id.decode() for job_id in redis.smembers('projects:global:jobs:completed')}
+
+def remove_jobs(redis, list_of_job_ids):
+    for job_id in list_of_job_ids:
+        redis.srem('projects:global:jobs:completed', job_id)
--- foundations_sdk/src/test/prototype/helpers/test_completed.py
+++ foundations_sdk/src/test/prototype/helpers/test_completed.py
@@ -6,15 +6,15 @@ Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 """
 
 from foundations_internal.testing.helpers import *
+from foundations_internal.testing.helpers.extensions import *
 from foundations_internal.testing.helpers.spec import Spec
 from foundations_internal.testing.helpers.conditional_return import ConditionalReturn
 
+import random
+
 class TestCompletedJobHelpers(Spec):
     
-    @let
-    def redis(self):
-        from fakeredis import FakeRedis
-        return FakeRedis()
+    redis = let_fake_redis()
 
     @let
     def listing(self):
@@ -25,14 +25,32 @@ class TestCompletedJobHelpers(Spec):
         import random
         return random.randint(1, 10)
 
+    @let
+    def random_job_id(self):
+        return random.choice(list(self.listing))
+
+    @let
+    def random_job_id_two(self):
+        return random.choice(list(self.listing))
+
     @set_up
     def set_up(self):
         for job_id in self.listing:
             self.redis.sadd('projects:global:jobs:completed', job_id)
 
-    def test_returns_all_completed_job(self):
+    def test_list_jobs_returns_all_completed_jobs(self):
         from foundations.prototype.helpers.completed import list_jobs
         self.assertEqual(self.listing, list_jobs(self.redis))
+
+    def test_remove_jobs_removes_all_completed_jobs(self):
+        from foundations.prototype.helpers.completed import remove_jobs, list_jobs
+        remove_jobs(self.redis, [self.random_job_id])
+        self.assertEqual(self.listing - {self.random_job_id}, list_jobs(self.redis))
+
+    def test_remove_jobs_removes_all_completed_jobs_multiple_jobs(self):
+        from foundations.prototype.helpers.completed import remove_jobs, list_jobs
+        remove_jobs(self.redis, [self.random_job_id, self.random_job_id_two])
+        self.assertEqual(self.listing - {self.random_job_id, self.random_job_id_two}, list_jobs(self.redis))
 
     def _random_uuid(self):
         from uuid import uuid4
