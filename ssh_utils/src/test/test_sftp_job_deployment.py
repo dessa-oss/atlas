04b8fd867acad308490a9540a5c232ad532ce142
Katherine <k.bancroft@dessa.com>
2019-03-07 12:03:14 -0500

    dp kb moved all the old tests into the new test folder so they are actually run, deleted old folder

--- ssh_utils/src/test/test_sftp_job_deployment.py
+++ ssh_utils/src/test/test_sftp_job_deployment.py
@@ -8,8 +8,66 @@ Written by Jinnah Ali-Clarke <j.ali-clarke@dessa.com>, 09 2018
 import unittest
 
 from foundations_ssh.sftp_job_deployment import SFTPJobDeployment
+import test.ssh_utils_fixtures.sftp_job_deployment_fixtures as sjf
+import foundations.constants as constants
 
 class TestSFTPJobDeployment(unittest.TestCase):
     def test_scheduler_backend(self):
         from foundations_ssh.default_legacy_backend import default_legacy_backend
         self.assertEqual(SFTPJobDeployment.scheduler_backend(), default_legacy_backend)
+    
+    def test_job_queued(self):
+        queued = sjf.QueuedMockDeployment()
+
+        self.assertEqual(constants.deployment_queued, queued.get_job_status())
+
+    def test_job_never_finishes(self):
+        never_done = sjf.NeverFinishDeployment()
+
+        self.assertEqual(constants.deployment_running, never_done.get_job_status())
+        
+    def test_job_completed_instantly(self):
+        done = sjf.SuccessfulMockDeployment()
+
+        self.assertEqual(constants.deployment_completed, done.get_job_status())
+
+    def test_job_failed_instantly(self):
+        failed = sjf.FailedMockDeployment()
+
+        self.assertEqual(constants.deployment_error, failed.get_job_status())
+
+    def test_takes_one_second(self):
+        deploy = sjf.TakesOneSecond()
+
+        for _ in range(0, 1):
+            self.assertEqual(constants.deployment_running, deploy.get_job_status())
+
+        self.assertEqual(constants.deployment_completed, deploy.get_job_status())
+        self.assertEqual(constants.deployment_completed, deploy.get_job_status())
+
+    def test_takes_two_seconds(self):
+        deploy = sjf.TakesTwoSeconds()
+
+        for _ in range(0, 2):
+            self.assertEqual(constants.deployment_running, deploy.get_job_status())
+
+        self.assertEqual(constants.deployment_completed, deploy.get_job_status())
+        self.assertEqual(constants.deployment_completed, deploy.get_job_status())
+
+    def test_takes_random_time(self):
+        deploy = sjf.SuccessfulTakesRandomTime()
+
+        while deploy.get_job_status() == constants.deployment_running:
+            pass
+
+        self.assertEqual(constants.deployment_completed, deploy.get_job_status())
+        self.assertEqual(constants.deployment_completed, deploy.get_job_status())
+
+    def test_fails_random_time(self):
+        deploy = sjf.FailedTakesRandomTime()
+
+        while deploy.get_job_status() == constants.deployment_running:
+            pass
+
+        self.assertEqual(constants.deployment_error, deploy.get_job_status())
+        self.assertEqual(constants.deployment_error, deploy.get_job_status())
