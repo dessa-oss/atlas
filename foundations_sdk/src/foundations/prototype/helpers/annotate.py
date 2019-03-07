1437b46a82b24541459c764db1b679019c9a084d
Pairing Station Hinton <t.rogers@dessa.com>
2019-03-07 11:48:57 -0500

    [TR PL] Allow multiple jobs to be annotated

--- foundations_sdk/src/foundations/prototype/helpers/annotate.py
+++ foundations_sdk/src/foundations/prototype/helpers/annotate.py
@@ -9,6 +9,9 @@ def job_annotations(redis, job_id):
     encoded_annotations = _encoded_job_annotations(redis, job_id).items()
     return {key.decode(): value.decode() for key, value in encoded_annotations}
 
+def annotations_for_multiple_jobs(redis, list_of_job_ids):
+    return {job_id: job_annotations(redis, job_id) for job_id in list_of_job_ids}
+
 def _encoded_job_annotations(redis, job_id):
     key = 'jobs:{}:annotations'.format(job_id)
     return redis.hgetall(key)
--- foundations_sdk/src/test/prototype/helpers/test_annotate.py
+++ foundations_sdk/src/test/prototype/helpers/test_annotate.py
@@ -37,11 +37,29 @@ class TestAnnotate(Spec):
     def annotations_two(self):
         return self._generate_annotations()
 
-    def test_returns_stored_annotations_for_single_job(self):
+    def test_job_annotations_returns_stored_annotations_for_single_job(self):
         from foundations_contrib.consumers.jobs.queued.annotate import Annotate
         
         Annotate(self.redis).call({'job_id': self.job_id, 'annotations': self.annotations}, None, {})
         self.assertEqual(self.annotations, job_annotations(self.redis, self.job_id))
+
+    def test_annotations_for_multiple_jobs_returns_annotations_for_single_job(self):
+        from foundations_contrib.consumers.jobs.queued.annotate import Annotate
+        
+        Annotate(self.redis).call({'job_id': self.job_id, 'annotations': self.annotations}, None, {})
+        self.assertEqual({self.job_id: self.annotations}, annotations_for_multiple_jobs(self.redis, [self.job_id]))
+
+    def test_annotations_for_multiple_jobs_returns_annotations_for_multiple_jobs(self):
+        from foundations_contrib.consumers.jobs.queued.annotate import Annotate
+        
+        annotate = Annotate(self.redis)
+        annotate.call({'job_id': self.job_id, 'annotations': self.annotations}, None, {})
+        annotate.call({'job_id': self.job_id_two, 'annotations': self.annotations_two}, None, {})
+
+        result_annotations = annotations_for_multiple_jobs(self.redis, [self.job_id, self.job_id_two])
+        expected_annotations = {self.job_id: self.annotations, self.job_id_two: self.annotations_two}
+
+        self.assertEqual(expected_annotations, result_annotations)
 
     def _generate_annotations(self):
         import random
