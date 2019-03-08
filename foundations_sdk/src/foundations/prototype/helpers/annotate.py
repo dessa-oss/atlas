1b23e2dc8712b74f5f7bcf683092c05748cd49ca
Pairing Station Hinton <t.rogers@dessa.com>
2019-03-08 11:26:25 -0500

    [TR] Use a redis pipeline for performance

--- foundations_sdk/src/foundations/prototype/helpers/annotate.py
+++ foundations_sdk/src/foundations/prototype/helpers/annotate.py
@@ -6,12 +6,33 @@ Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 """
 
 def job_annotations(redis, job_id):
-    encoded_annotations = _encoded_job_annotations(redis, job_id).items()
-    return {key.decode(): value.decode() for key, value in encoded_annotations}
+    encoded_annotations = _encoded_job_annotations(redis, job_id)
+    return _decode_job_annotations(encoded_annotations)
 
 def annotations_for_multiple_jobs(redis, list_of_job_ids):
-    return {job_id: job_annotations(redis, job_id) for job_id in list_of_job_ids}
+    futures = _job_annotation_futures(redis, list_of_job_ids)
+    return {job_id: future.get() for job_id, future in futures.items()}
 
+def _job_annotation_futures(redis, list_of_job_ids):
+    from foundations_contrib.redis_pipeline_wrapper import RedisPipelineWrapper
+    from promise import Promise
+    
+    pipeline = RedisPipelineWrapper(redis.pipeline())
+    encoded_futures = _multiple_encoded_job_annotations(pipeline, list_of_job_ids)
+    futures = _decode_job_annotation_futures(encoded_futures)
+    pipeline.execute()
+
+    return futures
+
+def _decode_job_annotation_futures(encoded_futures):
+    return {job_id: annotations_future.then(_decode_job_annotations) for job_id, annotations_future in encoded_futures.items()}
+
+def _multiple_encoded_job_annotations(pipeline, list_of_job_ids):
+    return {job_id: _encoded_job_annotations(pipeline, job_id) for job_id in list_of_job_ids}
+
+def _decode_job_annotations(encoded_annotations):
+    return {key.decode(): value.decode() for key, value in encoded_annotations.items()}
+    
 def _encoded_job_annotations(redis, job_id):
     key = 'jobs:{}:annotations'.format(job_id)
     return redis.hgetall(key)
--- foundations_sdk/src/foundations/prototype/projects.py
+++ foundations_sdk/src/foundations/prototype/projects.py
@@ -9,12 +9,14 @@ def get_metrics_for_all_jobs(project_name):
     import foundations
     from foundations_contrib.global_state import redis_connection
     from foundations.prototype.helpers.annotate import annotations_for_multiple_jobs
+    from foundations_contrib.redis_pipeline_wrapper import RedisPipelineWrapper
     from pandas import DataFrame
 
     metrics = foundations.get_metrics_for_all_jobs(project_name)
     metric_rows = list(metrics.T.to_dict().values())
 
     job_annotations = annotations_for_multiple_jobs(redis_connection, metrics['job_id'])
+
     for row in metric_rows:
         annotations = job_annotations[row['job_id']]
         for key, value in annotations.items():
