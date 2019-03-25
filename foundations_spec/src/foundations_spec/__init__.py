34795d6228bbb7f3b73c834cd3e4a5806f0089d6
pairing <developers@dessa.com>
2019-03-25 10:56:07 -0400

    [TR PL] Include spec as a part of deployed modules to ensure tests work

--- foundations_spec/src/foundations_spec/__init__.py
+++ foundations_spec/src/foundations_spec/__init__.py
@@ -9,3 +9,12 @@ from foundations_spec.helpers import *
 from foundations_spec.helpers.spec import Spec
 from foundations_spec.helpers.conditional_return import ConditionalReturn
 from foundations_spec.helpers.partial_callable_mock import PartialCallableMock
+
+def _append_module():
+    import sys
+    from foundations_internal.global_state import module_manager
+
+    module_manager.append_module(sys.modules[__name__])
+
+
+_append_module()
