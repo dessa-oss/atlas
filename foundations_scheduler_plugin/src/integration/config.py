c7f18c75f2fe159c1ee366c73364456625a5d706
Katherine <k.bancroft@dessa.com>
2019-04-04 13:39:55 -0400

    [KB TR] changed config to use spec extension to fetch network address

--- foundations_scheduler_plugin/src/integration/config.py
+++ foundations_scheduler_plugin/src/integration/config.py
@@ -1,17 +1,4 @@
-def _get_docker_adapter():
-    import ifaddr
-
-    for adapter in ifaddr.get_adapters():
-        if adapter.name == 'docker0':
-            return adapter
-
-def _redis_ip():
-    docker_adapter = _get_docker_adapter()
-    
-    for ip in docker_adapter.ips:
-        if isinstance(ip.ip, str):
-            return ip.ip
-
+from foundations_spec.extensions import get_network_address
 
 def _load_config():
     import os
@@ -22,6 +9,6 @@ def _load_config():
     config_manager['port'] = 31222
     config_manager['key_path'] = os.path.expanduser('~/.ssh/id_foundations_scheduler')
     config_manager['code_path'] = '/jobs'
-    config_manager['redis_url'] = 'redis://{}:6379'.format(_redis_ip())
+    config_manager['redis_url'] = 'redis://{}:6379'.format(get_network_address('docker0'))
 
 _load_config()
