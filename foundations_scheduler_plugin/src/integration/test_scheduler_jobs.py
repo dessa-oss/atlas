01ad7ea24ac4eb70a36ac7280c2833bd3dabc465
Katherine <k.bancroft@dessa.com>
2019-04-04 12:09:45 -0400

    [KB JA] specify config in setup and add ifaddr so that we can get a stable-ish ip for an adapter on which redis is listening

--- foundations_scheduler_plugin/src/integration/test_scheduler_jobs.py
+++ foundations_scheduler_plugin/src/integration/test_scheduler_jobs.py
@@ -8,13 +8,12 @@ Written by Thomas Rogers <t.rogers@dessa.com>, 11 2018
 from foundations_spec import *
 from foundations import create_stage
 
-@skip
 class TestSchedulerJobs(Spec):
     
     @let
     def job_source_bundle(self):
         from foundations_contrib.job_source_bundle import JobSourceBundle
-        return JobSourceBundle(self.job_id, '../')
+        return JobSourceBundle.for_deployment()
 
     @let
     def job_id(self):
@@ -31,7 +30,8 @@ class TestSchedulerJobs(Spec):
         @create_stage
         def _callback():
             pass
-        return _callback
+
+        return _callback()
 
     @let
     def deployment(self):
@@ -43,23 +43,51 @@ class TestSchedulerJobs(Spec):
         from foundations.deployment_wrapper import DeploymentWrapper
         return DeploymentWrapper(self.deployment)
 
+    @let
+    def redis_ip(self):
+        docker_adapter = self._get_docker_adapter()
+        
+        for ip in docker_adapter.ips:
+            if isinstance(ip.ip, str):
+                return ip.ip
+
+    def _get_docker_adapter(self):
+        import ifaddr
+
+        for adapter in ifaddr.get_adapters():
+            if adapter.name == 'docker0':
+                return adapter
+
     @set_up
     def set_up(self):
+        self.load_config()
         self.deployment.deploy()
         self._wait_for_job_to_complete()
 
     def test_runs_job(self):
-        self.assertEqual('complete', self.deployment_wrapper.get_job_status())
+        self.assertEqual('completed', self.deployment_wrapper.get_job_status())
 
+    @skip
     def test_creates_log(self):
         self.assertIn('Completed', self.deployment_wrapper.get_job_logs())
+    
+    def load_config(self):
+        import os
+        from foundations_contrib.global_state import config_manager
+        config_manager['remote_host'] = 'localhost'
+        config_manager['remote_user'] = 'job-uploader'
+        config_manager['port'] = 31222
+        config_manager['key_path'] = os.path.expanduser('~/.ssh/id_foundations_scheduler')
+        config_manager['code_path'] = '/jobs'
+        config_manager['redis_url'] = 'redis://{}:6379'.format(self.redis_ip)
 
     def _wait_for_job_to_complete(self):
         from time import sleep
 
         for _ in range(60):
             if self.deployment_wrapper.is_job_complete():
-                break
+                return
             sleep(1)
         
-        raise AssertionError('Job did not complete')
+        job_logs = self.deployment_wrapper.get_job_logs()
+        raise AssertionError('Job did not complete:\n{}'.format(job_logs))
