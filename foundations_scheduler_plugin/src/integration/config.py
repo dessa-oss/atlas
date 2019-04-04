8ac632dff858a75028a3798c2a14233251a6ee89
Katherine <k.bancroft@dessa.com>
2019-04-04 13:42:11 -0400

    [TR KB] Remove unneeded config

--- foundations_scheduler_plugin/src/integration/config.py
+++ foundations_scheduler_plugin/src/integration/config.py
@@ -7,7 +7,6 @@ def _load_config():
     config_manager['remote_host'] = 'localhost'
     config_manager['remote_user'] = 'job-uploader'
     config_manager['port'] = 31222
-    config_manager['key_path'] = os.path.expanduser('~/.ssh/id_foundations_scheduler')
     config_manager['code_path'] = '/jobs'
     config_manager['redis_url'] = 'redis://{}:6379'.format(get_network_address('docker0'))
 
