53d42d977f89d6a7cbf79e62da35600b513b0ffd
pairing <developers@dessa.com>
2019-04-09 14:19:22 -0400

    [TR KB] Refactor cache backend for gcp config translate

--- gcp_utils/src/foundations_gcp/config/gcp_config_translate.py
+++ gcp_utils/src/foundations_gcp/config/gcp_config_translate.py
@@ -42,12 +42,11 @@ def _log_level(config):
     return config.get('log_level', 'INFO')
 
 def _cache_implementation(config):
-    from foundations_contrib.config.mixin import storage_implementation
-    from foundations_contrib.bucket_cache_backend_for_config import BucketCacheBackendForConfig
+    from foundations_contrib.config.mixin import cache_implementation
     from foundations_gcp.gcp_bucket import GCPBucket
 
     cache_end_point = config['cache_config']['end_point']
-    return storage_implementation('cache_type', BucketCacheBackendForConfig, cache_end_point, GCPBucket)
+    return cache_implementation(cache_end_point, GCPBucket)
 
 def _redis_url(config):
     return config['results_config'].get('redis_end_point', 'redis://localhost:6379')
