8de64cacbcfd90b630ed31e57238caba5e082c8b
Thomas Rogers <t.rogers@dessa.com>
2019-04-10 16:01:12 -0400

    KB change way username is passed to script uploading to artifactory

--- upload_modules_to_artifactory.sh
+++ upload_modules_to_artifactory.sh
@@ -2,4 +2,4 @@
 cwd=`pwd`
 echo $NEXUS_USER
 
-twine upload -u $NEXUS_USER -p $NEXUS_PASSWORD --repository-url $NEXUS_URL ${cwd}/dist/*.whl
+twine upload -u jenkins-user -p $NEXUS_PASSWORD --repository-url $NEXUS_URL ${cwd}/dist/*.whl
