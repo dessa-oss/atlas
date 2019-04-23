38fef804724ad0010ed8265f4a2369e796c85f1c
Katherine <k.bancroft@dessa.com>
2019-04-23 13:14:24 -0400

    [KB JA] change copyright headers

--- foundations_production/src/integration/__init__.py
+++ foundations_production/src/integration/__init__.py
@@ -2,5 +2,5 @@
 Copyright (C) DeepLearning Financial Technologies Inc. - All Rights Reserved
 Unauthorized copying, distribution, reproduction, publication, use of this file, via any medium is strictly prohibited
 Proprietary and confidential
-Written by Katherine Bancroft <k.bancroft@dessa.com>, 06 2018
+Written by Susan Davis <s.davis@dessa.com>, 04 2019
 """
