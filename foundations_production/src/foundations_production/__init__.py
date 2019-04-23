55e1631065403d3a8a67dab418e49a0eade4fc47
pairing <developers@dessa.com>
2019-04-23 13:27:45 -0400

    [CC TR PL] add foundations production to module manager

--- foundations_production/src/foundations_production/__init__.py
+++ foundations_production/src/foundations_production/__init__.py
@@ -4,3 +4,10 @@ Unauthorized copying, distribution, reproduction, publication, use of this file,
 Proprietary and confidential
 Written by Susan Davis <s.davis@dessa.com>, 04 2019
 """
+
+def _append_module():
+    import sys
+    module_manager.append_module(sys.modules[__name__])
+
+
+_append_module()
