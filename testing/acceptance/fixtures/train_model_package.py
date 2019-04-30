de4170c84c46d55435b75950bbc0bfd39e614dee
pairing <developers@dessa.com>
2019-04-30 12:06:46 -0400

    [TR KB PL DP] Get initial version of acceptance test running

---
+++ testing/acceptance/fixtures/train_model_package.py
@@ -0,0 +1,76 @@
+import foundations
+import foundations_production
+import pandas
+
+class AddAverageToValueTransformer(object):
+
+    def __init__(self):
+        self.average = 0
+
+    def fit(self, data):
+        selected_data = data[["Sex", "Cabin", "Fare"]]
+        self.average = selected_data.mean()
+
+    def transform(self, data):
+        data[["Sex", "Cabin", "Fare"]] = data[["Sex", "Cabin", "Fare"]] + self.average
+        return data
+
+class Model(object):
+
+    def __init__(self):
+        self.max_fare = 0
+
+    def fit(self, input_training_features, input_training_targets, input_validation_features, input_validation_targets):
+        self.max_fare = input_training_features["Fare"].max()
+
+    def predict(self, input_features):
+        predicted_labels = []
+        for row in input_features["Fare"]:
+            predicted_labels.append(int(row < self.max_fare))
+        return pandas.DataFrame({"Survived": predicted_labels})
+
+def transformer_preprocessor(input_data):
+    transformer = foundations_production.Transformer(AddAverageToValueTransformer)
+    transformer.fit(input_data)
+    return transformer.transform(input_data)
+
+def split_inputs_and_targets(data):
+    if "Survived" in data:
+        return data[["Sex", "Cabin", "Fare"]], data[["Survived"]]
+    else:
+        return data[["Sex", "Cabin", "Fare"]], None
+
+def model_preprocessor(train_data, validation_data):
+    split_inputs_and_targets_stage = foundations.create_stage(split_inputs_and_targets)
+    train_features, train_targets = split_inputs_and_targets_stage(train_data).split(2)
+    validation_features, validation_targets = split_inputs_and_targets_stage(validation_data).split(2)
+
+    model_transformer = foundations_production.Model(Model)
+    model_transformer.fit(train_features, train_targets, validation_features, validation_targets)
+
+    return model_transformer.predict(validation_features)
+
+preprocessor = foundations_production.preprocessor(transformer_preprocessor)
+model = foundations_production.model(model_preprocessor)
+
+train_data = pandas.DataFrame({
+    "Sex": [0, 1, 4],
+    "Cabin": [200, 100, 0],
+    "Fare": [40, 20, -200],
+    "Survived": [0, 1, 1]
+})
+
+validation_data = pandas.DataFrame({
+    "Sex": [0],
+    "Cabin": [101],
+    "Fare": [10],
+    "Survived": [1]
+})
+
+preprocessed_train_data = preprocessor(train_data)
+
+preprocessor.set_inference_mode()
+preprocessed_validation_data = preprocessor(validation_data)
+
+validation_predictions = model(preprocessed_train_data, preprocessed_validation_data)
+
--- testing/acceptance/test_model_package.py
+++ testing/acceptance/test_model_package.py
@@ -6,108 +6,33 @@ Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 """
 
 from foundations_spec import *
-import foundations_production
 import foundations
+import foundations_production
+import acceptance.fixtures.train_model_package as train_model_package
+import acceptance.fixtures.production_model_package as production_model_package
 import pandas
-
 
 class TestModelPackage(Spec):
 
-    class AddAverageToValueTransformer(object):
-
-        def __init__(self):
-            self.average = 0
-
-        def fit(self, data):
-            self.average = data.mean()
-
-        def transform(self, data):
-            return data + self.average
-
-    class Model(object):
-
-        def __init__(self):
-            self.max_fare = 0
-
-        def fit(self, input_training_features, input_training_targets, input_validation_features, input_validation_targets):
-            self.max_fare = input_training_features["Fare"].max()
-
-        def predict(self, input_features):
-            predicted_labels = []
-            for row in input_features["Fare"]:
-                predicted_labels.append(int(row < self.max_fare))
-            return pandas.DataFrame({"Survived": predicted_labels})
-    
-    @skip('not ready')
     def test_can_create_and_consume_model_package(self):
-        foundations.set_project_name('production_titanic_test')
-
-        @foundations_production.preprocessor
-        def preprocessor(input_data):
-            transformer = foundations_production.Transformer(self.AddAverageToValueTransformer, list_of_columns=["Sex", "Cabin", "Fare"])
-            transformer.fit(input_data)
-            return transformer.transform(input_data)
-
-        train_data = pandas.DataFrame({
-            "Sex": [0, 1, 4],
-            "Cabin": [200, 100, 0],
-            "Fare": [40, 20, -200],
-            "Survived": [0, 1, 1]
-        })
-
-        validation_data = pandas.DataFrame({
-            "Sex": [0],
-            "Cabin": [101],
-            "Fare": [10],
-            "Survived": [1]
-        })
-
-        preprocessed_train_data = preprocessor(train_data)
-
-        preprocessor.set_inference_mode()
-        preprocessed_validation_data = preprocessor(validation_data)
-
-        @foundations_production.model
-        def model(train_data, validation_data):
-            train_features = train_data[["Sex", "Cabin", "Fare"]]
-            train_targets = train_data[["Survived"]]
-            validation_features = validation_data[["Sex", "Cabin", "Fare"]]
-            validation_targets = validation_data[["Survived"]]
-
-            model_transformer = foundations_production.Model(self.Model)
-            model_transformer.fit(train_features, train_targets, validation_features, validation_targets)
-            return model_transformer.predict(validation_features)
-
-        validation_predictions = model(train_data, validation_data)
-
-        job = validation_predictions.run()
-
+        job = train_model_package.validation_predictions.run()
         job.wait_for_deployment_to_complete()
-
-        production_dataset = pandas.DataFrame({
-            "Sex": [0, 1],
-            "Cabin": [101, 202],
-            "Fare": [10, 60]
-        })
-
         job_id = job.job_name()
 
         model_package = foundations_production.load_model_package(job_id)
 
+        production_dataset = pandas.DataFrame({
+            "Sex": [0],
+            "Cabin": [101],
+            "Fare": [10]
+        })
         preprocessed_production_dataset = model_package.preprocessor(production_dataset)
+        production_predictions = model_package.model(preprocessed_production_dataset, preprocessed_production_dataset)
 
-        production_predictions = model_package.model.predict(preprocessed_production_dataset)
-
-        @foundations.create_stage
-        def log_predictions_for_assertion(predictions):
-            foundations.log_metric("predictions", list(predictions["Survived"]))
-
-        production_job = log_predictions_for_assertion(production_predictions).run()
-
+        production_job = foundations.create_stage(production_model_package.log_predictions_for_assertion)(production_predictions).run()
         production_job.wait_for_deployment_to_complete()
 
-        metrics = foundations.get_metrics_for_all_jobs('production_titanic_test')
-
+        metrics = foundations.get_metrics_for_all_jobs('default')
         production_predictions = metrics.loc[metrics['job_id'] == production_job.job_name()].iloc[0]['predictions']
 
-        self.assertEqual([1,0], production_predictions)
+        self.assertEqual(1, production_predictions)
