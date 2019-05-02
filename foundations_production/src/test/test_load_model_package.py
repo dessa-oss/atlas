ed07625a87c6f140f7a49201c13ab3e44094f75a
Katherine <k.bancroft@dessa.com>
2019-05-02 14:30:59 -0400

    [PL KB] clean up test for load_model_package

--- foundations_production/src/test/test_load_model_package.py
+++ foundations_production/src/test/test_load_model_package.py
@@ -7,80 +7,28 @@ Written by Susan Davis <s.davis@dessa.com>, 04 2019
 
 from foundations_spec import *
 from foundations_production import load_model_package
-class MockClass(object):
-    def __call__(self, *args):
-        return args[0]
 
 class TestLoadModelPackage(Spec):
 
-    mock_transformer_callback = let_mock()
     mock_pipeline_archiver = let_mock()
-    mock_production_data = let_mock()
+    mock_preprocessor = let_mock()
+    
+    @let_now
+    def mock_load_preprocessor(self):
+        mock_load_preprocessor = self.patch('foundations_production.preprocessor_class.Preprocessor.load_preprocessor',ConditionalReturn())
+        mock_load_preprocessor.return_when(self.mock_preprocessor, self.mock_pipeline_archiver, 'transformer', self.job_id)
+        return mock_load_preprocessor
 
     @let
     def job_id(self):
         return self.faker.uuid4() 
-
-    @let
-    def mock_class(self):
-        return MockClass()
-    
-    @let
-    def serialized_mock_class(self):
-        from foundations_internal.serializer import serialize
-        return serialize(self.mock_class)
-    
-    @let_now
-    def foundations_context(self):
-        from foundations_internal.foundations_context import FoundationsContext
-        return self.patch('foundations_contrib.global_state.foundations_context', FoundationsContext(self.pipeline))
-    
-    @let_now
-    def pipeline_context(self):
-        from foundations_internal.pipeline_context import PipelineContext
-        return PipelineContext()
-
-    @let_now
-    def pipeline(self):
-        from foundations_internal.pipeline import Pipeline
-        return Pipeline(self.pipeline_context)
-
-    @set_up
-    def set_up_foundations_context(self):
-        self.pipeline_context.file_name = self.job_id
-
-    
-    @let
-    def fake_data(self):
-        return self.faker.sha1()
 
     @let_now
     def mock_get_pipeline_archiver(self):
         mock_get_pipeline_archiver = self.patch('foundations_contrib.archiving.get_pipeline_archiver_for_job', ConditionalReturn())
         mock_get_pipeline_archiver.return_when(self.mock_pipeline_archiver, self.job_id)
         return mock_get_pipeline_archiver
-
-    @set_up
-    def set_up(self):
-        self.mock_pipeline_archiver.fetch_artifact = ConditionalReturn()
-        self.mock_pipeline_archiver.fetch_artifact.return_when(self.serialized_mock_class, 'preprocessor/transformer.pkl')
-
-    def test_load_model_package_loads_transformer_preprocessor(self):
-        model_package = load_model_package(self.job_id)
-        actual_value = model_package.preprocessor(self.fake_data)
-        self.assertEqual(self.fake_data, actual_value.run_same_process())
-
-    def test_load_model_package_loads_transformer_preprocessor_with_correct_name(self):
-        model_package = load_model_package(self.job_id)
-        model_package.preprocessor(self.mock_production_data)
-        new_transformer_index = model_package.preprocessor.new_transformer(None)
-        self.assertEqual('transformer_0', new_transformer_index)
-
-    def test_load_model_package_loads_transformer_preprocessor_with_job_id(self):
-        model_package = load_model_package(self.job_id)
-        model_package.preprocessor(self.mock_production_data)
-        self.assertEqual(self.job_id, model_package.preprocessor.job_id.run_same_process())
-
+    
     def test_load_model_package_loads_model_preprocessor_with_correct_name(self):
         model_package = load_model_package(self.job_id)
         new_transformer_index = model_package.model.new_transformer(None)
@@ -96,6 +44,6 @@ class TestLoadModelPackage(Spec):
         model_package = load_model_package(self.job_id)
         self.assertIsInstance(model_package.model, ProductionModel)   
     
-    def test_transformer_preprocessor_inference_mode_is_true(self):
+    def test_load_model_package_returns_correct_preprocessor(self):
         model_package = load_model_package(self.job_id)
-        self.assertTrue(model_package.preprocessor.get_inference_mode())
+        self.assertEqual(self.mock_preprocessor, model_package.preprocessor)
