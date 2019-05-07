5f6d1f78721bde0d416b558fa1d23e712a29da50
pairing <developers@dessa.com>
2019-05-07 16:46:16 -0400

    [KB NN] Added run_model_package function

--- foundations_production/src/foundations_production/serving/package_runner.py
+++ foundations_production/src/foundations_production/serving/package_runner.py
@@ -7,3 +7,9 @@ Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 
 def run_prediction(model_package, data):
     return model_package.model.predict(data)
+
+def run_model_package(model_package_id, queue):
+    from foundations_production import load_model_package
+    load_model_package(model_package_id)
+
+    
--- foundations_production/src/test/serving/test_package_runner.py
+++ foundations_production/src/test/serving/test_package_runner.py
@@ -6,7 +6,7 @@ Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 """
 
 from foundations_spec import *
-from foundations_production.serving.package_runner import run_prediction
+from foundations_production.serving.package_runner import run_model_package, run_prediction
 
 class TestPackageRunner(Spec):
 
@@ -18,6 +18,12 @@ class TestPackageRunner(Spec):
     def prediction(self):
         return self.faker.word()
     
+    @let
+    def model_package_id(self):
+        return self.faker.uuid4()
+    
+    mock_load_model_package = let_patch_mock('foundations_production.load_model_package')
+    
     def test_run_prediction_runs_prediction_on_model_with_correct_data(self):
         model_package = Mock()
         model_package.model.predict = ConditionalReturn()
@@ -25,6 +31,11 @@ class TestPackageRunner(Spec):
 
         actual_prediction = run_prediction(model_package, self.fake_data)
         self.assertEqual(self.prediction, actual_prediction)
+    
+    def test_run_model_package_loads_model_package(self):
+        queue = Mock()
+        run_model_package(self.model_package_id, queue)
+        self.mock_load_model_package.assert_called_with(self.model_package_id)
 
         
         
