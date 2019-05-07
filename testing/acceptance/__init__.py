1dbc9209b53c937b5e7ccdf4d43b9ceab08cc080
pairing <developers@dessa.com>
2019-05-07 14:07:59 -0400

    [KB JA CZ] acceptance test for creating foundations serving server

--- testing/acceptance/__init__.py
+++ testing/acceptance/__init__.py
@@ -23,3 +23,4 @@ from acceptance.test_local_obfuscate_jobs import TestLocalObfuscateJobs
 from acceptance.test_set_environment import TestSetEnvironment
 from acceptance.test_model_package import TestModelPackage
 from acceptance.test_class_stage_deployment import TestClassStageDeployment
+from acceptance.test_deploy_model_package import TestDeployModelPackage
---
+++ testing/acceptance/test_deploy_model_package.py
@@ -0,0 +1,29 @@
+"""
+Copyright (C) DeepLearning Financial Technologies Inc. - All Rights Reserved
+Unauthorized copying, distribution, reproduction, publication, use of this file, via any medium is strictly prohibited
+Proprietary and confidential
+Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
+"""
+
+from foundations_spec import *
+
+class TestDeployModelPackage(Spec):
+    @let
+    def job_id(self):
+        return self.faker.uuid4()
+
+    @skip('not yet implemented')
+    def test_deploy_model_package_via_cli(self):
+        import subprocess
+
+        subprocess.run(['foundations', 'serving', 'deploy', 'rest', '--domain=localhost:5000', '--model-id={}'.format(self.job_id), '--slug=snail'])
+
+        base_url = 'http://localhost:5000/v1/snail/{}'.format(self.job_id)
+        self._assert_successful_get(base_url)
+
+        subprocess.run(['foundations', 'serving', 'stop'])
+
+    def _assert_successful_get(self, url):
+        import requests
+        response = requests.get(url)
+        self.assertEqual(200, response.status_code)
