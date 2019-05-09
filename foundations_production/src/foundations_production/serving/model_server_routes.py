de74d04a83164b0c45e104712358f5d937783a5d
Katherine <k.bancroft@dessa.com>
2019-05-09 14:14:36 -0400

    [DP CZ] add routes to model server

--- foundations_production/src/foundations_production/serving/model_server_routes.py
+++ foundations_production/src/foundations_production/serving/model_server_routes.py
@@ -6,4 +6,24 @@ Written by Susan Davis <s.davis@dessa.com>, 04 2019
 """
 
 def load_routes(flask_app):
-    pass
+    from flask import request
+    
+    @flask_app.route('/v1/<user_defined_model_name>', methods=[])
+    def manage_model_package(user_defined_model_name):
+        pass
+
+    @flask_app.route('/v1/<user_defined_model_name>/model', methods=[])
+    def train_all_model_packages(user_defined_model_name):
+        pass
+
+    @flask_app.route('/v1/<user_defined_model_name>/model/<version>', methods=[])
+    def train_one_model_package(user_defined_model_name):
+        pass
+
+    @flask_app.route('/v1/<user_defined_model_name>/predictions', methods=[])
+    def predictions_from_model_package(user_defined_model_name):
+        pass
+
+    @flask_app.route('/v1/<user_defined_model_name>/predictions/<id>', methods=[])
+    def predict_with_model_package(user_defined_model_name):
+        pass
--- foundations_production/src/test/serving/test_model_server_routes.py
+++ foundations_production/src/test/serving/test_model_server_routes.py
@@ -5,3 +5,33 @@ Proprietary and confidential
 Written by Susan Davis <s.davis@dessa.com>, 04 2019
 """
 
+import unittest
+from mock import Mock
+
+from foundations_spec.helpers import let, let_now, let_patch_mock, set_up
+from foundations_spec.helpers.spec import Spec
+from flask import Flask
+
+class TestModelServerRoutes(Spec):
+    
+    @set_up
+    def set_up(self):
+        from foundations_production.serving.model_server_routes import load_routes
+
+        self.app = Flask(__name__)
+        load_routes(self.app)
+
+    def test_manage_model_package_route_is_added(self):
+        self.assertIn('manage_model_package', self.app.view_functions)
+
+    def test_training_all_model_packages_route_is_added(self):
+        self.assertIn('train_all_model_packages', self.app.view_functions)
+
+    def test_training_one_model_package_route_is_added(self):
+        self.assertIn('train_one_model_package', self.app.view_functions)
+
+    def test_predictions_from_model_package_route_is_added(self):
+        self.assertIn('predictions_from_model_package', self.app.view_functions)
+
+    def test_predict_with_model_package_route_is_added(self):
+        self.assertIn('predict_with_model_package', self.app.view_functions)
