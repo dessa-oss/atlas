8b8c4b90a363dd6cb678de2983d07c56727a1eb5
Thomas Rogers <t.rogers@dessa.com>
2019-05-09 13:26:25 -0400

    [PL JA] load model package when retraining

--- foundations_production/src/foundations_production/serving/__init__.py
+++ foundations_production/src/foundations_production/serving/__init__.py
@@ -5,20 +5,23 @@ Proprietary and confidential
 Written by Susan Davis <s.davis@dessa.com>, 04 2019
 """
 
-def _load_data_stage(file_location):
-    from foundations_production.serving.data_from_file import data_from_file
-    return data_from_file(file_location)
-
-def _join_stage(*args):
-    return args
-
 def create_retraining_job(model_package_id, features_location, targets_location):
     import foundations
+    from foundations_production import load_model_package
+
+    load_model_package(model_package_id)
 
     data_from_file_stage = foundations.create_stage(_load_data_stage)
     join_stage = foundations.create_stage(_join_stage)
 
     features = data_from_file_stage(features_location)
     targets = data_from_file_stage(targets_location)
-
+    
     return join_stage(features, targets)
+
+def _load_data_stage(file_location):
+    from foundations_production.serving.data_from_file import data_from_file
+    return data_from_file(file_location)
+
+def _join_stage(*args):
+    return args
--- foundations_production/src/test/serving/test_create_retraining_job.py
+++ foundations_production/src/test/serving/test_create_retraining_job.py
@@ -11,6 +11,9 @@ from foundations_production.serving import create_retraining_job
 
 class TestCreateRetrainingJob(Spec):
     
+    mock_data_from_file = let_patch_mock('foundations_production.serving.data_from_file.data_from_file')
+    mock_load_model_package = let_patch_mock('foundations_production.load_model_package')
+
     @let
     def job_id(self):
         return self.faker.uuid4()
@@ -23,23 +26,22 @@ class TestCreateRetrainingJob(Spec):
     def fake_targets_path(self):
         return self.faker.file_path()
 
+    @set_up
+    def set_up(self):
+        self._retraining_job = create_retraining_job(self.job_id, features_location=self.fake_features_path, targets_location=self.fake_targets_path)
+
     def test_retraining_job_loads_features(self):
         self._test_file_loaded(self.fake_features_path)
-
-    def test_retraining_job_loads_files_only_when_job_executed(self):
-        mock_data_from_file = self.patch('foundations_production.serving.data_from_file.data_from_file', Mock())
-
-        job = create_retraining_job(self.job_id, features_location=self.fake_features_path, targets_location=self.fake_targets_path)
-
-        mock_data_from_file.assert_not_called()
 
     def test_retraining_job_loads_targets(self):
         self._test_file_loaded(self.fake_targets_path)
 
+    def test_retraining_job_loads_files_only_when_job_executed(self):
+        self.mock_data_from_file.assert_not_called()
+
+    def test_retraining_job_loads_model_package(self):
+        self.mock_load_model_package.assert_called_with(self.job_id)
+
     def _test_file_loaded(self, file_name):
-        mock_data_from_file = self.patch('foundations_production.serving.data_from_file.data_from_file', Mock())
-
-        job = create_retraining_job(self.job_id, features_location=self.fake_features_path, targets_location=self.fake_targets_path)
-        job.run_same_process()
-
-        mock_data_from_file.assert_any_call(file_name)
+        self._retraining_job.run_same_process()
+        self.mock_data_from_file.assert_any_call(file_name)
