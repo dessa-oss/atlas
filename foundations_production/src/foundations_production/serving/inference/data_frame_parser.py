ab57d2e6fbbde52c0fa220cfd07ea60629d82b32
foundations-team <foundations-team@dessa.com>
2019-05-09 13:57:37 -0400

    [TR] Refactor

--- foundations_production/src/foundations_production/serving/inference/data_frame_parser.py
+++ foundations_production/src/foundations_production/serving/inference/data_frame_parser.py
@@ -11,6 +11,8 @@ class DataFrameParser(object):
     def data_frame_for(self, input):
         from pandas import DataFrame
 
-        columns = [field['name'] for field in input['schema']]
-
+        columns = self._data_frame_columns(input)
         return DataFrame(input['rows'], columns=columns)
+
+    def _data_frame_columns(self, input):
+        return [field['name'] for field in input['schema']]
