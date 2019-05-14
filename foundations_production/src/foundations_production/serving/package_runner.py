092e688fecce991af856d7e0f5ddbaf645dfb39e
pairing <developers@dessa.com>
2019-05-14 11:44:56 -0400

    [KB NN] refactor

--- foundations_production/src/foundations_production/serving/package_runner.py
+++ foundations_production/src/foundations_production/serving/package_runner.py
@@ -6,26 +6,14 @@ Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 """
 
 def run_model_package(model_package_id, communicator):
-    from foundations_production.serving.inference.predictor import Predictor
-    import sys
-    
-    try:
-        predictor = Predictor.predictor_for(model_package_id)
-        communicator.set_response('SUCCESS: predictor created')
-    except Exception as e:
-        _send_exception(e, communicator)
+    predictor = _create_predictor_for(model_package_id, communicator)
+    if not predictor:
         return
 
     while True:
         json_input_data = communicator.get_action_request()
-        if json_input_data == 'STOP':
-            return
-        try:
-            json_predictions = predictor.json_predictions_for(json_input_data)
-            communicator.set_response(json_predictions)
-        except Exception as e:
-            _send_exception(e, communicator)
-            return
+        if not _successful_run_of_json_predictions_for(model_package_id, communicator, predictor, json_input_data):
+            return 
 
 def _send_exception(exception, communicator):
     expected_return = {
@@ -33,3 +21,26 @@ def _send_exception(exception, communicator):
         'value': str(exception)
     }
     communicator.set_response(expected_return)
+
+def _create_predictor_for(model_package_id, communicator):
+    from foundations_production.serving.inference.predictor import Predictor
+    
+    try:
+        predictor = Predictor.predictor_for(model_package_id)
+        communicator.set_response('SUCCESS: predictor created')
+    except Exception as e:
+        _send_exception(e, communicator)
+        predictor =  None
+
+    return predictor
+
+def _successful_run_of_json_predictions_for(model_package_id, communicator, predictor, json_input_data):
+    if json_input_data == 'STOP':
+        return False
+    try:
+        json_predictions = predictor.json_predictions_for(json_input_data)
+        communicator.set_response(json_predictions)
+        return True
+    except Exception as e:
+        _send_exception(e, communicator)
+        return False
