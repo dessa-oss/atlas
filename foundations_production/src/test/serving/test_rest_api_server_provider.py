efb077a3ed6361ba3c3aab9f61258cdbe8a3eb2d
Katherine <k.bancroft@dessa.com>
2019-05-15 11:48:32 -0400

    [NN DP PL] add tear down for rest api test; check that queue get api resource

--- foundations_production/src/test/serving/test_rest_api_server_provider.py
+++ foundations_production/src/test/serving/test_rest_api_server_provider.py
@@ -4,29 +4,45 @@ Unauthorized copying, distribution, reproduction, publication, use of this file,
 Proprietary and confidential
 Written by Susan Davis <s.davis@dessa.com>, 04 2019
 """
-import unittest
-from mock import patch, Mock
 
-from foundations_spec.helpers.spec import Spec
-from foundations_spec.helpers import let, let_patch_mock
+from foundations_spec import *
+from foundations_production.serving.rest_api_server_provider import get_rest_api_server, register_rest_api_server
 
 class TestRestAPIServerProvider(Spec):
 
-    def test_get_rest_api_server_returns_placeholder_if_server_is_not_running(self):
-        from foundations_production.serving.rest_api_server_provider import get_rest_api_server, register_rest_api_server
-        from foundations_production.serving.rest_api_server_provider import _RestAPIServerProvider
+    mock_rest_api_server_class = let_patch_mock('foundations_production.serving.rest_api_server.RestAPIServer')
 
+    @tear_down
+    def tear_down(self):
         register_rest_api_server(None)
+
+    @let
+    def fake_path(self):
+        return self.faker.uri_path()
+
+    def test_get_rest_api_server_returns_placeholder_as_detault_if_server_is_not_running(self):
+        from foundations_production.serving.rest_api_server_provider import _RestAPIServerProvider
 
         rest_api_server_like_object = get_rest_api_server()
         self.assertIsInstance(rest_api_server_like_object, _RestAPIServerProvider)
 
     def test_get_rest_api_server_returns_the_server_if_server_is_running(self):
-        from foundations_production.serving.rest_api_server_provider import get_rest_api_server, register_rest_api_server
-        from foundations_production.serving.rest_api_server import RestAPIServer
-
-        rest_api_server_instance = RestAPIServer()
-        register_rest_api_server(rest_api_server_instance)
+        mock_rest_api_server_instance = Mock()
+        self.mock_rest_api_server_class.return_value = mock_rest_api_server_instance
+        
+        register_rest_api_server(mock_rest_api_server_instance)
 
         rest_api_server_like_object = get_rest_api_server()
-        self.assertEqual(rest_api_server_like_object, rest_api_server_instance)
+        self.assertEqual(rest_api_server_like_object, mock_rest_api_server_instance)
+
+    def test_if_add_resource_called_and_server_not_running_route_and_resource_are_added_to_queue(self):
+        mock_rest_api_server_instance = Mock() 
+        mock_resource = Mock()
+        self.mock_rest_api_server_class.return_value = mock_rest_api_server_instance
+
+        rest_api_server_provider = get_rest_api_server()
+        rest_api_server_provider.api().add_resource(mock_resource, self.fake_path)
+
+        register_rest_api_server(mock_rest_api_server_instance)
+
+        mock_rest_api_server_instance.api().add_resource.assert_called_with(mock_resource, self.fake_path)
