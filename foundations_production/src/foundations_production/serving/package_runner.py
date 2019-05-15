6d31646e0f885516ceef4b337e74d7e9fbe0583e
Thomas Rogers <t.rogers@dessa.com>
2019-05-15 11:39:36 -0400

    [CZ KB JA] create predictor after chdir

--- foundations_production/src/foundations_production/serving/package_runner.py
+++ foundations_production/src/foundations_production/serving/package_runner.py
@@ -9,9 +9,9 @@ def run_model_package(model_package_id, communicator):
     import os
     from foundations_production.serving import create_job_workspace
 
-    predictor = _create_predictor_for(model_package_id, communicator)
     create_job_workspace(model_package_id)
     os.chdir('/tmp/foundations_workspaces/{}'.format(model_package_id))
+    predictor = _create_predictor_for(model_package_id, communicator)
 
     if not predictor:
         return
