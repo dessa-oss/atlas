dbfa766478d15ed1255358cc85020d1e38b60358
Thomas Rogers <t.rogers@dessa.com>
2019-05-15 12:03:04 -0400

    [CZ KB JA] create workspace before fetching job source

--- foundations_production/src/test/serving/test_create_job_workspace.py
+++ foundations_production/src/test/serving/test_create_job_workspace.py
@@ -17,6 +17,8 @@ class TestCreateJobWorkspace(Spec):
     mock_job_source_bundle_class = let_patch_mock('foundations_contrib.job_source_bundle.JobSourceBundle', ConditionalReturn())
     mock_job_source_bundle = let_mock()
 
+    mock_os_makedirs = let_patch_mock('os.makedirs')
+
     @let
     def job_id(self):
         return self.faker.uuid4()
@@ -27,13 +29,19 @@ class TestCreateJobWorkspace(Spec):
 
     @set_up
     def set_up(self):
+        self._workspace_dir_created = False
         self._fetch_job_source_called = False
         
         self.mock_get_pipeline_archiver_for_job.return_when(self.mock_pipeline_archiver, self.job_id)
         self.mock_job_source_bundle_class.return_when(self.mock_job_source_bundle, self.job_id, self.workspace_path)
 
-        self.mock_pipeline_archiver.fetch_job_source.side_effect = self._set_fetch_job_source_called
+        self.mock_os_makedirs.side_effect = self._set_workspace_dir_created
+        self.mock_pipeline_archiver.fetch_job_source.side_effect = self._check_workspace_dir_created_and_set_fetch_job_source_called
         self.mock_job_source_bundle.unbundle.side_effect = self._check_fetch_job_source_called
+
+    def test_job_workspace_directory_created(self):
+        create_job_workspace(self.job_id)
+        self.mock_os_makedirs.assert_called_with(self.workspace_path, exist_ok=True)
 
     def test_create_job_workspace_fetches_job_source_bundle_to_workspace_directory(self):
         create_job_workspace(self.job_id)
@@ -44,7 +52,13 @@ class TestCreateJobWorkspace(Spec):
         expected_calls = [call.unbundle(self.workspace_path), call.cleanup()]
         self.assertEqual(expected_calls, self.mock_job_source_bundle.method_calls)
     
-    def _set_fetch_job_source_called(self, *args):
+    def _set_workspace_dir_created(self, *args, **kwargs):
+        self._workspace_dir_created = True
+
+    def _check_workspace_dir_created_and_set_fetch_job_source_called(self, *args):
+        if not self._workspace_dir_created:
+            raise AssertionError('workspace directory should be created before fetching job source')
+        
         self._fetch_job_source_called = True
     
     def _check_fetch_job_source_called(self, *args):
