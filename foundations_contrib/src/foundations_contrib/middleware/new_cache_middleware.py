d315e8e48ba37fd03732866bac5df17bbcc63133
Thomas Rogers <thomasrogers@Macs-MacBook-Air.local>
2019-05-17 10:09:56 -0400

    [TR] Don't do anything related to caching if caching is disabled

--- foundations_contrib/src/foundations_contrib/middleware/new_cache_middleware.py
+++ foundations_contrib/src/foundations_contrib/middleware/new_cache_middleware.py
@@ -16,17 +16,19 @@ class NewCacheMiddleware(BasicStageMiddleware):
             pipeline_context, stage_config, stage_context, stage)
 
     def call(self, upstream_result_callback, filler_builder, filler_kwargs, args, kwargs, callback):
-        stage_cache = self._cache_implementation(
-            self._pipeline_context, self._stage, self._stage_config, args)
-        self._stage_context.cache_uuid = stage_cache.cache_name()
-
         if self._stage_config.allow_caching:
+            stage_cache = self._cache_implementation(
+                self._pipeline_context, self._stage, self._stage_config, args)
+            self._stage_context.cache_uuid = stage_cache.cache_name()
+
             cached_value = stage_cache.fetch_option()
             if cached_value.is_present():
                 self._stage_context.used_cache = True
                 return cached_value.get()
 
-        result = callback(args, kwargs)
-        stage_cache.submit(result)
+            result = callback(args, kwargs)
+            stage_cache.submit(result)
 
-        return result
+            return result
+        else:
+            return callback(args, kwargs)
