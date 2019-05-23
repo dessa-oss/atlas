7be5efd5f4179c896969f97dd4d093abc2882d2b
Katherine <k.bancroft@dessa.com>
2019-05-23 16:11:31 -0400

    [DP CZ] add tests to ensure unspecified endpoint creates LocalFileSystemBucket using artifact path

--- foundations_internal/src/foundations_internal/testing/shared_examples/test_bucket_from_scheme.py
+++ foundations_internal/src/foundations_internal/testing/shared_examples/test_bucket_from_scheme.py
@@ -187,3 +187,18 @@ class TestBucketFromScheme(object):
         result_config = self.translator.translate(self._configuration)
         config = result_config['artifact_end_point_implementation']
         self.assertEqual([GCPBucket, os.path.join(self.fake_bucket_path, self.fake_relative_path)], config['constructor_arguments'])
+
+    def test_no_result_artifact_returns_constructor_arguments_with_default_artifact_path(self):
+        from foundations_contrib.local_file_system_bucket import LocalFileSystemBucket
+
+        result_config = self.translator.translate(self._configuration)
+        config = result_config['artifact_end_point_implementation']
+        self.assertEqual([LocalFileSystemBucket, 'results'], config['constructor_arguments'])
+
+    def test_artifact_end_point_returns_constructor_arguments_with_gcp_scheme(self):
+        from foundations_contrib.local_file_system_bucket import LocalFileSystemBucket
+        
+        self._configuration['results_config']['artifact_path'] = self.fake_relative_path
+        result_config = self.translator.translate(self._configuration)
+        config = result_config['artifact_end_point_implementation']
+        self.assertEqual([LocalFileSystemBucket, self.fake_relative_path], config['constructor_arguments'])
