8245cafe336b5152e9088a30a820dd423e50855a
pairing <developers@dessa.com>
2019-05-30 16:15:56 -0400

    [TR CZ] refactor

--- foundations_contrib/src/foundations_contrib/archiving/artifact_downloader.py
+++ foundations_contrib/src/foundations_contrib/archiving/artifact_downloader.py
@@ -11,13 +11,14 @@ class ArtifactDownloader(object):
         self._archiver = archiver
     
     def download_files(self, source_directory, download_directory):
+        for file_path in self._artifact_file_list(source_directory):
+            self._download_file(file_path, download_directory)
+
+    def _artifact_file_list(self, source_directory):
         from fnmatch import fnmatch
 
         file_list = self._archiver.fetch_miscellaneous('job_artifact_listing.pkl')
-        filtered_file_list = [file_path for file_path in file_list if fnmatch(file_path, source_directory + '*')]
-
-        for file_path in filtered_file_list:
-            self._download_file(file_path, download_directory)
+        return [file_path for file_path in file_list if fnmatch(file_path, source_directory + '*')]
 
     def _download_file(self, file_path, download_directory):
         from os import makedirs
