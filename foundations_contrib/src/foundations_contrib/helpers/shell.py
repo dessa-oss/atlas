a53ba02846416f22312cb11fb922d39b0e00fd78
Thomas Rogers <thomasrogers@Macs-MacBook-Air.local>
2019-06-05 14:49:10 -0400

    [TR] Use bash.exe location from /bin instead of the git-bash.exe

--- foundations_contrib/src/foundations_contrib/helpers/shell.py
+++ foundations_contrib/src/foundations_contrib/helpers/shell.py
@@ -17,12 +17,16 @@ def _find_windows_bash():
     winreg = _winreg_module()
     import csv
     StringIO = _get_string_io()
+    from os.path import dirname
 
     sub_key = 'Directory\\shell\\git_shell\\command'
     value = winreg.QueryValue(winreg.HKEY_CLASSES_ROOT, sub_key)
     with StringIO(value) as file:
         reader = csv.reader(file, delimiter=' ', quotechar='"')
-        return list(reader)[0][0]
+        git_bash_location = list(reader)[0][0]
+        git_bash_directory = git_bash_location.split('\\git-bash.exe')[0]
+        bash_location = git_bash_directory + '\\bin\\bash.exe'
+        return bash_location
     
 def _get_string_io():
     try:
--- foundations_contrib/src/test/helpers/test_shell.py
+++ foundations_contrib/src/test/helpers/test_shell.py
@@ -34,12 +34,12 @@ class TestShell(Spec):
 
     @patch('os.name', 'nt')
     def test_find_bash_returns_bash_on_windows(self):
-        self.winreg.QueryValue.return_value = '"C:\\path to\\bash" 0 0'
-        self.assertEqual('C:\\path to\\bash', shell.find_bash())
+        self.winreg.QueryValue.return_value = '"C:\\path to\\git-bash.exe" 0 0'
+        self.assertEqual('C:\\path to\\bin\\bash.exe', shell.find_bash())
 
     @patch('os.name', 'nt')
     def test_find_bash_returns_bash_on_windows_different_path(self):
-        self.winreg.QueryValue.return_value = '"C:\\Program Files\\git\\bin\\bash" 3 2'
-        self.assertEqual('C:\\Program Files\\git\\bin\\bash', shell.find_bash())
+        self.winreg.QueryValue.return_value = '"C:\\Program Files\\git\\git-bash.exe" 3 2'
+        self.assertEqual('C:\\Program Files\\git\\bin\\bash.exe', shell.find_bash())
 
 
