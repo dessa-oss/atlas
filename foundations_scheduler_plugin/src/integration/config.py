2ec5a3d2cc99aa983f05e19c20bd9d5eb124c605
pairing <developers@dessa.com>
2019-06-14 11:50:29 -0400

    [NN JA] proper config for this integration test

--- foundations_scheduler_plugin/src/integration/config.py
+++ foundations_scheduler_plugin/src/integration/config.py
@@ -4,10 +4,19 @@ def _load_config():
     import os
     from foundations_contrib.global_state import config_manager
     
-    config_manager['remote_host'] = 'localhost'
+    running_on_ci = os.environ.get('RUNNING_ON_CI', 'FALSE') == 'TRUE'
+
+    if running_on_ci:
+        scheduler_host = os.environ['FOUNDATIONS_SCHEDULER_HOST']
+        redis_url = os.environ['FOUNDATIONS_SCHEDULER_ACCEPTANCE_REDIS_URL']
+    else:
+        scheduler_host = 'localhost'
+        redis_url = 'redis://{}:6379'.format(get_network_address('docker0'))
+
+    config_manager['remote_host'] = scheduler_host
     config_manager['remote_user'] = 'job-uploader'
     config_manager['port'] = 31222
     config_manager['code_path'] = '/jobs'
-    config_manager['redis_url'] = 'redis://{}:6379'.format(get_network_address('docker0'))
+    config_manager['redis_url'] = redis_url
 
 _load_config()
