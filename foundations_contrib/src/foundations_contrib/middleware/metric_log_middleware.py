d090ccccd1bfcd94012af27d18d553df12950af2
Katherine <k.bancroft@dessa.com>
2019-06-19 13:59:57 -0400

    [CZ JA] change interface to match other producers

--- foundations_contrib/src/foundations_contrib/middleware/metric_log_middleware.py
+++ foundations_contrib/src/foundations_contrib/middleware/metric_log_middleware.py
@@ -10,18 +10,20 @@ from foundations_contrib.middleware.basic_stage_middleware import BasicStageMidd
 
 class MetricLogMiddleware(BasicStageMiddleware):
 
-    class MetricsLogger(object):
+    class MetricLogged(object):
 
-        def __init__(self, message_router, project_name, job_id):
+        def __init__(self, message_router, project_name, job_id, key, value):
             self._message_router = message_router
             self._project_name = project_name
             self._job_id = job_id
+            self._metric_key = key
+            self._metric_value = value
 
-        def push_metric(self, key, value):
+        def push_message(self):
             message = {
                 'project_name': self._project_name, 
                 'job_id': self._job_id, 
-                'key': key, 'value': value
+                'key': self._metric_key, 'value': self._metric_value
             }
 
             self._message_router.push_message('job_metrics', message)
@@ -38,8 +40,8 @@ class MetricLogMiddleware(BasicStageMiddleware):
                 self._push_message_to_channel(metric['key'], metric['value'])
 
         def _push_message_to_channel(self, key, value):
-            metrics_logger = MetricLogMiddleware.MetricsLogger(self._message_router, self._project_name(), self._job_id())
-            metrics_logger.push_metric(key, value)
+            metrics_logged_producer = MetricLogMiddleware.MetricLogged(self._message_router, self._project_name(), self._job_id(), key, value)
+            metrics_logged_producer.push_message()
 
         def _project_name(self):
             return self._pipeline_context.provenance.project_name
