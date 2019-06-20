5debdf4a192e5885ce066bac303ec6b798c88c8d
pairing <developers@dessa.com>
2019-06-20 15:00:04 -0400

    [TR NN] Skip not yet ready acceptance tests

---
+++ foundations_sdk/src/test/staging/test_cache.py
@@ -0,0 +1,20 @@
+"""
+Copyright (C) DeepLearning Financial Technologies Inc. - All Rights Reserved
+Unauthorized copying, distribution, reproduction, publication, use of this file, via any medium is strictly prohibited
+Proprietary and confidential
+Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
+"""
+
+from foundations_spec import *
+from foundations.staging import cache
+
+class TestCache(Spec):
+    
+    mock_cached_stage_result = let_patch_mock('foundations.staging.cached_stage_result.cached_stage_result')
+    mock_foundations_stage = let_patch_mock('foundations.staging.cached_stage_result.foundations_stage')
+
+    mock_foundations_stage = let_mock()
+
+    @set_up
+    def set_up(self):
+        
