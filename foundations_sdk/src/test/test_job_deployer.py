00d673c30a80e1ba6ba563aeec96d69314ae3e4e
Katherine <k.bancroft@dessa.com>
2019-06-24 15:24:31 -0400

    [CZ JA PL] make sure job deployer deploys job

--- foundations_sdk/src/test/test_job_deployer.py
+++ foundations_sdk/src/test/test_job_deployer.py
@@ -11,13 +11,22 @@ from foundations.job_deployer import deploy_job
 class TestJobDeployer(Spec):
 
     mock_log_manager = let_patch_mock('foundations.log_manager')
+    mock_deployment_manager = let_patch_mock('foundations.global_state.deployment_manager')
+
     mock_logger = Mock()
+    mock_deployment = Mock()
 
     @set_up
     def set_up(self):
         mock_get_logger = ConditionalReturn()
         mock_get_logger.return_when(self.mock_logger, 'foundations.job_deployer')
         self.mock_log_manager.get_logger = mock_get_logger
+
+        mock_simple_deploy = ConditionalReturn()
+        mock_simple_deploy.return_when(self.mock_deployment, self.fake_pipeline_context_wrapper, self.fake_job_name, self.fake_job_params)
+        self.mock_deployment_manager.simple_deploy = mock_simple_deploy
+
+        self.mock_deployment.job_name.return_value = self.fake_job_name
 
     @let
     def fake_job_name(self):
@@ -34,3 +43,7 @@ class TestJobDeployer(Spec):
     def test_job_deployer_logs_job_deploying_message(self):
         deploy_job(self.fake_pipeline_context_wrapper, self.fake_job_name, self.fake_job_params)
         self.mock_logger.info.assert_called_with('Deploying job...')
+    
+    def test_job_deployer_returns_deployment_wrapper_with_same_job_name_as_was_passed_in(self):
+        job_deployment = deploy_job(self.fake_pipeline_context_wrapper, self.fake_job_name, self.fake_job_params)
+        self.assertEqual(self.fake_job_name, job_deployment.job_name())
