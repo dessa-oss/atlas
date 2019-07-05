8a8c54aa12cc44ffcbdb8995fc56ec0bc9d24193
Katherine <k.bancroft@dessa.com>
2019-07-05 09:25:41 -0400

    [JA] handle params stuff for jobs_deployed_with_foundations

--- testing/acceptance/fixtures/deployable_script_parameters_empty_params/project_code/script_to_run.py
+++ testing/acceptance/fixtures/deployable_script_parameters_empty_params/project_code/script_to_run.py
@@ -8,5 +8,8 @@ Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 import foundations
 import json
 
+from foundations_contrib.global_state import current_foundations_context
+
 params = foundations.load_parameters()
+print(current_foundations_context().job_id())
 print(json.dumps(params))
--- testing/acceptance/test_can_load_parameters.py
+++ testing/acceptance/test_can_load_parameters.py
@@ -7,7 +7,6 @@ Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 
 from foundations_spec import *
 
-@skip
 class TestCanLoadParameters(Spec):
 
     @let
@@ -40,12 +39,14 @@ class TestCanLoadParameters(Spec):
     def deployable_script_directory_empty_params(self):
         return 'acceptance/fixtures/deployable_script_parameters_empty_params'
 
+    @skip
     def test_can_load_parameters_within_python(self):
         self._test_can_load_parameters_within_python(self.script_directory, self.job_parameters)
 
     def test_can_load_parameters_within_foundations_deploy(self):
         self._test_can_load_parameters_within_foundations_deploy(self.deployable_script_directory, self.job_parameters)
 
+    @skip
     def test_can_load_parameters_as_empty_dict_within_python_empty_params(self):
         self._test_can_load_parameters_within_python(self.script_directory_empty_params, {})
 
@@ -55,6 +56,7 @@ class TestCanLoadParameters(Spec):
     def test_can_load_default_parameters_within_foundations_deploy_when_parameters_json_not_found(self):
         self._test_can_load_parameters_within_foundations_deploy(self.deployable_script_directory_no_parameters, {})
 
+    @skip
     def test_can_load_default_parameters_within_python_when_parameters_json_not_found(self):
         self._test_can_load_parameters_within_python(self.script_directory_no_parameters, {})
 
@@ -73,18 +75,21 @@ class TestCanLoadParameters(Spec):
 
         with ChangeDirectory(script_directory):
             completed_process = subprocess.run(command, stdout=subprocess.PIPE)
-            process_output = completed_process.stdout
+            process_output = completed_process.stdout.decode()
 
-        result_parameters = json.loads(process_output)
+        job_id, params_json = process_output.split('\n', 1)
+
+        result_parameters = json.loads(params_json)
         self.assertEqual(expected_loaded_parameters, result_parameters)
         self._assert_flattened_parameter_keys_in_project_job_parameter_names_set(path.basename(script_directory), expected_loaded_parameters)
+        self._assert_flattened_parameter_values_for_job_in_job_parameters(job_id, expected_loaded_parameters)
 
     def _assert_flattened_parameter_keys_in_project_job_parameter_names_set(self, project_name, expected_loaded_parameters):
         from foundations.job_parameters import flatten_parameter_dictionary
         from foundations_contrib.global_state import redis_connection
 
         flattened_parameters = flatten_parameter_dictionary(expected_loaded_parameters)
-        parameter_names = set(flattened_parameters)
+        parameter_names = set(map(lambda param_name: bytes(param_name, 'ascii'), flattened_parameters))
         logged_parameter_names = redis_connection.smembers('projects:{}:job_parameter_names'.format(project_name))
         self.assertEqual(parameter_names, logged_parameter_names)
 
