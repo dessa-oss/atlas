462c35ae57c63e51f3f951f136f0fbca9e28bca7
pairing <developers@dessa.com>
2019-07-08 14:17:32 -0400

    [TR AM] Remove the service when removing the model deployment

--- foundations_contrib/src/foundations_contrib/resources/model_serving/remove_deployment.sh
+++ foundations_contrib/src/foundations_contrib/resources/model_serving/remove_deployment.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-kubectl delete deployment -n foundations-scheduler-test foundations-model-package-deployment-$1
+kubectl delete deployment -n foundations-scheduler-test foundations-model-package-deployment-$1 && \
+    kubectl delete svc -n foundations-scheduler-test foundations-model-package-deployment-$1
