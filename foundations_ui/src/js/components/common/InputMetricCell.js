9f70567b2f98a35ba72aa00325468941ad5a0ba7
pippinlee <>
2019-07-08 17:30:28 -0400

    [PL] hover tooltop for metrics and params

--- foundations_ui/src/js/components/common/InputMetricCell.js
+++ foundations_ui/src/js/components/common/InputMetricCell.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import CommonActions from '../../actions/CommonActions';
+import HoverCell from '../JobListPage/cells/HoverCell';
 
 class InputMetricCell extends Component {
   constructor(props) {
     super(props);
+    this.toggleExpand = this.toggleExpand.bind(this);
     this.state = {
       value: this.props.value,
       isError: this.props.isError,
@@ -13,21 +15,40 @@ class InputMetricCell extends Component {
     };
   }
 
+  toggleExpand(value) {
+    this.setState({ expand: value });
+  }
+
   componentWillReceiveProps(nextProps) {
     this.setState({ value: nextProps.value });
   }
 
   render() {
     const {
-      value, isError, cellType, rowNumber,
+      value, isError, cellType, rowNumber, expand,
     } = this.state;
 
     const pClass = CommonActions.getInputMetricCellPClass(isError, cellType);
     const divClass = CommonActions.getInputMetricCellDivClass(isError, rowNumber);
 
+    let hover;
+
+    if (expand) {
+      hover = <HoverCell textToRender={value} />;
+    }
+
     return (
       <div className={divClass}>
-        <p className={pClass}>{value}</p>
+        <p
+          className={pClass}
+          onMouseEnter={() => this.toggleExpand(true)}
+          onMouseLeave={() => this.toggleExpand(false)}
+        >
+          {value}
+        </p>
+        <div>
+          {hover}
+        </div>
       </div>
     );
   }
