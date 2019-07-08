f6d4cd7be48b36233b7c6f827bdbda66a57b8980
pairing <developers@dessa.com>
2019-07-08 11:05:06 -0400

    [TR AM] Created script to deploy model server

--- foundations_contrib/src/foundations_contrib/resources/model_serving/depoy_serving.sh
+++ foundations_contrib/src/foundations_contrib/resources/model_serving/depoy_serving.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+export model_number=$(head /dev/urandom | LC_CTYPE=C tr -dc 0-9 | head -c 4 ; echo '')
+export job_id=$1
+export model_name=model-$model_number
 
+cat kubernetes-deployment.envsubst.yaml | envsubst | kubectl create -f -
