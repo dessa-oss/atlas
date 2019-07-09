6debfb0f00dd67cc1a3b94ed04594931dc3c2977
Thomas Rogers <thomasrogers@Macs-MacBook-Air.local>
2019-07-09 10:03:20 -0400

    [TR AM] Refactor api script

--- docker/model_package/api.py
+++ docker/model_package/api.py
@@ -6,19 +6,33 @@ app = Flask(__name__)
 CORS(app, supports_credentials=True)
 api = Api(app)
 
-def load_prediction_function():
+def job_root():
     from os import environ
-    import importlib
-    import yaml
-    import sys
 
     job_id = environ['JOB_ID']
-    job_root = '/archive/archive/{}/artifacts'.format(job_id)
-    sys.path.insert(0, job_root)
-    with open('{}/foundations_package_manifest.yaml'.format(job_root), 'r') as manifest_file:
-        manifest = yaml.load(manifest_file.read())
-    module_name = manifest['entrypoints']['predict']['module']
-    function_name = manifest['entrypoints']['predict']['function']
+    return '/archive/archive/{}/artifacts'.format(job_id)
+
+def job_manifest():
+    import yaml
+
+    with open(f'{job_root()}/foundations_package_manifest.yaml', 'r') as manifest_file:
+        return yaml.load(manifest_file.read())
+
+def module_name_and_function_name():
+    manifest = job_manifest()
+    prediction_definition = manifest['entrypoints']['predict']
+
+    return prediction_definition['module'], prediction_definition['function']
+
+def add_job_path_to_sys_path():
+    import sys
+    sys.path.insert(0, job_root())
+
+def load_prediction_function():
+    import importlib
+    
+    add_job_path_to_sys_path()
+    module_name, function_name = module_name_and_function_name()
 
     module = importlib.import_module(module_name)
     return getattr(module, function_name)
