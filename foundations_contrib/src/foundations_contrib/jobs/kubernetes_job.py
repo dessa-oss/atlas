b8a11793b52a926eb025146893bf59344839578f
pairing <developers@dessa.com>
2019-07-10 14:54:28 -0400

    [TR AM] Remove capture_output

--- foundations_contrib/src/foundations_contrib/jobs/kubernetes_job.py
+++ foundations_contrib/src/foundations_contrib/jobs/kubernetes_job.py
@@ -10,6 +10,6 @@ def cancel(job_id):
     import foundations_contrib
     from foundations_contrib.global_state import redis_connection
 
-    subprocess.run(['bash', './delete_job.sh', job_id], cwd=foundations_contrib.root() / 'resources/jobs', capture_output=True)
+    subprocess.run(['bash', './delete_job.sh', job_id], cwd=foundations_contrib.root() / 'resources/jobs')
     project_name = redis_connection.get(f'jobs:{job_id}:project').decode()
     redis_connection.srem(f'project:{project_name}:jobs:running', job_id)
