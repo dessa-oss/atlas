3ce2d69033cd750e5bcfdd9c641af8d21bf17e39
pairing <developers@dessa.com>
2019-07-11 11:34:56 -0400

    [AM] Committed deploy_serving.sh by mistake. Reverting change.

--- foundations_contrib/src/foundations_contrib/resources/model_serving/deploy_serving.sh
+++ foundations_contrib/src/foundations_contrib/resources/model_serving/deploy_serving.sh
@@ -4,18 +4,4 @@ export job_id=$1
 export model_name=model-$model_number
 
 cat kubernetes-deployment.envsubst.yaml | envsubst | kubectl create -f -
-echo Model $model_name serving, stuff cole says
-
-sleep 15
-model_pod=$(kubectl get po -n foundations-scheduler-test | grep $model_name | awk '{print $1}')
-
-model_status () {
-    return $(kubectl get po -n foundations-scheduler-test | grep $model_name | awk '{print $3}')
-}
-
-while [ "Running" != model_status ]
-do 
-    sleep 2
-done
-
-kubectl logs -f -n foundations-scheduler-test $model_pod
+echo Model $model_name successfully served
