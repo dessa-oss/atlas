967537f122e8331df495928e5f6024635f5b45b1
Thomas Rogers <thomasrogers@Macs-MacBook-Air.local>
2019-07-11 18:48:53 -0400

    [TR] Make module upload command support osx

--- devops/load_nexus_credentials.sh
+++ devops/load_nexus_credentials.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export NEXUS_PASSWORD=$(kubectl get secret -o yaml -n ci-pipeline jenkins-user-password | grep password: | awk '{print $2}' | base64 -d)
+export NEXUS_PASSWORD=$(kubectl get secret -o yaml -n ci-pipeline jenkins-user-password | grep password: | awk '{print $2}' | base64 --decode)
 export NEXUS_URL=https://nexus.shehanigans.net/repository/dessa-pypi/
 export NEXUS_USER=jenkins-user
 export NEXUS_DOCKER_REGISTRY=docker.shehanigans.net
