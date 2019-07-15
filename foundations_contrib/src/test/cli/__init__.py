f567604f37e553d45dfeee8f9c3dab3d47d1dcba
Katherine <k.bancroft@dessa.com>
2019-07-15 10:36:07 -0400

    [CZ JA] create command line job deployer utility in anticipation of refactoring cli

--- foundations_contrib/src/test/cli/__init__.py
+++ foundations_contrib/src/test/cli/__init__.py
@@ -13,3 +13,4 @@ from test.cli.test_command_line_interface import TestCommandLineInterface
 from test.cli.test_environment_fetcher import TestEnvironmentFetcher
 from test.cli.test_command_line_interface_deploy import TestCommandLineInterfaceDeploy
 from test.cli.test_model_package_server import TestModelPackageServer
+from test.cli.test_command_line_job_deployer import TestCommandLineJobDeployer
