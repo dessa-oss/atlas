04203976cf9aabad364f0bf6678dffa709846276
Thomas Rogers <t.rogers@dessa.com>
2019-07-24 13:30:57 -0400

    [TR KD] refactor

--- foundations_contrib/src/foundations_contrib/models/artifact_listing.py
+++ foundations_contrib/src/foundations_contrib/models/artifact_listing.py
@@ -7,9 +7,14 @@ Written by Kyle De Freitas <k.defreitas@dessa.com>, 08 2019
 
 def artifact_listing_for_job(job_id, archive):
     raw_file_paths = archive.list_files('user_artifacts/*', job_id)
-    
-    artifacts = []
+    return list(_artifact_listing_for_paths(raw_file_paths, job_id))
+
+def _artifact_listing_for_paths(raw_file_paths, job_id):
     for file_path in raw_file_paths:
-        artifacts.append(file_path[len(job_id) + len('/user_artifacts/'):])
-    
-    return artifacts
+        yield _file_path_without_artifact_prefix(file_path, job_id)
+
+def _file_path_without_artifact_prefix(file_path, job_id):
+    return file_path[_file_prefix_length(job_id):]
+
+def _file_prefix_length(job_id):
+    return len(job_id) + len('/user_artifacts/')
