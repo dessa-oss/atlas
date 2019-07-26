3155b11400fda6572aa1159f86143144cf9d4b12
amackillop <austin.mackillop@gmail.com>
2019-07-26 15:37:18 -0400

    [AM PL] Adding some files post conflict resolution

---
+++ foundations_ui/src/js/components/JobListPage/job-sidebar/Artifact.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+import FileDownload from 'js-file-download';
+
+export default function Artifact(props) {
+  const { filename, uri } = props;
+  return (
+    <li>
+      <p>{filename}</p>
+      <DownloadButton className="download-button" uri={uri} />
+    </li>
+  );
+}
+
+Artifact.propTypes = {
+  filename: PropTypes.string,
+  uri: PropTypes.string,
+};
+
+Artifact.defaultProps = {
+  filename: 'Artifact: Missing `filename` prop.',
+  uri: 'Artifact: Missing `uri` prop.',
+};
+
+// import ReactAudioPlayer from 'react-audio-player';
+
+
+function DownloadButton(props) {
+  const { uri } = props;
+  const getImage = async () => {
+    const response = await axios.get(uri, { responseType: 'blob' });
+    console.log(response);
+    FileDownload(response.data, 'test-data.tgz');
+  };
+
+  const getImageLocally
+  return (
+    <button type="button" onClick={getImage}> Download File </button>
+  );
+}
+
+DownloadButton.propTypes = {
+  uri: PropTypes.string,
+};
+
+DownloadButton.defaultProps = {
+  uri: 'DownloadButton: Missing `uri` prop.',
+};
---
+++ foundations_ui/src/js/components/JobListPage/job-sidebar/ArtifactList.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Artifact from './Artifact';
+
+export default function ArtifactList(props) {
+  const { handleClick, artifacts } = props;
+  // const artifactsObj = {
+  //   artifact1: {
+  //     filename: 'archive1.jpg',
+  //     location: '/some/path/to/file1/',
+  //   },
+  //   artifact2: {
+  //     filename: 'archive2.wav',
+  //     location: '/some/path/to/file2/',
+  //   },
+  //   artifact3: {
+  //     filename: 'archive3.xml',
+  //     location: '/some/path/to/file3/',
+  //   },
+  //   artifact4: {
+  //     filename: 'archive4.xml',
+  //     location: '/some/path/to/file3/',
+  //   },
+  //   artifact5: {
+  //     filename: 'archive5.xml',
+  //     location: '/some/path/to/file3/',
+  //   },
+  //   artifact6: {
+  //     filename: 'archive6.xml',
+  //     location: '/some/path/to/file3/',
+  //   },
+  //   artifact7: {
+  //     filename: 'archive7.xml',
+  //     location: '/some/path/to/file3/',
+  //   },
+  //   artifact8: {
+  //     filename: 'archive8.xml',
+  //     location: '/some/path/to/file3/',
+  //   },
+  //   artifact9: {
+  //     filename: 'archive9.xml',
+  //     location: '/some/path/to/file3/',
+  //   },
+  //   artifact10: {
+  //     filename: 'archive10.xml',
+  //     location: '/some/path/to/file3/',
+  //   },
+  //   artifact11: {
+  //     filename: 'archive11.xml',
+  //     location: '/some/path/to/file3/',
+  //   },
+  //   artifact12: {
+  //     filename: 'archive12.xml',
+  //     location: '/some/path/to/file3/',
+  //   },
+  //   artifact13: {
+  //     filename: 'archive13.xml',
+  //     location: '/some/path/to/file3/',
+  //   },
+  // };
+  // const files = artifacts.map((artifact) => {
+  //   console.log(artifact.filename);
+  //   return artifact.filename;
+  // });
+  const artifactComps = artifacts.map((artifact) => {
+    return <Artifact key={artifact.fname} filename={artifact.filename} uri={artifact.uri} onClick={handleClick} />;
+  });
+
+  return (
+    <ul className="artifact-list">
+      {artifactComps}
+    </ul>
+  );
+}
+
+ArtifactList.propTypes = {
+  handleClick: PropTypes.func,
+  artifacts: PropTypes.array,
+};
+
+const defaultClick = () => alert('ArtifactList: Missing `handleClick` prop.');
+ArtifactList.defaultProps = {
+  handleClick: defaultClick,
+  artifacts: ['ArtifactList: Artifacts missing.'],
+};
