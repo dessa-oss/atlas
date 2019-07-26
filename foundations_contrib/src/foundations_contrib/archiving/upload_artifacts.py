cfb6d3dcb8137a3171d8d49506c4198c3de11b18
Thomas Rogers <t.rogers@dessa.com>
2019-07-26 11:03:19 -0400

    [TR NN] Refactor in preparation for upload

--- foundations_contrib/src/foundations_contrib/archiving/upload_artifacts.py
+++ foundations_contrib/src/foundations_contrib/archiving/upload_artifacts.py
@@ -12,7 +12,7 @@ def upload_artifacts(job_id):
     _upload_artifacts_to_archiver(pipeline_archiver)
 
 def _upload_artifacts_to_archiver(pipeline_archiver):
-    list_of_files_to_upload = _list_of_files_to_upload()
+    list_of_files_to_upload = list_of_files_to_upload_from_artifact_path(_artifact_path())
 
     _upload_file_listing(list_of_files_to_upload, pipeline_archiver)
     _upload_artifact_files_to_archiver(list_of_files_to_upload, pipeline_archiver)
@@ -21,7 +21,7 @@ def _upload_artifact_files_to_archiver(list_of_files_to_upload, pipeline_archive
     for file_name in list_of_files_to_upload:
         pipeline_archiver.append_persisted_file(_file_name_without_artifact_path(file_name), file_name)
 
-def _list_of_files_to_upload():
+def list_of_files_to_upload_from_artifact_path(artifact_path):
     from foundations_contrib.archiving.file_names_for_artifacts_path import file_names_for_artifacts_path
     from os import walk
 
