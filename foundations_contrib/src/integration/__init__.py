f9a9aeca424ee00eba3141db3b6029ae6f5fe7ef
Katherine <k.bancroft@dessa.com>
2019-08-08 16:28:33 -0400

    [JA] fixing slack integrations

--- foundations_contrib/src/integration/__init__.py
+++ foundations_contrib/src/integration/__init__.py
@@ -9,7 +9,7 @@ Written by Katherine Bancroft <k.bancroft@dessa.com>, 06 2018
 from foundations_contrib.global_state import config_manager
 
 config_manager['job_notification_channel'] = 'spamity'
-config_manager['job_notification_channel_id'] = 'CFNA44YRJ'
+config_manager['job_notification_channel_id'] = 'CM6U16G4D'
 
 import foundations
 
