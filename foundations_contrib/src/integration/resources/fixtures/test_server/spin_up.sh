46453d1243dbce45b31947877db2d51881970ac6
foundations-team <foundations-team@dessa.com>
2019-08-09 10:41:03 -0400

    [AM NN] Working test cluster with ingress

--- foundations_contrib/src/integration/resources/fixtures/test_server/spin_up.sh
+++ foundations_contrib/src/integration/resources/fixtures/test_server/spin_up.sh
@@ -2,5 +2,6 @@
 docker build -t test-server:latest integration/resources/fixtures/test_server/
 
 kubectl apply -f integration/resources/fixtures/test_server/ingress-controller-mandatory.yaml
-kubectl apply -f integration/resources/fixtures/test_server/ingress-controller.yaml
 kubectl apply -f integration/resources/fixtures/test_server/deployment.yaml
+kubectl apply -f integration/resources/fixtures/test_server/ingress-controller.yaml
+kubectl apply -f integration/resources/fixtures/test_server/ingress.yaml
--- foundations_contrib/src/integration/resources/fixtures/test_server/tear_down.sh
+++ foundations_contrib/src/integration/resources/fixtures/test_server/tear_down.sh
@@ -1,3 +1,4 @@
 kubectl delete -f integration/resources/fixtures/test_server/ingress-controller-mandatory.yaml
 kubectl delete -f integration/resources/fixtures/test_server/ingress-controller.yaml
+kubectl delete -f integration/resources/fixtures/test_server/ingress.yaml
 kubectl delete -f integration/resources/fixtures/test_server/deployment.yaml
