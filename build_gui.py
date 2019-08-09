5ced70e307bedda949d9ae62b84ef7ac5913fb4e
Katherine <k.bancroft@dessa.com>
2019-08-09 13:44:25 -0400

    [JA] refactor dockerfiles

--- build_gui.py
+++ build_gui.py
@@ -6,14 +6,17 @@ client = docker.from_env()
 build_version = os.environ['build_version']
 nexus_registry = os.environ['NEXUS_DOCKER_REGISTRY']
 
-def build_and_tag_gui_image(path, dockerfile, repository):
+def build_and_tag_gui_image(path, dockerfile, repository, buildargs=None):
+    if buildargs is None:
+        buildargs = {}
+
     try:
-        image, image_logs = client.images.build(path=path, dockerfile=dockerfile, tag='{}:{}'.format(repository, build_version))
+        image, image_logs = client.images.build(path=path, dockerfile=dockerfile, tag='{}:{}'.format(repository, build_version), buildargs=buildargs)
         image.tag(repository, tag='latest')
     finally:
         for line in image_logs:
             print(line)
 
-build_and_tag_gui_image('.', 'docker/rest_api_Dockerfile', '{}/foundations-rest-api'.format(nexus_registry))
-build_and_tag_gui_image('.', 'docker/orbit_rest_api_Dockerfile', '{}/foundations-orbit-rest-api'.format(nexus_registry))
+build_and_tag_gui_image('.', 'docker/rest_api_Dockerfile', '{}/foundations-rest-api'.format(nexus_registry), buildargs={'main_file': 'run_api_server.py'})
+build_and_tag_gui_image('.', 'docker/rest_api_Dockerfile', '{}/foundations-orbit-rest-api'.format(nexus_registry), buildargs={'main_file': 'run_orbit_api_server.py'})
 build_and_tag_gui_image('foundations_ui', 'gui_Dockerfile', '{}/foundations-gui'.format(nexus_registry))
