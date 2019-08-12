7cdef49ba1d5a2d82772b560a3a5e282a04dda15
Katherine <k.bancroft@dessa.com>
2019-08-12 09:11:57 -0400

    [JA] fix path in dev_env.sh

--- dev_env.sh
+++ dev_env.sh
@@ -16,6 +16,6 @@ add_path "$cwd/foundations_spec/src" && \
     add_path "$cwd/foundations_scheduler_plugin/src" && \
     add_path "$cwd/foundations_production/src" && \
     add_path "$cwd/foundations_rest_api/src" && \
-    add_path "$cwd/foundations_rest_api_core/src" && \
+    add_path "$cwd/foundations_core_rest_api_components/src" && \
     add_path "$cwd/foundations_orbit_rest_api/src" && \
     add_path "$cwd/foundations_model_package/src"
