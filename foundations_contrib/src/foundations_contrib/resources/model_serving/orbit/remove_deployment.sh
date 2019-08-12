d9045d386294fd48a0d890d3fcc2596bfdcad4e2
foundations-team <foundations-team@dessa.com>
2019-08-12 17:08:53 -0400

    [NN] remove_deployment gracefully

--- foundations_contrib/src/foundations_contrib/resources/model_serving/orbit/remove_deployment.sh
+++ foundations_contrib/src/foundations_contrib/resources/model_serving/orbit/remove_deployment.sh
@@ -3,5 +3,5 @@ namespace="ingress-nginx-test"
 project=$1
 model=$2
 
-kubectl delete deployment -n $namespace $project-$model-deployment && \
-    kubectl delete svc -n $namespace $project-$model-service
+kubectl delete deployment -n $namespace $project-$model-deployment
+kubectl delete svc -n $namespace $project-$model-service
