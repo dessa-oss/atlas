4e9d3f388c7d2e0c613a895abef4f054a121c086
Katherine <k.bancroft@dessa.com>
2019-08-12 09:26:29 -0400

    [JA] add test to make sure api resource is registered

--- foundations_orbit_rest_api/src/test/v1/controllers/test_projects_controller.py
+++ foundations_orbit_rest_api/src/test/v1/controllers/test_projects_controller.py
@@ -7,10 +7,25 @@ Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 
 from foundations_spec import *
 
-from foundations_orbit_rest_api.v1.controllers.projects_controller import ProjectsController
-
 class TestProjectsController(Spec):
 
     def test_projects_controller_comes_from_core_rest_api_components_module(self):
+        import foundations_orbit_rest_api.v1.controllers.projects_controller as orbit
         import foundations_core_rest_api_components.v1.controllers.projects_controller as core
-        self.assertEqual(core.ProjectsController, ProjectsController)
+        self.assertEqual(core.ProjectsController, orbit.ProjectsController)
+
+    def test_projects_controller_registered_as_api_resource(self):
+        import importlib
+        import foundations_core_rest_api_components.v1.controllers.projects_controller as core
+        import foundations_orbit_rest_api.v1.controllers.projects_controller as orbit
+
+        mock_api_resource = self.patch('foundations_core_rest_api_components.utils.api_resource.api_resource', ConditionalReturn())
+        class_decorator = ConditionalReturn()
+        decorated_class = Mock()
+
+        mock_api_resource.return_when(class_decorator, '/api/v1/projects')
+        class_decorator.return_when(decorated_class, core.ProjectsController)
+
+        importlib.reload(orbit)
+
+        self.assertEqual(decorated_class, orbit.ProjectsController)
