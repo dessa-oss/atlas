a497dd60a7b8f7f3adcc002b9dc7f24d599e457f
foundations-team <foundations-team@dessa.com>
2019-08-13 15:53:05 -0400

    [NN] added missing files, orbit deployment yaml is now the way it should be

---
+++ foundations_contrib/src/integration/resources/fixtures/test_server/setup_test_server.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+project_name=$1
+model_name=$2
+namespace="ingress-nginx-test"
+
+model_status () {
+    echo $(kubectl -n $namespace get po | grep $project_name-$model_name | awk '{print $3}')
+}
+
+kubectl create -f integration/resources/fixtures/test_server/test-server-deployment-and-service.yaml && \
+python foundations_contrib/resources/model_serving/orbit/ingress_modifier.py $project_name $model_name
+
+while [ "Pending" == $(model_status) ] || [ "" == $(model_status) ] || [ "ContainerCreating" == $(model_status) ]
+do 
+    sleep 2
+done
+
