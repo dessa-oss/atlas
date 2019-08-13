1cecd23a51f80528f5488c2aba71ac5a7578c496
pairing <pairing@dessa.com>
2019-08-13 12:08:00 -0400

    [TR EL] Refactor

--- foundations_sdk/src/foundations/local_run/__init__.py
+++ foundations_sdk/src/foundations/local_run/__init__.py
@@ -11,16 +11,20 @@ def load_local_configuration_if_present():
     from foundations_contrib.global_state import current_foundations_context, message_router
     from foundations_contrib.producers.jobs.queue_job import QueueJob
     from uuid import uuid4
-    import os
-    import os.path
     
     if _default_environment_present():
         set_environment('default')
         pipeline_context = current_foundations_context().pipeline_context()
         pipeline_context.file_name = str(uuid4())
 
-        pipeline_context.provenance.project_name = os.path.basename(os.getcwd())
+        pipeline_context.provenance.project_name = _default_project_name()
         QueueJob(message_router, pipeline_context).push_message()
+
+def _default_project_name():
+    import os
+    import os.path
+
+    return os.path.basename(os.getcwd())
         
 def _default_environment_present():
     from foundations_contrib.cli.environment_fetcher import EnvironmentFetcher
