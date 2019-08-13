fc1cb597bf42777236914e9e71040a5df298893a
pairing <pairing@dessa.com>
2019-08-13 11:34:57 -0400

    [TR EL] Set a random job id upon loading a default configuration

--- foundations_sdk/src/foundations/local_run/__init__.py
+++ foundations_sdk/src/foundations/local_run/__init__.py
@@ -9,9 +9,12 @@ Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 def load_local_configuration_if_present():
     from foundations.config import set_environment
     from foundations_contrib.cli.environment_fetcher import EnvironmentFetcher
+    from foundations_contrib.global_state import current_foundations_context
+    from uuid import uuid4
     
     environment_fetcher = EnvironmentFetcher()
 
     local_environments, global_environments = environment_fetcher.get_all_environments()
     if local_environments and 'default' in local_environments or global_environments and 'default' in global_environments:
         set_environment('default')
+        current_foundations_context().pipeline_context().file_name = str(uuid4())
--- foundations_sdk/src/test/local_run/test_set_default_environment.py
+++ foundations_sdk/src/test/local_run/test_set_default_environment.py
@@ -12,9 +12,35 @@ class SetDefaultEnvironment(Spec):
 
     mock_environment_fetcher = let_patch_instance('foundations_contrib.cli.environment_fetcher.EnvironmentFetcher')
     mock_set_environment = let_patch_mock('foundations.config.set_environment')
+    mock_uuid4 = let_patch_mock('uuid.uuid4')
+
+    @let_now
+    def random_uuid(self):
+        import uuid
+
+        value = self.faker.uuid4()
+        self.mock_uuid4.return_value = uuid.UUID(value)
+        return value
+
+    @let
+    def pipeline_context(self):
+        from foundations_internal.pipeline_context import PipelineContext
+        return PipelineContext()
+    
+    @let_now
+    def foundations_context(self):
+        from foundations_internal.pipeline import Pipeline
+        from foundations_internal.foundations_context import FoundationsContext
+
+        pipeline = Pipeline(self.pipeline_context)
+        context = FoundationsContext(pipeline)
+        return self.patch('foundations_contrib.global_state.foundations_context', context)
+
+    @set_up
+    def set_up(self):
+        self.mock_environment_fetcher.get_all_environments.return_value = (['default'], [])
 
     def test_default_environment_loaded_when_present_locally(self):
-        self.mock_environment_fetcher.get_all_environments.return_value = (['default'], [])
         load_local_configuration_if_present()
         self.mock_set_environment.assert_called_with('default')
 
@@ -33,4 +59,6 @@ class SetDefaultEnvironment(Spec):
         load_local_configuration_if_present()
         self.mock_set_environment.assert_not_called()
 
-    
+    def test_sets_default_job_id(self):
+        load_local_configuration_if_present()
+        self.assertEqual(self.random_uuid, self.pipeline_context.file_name)
