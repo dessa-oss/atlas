9357ba314dc31674e610a46d455ab8e3584fa8a7
Alessio Iannella <alessio.iannella@ellefsontech.com>
2019-08-15 15:53:12 -0400

    [AI] ORBIT UI: System health new ui and fetch from apiary

--- foundations_ui_orbit/src/js/components/PackagePage/SystemHealth/index.js
+++ foundations_ui_orbit/src/js/components/PackagePage/SystemHealth/index.js
@@ -2,43 +2,106 @@ import React from "react";
 import { withRouter } from "react-router-dom";
 import Layout from "../Layout";
 import { getFromApiary } from "../../../actions/BaseActions";
+import PropTypes from "prop-types";
+import moment from "moment";
 
-const SystemHealth = () => {
-  const [showPopulationTab, setShowPopulationTab] = React.useState(true);
+const SystemHealth = props => {
+  const [showSchemaTab, setShowSchemaTab] = React.useState(true);
+  const [showPopulationTab, setShowPopulationTab] = React.useState(false);
   const [showDataQualityTab, setShowDataQualityTab] = React.useState(false);
-  const [listFiles, setListFiles] = React.useState([]);
-  const [selectedFile, setSelectedFile] = React.useState(null);
   const [data, setData] = React.useState();
+  const [options, setOptions] = React.useState([]);
+  const [selectedInferencePeriod, setSelectedInferencePeriod] = React.useState("");
+  const [selectedModelPackage, setSelectedModelPackage] = React.useState("");
+  const [selectedDataContract, setSelectedDataContract] = React.useState("");
+  const [rowCountDifference, setRowCountDifference] = React.useState("No Report Selected");
+
+  const switchToSchemaTab = () => {
+    setShowSchemaTab(true);
+    setShowPopulationTab(false);
+    setShowDataQualityTab(false);
+  };
 
   const switchToPopulationShiftTab = () => {
+    setShowSchemaTab(false);
     setShowPopulationTab(true);
     setShowDataQualityTab(false);
   };
 
   const switchToDataQualityTab = () => {
+    setShowSchemaTab(false);
     setShowPopulationTab(false);
     setShowDataQualityTab(true);
   };
 
-  const loadAnotherFile = fileName => {
-    setSelectedFile(fileName);
-    getFromApiary(`files/health/${fileName}`).then(result => {
+  const reload = () => {
+    const { location } = props;
+
+    getFromApiary(`projects/${location.state.project.name}/validation_report_list`).then(result => {
       if (result) {
-        setData(result);
+        let entries = [];
+        result.forEach(resultItem => {
+          const value = moment(resultItem.inference_period)
+            .format("YYYY-MM-DD")
+            .toString();
+          const found = entries.find(entry => entry.inference_period === value);
+
+          if (!found) {
+            const entry = {
+              inference_period: value,
+              model_packages: []
+            };
+            entries.push(entry);
+          }
+        });
+
+        result.forEach(resultItem => {
+          entries.forEach(entryItem => {
+            if (resultItem.inference_period === entryItem.inference_period) {
+              const found = entryItem.model_packages.find(item => item.model_package === resultItem.model_package);
+
+              if (!found) {
+                let newModelPackage = {
+                  model_package: resultItem.model_package,
+                  data_contracts: []
+                };
+                entryItem.model_packages.push(newModelPackage);
+              }
+            }
+          });
+        });
+
+        result.forEach(resultItem => {
+          entries.forEach(entry => {
+            if (entry.inference_period === resultItem.inference_period) {
+              entry.model_packages.forEach(modelPackage => {
+                if (modelPackage.model_package === resultItem.model_package) {
+                  let dataContracts = modelPackage.data_contracts;
+                  const found = dataContracts.find(dataContract => dataContract === resultItem.data_contract);
+
+                  if (!found) {
+                    modelPackage.data_contracts.push(resultItem.data_contract);
+                  }
+                }
+              });
+            }
+          });
+        });
+
+        const sortedEntries = entries.sort((a, b) => {
+          const dateA = new Date(a.inference_period);
+          const dateB = new Date(b.inference_period);
+
+          return dateA - dateB;
+        });
+
+        setOptions(sortedEntries);
       }
     });
   };
 
   React.useEffect(() => {
-    getFromApiary("files/health").then(result => {
-      if (result) {
-        setListFiles(result);
-        if (selectedFile === null) {
-          loadAnotherFile(result[0]);
-          document.getElementById("dropdownFiles").selectedIndex = 1;
-        }
-      }
-    });
+    reload();
   }, []);
 
   const getValidationColor = validationValue => {
@@ -52,11 +115,45 @@ const SystemHealth = () => {
     return "none";
   };
 
-  const onChangeOption = event => {
-    if (event.target.value !== "") {
-      loadAnotherFile(event.target.value);
-    } else {
-      setData(null);
+  const renderSchemaTab = () => {
+    if (showSchemaTab) {
+      if (data === undefined) {
+        return (
+          <div className="i--image-nothing-here health-state-zero">
+            <div className="text-no-reports-loaded">
+              You haven{"'"}t loaded any reports
+            </div>
+          </div>
+        );
+      }
+      return (
+        <div className="system-health-table table-2">
+          <table>
+            <tr>
+              <th>ATTRIBUTE NAME</th>
+              <th>DATA TYPE</th>
+              <th>ISSUE TYPE</th>
+              <th>VALIDATION OUTCOME</th>
+            </tr>
+            {data.schema.details_by_attribute.length > 0
+              && data.schema.details_by_attribute.map(row => {
+                console.log("ROW: ", row);
+                if (row === "") return;
+                return (
+                  <tr key={row.attribute_name}>
+                    <td>{row.attribute_name}</td>
+                    <td>{row.data_type}</td>
+                    <td>{row.issue_type || ""}</td>
+                    <td className={getValidationColor(row.validation_outcome)}>
+                      {row.validation_outcome}
+                      <div className="circle" />{" "}
+                    </td>
+                  </tr>
+                );
+              })}
+          </table>
+        </div>
+      );
     }
   };
 
@@ -75,17 +172,17 @@ const SystemHealth = () => {
         <div className="system-health-table table-2">
           <table>
             <tr>
-              <th>PSI</th>
-              <th>Attribute Name</th>
-              <th>validation outcome</th>
+              <th>ATTRIBUTE NAME</th>
+              <th>POPULATION SHIFT INDEX</th>
+              <th>VALIDATION OUTCOME</th>
             </tr>
             {data.population_shift.details_by_attribute.length > 0
               && data.population_shift.details_by_attribute.map(row => {
                 if (row === "") return;
                 return (
-                  <tr key={row.PSI}>
-                    <td>{row.PSI}</td>
+                  <tr key={row.attribute_name}>
                     <td>{row.attribute_name}</td>
+                    <td>{row.PSI}</td>
                     <td className={getValidationColor(row.validation_outcome)}>
                       {row.validation_outcome}
                       <div className="circle" />{" "}
@@ -103,7 +200,7 @@ const SystemHealth = () => {
     if (showDataQualityTab) {
       if (data === undefined) {
         return (
-          <div className="i--image-nothing-here">
+          <div className="i--image-nothing-here health-state-zero">
             <div className="text-no-reports-loaded">
               You haven{"'"}t loaded any reports
             </div>
@@ -114,33 +211,23 @@ const SystemHealth = () => {
         <div className="system-health-table table-2">
           <table>
             <tr>
-              <th>% non NUMERIC INFERENCE PERIOD</th>
-              <th>% non NUMERIC REFERENCE PERIOD</th>
-              <th>% null INFERENCE PERIOD</th>
-              <th>% null REFERENCE PERIOD</th>
-              <th>% REMAINING INFERENCE PERIOD</th>
-              <th>% REMAINING REFERENCE PERIOD</th>
-              <th>% zero INFERENCE PERIOD</th>
-              <th>% zero REFERENCE PERIOD</th>
-              <th>% ATTRIBUTE NAME</th>
-              <th>% VALIDATION OUTCOME</th>
+              <th>ATTRIBUTE NAME</th>
+              <th>VALUE</th>
+              <th>% IN REFERENCE DATA</th>
+              <th>% IN PRODUCTION DATA</th>
+              <th>DIFFERENCE</th>
+              <th>VALIDATION OUTCOME</th>
             </tr>
             {data.data_quality.details_by_attribute.length > 0
               && data.data_quality.details_by_attribute.map(row => {
                 if (row === "") return;
                 return (
-                  <tr key={row.pct_non_numeric_inference_period}>
-                    <td>
-                      {JSON.stringify(row.pct_non_numeric_inference_period)}
-                    </td>
-                    <td>{row.pct_non_numeric_reference_period}</td>
-                    <td>{row.pct_null_inference_period}</td>
-                    <td>{row.pct_null_reference_period}</td>
-                    <td>{row.pct_remaining_inference_period}</td>
-                    <td>{row.pct_remaining_reference_period}</td>
-                    <td>{row.pct_zero_inference_period}</td>
-                    <td>{row.pct_zero_reference_period}</td>
+                  <tr key={row.attribute_name}>
                     <td>{row.attribute_name}</td>
+                    <td>{row.value}</td>
+                    <td>{row.pct_in_reference_data}</td>
+                    <td>{row.pct_in_inference_data}</td>
+                    <td>{row.difference_in_pct}</td>
                     <td className={getValidationColor(row.validation_outcome)}>
                       {row.validation_outcome}
                       <div className="circle" />
@@ -153,6 +240,99 @@ const SystemHealth = () => {
       );
     }
   };
+
+  const onClickInferencePeriod = value => {
+    if (value === selectedInferencePeriod) {
+      setSelectedInferencePeriod("");
+      setSelectedModelPackage("");
+      setRowCountDifference("");
+    } else {
+      setSelectedInferencePeriod(value);
+    }
+  };
+
+  const onClickModelPackage = value => {
+    if (value === selectedModelPackage) {
+      setSelectedModelPackage("");
+    } else {
+      setSelectedModelPackage(value);
+    }
+  };
+
+  const onClickDataContract = value => {
+    setSelectedDataContract(value);
+
+    const { location } = props;
+
+    getFromApiary(`projects/${location.state.project.name}/validation_results`).then(result => {
+      if (result) {
+        setData(result);
+      }
+    });
+  };
+
+  const renderDataContracts = item => {
+    return item.data_contracts.map(contract => {
+      return (
+        <div
+          className={contract === selectedDataContract
+            ? "container-health-data-contract selected"
+            : "container-health-data-contract"}
+          key={contract}
+        >
+          <div
+            className={contract === selectedDataContract ? "label-data-contract selected" : "label-data-contract"}
+            onClick={() => onClickDataContract(contract)}
+          >
+            {contract}
+          </div>
+        </div>
+      );
+    });
+  };
+
+  const renderModelPackages = option => {
+    return option.model_packages.map(item => {
+      return (
+        <div
+          className="container-health-model-package"
+          key={item.model_package}
+        >
+          <div
+            className="label-model-package"
+            onClick={() => onClickModelPackage(item.model_package)}
+          >
+            {item.model_package === selectedModelPackage
+              ? `- ${item.model_package}`
+              : `+ ${item.model_package}`}
+          </div>
+          {item.model_package === selectedModelPackage && renderDataContracts(item)}
+        </div>
+      );
+    });
+  };
+
+  const renderOptions = () => {
+    return options.map(option => {
+      return (
+        <div
+          className="container-health-options"
+          key={option.inference_period}
+        >
+          <div
+            className="label-option"
+            onClick={() => onClickInferencePeriod(option.inference_period)}
+          >
+            {option.inference_period === selectedInferencePeriod
+              ? `- ${option.inference_period}`
+              : `+ ${option.inference_period}`}
+          </div>
+          {option.inference_period === selectedInferencePeriod && renderModelPackages(option)}
+        </div>
+      );
+    });
+  };
+
   // data != undefined ?
   const mainWindow = (
     <Layout tab="Health" title="Data Health">
@@ -167,49 +347,78 @@ const SystemHealth = () => {
               to catch data abnormalities in real-time. Please select a data
               validation report to review.
             </p>
-            <div className="model-performance-filter-container">
-              <label>Select report:</label>
-              {
-                <div className="sh-model-performance-filter">
-                  <select
-                    id="dropdownFiles"
-                    className="manage-interface-modal-select"
-                    onChange={onChangeOption}
-                  >
-                    {listFiles.length > 0
-                      && listFiles.map(option => {
-                        return <option key={option}>{option}</option>;
-                      })}
-                  </select>
-                </div>
-              }
+            <p className="label-select-reports">Select report:</p>
+            <div className="container-health-reports">
+              {renderOptions()}
             </div>
             <div className="summary-container">
               <p>Inference period:</p>
-              <p>
-                {data === undefined
-                  ? "no report selected"
-                  : data.inference_period}
+              <p>{data !== undefined ? data.inference_period : "No Report Selected"}
               </p>
-              <p>Reference period:</p>
-              <p>
-                {data === undefined
-                  ? "no report selected"
-                  : data.reference_period}
-              </p>
+              <p>Model Package:</p>
+              <p>{data !== undefined ? data.model_package : "No Report Selected"}</p>
+              <p>Data Contract:</p>
+              <p>{data !== undefined ? data.data_contract : "No Report Selected"}</p>
               <p>Row count difference:</p>
               <p>
-                {data === undefined ? "no report selected" : data.row_cnt_diff}
-              </p>
-              <p>Missing columns:</p>
-              <p>
-                {data === undefined
-                  ? "no report selected"
-                  : data.missing_columns}
+                {data !== undefined ? data.row_cnt_diff : "No Report Selected"}
               </p>
             </div>
           </div>
           <div className="right-side">
+            <div
+              className={
+                showSchemaTab
+                  ? "header-population-shift active"
+                  : "header-population-shift"
+              }
+              onClick={switchToSchemaTab}
+            >
+              <div className="header-title subheader font-bold">
+                SCHEMA CHECK
+              </div>
+              <div className="header-summary">
+                <div
+                  className={
+                    data === undefined
+                      || data.schema.summary.warning === 0
+                      ? "warning none"
+                      : "warning"
+                  }
+                >
+                  {data === undefined
+                    ? "N/A"
+                    : data.schema.summary.warning}
+                  <p>Warning</p>
+                </div>
+                <div
+                  className={
+                    data === undefined
+                      || data.schema.summary.healthy === 0
+                      ? "healthy none"
+                      : "healthy"
+                  }
+                >
+                  {data === undefined
+                    ? "N/A"
+                    : data.schema.summary.healthy}
+                  <p>Healthy</p>
+                </div>
+                <div
+                  className={
+                    data === undefined
+                      || data.schema.summary.critical === 0
+                      ? "critical none"
+                      : "critical"
+                  }
+                >
+                  {data === undefined
+                    ? "N/A"
+                    : data.schema.summary.critical}
+                  <p>Critical</p>
+                </div>
+              </div>
+            </div>
             <div
               className={
                 showPopulationTab
@@ -272,7 +481,7 @@ const SystemHealth = () => {
               onClick={switchToDataQualityTab}
             >
               <div className="header-title subheader font-bold">
-                DATA QUALITY
+                DATA ABNORMALITY
               </div>
               <div className="header-summary">
                 <div
@@ -316,6 +525,16 @@ const SystemHealth = () => {
             </div>
             <div className="data-display">
               <div
+                id="schema-table"
+                className={
+                  showSchemaTab
+                    ? "system-health-table-container"
+                    : "system-health-table-container hidden"
+                }
+              >
+                {renderSchemaTab()}
+              </div>
+              <div
                 id="population-shift-table"
                 className={
                   showPopulationTab
@@ -343,6 +562,14 @@ const SystemHealth = () => {
   );
 
   return <div>{mainWindow}</div>;
+};
+
+SystemHealth.propTypes = {
+  location: PropTypes.object
+};
+
+SystemHealth.defaultProps = {
+  location: { state: {} }
 };
 
 export default withRouter(SystemHealth);
