746b6d6cca613d885a34006c9ee046810fef28d2
Nichie E <deniseenriquez@ellefsontech.com>
2019-08-19 22:39:07 -0400

    dashboard style and structure

--- foundations_ui/src/js/components/JobOverviewPage/JobOverviewPage.js
+++ foundations_ui/src/js/components/JobOverviewPage/JobOverviewPage.js
@@ -40,6 +40,52 @@ class JobOverviewPage extends Component {
             </div>
           </div>
         </div>
+        <div className="dashboard-content-container row">
+          <section className="chart-and-notes col-md-8">
+            <div className="chart section-container">
+              <h3>Recent Jobs</h3>
+              <select>
+                <option selected="selected">
+                Metrics
+                </option>
+              </select>
+              <select>
+                <option selected="selected">
+                Sort by
+                </option>
+              </select>
+            </div>
+            <div className="notes section-container">
+              <h3>Notepad</h3>
+              <div className="notes-textarea">
+                <textarea placeholder="Type something..." />
+                <button type="button">Add Note</button>
+              </div>
+              <div className="notes-blocks">
+                <p>Mohammed R. <span>June 25th 2019</span></p>
+                <p>This is my message to you</p>
+              </div>
+            </div>
+          </section>
+          <div className="readme section-container col-md-4">
+            <h2>README.md</h2>
+            <textarea placeholder="Type something...">
+            Customer churn and engagement has become one
+            of the top issues for most banks.
+            It costs significantly more to acquire new customers
+            than retain existing ones, and it costs far more to
+            re-acquire defected customers. In fact, several empirical
+            studies and models have proven that churn remains one of
+            the biggest destructors of enterprise value for banks and
+            other consumer intensive companies.
+            1. Introduction
+            We aim to accomplish the following for this study:
+            Identify and visualize which factors contribute to customer churn:
+            Build a prediction model that will perform the following:
+            Classify if a customer is going to churn or not
+            </textarea>
+          </div>
+        </div>
       </div>
     );
   }
