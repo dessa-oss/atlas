45439a544363d51d0d75331360f31d50bf982d0a
Chris Elcombe <chris.elcombe@ellefsontech.com>
2019-08-20 12:45:34 -0400

    Format the graph data programatically and inject it via props rather than hardcoded

--- foundations_ui/src/js/components/JobOverviewPage/JobOverviewGraph.js
+++ foundations_ui/src/js/components/JobOverviewPage/JobOverviewGraph.js
@@ -8,11 +8,54 @@ class JobOverviewGraph extends Component {
     super(props);
     this.state = {
       metric: this.props.metric,
+      graphData: this.props.graphData,
+      formattedGraphData: [],
     };
+    this.formatGraphData = this.formatGraphData.bind(this);
+  }
+
+  componentDidMount() {
+    this.formatGraphData();
+  }
+
+  formatGraphData() {
+    const { graphData } = this.state;
+
+    let graphCopy = [...graphData];
+    // Assumes the following format
+    /*
+    [
+      [
+        [Date Object like {2019, 04, 04}, value like 12],
+        [...] more [date, value] pairs
+      ],
+      [
+        Another graph's [date, value] pairs
+      ],
+      [...]
+    ]
+    */
+    let seriesArray = [];
+    graphCopy.forEach((graph) => {
+      let seriesObject = {};
+      seriesObject.showInLegend = false;
+      graph.forEach((dataPoint) => {
+        // Make sure the types are as we expect
+        if (Array.isArray(dataPoint) && dataPoint.length > 1) {
+          let curDate = dataPoint[0];
+          if (typeof (curDate.GetYear) === 'function') {
+            dataPoint[0] = Date.UTC(curDate.GetYear(), curDate.GetMonth(), curDate.GetDay());
+          }
+        }
+      });
+      seriesObject.data = graph;
+      seriesArray.push(seriesObject);
+    });
+    this.setState({ formattedGraphData: seriesArray });
   }
 
   render() {
-    const { metric } = this.state;
+    const { metric, formattedGraphData } = this.state;
     const options = {
       chart: {
         type: 'spline',
@@ -36,32 +79,7 @@ class JobOverviewGraph extends Component {
       title: {
         text: '',
       },
-      series: [
-        {
-          showInLegend: false,
-          data: [[Date.UTC(2019, 1, 1), 1], [Date.UTC(2019, 2, 1), 3], [Date.UTC(2019, 4, 1), 8],
-            [Date.UTC(2019, 5, 1), 3], [Date.UTC(2019, 5, 29), 8], [Date.UTC(2019, 6, 1), 2],
-            [Date.UTC(2019, 7, 1), 5], [Date.UTC(2019, 8, 1), 4], [Date.UTC(2019, 9, 1), 12],
-            [Date.UTC(2019, 10, 1), 6], [Date.UTC(2019, 11, 1), 6], [Date.UTC(2019, 12, 1), 0],
-          ],
-        },
-        {
-          showInLegend: false,
-          data: [[Date.UTC(2019, 1, 1), 4], [Date.UTC(2019, 1, 15), 6], [Date.UTC(2019, 2, 1), 5],
-            [Date.UTC(2019, 4, 1), 2], [Date.UTC(2019, 5, 1), 13], [Date.UTC(2019, 6, 1), 4],
-            [Date.UTC(2019, 7, 1), 5], [Date.UTC(2019, 8, 1), 12], [Date.UTC(2019, 9, 1), 4],
-            [Date.UTC(2019, 10, 1), 9], [Date.UTC(2019, 11, 1), 4], [Date.UTC(2019, 12, 1), 1],
-          ],
-        },
-        {
-          showInLegend: false,
-          data: [[Date.UTC(2019, 1, 1), 9], [Date.UTC(2019, 2, 1), 7], [Date.UTC(2019, 4, 1), 7],
-            [Date.UTC(2019, 5, 1), 6], [Date.UTC(2019, 6, 1), 8], [Date.UTC(2019, 6, 14), 1],
-            [Date.UTC(2019, 7, 1), 3], [Date.UTC(2019, 8, 1), 8], [Date.UTC(2019, 9, 1), 2],
-            [Date.UTC(2019, 10, 1), 3], [Date.UTC(2019, 11, 1), 8], [Date.UTC(2019, 12, 1), 2],
-          ],
-        },
-      ],
+      series: formattedGraphData,
     };
 
     return (
@@ -87,10 +105,31 @@ class JobOverviewGraph extends Component {
 
 JobOverviewGraph.propTypes = {
   metric: PropTypes.string,
+  graphData: PropTypes.array,
 };
 
 JobOverviewGraph.defaultProps = {
   metric: 'metric',
+  graphData: [
+    [
+      [Date.UTC(2019, 1, 1), 9], [Date.UTC(2019, 2, 1), 7], [Date.UTC(2019, 4, 1), 7],
+      [Date.UTC(2019, 5, 1), 6], [Date.UTC(2019, 6, 1), 8], [Date.UTC(2019, 6, 14), 1],
+      [Date.UTC(2019, 7, 1), 3], [Date.UTC(2019, 8, 1), 8], [Date.UTC(2019, 9, 1), 2],
+      [Date.UTC(2019, 10, 1), 3], [Date.UTC(2019, 11, 1), 8], [Date.UTC(2019, 12, 1), 2],
+    ],
+    [
+      [Date.UTC(2019, 1, 1), 1], [Date.UTC(2019, 2, 1), 3], [Date.UTC(2019, 4, 1), 8],
+      [Date.UTC(2019, 5, 1), 3], [Date.UTC(2019, 5, 29), 8], [Date.UTC(2019, 6, 1), 2],
+      [Date.UTC(2019, 7, 1), 5], [Date.UTC(2019, 8, 1), 4], [Date.UTC(2019, 9, 1), 12],
+      [Date.UTC(2019, 10, 1), 6], [Date.UTC(2019, 11, 1), 6], [Date.UTC(2019, 12, 1), 0],
+    ],
+    [
+      [Date.UTC(2019, 1, 1), 4], [Date.UTC(2019, 1, 15), 6], [Date.UTC(2019, 2, 1), 5],
+      [Date.UTC(2019, 4, 1), 2], [Date.UTC(2019, 5, 1), 13], [Date.UTC(2019, 6, 1), 4],
+      [Date.UTC(2019, 7, 1), 5], [Date.UTC(2019, 8, 1), 12], [Date.UTC(2019, 9, 1), 4],
+      [Date.UTC(2019, 10, 1), 9], [Date.UTC(2019, 11, 1), 4], [Date.UTC(2019, 12, 1), 1],
+    ],
+  ],
 };
 
 export default JobOverviewGraph;
