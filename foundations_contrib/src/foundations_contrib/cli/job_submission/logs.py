5a8acb83add8f3a48505d6b01241291e539f48d4
pairing <pairing@dessa.com>
2019-08-27 14:30:53 -0400

    [TR] start on logging

--- foundations_contrib/src/foundations_contrib/cli/job_submission/logs.py
+++ foundations_contrib/src/foundations_contrib/cli/job_submission/logs.py
@@ -4,3 +4,16 @@ Unauthorized copying, distribution, reproduction, publication, use of this file,
 Proprietary and confidential
 Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 """
+
+def stream_job_logs(deployment):
+    from foundations_contrib.global_state import log_manager
+    
+    logger = log_manager.get_logger(__name__)
+    logger.info('Job is queued; Ctrl-C to stop streaming - job will not be interrupted or cancelled')
+    job_running = False
+
+    for item in deployment.stream_job_logs():
+        if not job_running:
+            logger.info('Job is running; streaming logs')
+            job_running = True
+        print(item)
