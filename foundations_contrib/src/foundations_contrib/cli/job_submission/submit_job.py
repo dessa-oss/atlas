4b8bf111624c79e186b8698d2ea13941d3c91d23
pairing <pairing@dessa.com>
2019-08-27 16:16:50 -0400

    [TR] initial job override config

--- foundations_contrib/src/foundations_contrib/cli/job_submission/submit_job.py
+++ foundations_contrib/src/foundations_contrib/cli/job_submission/submit_job.py
@@ -10,12 +10,26 @@ def submit(arguments):
     from foundations_contrib.cli.job_submission.deployment import deploy
     from foundations_contrib.cli.job_submission.logs import stream_job_logs
     from foundations_contrib.change_directory import ChangeDirectory
+    from foundations_contrib.global_state import config_manager
     import os
+    import os.path
+    import yaml
 
     current_directory = os.getcwd()
     with ChangeDirectory(arguments.job_dir or current_directory):
         load(arguments.scheduler_config or 'scheduler')
-        deployment = deploy(arguments.project_name, arguments.entrypoint, arguments.params)
+
+        job_config = {}
+        if os.path.exists('job.config.yaml'):
+            with open('job.config.yaml') as file:
+                job_config = yaml.load(file.read())
+                config_manager['log_level'] = job_config['log_level']
+
+        deployment = deploy(
+            job_config.get('project_name', arguments.project_name), 
+            job_config.get('entrypoint', arguments.entrypoint), 
+            job_config.get('params', arguments.params)
+        )
         try:
             stream_job_logs(deployment)
         except KeyboardInterrupt:
