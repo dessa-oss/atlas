05f272435bc9ce07d857d3f7fc54697c3fbc92ef
pairing <pairing@dessa.com>
2019-08-27 15:51:53 -0400

    [TR] get basic submission in

--- foundations_contrib/src/foundations_contrib/cli/job_submission/submit_job.py
+++ foundations_contrib/src/foundations_contrib/cli/job_submission/submit_job.py
@@ -7,9 +7,16 @@ Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 
 def submit(arguments):
     from foundations_contrib.cli.job_submission.config import load
+    from foundations_contrib.cli.job_submission.deployment import deploy
+    from foundations_contrib.cli.job_submission.logs import stream_job_logs
     from foundations_contrib.change_directory import ChangeDirectory
     import os
 
     current_directory = os.getcwd()
     with ChangeDirectory(arguments.job_dir or current_directory):
         load(arguments.scheduler_config or 'scheduler')
+        deployment = deploy(arguments.project_name, arguments.entrypoint, arguments.params)
+        try:
+            stream_job_logs(deployment)
+        except KeyboardInterrupt:
+            pass
