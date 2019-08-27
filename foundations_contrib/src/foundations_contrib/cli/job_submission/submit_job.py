ceda44c74639042a49820ade8a1cce73dd798ed9
pairing <pairing@dessa.com>
2019-08-27 15:44:36 -0400

    [TR] working on job submission

--- foundations_contrib/src/foundations_contrib/cli/job_submission/submit_job.py
+++ foundations_contrib/src/foundations_contrib/cli/job_submission/submit_job.py
@@ -4,3 +4,12 @@ Unauthorized copying, distribution, reproduction, publication, use of this file,
 Proprietary and confidential
 Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 """
+
+def submit(arguments):
+    from foundations_contrib.cli.job_submission.config import load
+    from foundations_contrib.change_directory import ChangeDirectory
+    import os
+
+    current_directory = os.getcwd()
+    with ChangeDirectory(arguments.job_dir or current_directory):
+        load(arguments.scheduler_config or 'scheduler')
