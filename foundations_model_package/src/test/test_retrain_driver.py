271ec29d0095040bd0d0b4475a58a510bc77b8b0
Katherine <k.bancroft@dessa.com>
2019-08-28 14:09:28 -0400

    [CZ JA] start on retrain driver

---
+++ foundations_model_package/src/test/test_retrain_driver.py
@@ -0,0 +1,12 @@
+"""
+Copyright (C) DeepLearning Financial Technologies Inc. - All Rights Reserved
+Unauthorized copying, distribution, reproduction, publication, use of this file, via any medium is strictly prohibited
+Proprietary and confidential
+Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
+"""
+
+from foundations_spec import *
+from foundations_model_package.retrain_driver import RetrainDriver
+
+class TestRetrainDriver(Spec):
+    pass
