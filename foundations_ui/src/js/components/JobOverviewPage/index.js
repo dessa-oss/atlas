900e93a38551691a0a9c26cdece3bd9f85460053
Alessio Iannella <alessio.iannella@ellefsontech.com>
2019-08-28 09:06:53 -0400

    [AI] ATLAS UI: set default tab

--- foundations_ui/src/js/components/JobOverviewPage/index.js
+++ foundations_ui/src/js/components/JobOverviewPage/index.js
@@ -11,7 +11,7 @@ class JobOverviewPage extends Component {
     super(props);
 
     this.state = {
-      tab: 'details',
+      tab: 'overview',
       tags: ['finance', 'marketing', 'buck', 'hana', 'lou', 'mama', 'DL', 'banking', 'regression', 'finance',
         'marketing', 'buck', 'hana', 'lou', 'mama', 'DL', 'banking', 'regression', 'finance', 'marketing', 'buck',
         'hana',
