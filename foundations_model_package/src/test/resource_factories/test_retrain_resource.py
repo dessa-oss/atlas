e41f7bf178831f0e74c409042d575c15a996398b
Katherine <k.bancroft@dessa.com>
2019-08-28 15:24:39 -0400

    [CZ JA] retrain resource generates random __name__

--- foundations_model_package/src/test/resource_factories/test_retrain_resource.py
+++ foundations_model_package/src/test/resource_factories/test_retrain_resource.py
@@ -25,3 +25,9 @@ class TestRetrainResource(Spec):
         resource_class = retrain_resource(self.module_name, self.function_name)
         resource = resource_class()
         self.assertIsInstance(resource, Resource)
+
+    def test_retrain_resource_has_different_name_each_time_when_constructed(self):
+        resource_class_0 = retrain_resource(self.module_name, self.function_name)
+        resource_class_1 = retrain_resource(self.module_name, self.function_name)
+
+        self.assertNotEqual(resource_class_0.__name__, resource_class_1.__name__)
