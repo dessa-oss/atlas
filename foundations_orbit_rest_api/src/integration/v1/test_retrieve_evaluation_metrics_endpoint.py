7319fae2400332417ffa97f6a32950bb0cce6f42
foundations-team <foundations-team@dessa.com>
2019-08-29 14:42:44 -0400

    [NN JA] rework integration test to reflect project-level behaviour

--- foundations_orbit_rest_api/src/integration/v1/test_retrieve_evaluation_metrics_endpoint.py
+++ foundations_orbit_rest_api/src/integration/v1/test_retrieve_evaluation_metrics_endpoint.py
@@ -12,7 +12,7 @@ from foundations_orbit_rest_api.global_state import app_manager
 @skip('not implemented')
 class TestRetrieveEvaluationMetricsEndpoint(Spec):
     client = app_manager.app().test_client()
-    url = '/api/v1/projects/test_project/this_job/metrics'
+    url = '/api/v1/projects/test_project/metrics'
 
     @let
     def redis(self):
@@ -21,16 +21,13 @@ class TestRetrieveEvaluationMetricsEndpoint(Spec):
 
     @set_up
     def set_up(self):
-        import os
-
-        os.environ['JOB_ID'] = 'this_job'
-
         self.redis.flushall()
 
     @tear_down
     def tear_down(self):
         import os
-        os.environ.pop('JOB_ID')
+        os.environ.pop('MODEL_NAME')
+        os.environ.pop('PROJECT_NAME')
 
     def _get_from_route(self):
         import json
@@ -40,7 +37,12 @@ class TestRetrieveEvaluationMetricsEndpoint(Spec):
         return json.loads(response_data)
 
     def test_retrieve_evaluation_metrics_gets_stored_metrics_from_redis(self):
+        import os
         from foundations_orbit import track_production_metrics
+
+        os.environ['PROJECT_NAME'] = 'test_project'
+
+        os.environ['MODEL_NAME'] = 'this_job'
 
         track_production_metrics('MSE', {
             '2019-02-01': 1.077,
@@ -56,6 +58,22 @@ class TestRetrieveEvaluationMetricsEndpoint(Spec):
             '2029-05-02': 17.43
         })
 
+        os.environ['MODEL_NAME'] = 'that_job'
+
+        track_production_metrics('MSE', {
+            '2019-02-01': 2.077,
+            '2019-03-01': 2.074,
+            '2019-04-01': 2.09,
+            '2019-05-01': 2.086
+        })
+
+        track_production_metrics('Customer Response (%)', {
+            '2029-02-02': 27.56,
+            '2029-03-02': 27.57,
+            '2029-04-02': 27.53,
+            '2029-05-02': 27.43
+        })
+
         expected_data = [
             {
                 'title': {'text': 'Customer Response (%) over time'},
@@ -66,7 +84,11 @@ class TestRetrieveEvaluationMetricsEndpoint(Spec):
                 'series': [
                     {
                         'data': [17.56, 17.57, 17.53, 17.43],
-                        'name': None
+                        'name': 'this_job'
+                    },
+                    {
+                        'data': [27.56, 27.57, 27.53, 27.43],
+                        'name': 'that_job'
                     }
                 ]
             },
@@ -79,7 +101,11 @@ class TestRetrieveEvaluationMetricsEndpoint(Spec):
                 'series': [
                     {
                         'data': [1.077, 1.074, 1.09, 1.086],
-                        'name': None
+                        'name': 'this_job'
+                    },
+                    {
+                        'data': [2.077, 2.074, 2.09, 2.086],
+                        'name': 'that_job'
                     }
                 ]
             }
