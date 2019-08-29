42d1dff6c69ddb8a81cf852333525e0dbc61a312
Nichie E <deniseenriquez@ellefsontech.com>
2019-08-29 13:25:06 -0400

    [NE]: ATLAS UI - status icon styles

--- foundations_ui/src/js/components/JobListPage/cells/StatusCell.js
+++ foundations_ui/src/js/components/JobListPage/cells/StatusCell.js
@@ -30,7 +30,21 @@ class StatusCell extends Component {
 
     return (
       <span className={divClass}>
-        <span className={statusClass} />
+        { statusClass.includes('status-running')
+          ? (
+            <svg className="spinner" width="18px" height="18px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+              <circle
+                className="path"
+                fill="none"
+                strokeWidth="6"
+                strokeLinecap="round"
+                cx="33"
+                cy="33"
+                r="30"
+              />
+            </svg>
+          )
+          : <span className={statusClass} />}
       </span>
     );
   }
