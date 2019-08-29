aacfa326463722af9bf942168d62fdab4ede8abb
foundations-team <foundations-team@dessa.com>
2019-08-29 13:53:16 -0400

    [NN JA] handle case where metric is logged with no values for job

--- foundations_orbit_rest_api/src/foundations_orbit_rest_api/production_metrics.py
+++ foundations_orbit_rest_api/src/foundations_orbit_rest_api/production_metrics.py
@@ -5,5 +5,15 @@ Proprietary and confidential
 Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 """
 
-def all_production_metrics(_):
-    return {}
+def _redis_key():
+    import os
+    job_id = os.environ['JOB_ID']
+    return f'models:{job_id}:production_metrics'
+
+def all_production_metrics(job_id):
+    from foundations_contrib.global_state import redis_connection
+    all_items = list(redis_connection.hgetall(_redis_key()).items())
+    if not all_items:
+        return {}
+    metric_name, metric_values = all_items[0]
+    return {metric_name.decode(): []}
