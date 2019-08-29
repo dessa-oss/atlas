ce5f89bc777bf6b50eb648410a5a7777d0c7b43e
pairing <pairing@dessa.com>
2019-08-29 16:12:49 -0400

    [TR] auto configure the scheduler in the rest api

--- docker/run_api_server.py
+++ docker/run_api_server.py
@@ -3,11 +3,31 @@ import os
 
 os.environ['FOUNDATIONS_COMMAND_LINE'] = 'True'
 
-from foundations import config_manager
+from foundations_contrib.global_state import config_manager
 from foundations_rest_api.global_state import app_manager
+from foundations_scheduler_plugin.config.scheduler import translate
+import subprocess
+import yaml
 
+nodes_yaml = subprocess.check_output(["/bin/bash", "-c", "kubectl get node -o yaml -l node-role.kubernetes.io/master="""]).decode()
+nodes = yaml.load(nodes_yaml)
+master_ip = nodes['items'][0]['status']['addresses'][0]['address']
+
+submission_config = {
+    'results_config': {
+        'redis_end_point': os.environ["REDIS_URL"]
+    },
+    'ssh_config': {
+        'host': master_ip,
+        'port': 31222,
+        'code_path': '/jobs',
+        'key_path': '~/.ssh/id_foundations_scheduler',
+        'user': 'job-uploader'
+    }
+}
+translated_submission_config = translate(submission_config)
 configuration = config_manager.config()
-configuration["redis_url"] = os.environ["REDIS_URL"]
+configuration.update(translated_submission_config)
 
 root_logger = logging.getLogger()
 root_logger.setLevel(logging.DEBUG)
