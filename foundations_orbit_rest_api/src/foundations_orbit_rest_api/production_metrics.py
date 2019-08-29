2755b52c1c45c62ad6a9a80770b784c32378d65c
foundations-team <foundations-team@dessa.com>
2019-08-29 13:59:59 -0400

    [NN JA] refactor

--- foundations_orbit_rest_api/src/foundations_orbit_rest_api/production_metrics.py
+++ foundations_orbit_rest_api/src/foundations_orbit_rest_api/production_metrics.py
@@ -5,21 +5,30 @@ Proprietary and confidential
 Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 """
 
-def _redis_key():
-    import os
-    job_id = os.environ['JOB_ID']
-    return f'models:{job_id}:production_metrics'
-
 def all_production_metrics(job_id):
-    import pickle
-    from foundations_contrib.global_state import redis_connection
-    all_items = list(redis_connection.hgetall(_redis_key()).items())
+    all_items = _serialized_items_from_redis(_redis_key())
+    
     if not all_items:
         return {}
+    
     metric_name, metric_values = all_items[0]
-    deserialized_metric_values = pickle.loads(metric_values)
+    deserialized_metric_values = _deserialized_metric_values(metric_values)
 
     if deserialized_metric_values:
         deserialized_metric_values = [deserialized_metric_values[0]]
 
     return {metric_name.decode(): deserialized_metric_values}
+
+def _deserialized_metric_values(metric_values):
+    import pickle
+    return pickle.loads(metric_values)
+
+def _serialized_items_from_redis(key):
+    from foundations_contrib.global_state import redis_connection
+    items_iterator = redis_connection.hgetall(key).items()
+    return list(items_iterator)
+
+def _redis_key():
+    import os
+    job_id = os.environ['JOB_ID']
+    return f'models:{job_id}:production_metrics'
