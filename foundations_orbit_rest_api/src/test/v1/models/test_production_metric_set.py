b5dc378e850e026e1db88de4f6674295ed0621f8
foundations-team <foundations-team@dessa.com>
2019-08-29 16:00:24 -0400

    [NN JA] load metric set where one metric pair logged

--- foundations_orbit_rest_api/src/test/v1/models/test_production_metric_set.py
+++ foundations_orbit_rest_api/src/test/v1/models/test_production_metric_set.py
@@ -117,8 +117,6 @@ class TestProductionMetricSet(Spec):
     def test_all_returns_promise_with_singleton_list_containing_one_empty_metric_set_if_metric_logged_with_no_values(self):
         from foundations_orbit import track_production_metrics
 
-        self.maxDiff = None
-
         self.environ['PROJECT_NAME'] = self.project_name
         self.environ['MODEL_NAME'] = self.model_name
 
@@ -131,6 +129,25 @@ class TestProductionMetricSet(Spec):
             yAxis={'title': {'text': self.metric_name}},
             xAxis={'categories': []},
             series={'data': [], 'name': self.model_name}
+        )
+
+        self.assertEqual([expected_metric_set], promise.evaluate())
+
+    def test_all_returns_promise_with_singleton_list_containing_singleton_metric_set_if_metric_logged_with_one_key_value_pair(self):
+        from foundations_orbit import track_production_metrics
+
+        self.environ['PROJECT_NAME'] = self.project_name
+        self.environ['MODEL_NAME'] = self.model_name
+
+        track_production_metrics(self.metric_name, {self.metric_column: self.metric_value})
+
+        promise = ProductionMetricSet.all(self.project_name)
+
+        expected_metric_set = ProductionMetricSet(
+            title={'text': f'{self.metric_name} over time'},
+            yAxis={'title': {'text': self.metric_name}},
+            xAxis={'categories': [self.metric_column]},
+            series={'data': [self.metric_value], 'name': self.model_name}
         )
 
         self.assertEqual([expected_metric_set], promise.evaluate())
