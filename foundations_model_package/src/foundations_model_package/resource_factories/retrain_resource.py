e39a4fbc2d84b5e06ce642f549b673089827b112
Katherine <k.bancroft@dessa.com>
2019-08-29 11:41:47 -0400

    [KD CZ] retrain resource submits foundations job and returns job_id

--- foundations_model_package/src/foundations_model_package/resource_factories/retrain_resource.py
+++ foundations_model_package/src/foundations_model_package/resource_factories/retrain_resource.py
@@ -5,12 +5,28 @@ Proprietary and confidential
 Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 """
 
-def retrain_resource(*args):
+def retrain_resource(retrain_driver):
     from flask_restful import Resource
     from uuid import uuid4
+    from flask import request
 
     class _RetrainResource(Resource):
-        pass
+        
+        def post(self):
+            import os
+            import foundations
+
+            project_name = os.environ['PROJECT_NAME']
+
+            with retrain_driver as driver_file:
+                entrypoint = driver_file
+
+            params = request.json
+
+            job_deployment = foundations.submit(project_name=project_name, entrypoint=entrypoint, params=params)
+            job_id = job_deployment.job_name()
+
+            return {'job_id': job_id}, 202
 
     _RetrainResource.__name__ = f'_RetrainResource_{uuid4()}'
 
