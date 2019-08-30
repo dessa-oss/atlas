0735b5d57d3aacdb6864b741f28b29033ddb371c
pairing <pairing@dessa.com>
2019-08-30 15:32:35 -0400

    [AM TR] make tensorboard controller work with tests

--- foundations_rest_api/src/test/v2beta/controllers/test_tensorboard_controller.py
+++ foundations_rest_api/src/test/v2beta/controllers/test_tensorboard_controller.py
@@ -13,17 +13,28 @@ from mock import patch
 from foundations.artifacts.syncable_directory import SyncableDirectory
 from foundations_rest_api.v2beta.controllers.tensorboard_controller import TensorboardController
 from foundations_rest_api.v2beta.models.property_model import PropertyModel
-from foundations_spec import Spec, let, let_now, set_up, Mock
+from foundations_spec import *
 
 class TestTensorboardController(Spec):
+
+    mock_request_post = let_patch_mock('requests.post')
 
     @let_now
     def config_manager(self):
         from foundations_contrib.config_manager import ConfigManager
 
         config_manager = ConfigManager()
-        config_manager['TENSORBOARD_HOST'] = '10.103.113.163'
+        config_manager['TENSORBOARD_API_HOST'] = self.api_host_name
+        config_manager['TENSORBOARD_HOST'] = self.host_name
         return self.patch('foundations_contrib.global_state.config_manager', config_manager)
+
+    @let
+    def api_host_name(self):
+        return f'http://{self.faker.word()}:32766'
+
+    @let
+    def host_name(self):
+        return f'http://{self.faker.word()}:32767'
 
     @let
     def controller(self):
@@ -33,12 +44,20 @@ class TestTensorboardController(Spec):
     def job_id(self):
         return [self.faker.uuid4()]
 
+    @let
+    def params(self):
+        return {'tensorboard_locations': [{'job_id': self.job_id, 'synced_directory': 'tb_data'}]}
+
     @set_up
     def set_up(self):
-        self.controller.params = {'tensorboard_locations': [{'job_id': self.job_id, 'synced_directory': 'tb_data'}]}
+        self.controller.params = self.params
 
-    @patch('requests.post', Mock(return_value=('Sucess', 200)))
-    def test_tensorboard_controller_post(self):
-        import requests
+    def test_tensorboard_controller_post_posts_to_the_tensorboard_api_server_with_given_params(self):
         self.controller.post()
-        requests.post.assert_called_with(f'http://10.103.113.163/create_sym_links', json=self.controller.params)
+        self.mock_request_post.assert_called_with(f'{self.api_host_name}/create_sym_links', json=self.params)
+
+    def test_tensorboard_controller_post(self):
+        self.controller.post()
+        self.assertEqual({'url': f'{self.host_name}'}, self.controller.post().as_json())
+ 
+        
