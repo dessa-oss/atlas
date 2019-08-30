7048dab7f8a9d4f14774ff0168a079db60c0c5ab
foundations-team <foundations-team@dessa.com>
2019-08-30 16:24:01 -0400

    Revert "[NN JA] add datetime attribute"
    This reverts commit 968a5f201b0590de46ec785a95c38437c17bc1c4.

--- foundations_orbit_sdk/src/test/test_data_contract.py
+++ foundations_orbit_sdk/src/test/test_data_contract.py
@@ -15,15 +15,6 @@ class TestDataContract(Spec):
     def contract_name(self):
         return self.faker.word()
 
-    @let_now
-    def datetime_today(self):
-        return self.faker.date_time()
-
-    @set_up
-    def set_up(self):
-        datetime = self.patch('datetime.datetime')
-        datetime.today.return_value = self.datetime_today
-
     def test_can_import_data_contract_from_foundations_orbit_top_level(self):
         import foundations_orbit
         self.assertEqual(DataContract, foundations_orbit.DataContract)
@@ -31,7 +22,3 @@ class TestDataContract(Spec):
     def test_data_contract_has_contract_name(self):
         contract = DataContract(self.contract_name)
         self.assertEqual(self.contract_name, contract.contract_name)
-
-    def test_data_contract_has_datetime_as_datetime_today(self):
-        contract = DataContract(self.contract_name)
-        self.assertEqual(self.datetime_today, contract.datetime)
