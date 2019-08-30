473cb81471ecf916cad6679f87542e81a800f3b1
foundations-team <foundations-team@dessa.com>
2019-08-30 16:35:39 -0400

    [NN JA] options has default special values numpy nan

--- foundations_orbit_sdk/src/foundations_orbit/data_contract.py
+++ foundations_orbit_sdk/src/foundations_orbit/data_contract.py
@@ -8,10 +8,12 @@ Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 class DataContract(object):
     
     def __init__(self, contract_name):
+        import numpy
         from foundations_orbit.data_contract_options import DataContractOptions
 
         self.options = DataContractOptions(
             max_bins=50,
             check_schema=True,
-            check_row_count=False
+            check_row_count=False,
+            special_values=[numpy.nan]
         )
