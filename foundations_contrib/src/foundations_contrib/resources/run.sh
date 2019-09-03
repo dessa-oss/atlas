a4fcb072ec8ff005110cb4b8e68782ddb619549a
thomasrogers03 <thomasrogers03@gmail.com>
2019-09-03 11:13:00 -0400

    [TR] revert run.sh to previous state

--- foundations_contrib/src/foundations_contrib/resources/run.sh
+++ foundations_contrib/src/foundations_contrib/resources/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 BASEDIR=$(dirname "$0")
-# VENV_DIRECTORY=$(mktemp -d)/venv
+VENV_DIRECTORY=$(mktemp -d)/venv
 export PYTHONDONTWRITEBYTECODE=1
 
 
@@ -29,44 +29,36 @@ if [[ -z "${python_path}" ]]; then
   python_path=$(which python || which python3)
 fi
 
-# pip_options="--disable-pip-version-check"
-
-# cd $BASEDIR && \
-#   mkdir -p job_source && \
-#   cd job_source && \
-#   tar -xf ../job.tgz && \
-#   cd .. && \
-#   with_output_redirect $python_path -m pip ${pip_options} install virtualenv && \
-#   with_output_redirect $python_path -m virtualenv --system-site-packages $VENV_DIRECTORY
+pip_options="--disable-pip-version-check"
 
 cd $BASEDIR && \
   mkdir -p job_source && \
   cd job_source && \
   tar -xf ../job.tgz && \
-  cd ..
+  cd .. && \
+  with_output_redirect $python_path -m pip ${pip_options} install virtualenv && \
+  with_output_redirect $python_path -m virtualenv --system-site-packages $VENV_DIRECTORY
 
-# with_output_redirect stat $VENV_DIRECTORY/bin/activate
-# if [ $? -eq 0 ]; then
-#   activate_path=$VENV_DIRECTORY/bin/activate
-# else
-#   activate_path=$VENV_DIRECTORY/Scripts/activate
-# fi
+with_output_redirect stat $VENV_DIRECTORY/bin/activate
+if [ $? -eq 0 ]; then
+  activate_path=$VENV_DIRECTORY/bin/activate
+else
+  activate_path=$VENV_DIRECTORY/Scripts/activate
+fi
 
-# cd $BASEDIR && \
-#   . $activate_path && \
-#   with_output_redirect python -m pip install ${pip_options} -r foundations_requirements.txt && \
-#   with_output_redirect echo Running python version `python --version` located at $(which python) && \
-#   touch requirements.txt && \
-#   with_output_redirect python -m pip install ${pip_options} -U -r requirements.txt && \
-#   PYTHONUNBUFFERED=TRUE python foundations_main.py
-
-PYTHONUNBUFFERED=TRUE $python_path foundations_main.py
+cd $BASEDIR && \
+  . $activate_path && \
+  with_output_redirect python -m pip install ${pip_options} -r foundations_requirements.txt && \
+  with_output_redirect echo Running python version `python --version` located at $(which python) && \
+  touch requirements.txt && \
+  with_output_redirect python -m pip install ${pip_options} -U -r requirements.txt && \
+  PYTHONUNBUFFERED=TRUE python foundations_main.py
   
 status=$?
 
-# deactivate
-# rm -rf $VENV_DIRECTORY && \
-#   rm -rf /tmp/pip* && \
-#   rm -rf $HOME/.cache/pip
+deactivate
+rm -rf $VENV_DIRECTORY && \
+  rm -rf /tmp/pip* && \
+  rm -rf $HOME/.cache/pip
 
 exit $status
