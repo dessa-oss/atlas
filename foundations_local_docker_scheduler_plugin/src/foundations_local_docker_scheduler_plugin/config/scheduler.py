32750c026fb484387f6940dc58883be08235aa51
ekhl <el@deeplearni.ng>
2019-09-04 22:44:11 -0400

    commented out section on retaining copy in job store

--- foundations_local_docker_scheduler_plugin/src/foundations_local_docker_scheduler_plugin/config/scheduler.py
+++ foundations_local_docker_scheduler_plugin/src/foundations_local_docker_scheduler_plugin/config/scheduler.py
@@ -4,11 +4,12 @@ def translate(config):
     result = {
         'deployment_implementation': _deployment_implementation(),
         'redis_url': config.get('redis_url', 'redis://localhost:6379'),
-        'worker_container_overrides': config['worker'],
-        'job_store_root': config['job_store_root'],
+        'worker_container_overrides': config.get('worker', {}),
+        # 'job_store_root': config['job_store_root'],
         'job_results_root': config['job_results_root'],
         'working_dir_root': config['working_dir_root'],
-        'scheduler_url': config['scheduler_url']
+        'scheduler_url': config['scheduler_url'],
+        'container_config_root': config['container_config_root']
     }
 
     return result
