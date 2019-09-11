3d59f32cd16212269a3bdf1454329f813cec191b
Katherine <k.bancroft@dessa.com>
2019-09-11 13:06:57 -0400

    [CZ JA] return listing from controller if listing exists

--- foundations_orbit_rest_api/src/foundations_orbit_rest_api/v1/controllers/validation_report_listings_controller.py
+++ foundations_orbit_rest_api/src/foundations_orbit_rest_api/v1/controllers/validation_report_listings_controller.py
@@ -5,9 +5,21 @@ Proprietary and confidential
 Written by Susan Davis <s.davis@dessa.com>, 11 2018
 """
 
+from foundations_core_rest_api_components.utils.api_resource import api_resource
+
+@api_resource('/api/v1/projects/<string:project_name>/validation_report_list')
 class ValidationReportListingsController(object):
     
     def index(self):
-        from foundations_core_rest_api_components.lazy_result import LazyResult
         from foundations_core_rest_api_components.response import Response
-        return Response('ValidationReportListings', LazyResult(lambda: []))
+        from foundations_orbit_rest_api.v1.models.validation_report_listing import ValidationReportListing
+
+        project_name = self.params.pop('project_name')
+        promise = ValidationReportListing.all(project_name=project_name).map(self._first_if_any)
+        return Response('ValidationReportListings', promise)
+
+    def _first_if_any(self, listings):
+        if listings:
+            return listings[0:1]
+
+        return []
--- foundations_orbit_rest_api/src/test/v1/controllers/test_validation_report_listings_controller.py
+++ foundations_orbit_rest_api/src/test/v1/controllers/test_validation_report_listings_controller.py
@@ -11,6 +11,34 @@ from foundations_orbit_rest_api.v1.controllers import ValidationReportListingsCo
 
 class TestValidationReportListingsController(Spec):
 
+    @let
+    def project_name(self):
+        return self.faker.word()
+
+    @let
+    def model_package(self):
+        return self.faker.word()
+
+    @let
+    def data_contract(self):
+        return self.faker.word()
+
+    @let
+    def inference_period(self):
+        return self.faker.date()
+
+    @let
+    def model_package_2(self):
+        return self.faker.word()
+
+    @let
+    def data_contract_2(self):
+        return self.faker.word()
+
+    @let
+    def inference_period_2(self):
+        return self.faker.date()
+
     @let_now
     def redis_connection(self):
         import fakeredis
@@ -23,10 +51,33 @@ class TestValidationReportListingsController(Spec):
     @set_up
     def set_up(self):
         self.redis_connection.flushall()
+        self.controller.params = {'project_name': self.project_name}
 
-    def test_validation_report_listings_controller_gives_empty_list_as_response_if_no_reports_in_redis(self):
+    def test_index_returns_empty_list_as_response_if_no_reports_in_redis(self):
         self.assertEqual([], self.controller.index().as_json())
 
     def test_index_returns_resource_with_resource_name_validation_report_listings(self):
         result = self.controller.index()
         self.assertEqual('ValidationReportListings', result.resource_name())
+
+    def test_index_returns_list_with_one_listing_if_exists_in_redis(self):
+        self._register_report(self.project_name, self.model_package, self.data_contract, self.inference_period)
+        result = self.controller.index()
+
+        expected_result = [
+            {
+                "data_contract": self.data_contract,
+                "model_package": self.model_package,
+                "inference_period": self.inference_period
+            }
+        ]
+
+        self.assertEqual(expected_result, result.as_json())
+
+    @staticmethod
+    def _key_to_write(project_name, model_package, data_contract):
+        return f'projects:{project_name}:models:{model_package}:validation:{data_contract}'
+
+    def _register_report(self, project_name, model_package, data_contract, inference_period):
+        key_to_write = self._key_to_write(project_name, model_package, data_contract)
+        self.redis_connection.hset(key_to_write, inference_period, 'dummy_report')
