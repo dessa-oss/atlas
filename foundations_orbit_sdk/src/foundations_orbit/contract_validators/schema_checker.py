79a3a579e2165993b529d39df53b422ce76a2267
Katherine <k.bancroft@dessa.com>
2019-09-12 14:14:13 -0400

    [CZ JA] refactor schema checker out into own class

--- foundations_orbit_sdk/src/foundations_orbit/contract_validators/schema_checker.py
+++ foundations_orbit_sdk/src/foundations_orbit/contract_validators/schema_checker.py
@@ -7,8 +7,75 @@ Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 
 class SchemaChecker(object):
     
-    def __init__(self, ref_column_names, ref_column_types):
-        pass
+    def __init__(self, column_names, column_types):
+        self._column_names = column_names
+        self._column_names_set = set(column_names)
 
-    def schema_check_results(self, column_names, column_types):
-        return {'passed': True}
+        self._column_types = column_types
+
+    def schema_check_results(self, columns_to_validate, types_to_validate):
+        import pandas
+
+        schema_check_results = {}
+
+        if self._column_names_match(columns_to_validate):
+            if self._data_types_match(types_to_validate):
+                schema_check_results['passed'] = True
+                return schema_check_results
+
+            schema_check_results['passed'] = False
+            schema_check_results['error_message'] = 'column datatype mismatches'
+            schema_check_results.update(self._type_mismatch_error_information(types_to_validate))
+            return schema_check_results
+
+        schema_check_results['passed'] = False
+
+        current_column_names = set(columns_to_validate)
+
+        if self._column_sets_not_equal(current_column_names):
+            schema_check_results['error_message'] = 'column sets not equal'
+            schema_check_results.update(self._column_sets_not_equal_error_information(current_column_names))
+            return schema_check_results
+
+        schema_check_results['error_message'] = 'columns not in order'
+
+        ref_column_series = pandas.Series(self._column_names)
+        current_column_series = pandas.Series(columns_to_validate)
+
+        schema_check_results.update(self._column_sets_in_wrong_order_information(ref_column_series, current_column_series))
+        return schema_check_results
+
+    def _column_names_match(self, columns_to_validate):
+        return self._column_names == columns_to_validate
+
+    def _data_types_match(self, types_to_validate):
+        return self._column_types == types_to_validate
+
+    def _column_sets_not_equal(self, current_column_names):
+        return self._column_names_set != current_column_names
+
+    def _column_sets_not_equal_error_information(self, current_column_names):
+        missing_in_ref = current_column_names - self._column_names_set
+        missing_in_current = self._column_names_set - current_column_names
+
+        return {'missing_in_ref': list(missing_in_ref), 'missing_in_current': list(missing_in_current)}
+
+    @staticmethod
+    def _column_sets_in_wrong_order_information(ref_column_series, current_column_series):
+        columns_out_of_order = current_column_series[current_column_series != ref_column_series]
+        return {'columns_out_of_order': list(columns_out_of_order)}
+
+    def _type_mismatch_error_information(self, types_to_validate):
+        mismatched_columns = {}
+
+        for column_name in self._column_types.keys():
+            ref_type = self._column_types[column_name]
+            current_type = types_to_validate[column_name]
+
+            if ref_type != current_type:
+                mismatched_columns[column_name] = {
+                    'ref_type': ref_type,
+                    'current_type': current_type
+                }
+
+        return {'cols': mismatched_columns}
--- foundations_orbit_sdk/src/test/contract_validators/test_schema_checker.py
+++ foundations_orbit_sdk/src/test/contract_validators/test_schema_checker.py
@@ -16,10 +16,188 @@ class TestSchemaChecker(Spec):
         import pandas
         return pandas.DataFrame()
 
+    @let
+    def column_name(self):
+        return self.faker.word()
+
+    @let
+    def column_name_2(self):
+        return self._generate_distinct([self.column_name], self.faker.word)
+
+    @let
+    def column_name_3(self):
+        return self._generate_distinct([self.column_name, self.column_name_2], self.faker.word)
+
+    @let
+    def column_name_4(self):
+        return self._generate_distinct([self.column_name, self.column_name_2, self.column_name_3], self.faker.word)
+
+    @let_now
+    def one_column_dataframe(self):
+        import numpy
+        import pandas
+
+        return pandas.DataFrame(columns=[self.column_name], data=[4], dtype=numpy.int8)
+
+    @let_now
+    def one_column_dataframe_two_rows(self):
+        import numpy
+        import pandas
+
+        return pandas.DataFrame(columns=[self.column_name], data=[4, 5], dtype=numpy.int8)
+
+    @let_now
+    def one_column_dataframe_four_rows(self):
+        import numpy
+        import pandas
+
+        return pandas.DataFrame(columns=[self.column_name], data=[4, 5, 6, 7], dtype=numpy.int8)
+
+    @let_now
+    def one_column_dataframe_different_data_type(self):
+        import numpy
+        import pandas
+
+        return pandas.DataFrame(columns=[self.column_name], data=[4], dtype=numpy.int16)
+
+    @let_now
+    def one_column_dataframe_different_column_name(self):
+        import pandas
+        return pandas.DataFrame(columns=[self.column_name_2])
+
+    @let_now
+    def two_column_dataframe_no_rows(self):
+        import pandas
+        return pandas.DataFrame(columns=[self.column_name, self.column_name_2])
+
+    @let_now
+    def two_column_dataframe(self):
+        import pandas
+        return pandas.DataFrame(columns=[self.column_name, self.column_name_2], data=[[1, 2.0]])
+
+    @let_now
+    def two_column_dataframe_different_types(self):
+        import pandas
+        return pandas.DataFrame(columns=[self.column_name, self.column_name_2], data=[[1, 2]])
+
+    @let_now
+    def two_column_dataframe_no_rows_different_second_column(self):
+        import pandas
+        return pandas.DataFrame(columns=[self.column_name, self.column_name_3])
+
+    @let_now
+    def two_column_dataframe_columns_wrong_order(self):
+        import pandas
+        return pandas.DataFrame(columns=[self.column_name_2, self.column_name])
+
+    @let_now
+    def four_column_dataframe_no_rows(self):
+        import pandas
+        return pandas.DataFrame(columns=[self.column_name, self.column_name_2, self.column_name_3, self.column_name_4])
+
+    @let_now
+    def four_column_dataframe_no_rows_different_order(self):
+        import pandas
+        return pandas.DataFrame(columns=[self.column_name_4, self.column_name_2, self.column_name_3, self.column_name])
+
+    def _generate_distinct(self, reference_values, generating_callback):
+        candidate_value = generating_callback()
+        return candidate_value if candidate_value not in reference_values else self._generate_distinct(reference_values, generating_callback)
+
     def test_check_schema_of_empty_dataframe_against_itself_passes(self):
-        schema_checker = self._schema_checker_from_dataframe(self.empty_dataframe)
-        column_names, column_types = self._dataframe_statistics(self.empty_dataframe)
-        self.assertEqual({'passed': True}, schema_checker.schema_check_results(column_names, column_types))
+        self._assert_schema_check_results_for_dataframe(self.empty_dataframe, self.empty_dataframe, {'passed': True})
+
+    def test_check_schema_of_dataframe_with_zero_columns_against_dataframe_with_one_column_fails_schema_check(self):
+        expected_schema_check_results = {
+            'passed': False,
+            'error_message': 'column sets not equal',
+            'missing_in_ref': [],
+            'missing_in_current': [self.column_name]
+        }
+
+        self._assert_schema_check_results_for_dataframe(self.one_column_dataframe, self.empty_dataframe, expected_schema_check_results)
+
+    def test_check_schema_of_dataframe_with_one_column_against_another_dataframe_with_one_column_but_different_name_fails_schema_check(self):
+        expected_schema_check_results = {
+            'passed': False,
+            'error_message': 'column sets not equal',
+            'missing_in_ref': [self.column_name_2],
+            'missing_in_current': [self.column_name]
+        }
+
+        self._assert_schema_check_results_for_dataframe(self.one_column_dataframe, self.one_column_dataframe_different_column_name, expected_schema_check_results)
+
+    def test_check_schema_of_dataframe_with_one_column_against_another_dataframe_with_two_columns_first_column_the_same_fails_schema_check(self):
+        expected_schema_check_results = {
+            'passed': False,
+            'error_message': 'column sets not equal',
+            'missing_in_ref': [self.column_name_2],
+            'missing_in_current': []
+        }
+
+        self._assert_schema_check_results_for_dataframe(self.one_column_dataframe, self.two_column_dataframe_no_rows, expected_schema_check_results)
+
+    def test_check_schema_of_dataframe_with_one_column_against_itself_passes_schema_check(self):
+        self._assert_schema_check_results_for_dataframe(self.one_column_dataframe, self.one_column_dataframe, {'passed': True})
+
+    def test_check_schema_of_dataframe_with_multiple_columns_against_itself_passes_schema_check(self):
+        self._assert_schema_check_results_for_dataframe(self.two_column_dataframe_no_rows, self.two_column_dataframe_no_rows, {'passed': True})
+
+    def test_check_schema_of_dataframe_with_two_columns_against_different_dataframe_with_two_columns_fails_schema_check(self):
+        expected_schema_check_results = {
+            'passed': False,
+            'error_message': 'column sets not equal',
+            'missing_in_ref': [self.column_name_3],
+            'missing_in_current': [self.column_name_2]
+        }
+
+        self._assert_schema_check_results_for_dataframe(self.two_column_dataframe_no_rows, self.two_column_dataframe_no_rows_different_second_column, expected_schema_check_results)
+
+    def test_check_schema_of_column_names_wrong_order_fails_schema_check(self):
+        expected_schema_check_results = {
+            'passed': False,
+            'error_message': 'columns not in order',
+            'columns_out_of_order': [self.column_name_2, self.column_name]
+        }
+
+        self._assert_schema_check_results_for_dataframe(self.two_column_dataframe_no_rows, self.two_column_dataframe_columns_wrong_order, expected_schema_check_results)
+
+    def test_check_schema_of_column_names_wrong_order_fails_schema_check_more_rows(self):
+        expected_schema_check_results = {
+            'passed': False,
+            'error_message': 'columns not in order',
+            'columns_out_of_order': [self.column_name_4, self.column_name]
+        }
+
+        self._assert_schema_check_results_for_dataframe(self.four_column_dataframe_no_rows, self.four_column_dataframe_no_rows_different_order, expected_schema_check_results)
+
+    def test_check_schema_of_single_column_dataframe_column_types_fails_when_column_type_does_not_match(self):
+        expected_schema_check_results = {
+            'passed': False,
+            'error_message': 'column datatype mismatches',
+            'cols': {
+                self.column_name: {
+                    'ref_type': 'int8',
+                    'current_type': 'int16'
+                }
+            }
+        }
+
+        self._assert_schema_check_results_for_dataframe(self.one_column_dataframe, self.one_column_dataframe_different_data_type, expected_schema_check_results)
+
+    def test_check_schema_of_two_column_dataframe_column_types_fails_when_column_type_does_not_match(self):
+        expected_schema_check_results = {
+            'passed': False,
+            'error_message': 'column datatype mismatches',
+            'cols': {
+                self.column_name_2: {
+                    'ref_type': 'float64',
+                    'current_type': 'int64'
+                }
+            }
+        }
+
+        self._assert_schema_check_results_for_dataframe(self.two_column_dataframe, self.two_column_dataframe_different_types, expected_schema_check_results)
 
     def _dataframe_statistics(self, dataframe):
         column_names = list(dataframe.columns)
@@ -30,3 +208,9 @@ class TestSchemaChecker(Spec):
     def _schema_checker_from_dataframe(self, dataframe):
         column_names, column_types = self._dataframe_statistics(dataframe)
         return SchemaChecker(column_names, column_types)
+
+    def _assert_schema_check_results_for_dataframe(self, ref_dataframe, current_dataframe, expected_results):
+        schema_checker = self._schema_checker_from_dataframe(ref_dataframe)
+        column_names, column_types = self._dataframe_statistics(current_dataframe)
+
+        self.assertEqual(expected_results, schema_checker.schema_check_results(column_names, column_types))
