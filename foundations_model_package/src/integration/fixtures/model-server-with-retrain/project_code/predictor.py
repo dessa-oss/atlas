112735da61746467ccccbe199a2fb58597d0a412
pairing <developers@dessa.com>
2019-09-12 09:37:16 -0400

    [KD] added fixtures

--- foundations_model_package/src/integration/fixtures/model-server-with-retrain/project_code/predictor.py
+++ foundations_model_package/src/integration/fixtures/model-server-with-retrain/project_code/predictor.py
@@ -1,8 +1,8 @@
 import foundations
 import pickle
 
-with open('model_file', 'rb') as model_file:
-    model_result = pickle.load(model_file)
+# with open('model_file', 'rb') as model_file:
+#     model_result = pickle.load(model_file)
 
-def predict(a):
-    return {'a': a + model_result}
+def predict(a, b):
+    return {'a': a+1, 'b': b+2}
