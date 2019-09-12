ad4937b131b131b466c5bd9280c41fa7dd96f335
Katherine <k.bancroft@dessa.com>
2019-09-12 16:00:54 -0400

    [JA CZ] compute l infinity

--- foundations_orbit_sdk/src/foundations_orbit/contract_validators/statistics.py
+++ foundations_orbit_sdk/src/foundations_orbit/contract_validators/statistics.py
@@ -5,5 +5,11 @@ Proprietary and confidential
 Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 """
 
-def l_infinity(*args):
-    raise ValueError('cannot take l_infinity distance of empty arrays')
+def l_infinity(reference_percentages, current_percentages):
+    import numpy
+
+    if len(reference_percentages) == 0:
+        raise ValueError('cannot take l_infinity distance of empty arrays')
+    
+    absolute_difference = numpy.abs(reference_percentages - current_percentages)
+    return numpy.max(absolute_difference)
