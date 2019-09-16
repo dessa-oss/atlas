8d02e9765547f60bbd59a512ab63ec5a3b7f50d2
shazraz <s.raza@dessa.com>
2019-09-16 18:54:35 -0400

    Updated workers to use run.sh as entrypoint

---
+++ docker/worker_images/run.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+cd /job/job_source
+if [ -f requirements.txt ]; then
+    echo "Installing user requirements"
+    pip install -r requirements.txt
+else
+    echo "No user requirements found."
+fi
+
+python "$@"
