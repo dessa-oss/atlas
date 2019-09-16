8be489bdc3ec9a955f0037019ff9ce77453dbd56
Thomas Rogers <t.rogers@dessa.com>
2019-09-16 11:24:06 -0400

    [TR AM CZ NN] apply the orbit model

--- foundations_contrib/src/integration/resources/fixtures/test_server/setup_test_server.sh
+++ foundations_contrib/src/integration/resources/fixtures/test_server/setup_test_server.sh
@@ -9,11 +9,13 @@ model_status () {
     echo $(kubectl -n $namespace get po | grep $project_name-$model_name | awk '{print $3}')
 }
 
-envsubst < integration/resources/fixtures/test_server/test-server-deployment-and-service.yaml | kubectl create -f - && \
-python foundations_contrib/resources/model_serving/orbit/ingress_modifier.py $project_name $model_name
+envsubst < integration/resources/fixtures/test_server/test-server-deployment-and-service.yaml | kubectl apply -f - && \
+    python foundations_contrib/resources/model_serving/orbit/ingress_modifier.py $project_name $model_name
+status=$?
 
 while [ "Pending" == $(model_status) ] || [ "" == $(model_status) ] || [ "ContainerCreating" == $(model_status) ]
 do 
     sleep 2
 done
 
+exit $status
