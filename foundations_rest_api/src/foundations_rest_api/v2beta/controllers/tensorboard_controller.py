6c9afbd7762a7a969d25ea2804c3f72b1ee9169c
ekhl <el@deeplearni.ng>
2019-09-16 22:54:46 -0400

    [EL] fixed typos in tensorboard controller

--- foundations_rest_api/src/foundations_rest_api/v2beta/controllers/tensorboard_controller.py
+++ foundations_rest_api/src/foundations_rest_api/v2beta/controllers/tensorboard_controller.py
@@ -21,20 +21,20 @@ class TensorboardController:
         if job_ids is None:
             return Response('Bad Request', LazyResult(lambda: 'Expected a json object with key \'job_ids\''), status=400)
         
-        tb_locations = _transform_request(job_ids)
+        tb_locations = self._transform_request(job_ids)
 
         response = requests.post(f'{self._tensorboard_api_host()}/create_sym_links', json=tb_locations)
         if response.status_code == 400:
             return Response('Bad Request', LazyResult(lambda: response.text), status=400)
         elif response.status_code == 500:
-            return Response('Tensorboard Rest API Error', LazyResult(lambda: 'Internal Server Error', status=500)) 
+            return Response('Tensorboard Rest API Error', LazyResult(lambda: 'Internal Server Error'), status=500)
         return Response('Jobs', LazyResult(lambda: {'url': f'{self._tensorboard_host()}'}))
     
     @staticmethod
     def _transform_request(job_ids):
         path = 'archive/{}/synced_directories/__tensorboard__'
-        tb_locations = [{'job_id': str(job_id), 'synced_directory': path.format(job_id)} 
-                        for job_id in job_ids}]
+        tb_locations = [{'job_id': str(job_id), 'synced_directory': path.format(job_id)}
+                        for job_id in job_ids]
         return {'tensorboard_locations': tb_locations}
 
     @staticmethod
