8f5f22cc1f8f340293e17df1d76709b1aa6437d9
Alessio Iannella <alessio.iannella@ellefsontech.com>
2019-09-18 21:15:54 -0400

    [AI] ATLAS UI: show 10 tags and view more on projects page

--- foundations_ui/src/js/components/ProjectPage/ProjectSummary.js
+++ foundations_ui/src/js/components/ProjectPage/ProjectSummary.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
+import HoverCell from '../JobListPage/cells/HoverCell';
 
 const ProjectSummary = (props) => {
+  const [showAllTags, setShowAllTags] = React.useState(false);
+
   const packageClick = () => {
     const { history, project } = props;
 
@@ -14,7 +17,22 @@ const ProjectSummary = (props) => {
     );
   };
 
+  const onClickShowTags = () => {
+    const { project } = props;
+    const value = !showAllTags;
+    setShowAllTags(value);
+  };
+
+  const onMouseLeave = () => {
+    setShowAllTags(false);
+  };
+
   const { project } = props;
+
+  let expandedTagSpans = [];
+  project.tags.forEach((tag) => {
+    expandedTagSpans.push(<span key={'tag-'.concat(tag)}>{tag}</span>);
+  });
 
   return (
     <div
@@ -42,9 +60,22 @@ const ProjectSummary = (props) => {
       </div>
       <div className="project-summary-tags-container">
         <p>tags</p>
-        {project.tags.slice(0, 5).map((tag) => {
+        {project.tags.slice(0, 10).map((tag) => {
           return <span key={tag}>{tag}</span>;
         })}
+        {project.tags.length > 10
+          && (
+            <span
+              className="span-more"
+              role="presentation"
+              onClick={onClickShowTags}
+              onKeyDown={() => {}}
+            >
+                ...
+            </span>
+          )
+        }
+        {showAllTags && <HoverCell onMouseLeave={onMouseLeave} textToRender={expandedTagSpans} />}
       </div>
     </div>
   );
