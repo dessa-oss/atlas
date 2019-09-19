b6ab5382e1eeb9d5a698e54770f6b9c58513c9d1
Calvin Choi <calvinchoi@Calvins-MacBook-Pro.local>
2019-09-19 13:08:17 -0400

    [CC PL] More separate CE build files

--- build_gui_ce.sh
+++ build_gui_ce.sh
@@ -12,6 +12,6 @@ then
         mkdir -p tmp/pip_wheels && \
         cp dist/* tmp/pip_wheels && \
         cp docker/gui_ce_Dockerfile foundations_ui && \
-        python build_gui.py && \
+        python build_gui_ce.py && \
         rm -rf foundations_ui/gui_ce_Dockerfile
 fi
