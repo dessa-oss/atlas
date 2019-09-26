79ced58e356586b8fe372abd7f733b38c9330988
shazraz <s.raza@dessa.com>
2019-09-26 16:13:40 -0400

    [SR] Removed pycrypto from UI and REST API requirements

--- foundations_rest_api/setup.py
+++ foundations_rest_api/setup.py
@@ -36,7 +36,6 @@ setup(
         'Flask-Cors==3.0.6',
         'Flask==1.1.0',
         'Werkzeug==0.15.4',
-        'pycrypto==2.6.1',
     ],
     packages=find_packages(package_source),
     package_dir={'': package_source},
