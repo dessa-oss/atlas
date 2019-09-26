35b040a7711e6ad0ef51f3a3e29a18196b69d2ba
Alessio Iannella <alessio.iannella@ellefsontech.com>
2019-09-26 14:58:34 -0400

    [AI] ORBIT UI - #639 : refactor code to set interval every 30s

--- foundations_ui_orbit/src/js/components/PackagePage/PackageToolbar.js
+++ foundations_ui_orbit/src/js/components/PackagePage/PackageToolbar.js
@@ -5,118 +5,166 @@ import PackagePageHeader from "./PackagePageHeader";
 import { get, getMaster, postMaster } from "../../actions/BaseActions";
 import moment from "moment";
 
-const PackageToolbar = props => {
-  const [demoOpen, setDemoOpen] = React.useState(true);
-  const [selectedDate, setSelectedDate] = React.useState("");
-  const [resetting, setResetting] = React.useState(false);
-  const [attribute, setAttribute] = React.useState("");
+class PackageToolbar extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const reload = () => {
+    this.state = {
+      demoOpen: true,
+      selectedDate: "",
+      resetting: false,
+      attribute: "",
+      timerId: -1
+    };
+
+    this.onClickOpenDemo = this.onClickOpenDemo.bind(this);
+    this.onClickCloseDemo = this.onClickCloseDemo.bind(this);
+    this.onClickReset = this.onClickReset.bind(this);
+    this.onClickSendIssue = this.onClickSendIssue.bind(this);
+    this.onChangeAttribute = this.onChangeAttribute.bind(this);
+  }
+
+  reload() {
     getMaster("simulator/get_date").then(result => {
       let formattedDate = moment(result.current_date).format("YYYY-MM-DD").toString();
 
-      setSelectedDate(formattedDate);
+      this.setState({
+        selectedDate: formattedDate
+      });
     });
-  };
+  }
 
-  React.useEffect(() => {
-    reload();
-  }, []);
+  componentDidMount() {
+    this.reload();
+    const value = setInterval(() => {
+      this.reload();
+    }, 30000);
+    this.setState({
+      timerId: value
+    });
+  }
 
-  const onClickOpenDemo = () => {
-    setDemoOpen(true);
-  };
+  clearTimer() {
+    const { timerId } = this.state;
+    clearInterval(timerId);
+  }
 
-  const onClickCloseDemo = () => {
-    setDemoOpen(false);
-  };
+  componentWillUnmount() {
+    this.clearTimer();
+  }
 
-  const onChangeAttribute = e => {
-    setAttribute(e.target.value);
-  };
+  onClickOpenDemo() {
+    this.setState({
+      demoOpen: true
+    });
+  }
 
-  const onClickReset = () => {
+  onClickCloseDemo() {
+    this.setState({
+      demoOpen: false
+    });
+  }
+
+  onChangeAttribute(e) {
+    this.setState({
+      attribute: e.target.value
+    });
+  }
+
+  onClickReset() {
     postMaster("simulator_admin/restart", {})
       .then(() => {
-        reload();
+        this.reload();
       });
-  };
+  }
 
-  const onClickSendIssue = () => {
+  onClickSendIssue() {
+    const { attribute } = this.state;
     if (attribute !== "") {
-      setResetting(true);
+      this.setState({
+        resetting: true
+      });
 
       getMaster(`simulator/fix_special_value?column_name=${attribute}`)
         .then(() => {
-          setResetting(false);
-          reload();
+          this.setState({
+            resetting: false
+          });
+          this.reload();
         })
         .catch(() => {
-          setResetting(false);
+          this.setState({
+            resetting: false
+          });
         });
     }
-  };
+  }
 
-  const { project, title, openTutorial } = props;
+  render() {
+    const { project, title, openTutorial } = this.props;
+    const {
+      demoOpen, resetting, selectedDate, attribute
+    } = this.state;
 
-  return (
-    <div className="layout-package-toolbar-container">
-      <div className="job-header-logo-container">
-        <div className="i--icon-logo" />
-        <PackagePageHeader
-          pageName={project.name}
-          pageSubName={
-            title === ""
-              ? "Inference Automation and Model Management"
-              : title
-          }
-        />
-        <div className="icon-tutorial" onClick={openTutorial}>
-          <p>?</p>
-        </div>
-        {demoOpen === true ? (
-          <div className="container-layout-demo">
-            <div className="container-demo-arrow">
+    return (
+      <div className="layout-package-toolbar-container">
+        <div className="job-header-logo-container">
+          <div className="i--icon-logo" />
+          <PackagePageHeader
+            pageName={project.name}
+            pageSubName={
+              title === ""
+                ? "Inference Automation and Model Management"
+                : title
+            }
+          />
+          <div className="icon-tutorial" onClick={openTutorial}>
+            <p>?</p>
+          </div>
+          {demoOpen === true ? (
+            <div className="container-layout-demo">
+              <div className="container-demo-arrow">
+                <i
+                  className="arrow right demo-icon-arrow"
+                  onClick={this.onClickCloseDemo}
+                />
+              </div>
+              {resetting === true && (
+                <div className="container-resetting">
+                  <p>Request sent. This might take a moment.</p>
+                </div>
+              )}
+              {resetting === false && (
+                <div className="container-demo-actions">
+                  <div className="container-reset">
+                    <p className="label-date">{selectedDate}</p>
+                    <button type="button" className="b--secondary-text button-reset" onClick={this.onClickReset}>
+                      RESET TRIAL
+                    </button>
+                  </div>
+                  <div className="container-issue">
+                    <p>Report Data Issue</p>
+                    <input value={attribute} placeholder="Attribute name" onChange={this.onChangeAttribute} />
+                    <button type="button" className="b--secondary-text button-reset" onClick={this.onClickSendIssue}>
+                      SEND
+                    </button>
+                  </div>
+                </div>
+              )}
+            </div>
+          ) : (
+            <div className="container-layout-demo hidden">
               <i
-                className="arrow right demo-icon-arrow"
-                onClick={onClickCloseDemo}
+                className="arrow left demo-icon-arrow-left"
+                onClick={this.onClickOpenDemo}
               />
             </div>
-            {resetting === true && (
-              <div className="container-resetting">
-                <p>Request sent. This might take a moment.</p>
-              </div>
-            )}
-            {resetting === false && (
-              <div className="container-demo-actions">
-                <div className="container-reset">
-                  <p className="label-date">{selectedDate}</p>
-                  <button type="button" className="b--secondary-text button-reset" onClick={onClickReset}>
-                    RESET TRIAL
-                  </button>
-                </div>
-                <div className="container-issue">
-                  <p>Report Data Issue</p>
-                  <input value={attribute} placeholder="Attribute name" onChange={onChangeAttribute} />
-                  <button type="button" className="b--secondary-text button-reset" onClick={onClickSendIssue}>
-                    SEND
-                  </button>
-                </div>
-              </div>
-            )}
-          </div>
-        ) : (
-          <div className="container-layout-demo hidden">
-            <i
-              className="arrow left demo-icon-arrow-left"
-              onClick={onClickOpenDemo}
-            />
-          </div>
-        )}
+          )}
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 PackageToolbar.propTypes = {
   project: PropTypes.object,
