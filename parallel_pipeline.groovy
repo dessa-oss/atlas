28c56681813c05ce421e7b2963f3c237ab2dc758
pairing <developers@dessa.com>
2019-09-30 08:21:27 -0400

    [KD NN] added another step to rebuild distribution

--- parallel_pipeline.groovy
+++ parallel_pipeline.groovy
@@ -36,70 +36,51 @@ pipeline{
                 }
             }
         }
-        // stage('Python3 Run Unit Tests') {
-        //     steps {
-        //         container("python3") {
-        //             sh "./run_unit_tests.sh"
-        //         }
-        //     }
-        // }
-        // stage('Build Model Package Image and Push to Testing Env') {
-        //     steps {
-        //         container("python3") {
-        //             ws("${WORKSPACE}/foundations_model_package/src"){
-        //                 sh 'docker login docker.shehanigans.net -u $NEXUS_USER -p $NEXUS_PASSWORD'
-        //                 sh 'docker login docker-staging.shehanigans.net -u $NEXUS_USER -p $NEXUS_PASSWORD'
-        //                 sh './build.sh'
-        //                 sh 'docker push docker-staging.shehanigans.net/foundations-model-package:latest'
-        //                 sh './pull_staging_image_onto_scheduler.sh'
-        //             }
-        //         }
-        //     }
-        // }
-        // stage('Python3 Run Integration Tests') {
-        //     steps {
-        //         container("python3") {
-        //             sh 'export FOUNDATIONS_SCHEDULER_HOST=$FOUNDATIONS_SCHEDULER_ACCEPTANCE_HOST && ./run_integration_tests.sh'
-        //         }
-        //     }
-        // }
+        stage('Python3 Run Unit Tests') {
+            steps {
+                container("python3") {
+                    sh "./run_unit_tests.sh"
+                }
+            }
+        }
+        stage('Build Model Package Image and Push to Testing Env') {
+            steps {
+                container("python3") {
+                    ws("${WORKSPACE}/foundations_model_package/src"){
+                        sh 'docker login docker.shehanigans.net -u $NEXUS_USER -p $NEXUS_PASSWORD'
+                        sh 'docker login docker-staging.shehanigans.net -u $NEXUS_USER -p $NEXUS_PASSWORD'
+                        sh './build.sh'
+                        sh 'docker push docker-staging.shehanigans.net/foundations-model-package:latest'
+                        sh './pull_staging_image_onto_scheduler.sh'
+                    }
+                }
+            }
+        }
+        stage('Rebuild Python package using the wheels') {
+            steps {
+                container("python3") {
+                    sh 'python -m pip install ./dist/*.whl'
+                }
+            }
+        }
+        stage('Python3 Run Integration Tests') {
+            steps {
+                container("python3") {
+                    sh 'export FOUNDATIONS_SCHEDULER_HOST=$FOUNDATIONS_SCHEDULER_ACCEPTANCE_HOST && ./run_integration_tests.sh'
+                }
+            }
+        }
         stage('Python3 All Foundations Acceptance Tests'){
+            failFast true
             parallel{
+                
                 stage('Parallel Foundations Acceptance Tests') {
                     stages {
-                        // stage('Preparation') {
-                        //     steps {
-                        //         script {
-                        //             git branch: 'trunk', credentialsId: 'devops', url: 'git@github.com:DeepLearnI/foundations.git'
-                        //         }
-                        //     }
-                        // }
-                        // stage('Get Foundations Scheduler') {
-                        //     steps {
-                        //         container("python3-1") {
-                        //             sh 'python -m pip install -U foundations-scheduler'
-                        //         }
-                        //     }
-                        // }
-                        // stage('Python3 Foundations Install Test Requirements') {
-                        //     steps {
-                        //         container("python3-1") {
-                        //             sh "./ci_install_requirements.sh"
-                        //         }
-                        //     }
-                        // }
-                        // stage('Build Foundations Wheels') {
-                        //     steps {
-                        //         container("python3-1") {
-                        //             sh "./build_dist.sh"
-                        //         }
-                        //     }
-                        // }
                         stage('Python3 Foundations Acceptance Tests'){
                             steps{
                                 container("python3-1") {
                                     ws("${WORKSPACE}/testing") {
-                                        sh "python -m pip install ../dist/*.whl && python -Wi -m unittest -f -v acceptance"
+                                        sh "python -m pip install --no-index ../dist/*.whl && python -Wi -m unittest -f -v acceptance"
                                     }
                                 }
                             }
@@ -108,40 +89,11 @@ pipeline{
                 }
                 stage('Parallel Foundations Acceptance Tests for Stageless Deploys') {
                     stages{
-                        // stage('Preparation') {
-                        //     steps {
-                        //         script {
-                        //             git branch: 'trunk', credentialsId: 'devops', url: 'git@github.com:DeepLearnI/foundations.git'
-                        //         }
-                        //     }
-                        // }
-                        // stage('Get Foundations Scheduler') {
-                        //     steps {
-                        //         container("python3-2") {
-                        //             sh 'python -m pip install -U foundations-scheduler'
-                        //         }
-                                
-                        //     }
-                        // }
-                        // stage('Python3 Foundations Install Test Requirements') {
-                        //     steps {
-                        //         container("python3-2") {
-                        //             sh "./ci_install_requirements.sh"
-                        //         }
-                        //     }
-                        // }
-                        // stage('Build Foundations Wheels') {
-                        //     steps {
-                        //         container("python3-2") {
-                        //             sh "./build_dist.sh"
-                        //         }
-                        //     }
-                        // }
                         stage('Parallel Foundations Acceptance Tests for Stageless Deploys'){
                             steps {
                                 container("python3-2") {
                                     ws("${WORKSPACE}/testing") {
-                                        sh 'python -m pip install ../dist/*.whl && cp -r ../testing/* . && python -Wi -m unittest -f -v stageless_acceptance'
+                                        sh 'python -m pip install --no-index ../dist/*.whl && cp -r ../testing/* . && python -Wi -m unittest -f -v stageless_acceptance'
                                     }
                                 }
                             }
@@ -150,40 +102,11 @@ pipeline{
                 }
                 stage('Parallel Foundations Scheduler Acceptance Tests for Remote Deploys') {
                     stages{
-                            // stage('Preparation') {
-                            //     steps {
-                            //         script {
-                            //             git branch: 'trunk', credentialsId: 'devops', url: 'git@github.com:DeepLearnI/foundations.git'
-                            //         }
-                            //     }
-                            // }
-                            // stage('Get Foundations Scheduler') {
-                            //     steps {
-                            //         container("python3-3") {
-                            //             sh 'python -m pip install -U foundations-scheduler'
-                            //         }
-                                    
-                            //     }
-                            // }
-                            // stage('Python3 Foundations Install Test Requirements') {
-                            //     steps {
-                            //         container("python3-3") {
-                            //             sh "./ci_install_requirements.sh"
-                            //         }
-                            //     }
-                            // }
-                            // stage('Build Foundations Wheels') {
-                            //     steps {
-                            //         container("python3-3") {
-                            //             sh "./build_dist.sh"
-                            //         }
-                            //     }
-                            // }
                         stage('Python3 Foundations Scheduler Acceptance Tests for Remote Deploys') {
                             steps {
                                 container("python3-3") {
                                     ws("${WORKSPACE}/testing") {
-                                        sh 'python -m pip install ../dist/*.whl && cp -r ../testing/* . &&  export FOUNDATIONS_SCHEDULER_HOST=$FOUNDATIONS_SCHEDULER_ACCEPTANCE_HOST && python -Wi -m unittest -f -v scheduler_acceptance'
+                                        sh 'python -m pip install --no-index ../dist/*.whl && cp -r ../testing/* . &&  export FOUNDATIONS_SCHEDULER_HOST=$FOUNDATIONS_SCHEDULER_ACCEPTANCE_HOST && python -Wi -m unittest -f -v scheduler_acceptance'
                                     }
                                 }
                             }
@@ -192,40 +115,11 @@ pipeline{
                 }
                 stage('Parallel Foundations Orbit Acceptance Tests') {
                     stages{
-                        // stage('Preparation') {
-                        //     steps {
-                        //         script {
-                        //             git branch: 'trunk', credentialsId: 'devops', url: 'git@github.com:DeepLearnI/foundations.git'
-                        //         }
-                        //     }
-                        // }
-                        // stage('Get Foundations Scheduler') {
-                        //     steps {
-                        //         container("python3-4") {
-                        //             sh 'python -m pip install -U foundations-scheduler'
-                        //         }
-                                
-                        //     }
-                        // }
-                        // stage('Python3 Foundations Install Test Requirements') {
-                        //     steps {
-                        //         container("python3-4") {
-                        //             sh "./ci_install_requirements.sh"
-                        //         }
-                        //     }
-                        // }
-                        // stage('Build Foundations Wheels') {
-                        //     steps {
-                        //         container("python3-4") {
-                        //             sh "./build_dist.sh"
-                        //         }
-                        //     }
-                        // }
                         stage('Python3 Foundations Orbit Acceptance Tests') {
                             steps {
                                 container("python3-4") {
                                     ws("${WORKSPACE}/testing") {
-                                        sh 'python -m pip install ../dist/*.whl && cp -r ../testing/* . && export FOUNDATIONS_SCHEDULER_HOST=$FOUNDATIONS_SCHEDULER_ACCEPTANCE_HOST && python -Wi -m unittest -f -v orbit_acceptance'
+                                        sh 'python -m pip install --no-index ../dist/*.whl && cp -r ../testing/* . && export FOUNDATIONS_SCHEDULER_HOST=$FOUNDATIONS_SCHEDULER_ACCEPTANCE_HOST && python -Wi -m unittest -f -v orbit_acceptance'
                                     }
                                 }
                             }
@@ -234,34 +128,11 @@ pipeline{
                 }
                 stage('Parallel Foundations REST API Acceptance Tests') {
                     stages{
-
-                        // stage('Get Foundations Scheduler') {
-                        //     steps {
-                        //         container("python3-5") {
-                        //             sh 'python -m pip install -U foundations-scheduler'
-                        //         }
-                                
-                        //     }
-                        // }
-                        // stage('Python3 Foundations Install Test Requirements') {
-                        //     steps {
-                        //         container("python3-5") {
-                        //             sh "./ci_install_requirements.sh"
-                        //         }
-                        //     }
-                        // }
-                        // stage('Build Foundations Wheels') {
-                        //     steps {
-                        //         container("python3-5") {
-                        //             sh ". ./dev_env.sh && ./build_dist.sh"
-                        //         }
-                        //     }
-                        // }
                         stage('Python3 Foundations REST API Acceptance Tests') {
                             steps {
                                 container("python3-5") {
                                     ws("${WORKSPACE}/foundations_rest_api/src") {
-                                        sh "python -m pip install ../../dist/*.whl && python -Wi -m unittest -f -v acceptance"
+                                        sh "python -m pip install  --no-index ../../dist/*.whl && python -Wi -m unittest -f -v acceptance"
                                     }
                                 }
                             }
