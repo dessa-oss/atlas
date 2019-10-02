195fe8f24dcdaa708b6892a8219e389fec18e0b9
pairing <developers@dessa.com>
2019-10-02 09:01:17 -0400

    [KD CZ] converted deploy test to submit job acceptance test

--- testing/acceptance/test_submit_job_via_cli.py
+++ testing/acceptance/test_submit_job_via_cli.py
@@ -11,7 +11,7 @@ from acceptance.mixins.job_deploy_function_test_scaffold import JobDeployFunctio
 import foundations
 from contextlib import contextmanager
 
-class TestDeployJobViaCLI(Spec, JobDeployFunctionTestScaffold):
+class TestSubmitJobViaCLI(Spec, JobDeployFunctionTestScaffold):
 
     def _log_level(self):
         return 'INFO'
@@ -42,18 +42,18 @@ class TestDeployJobViaCLI(Spec, JobDeployFunctionTestScaffold):
             else:
                 del config[key]
 
-    def test_deploy_job_with_all_arguments_specified_deploys_job(self):
-        self._test_deploy_job_with_all_arguments_specified_deploys_job()
+    def test_submit_job_with_all_arguments_specified_submits_job(self):
+        self._test_submit_job_with_all_arguments_specified_submits_job()
 
-    def test_deploy_job_with_no_arguments_specified_deploys_job_with_defaults(self):
-        self._test_deploy_job_with_no_arguments_specified_deploys_job_with_defaults()
+    def test_submit_job_with_no_arguments_specified_submits_job_with_defaults(self):
+        self._test_submit_job_with_no_arguments_specified_submits_job_with_defaults()
 
-    def _deploy_job_with_defaults(self):
-        cli_deploy_process = self._run_process(['python', '-m', 'foundations', 'deploy'])
-        self._check_if_successful(cli_deploy_process)
-        return cli_deploy_process
+    def _submit_job_with_defaults(self):
+        cli_submit_process = self._run_process(['python', '-m', 'foundations', 'submit'])
+        self._check_if_successful(cli_submit_process)
+        return cli_submit_process
 
-    def _deploy_job(self, **kwargs):
+    def _submit_job(self, **kwargs):
         import json
         import os
         import os.path as path
@@ -66,32 +66,31 @@ class TestDeployJobViaCLI(Spec, JobDeployFunctionTestScaffold):
             json.dump(kwargs['params'], params_file)
 
         command_to_run = [
-            'python', '-m', 'foundations', 'deploy',
-            '--job-directory={}'.format(job_directory),
-            '--env={}'.format(kwargs['env']),
+            'python', '-m', 'foundations', 'submit',
+            '--job-dir={}'.format(job_directory),
             '--entrypoint={}'.format(kwargs['entrypoint']),
-            '--project-name={}'.format(kwargs['project_name'])
+            '--project-name={}'.format(kwargs['project_name']),
         ]
 
-        cli_deploy_process = self._run_process(command_to_run)
+        cli_submit_process = self._run_process(command_to_run)
         os.remove(params_file_path)
-        self._check_if_successful(cli_deploy_process)
-        return cli_deploy_process
+        self._check_if_successful(cli_submit_process)
+        return cli_submit_process
 
-    def _job_id_from_logs(self, driver_deploy_completed_process):
+    def _job_id_from_logs(self, driver_submit_completed_process):
         import re
 
-        logs_parsing_regex = re.compile("Job '(.*)' deployed")
-        logs = self._driver_stdout(driver_deploy_completed_process)
+        logs_parsing_regex = re.compile("Job '(.*)' deployed.")
+        logs = self._driver_stdout(driver_submit_completed_process)
         return logs_parsing_regex.findall(logs)[0]
 
-    def _driver_stdout(self, driver_deploy_completed_process):
-        return driver_deploy_completed_process.stdout.decode()
+    def _driver_stdout(self, driver_submit_completed_process):
+        return driver_submit_completed_process.stdout.decode()
 
     def _uuid(self, driver_process):
         return self._job_id_from_logs(driver_process)
 
     def _check_if_successful(self, driver_process):
         if driver_process.returncode != 0:
-            raise AssertionError(f'deploy failed:\nstdout:\n{driver_process.stdout.decode()}\nstderr:\n{driver_process.stderr.decode()}')
+            raise AssertionError(f'submit failed:\nstdout:\n{driver_process.stdout.decode()}\nstderr:\n{driver_process.stderr.decode()}')
                 
