cb97f2195de5d6cb5f9e2c8006af2d0b71987e51
pairing <developers@dessa.com>
2019-10-15 14:02:21 -0400

    [TR CZ] start building a folder based job source bundle

--- foundations_contrib/src/foundations_contrib/job_bundling/folder_job_source_bundle.py
+++ foundations_contrib/src/foundations_contrib/job_bundling/folder_job_source_bundle.py
@@ -5,5 +5,8 @@ Proprietary and confidential
 Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 """
 
-from test.job_bundling.test_script_environment import TestScriptEnvironment
-from test.job_bundling.test_folder_job_source_bundle import TestFolderJobSourceBundle
+
+class FolderJobSourceBundle(object):
+    
+    def job_archive(self):
+        return '.'
---
+++ foundations_contrib/src/test/job_bundling/test_folder_job_source_bundle.py
@@ -0,0 +1,21 @@
+"""
+Copyright (C) DeepLearning Financial Technologies Inc. - All Rights Reserved
+Unauthorized copying, distribution, reproduction, publication, use of this file, via any medium is strictly prohibited
+Proprietary and confidential
+Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
+"""
+
+
+from foundations_spec import *
+
+class TestFolderJobSourceBundle(Spec):
+
+    @let
+    def bundle(self):
+        from foundations_contrib.job_bundling.folder_job_source_bundle import FolderJobSourceBundle
+        return FolderJobSourceBundle()
+        
+    
+    def test_folder_job_source_bundle_job_archive_returns_cwd(self):
+        job_archive = self.bundle.job_archive()
+        self.assertEqual('.', job_archive)
