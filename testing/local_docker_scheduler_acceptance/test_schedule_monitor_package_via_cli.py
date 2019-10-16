80fbc815f04d0d1fe7c0b7b7566af5e61099643f
Katherine <k.bancroft@dessa.com>
2019-10-16 15:39:28 -0400

    [KD AM] Refactor

--- testing/local_docker_scheduler_acceptance/test_schedule_monitor_package_via_cli.py
+++ testing/local_docker_scheduler_acceptance/test_schedule_monitor_package_via_cli.py
@@ -10,7 +10,7 @@ from foundations_spec import *
 import subprocess
 from foundations_orbit_rest_api.v1.models.production_metric_set import ProductionMetricSet
 
-@skip
+@skip('Not implemented')
 class TestScheduleMonitorPackageViaCli(Spec):
     
     @let
@@ -28,6 +28,10 @@ class TestScheduleMonitorPackageViaCli(Spec):
     @let
     def monitor_package_dir(self):
         return 'local_docker_scheduler_acceptance/fixtures/this_cool_monitor'
+
+    @let
+    def env(self):
+        return 'scheduler'
 
     @let
     def foundations_home(self):
@@ -51,18 +55,19 @@ class TestScheduleMonitorPackageViaCli(Spec):
         scheduler_address = os.environ['LOCAL_DOCKER_SCHEDULER_HOST']
         return requests.delete(f'http://{scheduler_address}:5000/scheduled_jobs/{job_name}')
 
+    def _start_monitor(self):
+        command = (f'python -m foundations monitor start {self.monitor_package_dir} main.py '
+                   f'--name={self.monitor_name} --project_name={self.project_name} --env={self.env}')
+        return subprocess.run(command, shell=True)
+
+    def _call_monitor_with_command(self, operation):
+        command = f'python -m foundations monitor {operation} {self.monitor_name} {self.project_name} --env={self.env}'
+        return subprocess.run(command, shell=True)
+
     def test_schedule_monitor_package_via_cli_runs_package_code_on_a_schedule(self):
         import time
         
-        result = subprocess.run(
-            [
-                'python', '-m',
-                'foundations', 'orbit', 'monitor', 'start',
-                f'--monitor={self.monitor_name}',
-                f'--project-name={self.project_name}',
-                f'--job-directory={self.monitor_package_dir}'
-            ]
-        )
+        result = self._start_monitor()
 
         self.assertEqual(0, result.returncode)
 
@@ -82,15 +87,7 @@ class TestScheduleMonitorPackageViaCli(Spec):
     def test_pause_scheduled_monitor_package_via_cli_halts_execution_but_can_resume_later(self):
         import time
 
-        subprocess.run(
-            [
-                'python', '-m',
-                'foundations', 'orbit', 'monitor', 'start',
-                f'--monitor={self.monitor_name}',
-                f'--project-name={self.project_name}',
-                f'--job-directory={self.monitor_package_dir}'
-            ]
-        )
+        result = self._start_monitor()
 
         pause_result = subprocess.run(
             [
@@ -107,13 +104,7 @@ class TestScheduleMonitorPackageViaCli(Spec):
         metric_sets_before_resume = ProductionMetricSet.all(self.project_name).evaluate()
         self.assertIn(len(metric_sets_before_resume), [0, 1])
 
-        resume_result = subprocess.run(
-            [
-                'foundations', 'orbit', 'monitor', 'resume',
-                f'--monitor={self.monitor_name}',
-                f'--project_name={self.project_name}'
-            ]
-        )
+        resume_result = self._call_monitor_with_command('resume')
 
         self.assertEqual(0, resume_result.returncode)
 
