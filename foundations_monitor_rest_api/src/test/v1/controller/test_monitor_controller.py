8b048c11156b816234159d2e007736c59324ef75
Katherine <k.bancroft@dessa.com>
2019-10-18 16:37:44 -0400

    [SD KD] changed folder for monitor controller

---
+++ foundations_monitor_rest_api/src/test/v1/controller/test_monitor_controller.py
@@ -0,0 +1,46 @@
+"""
+Copyright (C) DeepLearning Financial Technologies Inc. - All Rights Reserved
+Unauthorized copying, distribution, reproduction, publication, use of this file, via any medium is strictly prohibited
+Proprietary and confidential
+Written by Susan Davis <s.davis@dessa.com>, 10 2019
+"""
+
+from foundations_spec import *
+from foundations_monitor_rest_api.v1.controller.monitor_controller import MonitorController
+
+class TestMonitorController(Spec):
+    
+
+    @let
+    def project_name(self):
+        return self.faker.word()
+
+    @let
+    def monitor_name(self):
+        return self.faker.word()
+
+    @let
+    def monitor_id(self):
+        return f'{self.project_name}-{self.monitor_name}'
+
+    @set_up
+    def set_up(self):
+        self.cron_job_scheduler_class = let_mock('foundations_local_docker_scheduler_plugin.cron_job_scheduler.CronJobScheduler')
+        self.cron_job_scheduler = Mock()
+        self.cron_job_scheduler_class.return_when = self.cron_job_scheduler
+
+    # request for monitors should return empty list for project that has no monitors
+    # request for monitors for a project that doesn't exist then produces 404
+    # request for monitors should return list with expected projects based on configured monitors for a project
+    
+    def test_request_for_monitors_with_project_that_has_no_monitors_produces_empty_list(self):
+
+        self.cron_job_scheduler.
+
+        expected_results = []
+
+        monitor_controller = MonitorController()
+        results = monitor_controller.index()
+
+        self.assertEqual(expected_results, results[0])
+
