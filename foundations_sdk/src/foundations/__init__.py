6b24d8d23f81c9225360f7c8c4cc3b75ddc9b812
pairing <developers@dessa.com>
2019-10-18 11:24:09 -0400

    [SR CZ] import orbit commands in foundations

--- foundations_sdk/src/foundations/__init__.py
+++ foundations_sdk/src/foundations/__init__.py
@@ -71,7 +71,7 @@ from foundations.job_actions import *
 from foundations.artifacts import *
 from foundations.local_run import set_up_default_environment_if_present
 #Commented for Atlas CE
-#from foundations_orbit import track_production_metrics
+from foundations_orbit import *
 from foundations.set_tensorboard_logdir import set_tensorboard_logdir
 
 def _append_module():
