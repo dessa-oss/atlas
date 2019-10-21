76ce7832dd098ef8eb9a3f643ea1e50aa59799c4
Dessa-ColeClifford <c.clifford@dessa.com>
2019-10-21 16:33:32 -0400

    [SD CC SR] Updated Orbit rest api to specify which foundations modules that it needs during building of the Docker image

--- foundations_orbit_rest_api/setup.py
+++ foundations_orbit_rest_api/setup.py
@@ -28,11 +28,11 @@ setup(
     install_requires=[
         'dessa_foundations=={}'.format(build_version),
         'foundations_core_rest_api_components=={}'.format(build_version),
+        'foundations-local-docker-scheduler-plugin=={}'.format(build_version),
         'flask-restful==0.3.6',
         'Flask-Cors==3.0.6',
         'Flask==1.1.0',
-        'Werkzeug==0.15.4',
-        'pycrypto==2.6.1',
+        'Werkzeug==0.15.4'
     ],
     packages=find_packages('src'),
     package_dir={'':'src'},
