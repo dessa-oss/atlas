3875ca1a4c1b20741325598b0cbc6adfd8259e2d
Kyle De Freitas <k.defreitas@dessa.com>
2019-10-22 09:15:32 -0400

    [KD] added integration test for accessing single monitor record

--- foundations_orbit_rest_api/src/integration/v1/test_scheduled_monitor_endpoint.py
+++ foundations_orbit_rest_api/src/integration/v1/test_scheduled_monitor_endpoint.py
@@ -54,6 +54,12 @@ class TestScheduledMonitorEndpoint(Spec):
 
     def test_get_request_retrieves_monitors(self):
         self._start_monitor()
+        monitor = self._get_monitor()
+        self.assertEqual(1, len(monitor.keys()))
+        self.assertIsNotNone(monitor[self.monitor_id])
+
+    def test_get_request_by_monitor_id_retrieves_specific_monitor(self):
+        self._start_monitor()
         monitors = self._get_all_monitors()
         self.assertTrue(self.monitor_id in monitors.keys())
 
@@ -96,9 +102,12 @@ class TestScheduledMonitorEndpoint(Spec):
         return json.loads(response_data)
 
     def _get_monitor(self, monitor_name=None):
+        import json
         monitor_name = self.monitor_name if monitor_name is None else monitor_name
         monitor_url = f'{self.base_url}/{monitor_name}'
-        return self.client.get(monitor_url)
+        response =  self.client.get(monitor_url)
+        response_data = response.data.decode()
+        return json.loads(response_data)
 
     def _pause_monitor(self, monitor_name=None):
         payload = {'status': 'pause'}
