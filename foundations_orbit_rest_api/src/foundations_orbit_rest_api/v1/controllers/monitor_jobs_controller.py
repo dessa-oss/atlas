2c0422c28acfc744bbf1f47840e1fa9741a84b61
pairing <developers@dessa.com>
2019-10-24 17:52:54 -0400

    [CZ] test and implement deleting monitor jobs from orbit rest api

--- foundations_orbit_rest_api/src/foundations_orbit_rest_api/v1/controllers/monitor_jobs_controller.py
+++ foundations_orbit_rest_api/src/foundations_orbit_rest_api/v1/controllers/monitor_jobs_controller.py
@@ -30,3 +30,25 @@ class MonitorJobsController(object):
 
         return Response('Monitors', response_body, status=200, fallback=fallback)
 
+    def delete(self):
+        from foundations_orbit_rest_api.v1.models.monitor import Monitor
+        from foundations_core_rest_api_components.lazy_result import LazyResult
+        from foundations_core_rest_api_components.response import Response
+
+        project_name = self.params.pop('project_name')
+        monitor_name = self.params.pop('monitor_name')
+        job_id = self.params.pop('job_id')
+
+        response_body = Monitor.delete_job(project_name, monitor_name, job_id)
+
+        failure_response_data = {
+            'project_name': project_name,
+            'monitor_name': monitor_name,
+            'job_id': job_id,
+            'error': 'does not exist'
+        }
+
+        fallback = Response('asdf', LazyResult(lambda: failure_response_data), status=404)
+
+        return Response('Monitors', response_body, fallback=fallback)
+
--- foundations_orbit_rest_api/src/test/v1/controllers/__init__.py
+++ foundations_orbit_rest_api/src/test/v1/controllers/__init__.py
@@ -11,3 +11,4 @@ from test.v1.controllers.test_project_controller import TestProjectController
 from test.v1.controllers.test_validation_report_listings_controller import TestValidationReportListingsController
 from test.v1.controllers.test_validation_reports_controller import TestValidationReportsController
 from test.v1.controllers.test_scheduled_monitor_controller import TestScheduledMonitorController
+from test.v1.controllers.test_monitor_jobs_controller import TestMonitorJobsController
