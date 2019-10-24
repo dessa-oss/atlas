6e4dd3c84bdc1e49d885b60de9f7573c1d270cb9
pairing <developers@dessa.com>
2019-10-24 15:31:59 -0400

    [CZ] test and implement deleting job_id from monitor list in redis

---
+++ foundations_orbit_rest_api/src/test/v1/models/test_monitor.py
@@ -0,0 +1,119 @@
+"""
+Copyright (C) DeepLearning Financial Technologies Inc. - All Rights Reserved
+Unauthorized copying, distribution, reproduction, publication, use of this file, via any medium is strictly prohibited
+Proprietary and confidential
+Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
+"""
+
+from foundations_spec import *
+from foundations_orbit_rest_api.v1.models.monitor import Monitor
+
+class TestMonitor(Spec):
+
+    @let_now
+    def redis_connection(self):
+        import fakeredis
+        return self.patch('foundations_contrib.global_state.redis_connection', fakeredis.FakeRedis())
+
+    @let
+    def project_name(self):
+        return self.faker.word()
+
+    @let
+    def monitor_name(self):
+        return self.faker.word()
+    
+    @let
+    def job_id(self):
+        return f'{self.project_name}-{self.monitor_name}_{self.faker.random_int()}'
+
+    @let
+    def job_id_2(self):
+        return f'{self.project_name}-{self.monitor_name}_{self.faker.random_int()}'
+
+    @set_up
+    def set_up(self):
+        self.redis_connection.flushall()
+
+    def _data_to_set(self):
+        return [
+            {
+                'project': 'so',
+                'job_id': self.job_id,
+                'user': 'pairing',
+                'start_time': 1571931153.0313132,
+                'completed_time': 1571931153.6426458,
+                'state': 'completed'
+            }
+            , {
+                'project': 'so',
+                'job_id': self.job_id_2,
+                'user': 'pairing',
+                'start_time': 1571931156.550436,
+                'state': 'running'
+            }
+        ]
+
+    def _expected_data(self):
+        return [
+            {
+                'project_name': 'so',
+                'job_id': self.job_id,
+                'user': 'pairing',
+                'job_parameters': {},
+                'input_params': [],
+                'output_metrics': [],
+                'status': 'completed',
+                'start_time': 1571931153.0313132,
+                'completed_time': 1571931153.6426458,
+                'tags': {}
+            }
+            , {
+                'project_name': 'so',
+                'job_id': self.job_id_2,
+                'user': 'pairing',
+                'job_parameters': {},
+                'input_params': [],
+                'output_metrics': [],
+                'status': 'running',
+                'start_time': 1571931156.550436,
+                'completed_time': None,
+                'tags': {}
+            }
+        ]
+    
+    def test_jobs_ids_from_monitors_dictionary_returns_none_if_monitor_does_not_exist(self):
+        promise = Monitor.job_ids_from_monitors_dictionary(self.project_name, self.monitor_name)
+        self.assertEqual(None, promise.evaluate())
+
+    def test_jobs_ids_from_monitors_dictionary_returns_data_from_redis(self):
+        self._create_job_information_for_monitor(self.project_name, self.monitor_name, self._data_to_set())
+
+        promise = Monitor.job_ids_from_monitors_dictionary(self.project_name, self.monitor_name)
+
+        expected_result = self._expected_data()
+        expected_result = sorted(expected_result, key=lambda data: data['job_id'])
+        result = promise.evaluate()
+        result = sorted(result, key=lambda data: data['job_id'])
+        
+        self.assertEqual(expected_result, result)
+
+    def test_delete_job_from_monitors_dictionary_removes_job_from_redis(self):
+        self._create_job_information_for_monitor(self.project_name, self.monitor_name, self._data_to_set())
+
+        delete_promise = Monitor.delete_job(self.project_name, self.monitor_name, self.job_id)
+        delete_promise.evaluate()
+
+        promise = Monitor.job_ids_from_monitors_dictionary(self.project_name, self.monitor_name)
+
+        expected_result = self._expected_data()
+        del expected_result[0]
+
+        self.assertEqual(expected_result, promise.evaluate())
+
+    def _create_job_information_for_monitor(self, project_name, monitor_name, job_data):
+        for job_listing in job_data:
+            job_id = job_listing.pop('job_id')
+            self.redis_connection.sadd(f'projects:{self.project_name}:monitors:{self.monitor_name}:jobs', job_id)
+            for job_key, job_value in job_listing.items():
+                self.redis_connection.set(f'jobs:{job_id}:{job_key}', job_value)
