75f7055781734fd46c6eb228fd5be5cb7af80fff
Kyle De Freitas <k.defreitas@dessa.com>
2019-10-28 10:26:57 -0400

    [KD] updated the GUI installation to use  local files

--- build_gui_orbit_team.sh
+++ build_gui_orbit_team.sh
@@ -8,12 +8,8 @@ export build_version=`echo $pip_version | sed 's/+/_/g'`
 
 if [ $? -eq 0 ]
 then
-    rm -rf tmp/pip_wheels && \
-        mkdir -p tmp/pip_wheels && \
-        pip download --dest tmp/pip_wheels dessa_foundations==$pip_version && \
-        pip download --dest tmp/pip_wheels foundations-core_rest_api_components==$pip_version && \
-        pip download --dest tmp/pip_wheels foundations-rest-api==$pip_version && \
-        pip download --dest tmp/pip_wheels foundations-orbit-rest-api==$pip_version && \
-        pip download --dest tmp/pip_wheels foundations_orbit==$pip_version && \
-        python build_gui_orbit_team.py
+    rm -rf tmp/pip_wheels \
+        && mkdir -p tmp/pip_wheels \
+        && cp dist/* tmp/pip_wheels \
+        && python build_gui_orbit_team.py
 fi
