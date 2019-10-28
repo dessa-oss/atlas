51802de496569291db0004535940d1d66d7deb3d
Kyle De Freitas <k.defreitas@dessa.com>
2019-10-28 16:20:50 -0400

    [KD] updated orbit docker images to not use alpine

--- build_gui_orbit_team.py
+++ build_gui_orbit_team.py
@@ -10,7 +10,7 @@ def main(argv):
     rest_api_docker_file = 'docker/rest_api_orbit_team_Dockerfile'
     rest_api_main_file = 'run_orbit_api_server.py'
     gui_directory = 'foundations_ui_orbit'
-    gui_docker_file = 'orbit_gui_Dockerfile'
+    gui_docker_file = '../docker/gui_orbit_Dockerfile'
     gui_docker_image = 'foundations-orbit-gui'
 
     build_and_tag_gui_image('.', rest_api_docker_file, f'{nexus_registry}/{rest_api_docker_image}', buildargs={'main_file': rest_api_main_file})
