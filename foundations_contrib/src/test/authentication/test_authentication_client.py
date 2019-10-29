c6c3a04662847bb9ad529350e1f0c99a57ba266b
amackillop <austin.mackillop@gmail.com>
2019-10-29 16:19:49 -0400

    [AM] Test browser_login

--- foundations_contrib/src/test/authentication/test_authentication_client.py
+++ foundations_contrib/src/test/authentication/test_authentication_client.py
@@ -9,6 +9,7 @@ from foundations_spec import (
 
 from foundations_contrib.authentication.authentication_client import (
     AuthenticationClient,
+    keycloak_client,
 )
 
 from mock import mock_open, patch, Mock
@@ -18,7 +19,6 @@ class TestAuthenticationClient(Spec):
 
     conf = "test/authentication/fixtures/auth_config.json"
     redirect_url = "http://some_outher_url:0000"
-    mock_keycloak_instance = let_mock()
 
     @set_up
     def set_up(self):
@@ -31,15 +31,12 @@ class TestAuthenticationClient(Spec):
                 self.auth_client = AuthenticationClient(self.conf, self.redirect_url)
                 self.mock_keycloak = self.auth_client.client
 
-    def test_keycloak_client_creates_a_keycloak_open_id_instance(self):
-        from foundations_contrib.authentication.authentication_client import (
-            keycloak_client,
-        )
-
+    def test_keycloak_client_uses_a_config_to_create_a_keycloak_open_id_instance(self):
         self.mock_keycloak_class = self.patch(
             "foundations_contrib.authentication.authentication_client.KeycloakOpenID",
             ConditionalReturn(),
         )
+        # These values should match the auth_config in the fixtures
         self.mock_keycloak_class.return_when(
             "KeycloakInstance",
             server_url="http://some_host:8080/auth/",
@@ -53,4 +50,9 @@ class TestAuthenticationClient(Spec):
     def test_authentication_url_calls_keycloak_auth_url_with_the_redirect(self):
         self.auth_client.authentication_url()
         self.mock_keycloak.auth_url.assert_called_once_with(self.redirect_url)
+
+    def test_browser_login_opens_window_to_authentication_url(self):
+        with patch('webbrowser.open') as mock_browser:
+            self.auth_client.browser_login()
+            mock_browser.assert_called_once_with(self.auth_client.authentication_url())
 
