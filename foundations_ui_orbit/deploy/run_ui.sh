12ee021ca78fc85e4eed0223ef4f7ec4267d8556
Shahzad <sr@Shahzads-MacBook-Pro.local>
2019-10-30 10:04:58 -0400

    [SR] Added Atlas REST API to Orbit UI nginx.conf and run script

--- foundations_ui_orbit/deploy/run_ui.sh
+++ foundations_ui_orbit/deploy/run_ui.sh
@@ -3,5 +3,5 @@
 export REACT_APP_API_URL="http://localhost:37722/api/v1/"
 export REACT_APP_APIARY_URL="http://private-d03986-iannelladessa.apiary-mock.com/api/v1/";
 
-envsubst '$SERVER_NAME$FOUNDATIONS_REST_API' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf && \
+envsubst '$SERVER_NAME$FOUNDATIONS_REST_API$ATLAS_REST_API' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf && \
     nginx -g 'daemon off;'
