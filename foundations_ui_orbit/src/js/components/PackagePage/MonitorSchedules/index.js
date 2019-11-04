b27086a22f5c075b54fab8b3a02c6eeff7b66ccb
Calvin Choi <calvinchoi@Calvins-MacBook-Pro.local>
2019-11-04 16:09:34 -0500

    [CALVIN] Some formatting in monitor page

--- foundations_ui_orbit/src/js/components/PackagePage/MonitorSchedules/index.js
+++ foundations_ui_orbit/src/js/components/PackagePage/MonitorSchedules/index.js
@@ -58,7 +58,7 @@ class MonitorSchedules extends Component {
     return (
       <Layout tab="Schedules" title="Data Health">
         <div className="monitor-schedules-container">
-          <h3 className="section-title">Monitor Schedules</h3>
+          <div className="section-title font-bold">Monitor Schedules</div>
           <div className="schedule-details">
             <MonitorListTable
               onClickRow={this.selectRow}
