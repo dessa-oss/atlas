2e018fef2d4031b64abdd8e5942a49405820be55
amackillop <austin.mackillop@gmail.com>
2019-11-06 16:41:59 -0500

    [AM PL] Print token for dev purposes

--- foundations_rest_api/src/foundations_rest_api/v2beta/controllers/authentication_controller.py
+++ foundations_rest_api/src/foundations_rest_api/v2beta/controllers/authentication_controller.py
@@ -32,7 +32,9 @@ class AuthenticationController(Resource):
     def _login(self):
         code = request.args.get("code", None)
         if code:
-            return redirect('http://localhost:3000/', 302, self.client.token_using_auth_code(code=code))
+            token_response = self.client.token_using_auth_code(code=code)
+            print(token_response)
+            return redirect('http://localhost:3000/', 302, token_response)
         
         return redirect(self.client.authentication_url())
 
