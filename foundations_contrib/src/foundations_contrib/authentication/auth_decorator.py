0262736ce290ef670012e215dd2b9c97ea3409a4
amackillop <austin.mackillop@gmail.com>
2019-11-06 15:20:33 -0500

    [AM] Factor out token verification for direct usage

--- foundations_contrib/src/foundations_contrib/authentication/auth_decorator.py
+++ foundations_contrib/src/foundations_contrib/authentication/auth_decorator.py
@@ -2,12 +2,10 @@
 
 from functools import wraps
 
-from flask import _request_ctx_stack
 from jose import jwt
 
-from foundations_core_rest_api_components.exceptions import AuthError
-from .authentication_client import AuthenticationClient
-from .utils import get_token_from_header
+from foundations_contrib.authentication.authentication_client import AuthenticationClient
+from foundations_contrib.authentication.utils import get_token_from_header, verify_token
 
 
 def requires_auth(auth_client: AuthenticationClient):
@@ -18,57 +16,9 @@ def requires_auth(auth_client: AuthenticationClient):
         def verify(*args, **kwargs):
             token = get_token_from_header()
             jwks = auth_client.json_web_key_set
-            unverified_header = jwt.get_unverified_header(token)
-            rsa_key = {}
-            for key in jwks["keys"]:
-                if key["kid"] == unverified_header["kid"]:
-                    rsa_key = {
-                        "kty": key["kty"],
-                        "kid": key["kid"],
-                        "use": key["use"],
-                        "n": key["n"],
-                        "e": key["e"],
-                    }
-            if rsa_key:
-                try:
-                    payload = jwt.decode(
-                        token,
-                        rsa_key,
-                        algorithms=["RS256"],
-                        audience="account",
-                        issuer=auth_client.metadata["issuer"],
-                    )
-                except jwt.ExpiredSignatureError:
-                    raise AuthError(
-                        {"code": "token_expired", "description": "Token is expired."},
-                        401,
-                    )
-                except jwt.JWTClaimsError:
-                    raise AuthError(
-                        {
-                            "code": "invalid_claims",
-                            "description": "Incorrect claims, please check the audience and issuer.",
-                        },
-                        401,
-                    )
-                except Exception:
-                    raise AuthError(
-                        {
-                            "code": "invalid_header",
-                            "description": "Unable to parse authentication token.",
-                        },
-                        401,
-                    )
-
-                _request_ctx_stack.top.current_user = payload
-                return func(*args, **kwargs)
-            raise AuthError(
-                {
-                    "code": "invalid_header",
-                    "description": "Unable to find appropriate key.",
-                },
-                401,
-            )
+            issuer = auth_client.metadata["issuer"]
+            verify_token(token, jwks, issuer)
+            return func(*args, **kwargs)
 
         return verify
 
@@ -88,3 +38,61 @@ def requires_scope(required_scope):
             if token_scope == required_scope:
                 return True
     return False
+
+
+
+# # Dev purposes
+# from flask import Flask, jsonify
+# from flask_cors import cross_origin
+
+# from foundations_core_rest_api_components.exceptions import AuthError
+# from foundations_contrib.authentication.configs import ATLAS
+
+# client = AuthenticationClient(ATLAS, redirect_url="/api/v2beta/auth")
+
+# APP = Flask(__name__)
+
+# # This doesn't need authentication
+# @APP.route("/api/public")
+# @cross_origin(headers=["Content-Type", "Authorization"])
+# def public():
+#     response = (
+#         "Hello from a public endpoint! You don't need to be authenticated to see this."
+#     )
+#     return jsonify(message=response)
+
+
+# # This needs authentication
+# @APP.route("/api/private")
+# @cross_origin(headers=["Content-Type", "Authorization"])
+# @requires_auth(client)
+# def private():
+#     response = (
+#         "Hello from a private endpoint! You need to be authenticated to see this."
+#     )
+#     return jsonify(message=response)
+
+
+# # This needs authorization
+# @APP.route("/api/private-scoped")
+# @cross_origin(headers=["Content-Type", "Authorization"])
+# @requires_auth(client)
+# def private_scoped():
+#     if requires_scope("profile"):
+#         response = "Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this."
+#         return jsonify(message=response)
+#     raise AuthError(
+#         {
+#             "code": "Unauthorized",
+#             "description": "You don't have access to this resource",
+#         },
+#         403,
+#     )
+
+# @APP.errorhandler(AuthError)
+# def handle_auth_error(exc):
+#     response = jsonify(exc.error)
+#     response.status_code = exc.status_code
+#     return response
+
+# APP.run()
--- foundations_contrib/src/foundations_contrib/authentication/utils.py
+++ foundations_contrib/src/foundations_contrib/authentication/utils.py
@@ -1,4 +1,6 @@
-from flask import request
+from flask import request, _request_ctx_stack
+from jose import jwt
+
 from foundations_core_rest_api_components.exceptions import AuthError
 
 
@@ -40,3 +42,71 @@ def get_token_from_header():
 
     token = parts[1]
     return token
+
+def verify_token(token: str, jwks: dict, issuer: str) -> None:
+    unverified_header = jwt.get_unverified_header(token)
+    rsa_key = {}
+    for key in jwks["keys"]:
+        if key["kid"] == unverified_header["kid"]:
+            rsa_key = {
+                "kty": key["kty"],
+                "kid": key["kid"],
+                "use": key["use"],
+                "n": key["n"],
+                "e": key["e"],
+            }
+    if rsa_key:
+        try:
+            payload = jwt.decode(
+                token,
+                rsa_key,
+                algorithms=["RS256"],
+                audience="account",
+                issuer=issuer,
+            )
+        except jwt.ExpiredSignatureError:
+            raise AuthError(
+                {"code": "token_expired", "description": "Token is expired."},
+                401,
+            )
+        except jwt.JWTClaimsError:
+            raise AuthError(
+                {
+                    "code": "invalid_claims",
+                    "description": "Incorrect claims, please check the audience and issuer.",
+                },
+                401,
+            )
+        except Exception:
+            raise AuthError(
+                {
+                    "code": "invalid_header",
+                    "description": "Unable to parse authentication token.",
+                },
+                401,
+            )
+
+        _request_ctx_stack.top.current_user = payload
+        return
+ 
+    raise AuthError(
+        {
+            "code": "invalid_header",
+            "description": "Unable to find appropriate key.",
+        },
+        401,
+    )
+
+def requires_scope(required_scope):
+    """Determines if the required scope is present in the Access Token
+    Args:
+        required_scope (str): The scope required to access the resource
+    """
+    token = get_token_from_header()
+    unverified_claims = jwt.get_unverified_claims(token)
+    if unverified_claims.get("scope"):
+        token_scopes = unverified_claims["scope"].split()
+        for token_scope in token_scopes:
+            if token_scope == required_scope:
+                return True
+    return False
