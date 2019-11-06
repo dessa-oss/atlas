99c1fbd3c3f8c16c61376d98a9fa8f67eeef0f91
amackillop <austin.mackillop@gmail.com>
2019-11-06 11:39:05 -0500

    [AM] Added user info controller

---
+++ foundations_rest_api/src/foundations_rest_api/v2beta/controllers/user_info_controller.py
@@ -0,0 +1,30 @@
+"""
+Copyright (C) DeepLearning Financial Technologies Inc. - All Rights Reserved
+Unauthorized copying, distribution, reproduction, publication, use of this file, via any medium is strictly prohibited
+Proprietary and confidential
+Written by Susan Davis <s.davis@dessa.com>, 06 2018
+"""
+import os.path
+import subprocess as sp
+import webbrowser
+
+from foundations_rest_api.utils.api_resource import api_resource
+from foundations_core_rest_api_components.response import Response
+from foundations_core_rest_api_components.lazy_result import LazyResult
+
+from foundations_contrib.authentication.authentication_client import (
+    AuthenticationClient,
+)
+from foundations_contrib.authentication.utils import get_token_from_header
+from foundations_contrib.authentication.configs import ATLAS
+
+
+@api_resource("/api/v2beta/auth/userinfo")
+class UserInfoController:
+    client = AuthenticationClient(ATLAS, redirect_url="/api/v2beta/auth")
+
+    def index(self):
+        token = get_token_from_header()
+        info = self.client.user_info(token)
+        return Response("UserInfo", LazyResult(lambda: info))
+
---
+++ foundations_rest_api/src/test/v2beta/controllers/test_user_info_controller.py
@@ -0,0 +1,27 @@
+"""
+Copyright (C) DeepLearning Financial Technologies Inc. - All Rights Reserved
+Unauthorized copying, distribution, reproduction, publication, use of this file, via any medium is strictly prohibited
+Proprietary and confidential
+Written by Dariem Perez <d.perez@dessa.com>, 11 2018
+"""
+
+from foundations_spec import Spec
+from mock import patch
+
+class TestUserInfoController(Spec):
+    @patch(
+        "foundations_contrib.authentication.authentication_client.AuthenticationClient",
+        autospec=True,
+    )
+    def test_user_info_controller_uses_auth_client_to_get_info(self, mock_contructor):
+        from foundations_rest_api.v2beta.controllers.user_info_controller import (
+            UserInfoController,
+        )
+        from foundations_rest_api.global_state import app_manager
+
+        headers = [("Authorization", "bearer token")]
+
+        with app_manager.app().test_request_context(headers=headers):
+            auth_client = mock_contructor("conf", "redirect_url")
+            UserInfoController().index()
+            auth_client.user_info.assert_called_once_with("token")
