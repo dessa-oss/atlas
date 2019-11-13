50b0ef70f476bf5d9f5a8ce06f204e7eefae1692
calvinchoi <c.choi@dessa.com>
2019-11-13 15:09:36 -0500

    [CALVIN NN] Added data contract details to frontend

---
+++ foundations_ui_orbit/src/js/components/common/ModalInfo.js
@@ -0,0 +1,50 @@
+import React from "react";
+import PropTypes from "prop-types";
+import CommonActions from "../../actions/CommonActions";
+
+class ModalInfo extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { message: "" };
+
+    this.reload = this.reload.bind(this);
+  }
+
+  componentDidMount() {
+    this.reload();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (!CommonActions.deepEqual(this.props, prevProps)) {
+      this.reload();
+    }
+  }
+
+  reload() {
+    const { fetchInfo } = this.props;
+
+    fetchInfo().then(result => this.setState({ message: result }));
+  }
+
+  render() {
+    let { message } = this.state;
+
+    return (
+      <div className="centered-modal-container">
+        {message}
+      </div>
+    );
+  }
+}
+
+
+ModalInfo.propTypes = {
+  fetchInfo: PropTypes.func
+};
+
+ModalInfo.defaultProps = {
+  fetchInfo: () => {}
+};
+
+export default ModalInfo;
