a755a2ed27782e6c3b5be0da6ba7e525c4e9856d
shazraz <s.raza@dessa.com>
2019-11-19 10:31:04 -0500

    Revert "[NN SR] Improved performance of data contract creation"
    This reverts commit 6ce8432d

--- foundations_orbit_sdk/src/foundations_orbit/utils/get_column_types.py
+++ foundations_orbit_sdk/src/foundations_orbit/utils/get_column_types.py
@@ -7,29 +7,23 @@ Written by Susan Davis <s.davis@dessa.com>, 06 2018
 
 
 def get_column_types(dataframe):
-        from pandas.api.types import is_string_dtype, is_bool_dtype, is_datetime64_any_dtype
-
+        import numpy
+        import datetime
         column_names = list(dataframe.columns)
         column_types = {column_name: str(dataframe.dtypes[column_name]) for column_name in column_names}
-        object_column_names = list(dataframe.dtypes[dataframe.dtypes == 'object'].index)
 
-        for col_name in object_column_names:
-            object_type_column = dataframe[col_name]
-            if _column_has_nans(object_type_column):
-                object_type_column = _filter_nans_from_column(object_type_column)
-
-            if is_string_dtype(object_type_column):
-                column_types[col_name] = 'str'
-            elif is_datetime64_any_dtype(object_type_column):
-                column_types[col_name] = 'datetime'
-            elif is_bool_dtype(object_type_column):
-                column_types[col_name] = 'bool'
+        for col_name, col_type in column_types.items():
+            if col_type == "object":
+                object_type_column = dataframe[col_name]
+                string_column_mask = ['str' in str(type(value)) or value != value for value in object_type_column]
+                date_column_mask = [type(value) == datetime or value != value for value in object_type_column]
+                bool_column_mask = [type(value) == bool or value != value for value in object_type_column]
+                if all(string_column_mask):
+                    column_types[col_name] = 'str'
+                elif all(date_column_mask):
+                    column_types[col_name] = 'datetime'
+                elif all(bool_column_mask):
+                    column_types[col_name] = 'bool'
+                
 
         return column_names, column_types
-
-
-def _column_has_nans(column):
-    return column.isna().sum()
-
-def _filter_nans_from_column(column):
-    return column[~column.isna()]
