818c3fe27eddc4b3cfabd2a8e8ad7dd798a31c8e
pippinlee <>
2019-11-20 11:47:53 -0500

    [pl] new support page styling

--- foundations_ui/src/js/components/SupportPage/SupportPage.js
+++ foundations_ui/src/js/components/SupportPage/SupportPage.js
@@ -33,61 +33,25 @@ class SupportPage extends Component {
       <div>
         <CommonHeader />
         <div className="support-page-container">
-          <div>
-            <h2 className="font-bold">Got a technical question?</h2>
-            <p>Our Engineers are very active on StackOverflow and constantly monitoring the 
-              <span className="font-bold"> &quot;foundations-atlas&quot; tag, </span>
-              help build the community by asking the question publicly.
-            </p>
-            <div tabIndex={0} role="button" onKeyPress={this.onClickStackOverflow} onClick={this.onClickStackOverflow}>
-              <h2 className="font-bold">Ask on StackOverflow</h2>
+          <div className="support-container">
+            <h1>How can we help?</h1>
+            <div>
+              <h2 className="font-bold">Got a technical question?</h2>
+              <p>Our Engineers are very active on <a href="https://www.stackoverflow.com/" target="blank">Stack Overflow</a> and constantly monitoring the 
+                <span className="font-bold"> &quot;foundations-atlas&quot; tag, </span>
+                help build the community by asking the question publicly.
+              </p>
             </div>
-          </div>
-          <div>
-            <h2 className="font-bold">Need immediate help?</h2>
-            <p>If the documentation and StackOverflow weren&apos;t helpful you can reach out to us via e-mail at
-              <span />
-              <span
-                tabIndex={0}
-                role="button"
-                onKeyPress={this.onClickEmailUs}
-                onClick={this.onClickEmailUs}
-                className="underline"
-              >
-                 foundations@dessa.com
-              </span>
-            </p>
-            <div
-              tabIndex={0}
-              role="button"
-              onKeyPress={this.onClickEmailUs}
-              onClick={this.onClickEmailUs}
-            >
-              <h2 className="font-bold">E-mail Us</h2>
+            <div>
+              <h2 className="font-bold">Need immediate help?</h2>
+              <p>If you don&apos;t find what you&apos;re looking for in our documentation or StackOverflow, you can reach out to us via e-mail at <a href="mailto:foundations@dessa.com" target="blank">foundations@dessa.com</a>
+              </p>
             </div>
-          </div>
-          <div>
-            <h2 className="font-bold">Get Foundations Enterprise</h2>
-            <p>Love using Foundations? Want to implement it for your team? Or want to learn more about what Enterprise
-              version has to offer? Get in touch with us at 
-              <span />
-              <span
-                tabIndex={0}
-                role="button"
-                onKeyPress={this.onClickSales}
-                onClick={this.onClickSales}
-                className="underline"
-              >
-                deploy@dessa.com
-              </span>
-            </p>
-            <div
-              tabIndex={0}
-              role="button"
-              onKeyPress={this.onClickSales}
-              onClick={this.onClickSales}
-            >
-              <h2 className="font-bold">Contact Sales</h2>
+            <div>
+              <h2 className="font-bold">Foundations f or Enterprise?</h2>
+              <p>Love using Foundations? Want to implement it for your team? Or want to learn more about what Enterprise
+                version has to offer? Get in touch with us at <a href="mailto:deploy@dessa.com" target="blank">deploy@dessa.com</a>
+              </p>
             </div>
           </div>
         </div>
