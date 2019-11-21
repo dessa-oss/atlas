e534295d1f8c2c7a33812572eaa1bdc99ef1ed2d
pairing <developers@dessa.com>
2019-11-21 10:07:18 -0500

    [CALVIN] Bug fixes to startup/teardown scripts for setting up environment to develop frontend

--- devops/startup_frontend_dev_orbit.sh
+++ devops/startup_frontend_dev_orbit.sh
@@ -10,7 +10,7 @@ export SCHEDULER_PORT=5000
 
 export FOUNDATIONS_SCHEDULER_URL=localhost:${SCHEDULER_PORT}
 
-export REDIS_HOST=redis
+export REDIS_HOST=localhost
 export REDIS_PORT=6379
 export ARCHIVE_DIR=$FOUNDATIONS/job_data
 export WORKING_DIR=$FOUNDATIONS/local_docker_scheduler/work_dir
@@ -20,7 +20,7 @@ export REACT_APP_API_URL="http://127.0.0.1:${ORBIT_PORT}/api/v1/"
 export REACT_APP_ATLAS_URL="http://127.0.0.1:${ATLAS_PORT}/api/v2beta/"
 export REACT_APP_APIARY_URL="http://private-d03986-iannelladessa.apiary-mock.com/api/v1/"
 
-source devops/teardown_frontend_dev_orbit.sh
+./devops/teardown_frontend_dev_orbit.sh
 
 echo "Note: Ensure you have redis running on port ${REDIS_PORT}"
 
@@ -31,7 +31,7 @@ echo "Running Orbit REST API on port ${ORBIT_PORT}"
 python devops/startup_orbit_api.py ${ORBIT_PORT} > $FOUNDATIONS/logs/atlas_rest_api.log 2>&1 &
 
 echo "Running local docker scheduler on port ${SCHEDULER_PORT}"
-cd ../local_docker_scheduler && python -m local_docker_scheduler -p ${SCHEDULER_PORT} > $FOUNDATIONS/logs/scheduler.log 2>&1 &
+cd ../local-docker-scheduler && python -m local_docker_scheduler -p ${SCHEDULER_PORT} > $FOUNDATIONS/logs/scheduler.log 2>&1 &
 
 echo Running UI on port 3000
 cd foundations_ui_orbit && \
