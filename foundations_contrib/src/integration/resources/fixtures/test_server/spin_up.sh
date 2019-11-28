1ec11620a3c12688a95984fd047c30969d11357e
Nekhil Nagia <nekhilnagia16@gmail.com>
2019-11-28 15:11:41 -0500

    [AM NN] Removed lots of k8s stuff

--- foundations_contrib/src/integration/resources/fixtures/test_server/spin_up.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-kubectl apply -f integration/resources/fixtures/test_server/ingress-controller-mandatory.yaml && \
-kubectl apply -f integration/resources/fixtures/test_server/ingress-controller.yaml && \
-./integration/resources/fixtures/test_server/get_image_pull_secret_in_namespace.sh
--- foundations_contrib/src/integration/resources/fixtures/test_server/tear_down.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export project_name=$1
-
-kubectl -n foundations-scheduler-test delete ingress foundations-model-package-${project_name}-ingress
