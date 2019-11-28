ebb44abc0325a7b16439df5e86856ed6be976493
Kyle De Freitas <k.defreitas@dessa.com>
2019-11-28 16:34:34 -0500

    Updated dev environment script to create directories and install dependencies

--- devops/startup_frontend_dev_orbit.sh
+++ devops/startup_frontend_dev_orbit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 source dev_env.sh
-export REDIS_URL=redis://localhost:6379
+
 export FOUNDATIONS=${FOUNDATIONS_HOME:-~/.foundations}
 
 export ATLAS_PORT=37722
@@ -12,9 +12,17 @@ export FOUNDATIONS_SCHEDULER_URL=http://localhost:${SCHEDULER_PORT}
 
 export REDIS_HOST=localhost
 export REDIS_PORT=6379
+export REDIS_URL="redis://$REDIS_HOST:$REDIS_PORT"
+
 export ARCHIVE_DIR=$FOUNDATIONS/job_data
+mkdir -p ARCHIVE_DIR
+
 export WORKING_DIR=$FOUNDATIONS/local_docker_scheduler/work_dir
+mkdir -p WORKING_DIR
+
 export JOB_BUNDLE_STORE_DIR=$FOUNDATIONS/job_bundle_store_dir
+mkdir -p $JOB_BUNDLE_STORE_DIR
+
 export NUM_WORKERS=0
 
 export REACT_APP_API_URL="http://127.0.0.1:${ORBIT_PORT}/api/v1/"
@@ -23,7 +31,7 @@ export REACT_APP_APIARY_URL="http://private-d03986-iannelladessa.apiary-mock.com
 
 ./devops/teardown_frontend_dev_orbit.sh
 
-echo "Note: Ensure you have redis running on port ${REDIS_PORT}"
+echo "Note: Ensure you have redis running and accessible at ${REDIS_URL}"
 
 echo "Running Atlas REST API on port ${ATLAS_PORT}"
 python devops/startup_atlas_api.py ${ATLAS_PORT} &
@@ -34,9 +42,19 @@ python devops/startup_orbit_api.py ${ORBIT_PORT} &
 echo "Attempting to run scheduler with foundations home set to $FOUNDATIONS"
 
 echo "Running local docker scheduler on port ${SCHEDULER_PORT}"
-cd ../local-docker-scheduler && python -m local_docker_scheduler -p ${SCHEDULER_PORT} > $FOUNDATIONS/logs/scheduler.log 2>&1 &
+cd ../local-docker-scheduler \
+  && echo "Updated dependencies for local_docker_scheduler" \
+  && pip install -r requirements.txt \
+  && python -m local_docker_scheduler -p ${SCHEDULER_PORT} > $FOUNDATIONS/logs/scheduler.log 2>&1 &
 
 echo Running UI on port 3000
 cd foundations_ui_orbit && \
+  echo "Install UI dependencies" && \
   yarn install && \
-  yarn start
+  echo "Starting the UI in development mode with yarn" && \
+  yarn start > $FOUNDATIONS/logs/yarn.log 2>&1 &
+
+echo "Check log files for status of programs:"
+echo "    $FOUNDATIONS/logs/scheduler.log"
+echo "    $FOUNDATIONS/logs/yarn.log"
+echo "    $FOUNDATIONS/logs/orbit_rest_api.log"
