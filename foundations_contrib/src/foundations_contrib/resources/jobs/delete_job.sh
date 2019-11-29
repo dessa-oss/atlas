419e5c180f4f759970534dd5366fe781c919caf8
Nekhil Nagia <nekhilnagia16@gmail.com>
2019-11-29 14:26:22 -0500

    [NN] Removed kubernetes jobs concept

--- foundations_contrib/src/foundations_contrib/resources/jobs/delete_job.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-job_id=$1
-
-kubectl delete fndj -n foundations-scheduler-test $job_id --ignore-not-found && \
-    kubectl delete job -n foundations-scheduler-test foundations-job-$job_id --ignore-not-found
