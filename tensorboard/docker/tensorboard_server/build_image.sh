76fac8118a0494c19762d86ef072af58430df457
pairing <developers@dessa.com>
2019-12-03 14:47:57 -0500

    [PL KD] added build and push for tensorboard and added to CE pipeline

--- tensorboard/docker/tensorboard_server/build_image.sh
+++ tensorboard/docker/tensorboard_server/build_image.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-repo=${1:-docker.shehanigans.net}
-tag=${2:-latest}
+pip_version=`python get_version.py`
+export build_version=`echo $pip_version | sed 's/+/_/g'`
+export NEXUS_DOCKER_REGISTRY=${NEXUS_DOCKER_REGISTRY:-docker.shehanigans.net}
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-docker build -t $repo/tensorboard-server:$tag $DIR
+docker build -t $NEXUS_DOCKER_REGISTRY/atlas-ce/tensorboard-server:$build_version $DIR
+docker tag $NEXUS_DOCKER_REGISTRY/atlas-ce/tensorboard-server:$build_version $NEXUS_DOCKER_REGISTRY/atlas-ce/tensorboard-server:latest
