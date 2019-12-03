60af8dea1feea12b562cc3163be3e030fb61e26e
calvinchoi <c.choi@dessa.com>
2019-12-03 10:40:13 -0500

    [CALVIN CZ] Fix flakiness of cypress tests due to timing

--- foundations_ui/cypress/fixtures/atlas_scheduler/job_submission/submit.py
+++ foundations_ui/cypress/fixtures/atlas_scheduler/job_submission/submit.py
@@ -4,4 +4,5 @@ os.environ['FOUNDATIONS_COMMAND_LINE'] = 'True'
 import foundations
 import sys
 
-foundations.submit(scheduler_config="scheduler", job_directory=sys.argv[1], command=["main.py", sys.argv[2], sys.argv[3], sys.argv[4]])
+deployment = foundations.submit(scheduler_config="scheduler", job_directory=sys.argv[1], command=["main.py", sys.argv[2], sys.argv[3], sys.argv[4]])
+deployment.wait_for_deployment_to_complete(wait_seconds=10)
