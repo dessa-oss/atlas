ada2eb69a235fe7c23290efec9f84215f2e17d68
calvinchoi <c.choi@dessa.com>
2019-12-06 09:51:42 -0500

    [CALVIN PL] Added Atlas install script for uat pipeline

---
+++ devops/install_scripts/install_atlas_for_uat.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir atlas_install
+cd atlas_install
+conda update -n base -c defaults conda --yes
+conda create -n atlas_ce_env python=3.6 --yes
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate atlas_ce_env
+wget $1  # https://s3.amazonaws.com/foundations-public/atlas_ce_installer.py
+yes | python atlas_ce_installer.py
+atlas-server start > /dev/null 2>&1 &
