bade2acff28785e4ad9e2777573bbd7cbe22f9d9
pippinlee <>
2019-12-06 14:43:16 -0500

    [calvin PL] update botocore library to avoid credential error issue

--- ci_install_requirements.sh
+++ ci_install_requirements.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-pip install ansible==2.8.2 boto==2.49.0 boto3==1.9.86 botocore==1.12.183 wget==3.2
-apt-get install -y jq
+pip install ansible==2.8.2 boto==2.49.0 boto3==1.9.86 botocore==1.12.200 wget==3.2
 pip install -i https://$NEXUS_USER:$NEXUS_PASSWORD@$NEXUS_PYPI_PATH --extra-index-url https://pypi.org/simple -U -r requirements_test.txt
