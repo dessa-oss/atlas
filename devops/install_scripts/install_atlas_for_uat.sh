b41df17e1b995bdb9272efce5ccbe6ad7232b8e9
calvinchoi <c.choi@dessa.com>
2019-12-10 16:31:59 -0500

    [CALVIN PL] Update UAT install script to pull installer from e3

--- devops/install_scripts/install_atlas_for_uat.sh
+++ devops/install_scripts/install_atlas_for_uat.sh
@@ -6,6 +6,7 @@ conda update -n base -c defaults conda --yes
 conda create -n atlas_ce_env python=3.6 --yes
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate atlas_ce_env
-wget $1
-yes | python atlas_ce_installer.py
+aws s3 cp $1 ./atlas_ce_installer.sh
+chmod +x atlas_ce_installer.sh
+yes | atlas_ce_installer.sh
 atlas-server start > /dev/null 2>&1 &
