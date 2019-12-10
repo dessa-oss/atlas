6a515acbd76ea1bf5108fc0f58e2137ab44307c3
Kyle De Freitas <k.defreitas@dessa.com>
2019-12-10 15:42:39 -0500

    [CALVIN PL] parameterize URL for s3 installer link for download

--- devops/install_scripts/install_atlas_for_uat.sh
+++ devops/install_scripts/install_atlas_for_uat.sh
@@ -6,6 +6,6 @@ conda update -n base -c defaults conda --yes
 conda create -n atlas_ce_env python=3.6 --yes
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate atlas_ce_env
-wget https://s3.amazonaws.com/foundations-public/atlas_ce_installer.py
+wget $1
 yes | python atlas_ce_installer.py
 atlas-server start > /dev/null 2>&1 &
