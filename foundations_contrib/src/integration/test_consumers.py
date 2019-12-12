540340cef03188ad4b6d93e95eece8c85a710505
Dessa-ColeClifford <c.clifford@dessa.com>
2019-12-12 10:50:39 -0500

    [CC SR] Fixed broken acceptance tests

--- foundations_contrib/src/integration/test_consumers.py
+++ foundations_contrib/src/integration/test_consumers.py
@@ -8,7 +8,7 @@ Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
 import unittest
 from foundations_spec import *
 
-@quarantine
+
 class TestConsumers(unittest.TestCase):
 
     def setUp(self):
