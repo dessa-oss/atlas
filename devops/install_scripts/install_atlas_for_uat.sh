d314843b9e54662bfe1bc15d10c79b5fa7b038f8
pairing <developers@dessa.com>
2019-12-12 10:02:12 -0500

    [CALVIN CZ] do not print logs when downloading from s3

--- devops/install_scripts/install_atlas_for_uat.sh
+++ devops/install_scripts/install_atlas_for_uat.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-mkdir atlas_install
-cd atlas_install
-conda update -n base -c defaults conda --yes
-conda create -n atlas_ce_env python=3.6 --yes
-source ~/anaconda3/etc/profile.d/conda.sh
-conda activate atlas_ce_env
-aws s3 cp $1 ./atlas_ce_installer.sh
-chmod +x atlas_ce_installer.sh
-yes | ./atlas_ce_installer.sh
+mkdir atlas_install && \
+cd atlas_install && \
+conda update -n base -c defaults conda --yes && \
+conda create -n atlas_ce_env python=3.6 --yes && \
+source ~/anaconda3/etc/profile.d/conda.sh && \
+conda activate atlas_ce_env && \
+aws s3 cp --quiet $1 ./atlas_ce_installer.sh && \
+echo 'Downloaded successfully' && \
+chmod +x atlas_ce_installer.sh && \
+yes | ./atlas_ce_installer.sh && \
 atlas-server start > /dev/null 2>&1 &
