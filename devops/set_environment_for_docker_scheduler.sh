f8aebcf43b32b4ce6fb50a77156e7c542b1c3566
pairing <dessa-devops@dessa.com>
2019-12-13 15:21:44 -0500

    [KD] updated the setup script based on current strategy for running orbit acceptance tests

--- devops/set_environment_for_docker_scheduler.sh
+++ devops/set_environment_for_docker_scheduler.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
 
-export REDIS_URL=redis://localhost:6379
 export FOUNDATIONS=${FOUNDATIONS_HOME:-~/.foundations}
 
-export ATLAS_PORT=37722
-export ORBIT_PORT=37222
-export SCHEDULER_PORT=5000
+export ATLAS_PORT=${ATLAS_PORT:-37722}
+export ORBIT_PORT=${ORBIT_PORT:-37222}
 
-export FOUNDATIONS_SCHEDULER_URL=http://localhost:${SCHEDULER_PORT}
-export LOCAL_DOCKER_SCHEDULER_HOST=localhost
-export REMOTE_FOUNDATIONS_HOME=$FOUNDATIONS
+export SCHEDULER_PORT=${SCHEDULER_PORT:-5000}
+export SCHEDULER_HOST=${SCHEDULER_HOST:-localhost}
+export FOUNDATIONS_SCHEDULER_URL=http://${SCHEDULER_HOST}:${SCHEDULER_PORT}
 
-export REDIS_HOST=localhost
-export REDIS_PORT=6379
+export REDIS_HOST=${REDIS_HOST:-localhost}
+export REDIS_PORT=${REDIS_PORT:-6379}
+export REDIS_URL="redis://$REDIS_HOST:$REDIS_PORT"
+
+export F9S_LOG_DIR=$FOUNDATIONS/logs
+mkdir -p $F9S_LOG_DIR
+
 export ARCHIVE_DIR=$FOUNDATIONS/job_data
+mkdir -p $ARCHIVE_DIR
+
 export WORKING_DIR=$FOUNDATIONS/local_docker_scheduler/work_dir
+mkdir -p $WORKING_DIR
+
 export JOB_BUNDLE_STORE_DIR=$FOUNDATIONS/job_bundle_store_dir
+mkdir -p $JOB_BUNDLE_STORE_DIR
+
 export NUM_WORKERS=0
+
+export REACT_APP_API_URL="http://127.0.0.1:${ORBIT_PORT}/api/v1/"
+export REACT_APP_ATLAS_URL="http://127.0.0.1:${ATLAS_PORT}/api/v2beta/"
+export REACT_APP_APIARY_URL="http://private-d03986-iannelladessa.apiary-mock.com/api/v1/"
+
+export LOCAL_DOCKER_SCHEDULER_DIR=../local-docker-scheduler
+
+export LOCAL_DOCKER_SCHEDULER_HOST=${LOCAL_DOCKER_SCHEDULER_HOST:-localhost}
+export REMOTE_FOUNDATIONS_HOME=${REMOTE_FOUNDATIONS_HOME:-$FOUNDATIONS}
--- devops/startup_frontend_dev_orbit.sh
+++ devops/startup_frontend_dev_orbit.sh
@@ -1,33 +1,7 @@
 #!/bin/bash
 
 source activate_dev_env.sh
-
-export FOUNDATIONS=${FOUNDATIONS_HOME:-~/.foundations}
-
-export ATLAS_PORT=37722
-export ORBIT_PORT=37222
-export SCHEDULER_PORT=5000
-
-export FOUNDATIONS_SCHEDULER_URL=http://localhost:${SCHEDULER_PORT}
-
-export REDIS_HOST=localhost
-export REDIS_PORT=6379
-export REDIS_URL="redis://$REDIS_HOST:$REDIS_PORT"
-
-export ARCHIVE_DIR=$FOUNDATIONS/job_data
-mkdir -p ARCHIVE_DIR
-
-export WORKING_DIR=$FOUNDATIONS/local_docker_scheduler/work_dir
-mkdir -p WORKING_DIR
-
-export JOB_BUNDLE_STORE_DIR=$FOUNDATIONS/job_bundle_store_dir
-mkdir -p $JOB_BUNDLE_STORE_DIR
-
-export NUM_WORKERS=0
-
-export REACT_APP_API_URL="http://127.0.0.1:${ORBIT_PORT}/api/v1/"
-export REACT_APP_ATLAS_URL="http://127.0.0.1:${ATLAS_PORT}/api/v2beta/"
-export REACT_APP_APIARY_URL="http://private-d03986-iannelladessa.apiary-mock.com/api/v1/"
+source ./devops/set_environment_for_docker_scheduler.sh
 
 ./devops/teardown_frontend_dev_orbit.sh
 
@@ -39,25 +13,22 @@ python devops/startup_atlas_api.py ${ATLAS_PORT} &
 echo "Running Orbit REST API on port ${ORBIT_PORT}"
 python devops/startup_orbit_api.py ${ORBIT_PORT} &
 
-echo "Attempting to run scheduler with foundations home set to $FOUNDATIONS"
-
-# SCHEDULER_CONDA_ENV=local_docker_scheduler
-# echo "Creating environment for local_docker_scheduler"
-# conda env list | grep $SCHEDULER_CONDA_ENV
-# if $? == 1; then
-#     conda create --name $SCHEDULER_CONDA_ENV python=3.6 -y
-# fi
-# previous_env=$(conda env list | grep "*" | awk '{print $1}')
-# echo "Running local docker scheduler on port ${SCHEDULER_PORT}"
-# cd ../local-docker-scheduler \
-#   && echo "Updated dependencies for $SCHEDULER_CONDA_ENV" \
-#   && conda activate $SCHEDULER_CONDA_ENV \
-#   && pip install -r requirements.txt \
-#   && python -m local_docker_scheduler -p ${SCHEDULER_PORT} > $FOUNDATIONS/logs/scheduler.log 2>&1 &
-# conda activate $previous_env
-
-cd ../local-docker-scheduler \
-  && pip install -r requirements.txt \
+echo "Generating database.config.yaml at $FOUNDATIONS/config/local_docker_scheduler/" \
+  && cat ./devops/envsubsts/database.config.envsubst.yaml | envsubst > $FOUNDATIONS/config/local_docker_scheduler/database.config.yaml \
+  && echo '' \
+  && echo 'Creating a symbolic link for the scheduler to run locally between $FOUNDATIONS/config/local_docker_scheduler/database.config.yaml -> $LOCAL_DOCKER_SCHEDULER_DIR/database.config.yaml' \
+  && echo '' \
+  && rm -f $LOCAL_DOCKER_SCHEDULER_DIR/database.config.yaml \
+  && ln -s $FOUNDATIONS/config/local_docker_scheduler/database.config.yaml $LOCAL_DOCKER_SCHEDULER_DIR/database.config.yaml \
+  && echo "Generating tracker.config.yaml at $FOUNDATIONS/config/local_docker_scheduler/" \
+  && cat ./devops/envsubsts/tracker_client_plugins.envsubst.yaml | envsubst > $FOUNDATIONS/config/local_docker_scheduler/tracker_client_plugins.yaml \
+  && echo '' \
+  && echo 'Creating a symbolic link for the scheduler to run locally between $FOUNDATIONS/config/local_docker_scheduler/tracker_client_plugins.yaml -> $LOCAL_DOCKER_SCHEDULER_DIR/tracker_client_plugins.yaml' \
+  && echo '' \
+  && rm -f $LOCAL_DOCKER_SCHEDULER_DIR/tracker_client_plugins.yaml \
+  && ln -s $FOUNDATIONS/config/local_docker_scheduler/tracker_client_plugins.yaml $LOCAL_DOCKER_SCHEDULER_DIR/tracker_client_plugins.yaml \
+  && cd $LOCAL_DOCKER_SCHEDULER_DIR \
+  && pip install -r requirements_dev.txt \
   && python -m local_docker_scheduler -p ${SCHEDULER_PORT} > $FOUNDATIONS/logs/scheduler.log 2>&1 &
 
 cd foundations_ui_orbit && \
@@ -69,6 +40,6 @@ cd foundations_ui_orbit && \
 echo "Running UI on port 3000"
 
 echo "Check log files for status of programs:"
-echo "    $FOUNDATIONS/logs/scheduler.log"
-echo "    $FOUNDATIONS/logs/yarn.log"
-echo "    $FOUNDATIONS/logs/orbit_rest_api.log"
+echo "    tail -f $FOUNDATIONS/logs/scheduler.log"
+echo "    tail -f $FOUNDATIONS/logs/yarn.log"
+echo "    tail -f $FOUNDATIONS/logs/orbit_rest_api.log"
