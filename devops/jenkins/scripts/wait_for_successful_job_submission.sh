f3430a7b458beb75a289cb1b048de94791d974d2
pairing <developers@dessa.com>
2019-12-17 13:45:55 -0500

    [NN CZ] don't stream job logs in test submit

--- devops/jenkins/scripts/wait_for_successful_job_submission.sh
+++ devops/jenkins/scripts/wait_for_successful_job_submission.sh
@@ -1,7 +1,7 @@
 attempt_counter=0
 max_attempts=$1
 
-until $(foundations submit scheduler . main.py); do
+until "$(foundations submit scheduler . main.py --stream-job-logs False)"; do
     if [ ${attempt_counter} -eq ${max_attempts} ];then
       echo "Max attempts reached"
       exit 1
