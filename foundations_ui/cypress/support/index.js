4c52c0332f594c57cba8a7ae2f3eb8cbd8077f6a
pairing <developers@dessa.com>
2019-12-17 10:25:17 -0500

    [NN CZ] move before hook to correct in index file

--- foundations_ui/cypress/support/index.js
+++ foundations_ui/cypress/support/index.js
@@ -1,4 +1,23 @@
-before(() => cy.exec('python cypress/fixtures/atlas_scheduler/envsubst.py'));
+
+const command_to_run = {
+    testName: `Test Local Job Submission with CLI from outside job directory`,
+    projectName: 'local_job_submission',
+    command: `export FOUNDATIONS_HOME=\`pwd\`/cypress/fixtures/atlas_scheduler/.foundations && cd cypress/fixtures/atlas_scheduler/local_job_submission/ && python local_job_submission_project/main.py 0 "Complete_Task_1" dummy`
+}
+
+const schedulerIP = Cypress.env('SCHEDULER_IP');
+const schedulerRedisPort = Cypress.env('SCHEDULER_REDIS_PORT');
+const guiHost = Cypress.env('GUI_HOST');
+const guiPort = Cypress.env('GUI_PORT');
+
+before(() => {
+    cy.exec('python cypress/fixtures/atlas_scheduler/envsubst.py');
+    cy.exec(`redis-cli -h ${schedulerIP} -p ${schedulerRedisPort} flushall`);
+    cy.exec(command_to_run.command);
+    cy.visit(`http://${guiHost}:${guiPort}/projects`);
+    cy.contains(command_to_run.projectName).should('exist');
+});
+
 
 beforeEach(() => {
   if (Cypress.env('ATLAS_EDITION') === 'TEAM') {
