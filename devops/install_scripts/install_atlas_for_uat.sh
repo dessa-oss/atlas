9f4ca8a183d08128b61055283b96db95d8acedd0
pairing <developers@dessa.com>
2019-12-17 15:35:57 -0500

    [NN CZ] move waiting for atlas server into install atlas for uat script

--- devops/install_scripts/install_atlas_for_uat.sh
+++ devops/install_scripts/install_atlas_for_uat.sh
@@ -10,3 +10,7 @@ echo 'Downloaded successfully' && \
 chmod +x atlas_ce_installer.sh && \
 yes | ./atlas_ce_installer.sh
 atlas-server start > /dev/null 2>&1 &
+mkdir -p test && \
+echo import foundations >> test/main.py && \
+chmod +x wait_for_successful_job_submission.sh && \
+./wait_for_successful_job_submission.sh 20
