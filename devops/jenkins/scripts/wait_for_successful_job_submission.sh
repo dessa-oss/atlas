218a950b8011c29f75a7bfaae9e54c68a9a0749b
pairing <developers@dessa.com>
2019-12-17 14:53:55 -0500

    [NN CZ] wait for job submission in pipeline

--- devops/jenkins/scripts/wait_for_successful_job_submission.sh
+++ devops/jenkins/scripts/wait_for_successful_job_submission.sh
@@ -1,7 +1,7 @@
 attempt_counter=0
 max_attempts=$1
 
-until "$(foundations submit scheduler . main.py --stream-job-logs False)"; do
+until $(foundations submit scheduler test main.py >> /dev/null); do
     if [ ${attempt_counter} -eq ${max_attempts} ];then
       echo "Max attempts reached"
       exit 1
