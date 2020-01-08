b40766f95d1ca6f418f252b9621056be9e458fa9
Nekhil Nagia <nekhilnagia16@gmail.com>
2020-01-08 12:01:03 -0500

    [PL NN] debugging if test is failing as expected

--- testing/auth_acceptance/__init__.py
+++ testing/auth_acceptance/__init__.py
@@ -8,5 +8,5 @@ Written by Susan Davis <s.davis@dessa.com>, 11 2019
 from auth_acceptance.config import setup_auth_home_config
 setup_auth_home_config()
 
-from auth_acceptance.test_auth_via_cli import TestAuthViaClient
+# from auth_acceptance.test_auth_via_cli import TestAuthViaClient
 from auth_acceptance.test_submit_with_auth import TestSubmitWithAuth
