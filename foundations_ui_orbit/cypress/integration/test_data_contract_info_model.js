55d7bd5d5848151a3dbfba3ca3e1a41d7d7e0084
pairing <dessa-devops@dessa.com>
2020-01-08 14:41:53 -0500

    [AM CALVIN] Skip old cypress test

--- foundations_ui_orbit/cypress/integration/test_data_contract_info_model.js
+++ foundations_ui_orbit/cypress/integration/test_data_contract_info_model.js
@@ -1,10 +1,10 @@
-describe('Test Data Contract Info', () => {
+describe.skip('Test Data Contract Info', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000/projects');
-    cy.contains('test').click({force: true});
-    cy.get('.icon-monitor').click({force: true});
-    cy.get('.validation-results-table-row').first().click({force: true});
-  });  
+    cy.contains('test').click({ force: true });
+    cy.get('.icon-monitor').click({ force: true });
+    cy.get('.validation-results-table-row').first().click({ force: true });
+  });
 
   it('Has Button', () => {
     cy.get('.i--icon-open').should('exist');
@@ -12,7 +12,7 @@ describe('Test Data Contract Info', () => {
 
   it('Clicking Button Opens Modal', () => {
     cy.get('[data-cy=data-contract-info-modal').should('not.exist');
-    cy.get('.i--icon-open').click({force: true});
+    cy.get('.i--icon-open').click({ force: true });
     cy.get('[data-cy=data-contract-info-modal').should('exist');
   })
 });
