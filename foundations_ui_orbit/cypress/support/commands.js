050ac3282b68c6af76047c2b10859e115ebcd047
pairing <dessa-devops@dessa.com>
2020-01-08 14:41:28 -0500

    [AM CALVIN] Add cypress custom commands

--- foundations_ui_orbit/cypress/support/commands.js
+++ foundations_ui_orbit/cypress/support/commands.js
@@ -23,3 +23,8 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('forceClick', { prevSubject: 'element' }, element => cy.wrap(element).click({ force: true }));
+Cypress.Commands.add('forceType', { prevSubject: 'element' }, (element, keys) => cy.wrap(element).type(keys, { force: true }));
+Cypress.Commands.add('cyGet', dataClass => cy.get(`[data-class=${dataClass}]`));
+Cypress.Commands.add('cyFind', { prevSubject: 'element' }, (element, dataClass) => cy.wrap(element).find(`[data-class=${dataClass}]`));
--- foundations_ui_orbit/cypress/support/index.js
+++ foundations_ui_orbit/cypress/support/index.js
@@ -1,3 +1,5 @@
+import './commands';
+
 before(() => {
   cy.exec('python cypress/fixtures/orbit_acceptance/envsubst.py');
 });
