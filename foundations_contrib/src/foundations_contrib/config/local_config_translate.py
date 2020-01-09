1741f5c640cc3a714ab90b3cbe153f80bb98b2c6
pairing <developers@dessa.com>
2020-01-09 11:06:46 -0500

    Revert "[CALVIN CZ] Removed local config translate"
    This reverts commit 314699414ac94cdc3d003f2a8f24f6eb19ccfccd.

---
+++ foundations_contrib/src/foundations_contrib/config/local_config_translate.py
@@ -0,0 +1,74 @@
+"""
+Copyright (C) DeepLearning Financial Technologies Inc. - All Rights Reserved
+Unauthorized copying, distribution, reproduction, publication, use of this file, via any medium is strictly prohibited
+Proprietary and confidential
+Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
+"""
+
+from os.path import join
+from foundations_internal.config.common_translate import get_translate_implementation
+
+
+def get_translator_config(config_option):
+    config_options = {
+        'get_result_end_point': _get_result_end_point,
+        'archive_implementation': _archive_implementation,
+        'archive_listing_implementation': _archive_listing_implementation,
+        'deployment_implementation': _deployment_implementation,
+        'project_listing_implementation': _project_listing_implementation,
+        'cache_implementation': _cache_implementation,
+    }
+    return config_options[config_option]
+
+def _get_result_end_point(config):
+    return config['results_config'].get('archive_end_point', _get_default_archive_end_point())
+
+def _get_default_archive_end_point():
+    from os.path import expanduser
+    from os.path import join
+    from foundations_contrib.utils import foundations_home
+
+    return join(expanduser(foundations_home()), 'job_data')
+
+def _cache_implementation(config):
+    from foundations_contrib.config.mixin import cache_implementation
+    from foundations_contrib.local_file_system_cache_backend import LocalFileSystemCacheBackend
+    from foundations_contrib.local_file_system_bucket import LocalFileSystemBucket
+
+    cache_config = config['cache_config']
+    if 'end_point' in cache_config:
+        cache_end_point = cache_config['end_point']
+        return cache_implementation(cache_end_point, LocalFileSystemBucket)
+
+    cache_end_point = _get_default_archive_end_point()
+    cache_path = join(cache_end_point, 'cache')
+    return {
+        'cache_type': LocalFileSystemCacheBackend,
+        'constructor_arguments': [cache_path]
+    }
+
+def _project_listing_implementation(result_end_point):
+    from foundations_contrib.config.mixin import project_listing_implementation
+    from foundations_contrib.local_file_system_bucket import LocalFileSystemBucket
+
+    return project_listing_implementation(result_end_point, LocalFileSystemBucket)
+
+def _deployment_implementation():
+    from foundations_contrib.local_shell_job_deployment import LocalShellJobDeployment
+    return {
+        'deployment_type': LocalShellJobDeployment
+    }
+
+def _archive_listing_implementation(result_end_point):
+    from foundations_contrib.config.mixin import archive_listing_implementation
+    from foundations_contrib.local_file_system_bucket import LocalFileSystemBucket
+
+    return archive_listing_implementation(result_end_point, LocalFileSystemBucket)
+
+def _archive_implementation(result_end_point):
+    from foundations_contrib.config.mixin import archive_implementation
+    from foundations_contrib.local_file_system_bucket import LocalFileSystemBucket
+
+    return archive_implementation(result_end_point, LocalFileSystemBucket)
+
+translate = get_translate_implementation(get_translator_config)
---
+++ foundations_contrib/src/test/config/test_local_config_translate.py
@@ -0,0 +1,86 @@
+"""
+Copyright (C) DeepLearning Financial Technologies Inc. - All Rights Reserved
+Unauthorized copying, distribution, reproduction, publication, use of this file, via any medium is strictly prohibited
+Proprietary and confidential
+Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
+"""
+
+import unittest
+from mock import Mock, patch
+
+from foundations_spec.helpers.spec import Spec
+from foundations_spec.helpers import let, let_patch_mock, set_up
+from foundations_internal.testing.shared_examples.config_translates import ConfigTranslates
+from foundations_internal.testing.shared_examples.test_bucket_from_scheme import TestBucketFromScheme
+
+class TestLocalConfigTranslate(Spec, ConfigTranslates, TestBucketFromScheme):
+
+    @let
+    def translator(self):
+        import foundations_contrib.config.local_config_translate as translator
+        return translator
+
+    @let
+    def archive_type(self):
+        from foundations_contrib.local_file_system_pipeline_archive import LocalFileSystemPipelineArchive
+        return LocalFileSystemPipelineArchive
+
+    @let
+    def listing_type(self):
+        from foundations_contrib.local_file_system_pipeline_listing import LocalFileSystemPipelineListing
+        return LocalFileSystemPipelineListing
+
+    @let
+    def cache_type(self):
+        from foundations_contrib.local_file_system_cache_backend import LocalFileSystemCacheBackend
+        return LocalFileSystemCacheBackend
+
+    @let
+    def bucket_type(self):
+        from foundations_contrib.local_file_system_bucket import LocalFileSystemBucket
+        return LocalFileSystemBucket
+
+    expanduser = let_patch_mock('os.path.expanduser')
+
+    @set_up
+    def set_up(self):
+        del self._configuration['results_config']['archive_end_point']
+        del self._configuration['cache_config']['end_point']
+
+        # ensure expanduser is patched
+        self.expanduser.return_value = ''
+
+    def test_ensure_expandpath_called_properly(self):
+        self.patch('os.environ', {})
+        result_config = self.translator.translate(self._configuration)
+        self.expanduser.assert_called_with('~/.foundations')
+
+    def test_returns_deployment_with_local_type(self):
+        from foundations_contrib.local_shell_job_deployment import LocalShellJobDeployment
+
+        result_config = self.translator.translate(self._configuration)
+        config = result_config['deployment_implementation']
+        self.assertEqual(config['deployment_type'], LocalShellJobDeployment)
+
+    def test_returns_cache_with_default_path(self):
+        self.expanduser.return_value = '/home/lou/.foundations'
+        result_config = self.translator.translate(self._configuration)
+        config = result_config['cache_implementation']
+        self.assertEqual(['/home/lou/.foundations/job_data/cache'], config['constructor_arguments'])
+
+    def test_returns_cache_with_default_path_different_home(self):
+        self.expanduser.return_value = '/home/hana/.foundations'
+        result_config = self.translator.translate(self._configuration)
+        config = result_config['cache_implementation']
+        self.assertEqual(['/home/hana/.foundations/job_data/cache'], config['constructor_arguments'])
+
+    def test_supports_missing_ssh_config(self):
+        del self._configuration['ssh_config']
+        with self.assert_does_not_raise():
+            result_config = self.translator.translate(self._configuration)
+
+    def test_support_using_windows_paths(self):
+        result_config = self.translator.translate(self._configuration)
+        self._configuration['results_config']['archive_end_point'] = 'D:\\username\\folder\\another_folder'
+        with self.assert_does_not_raise():
+            result_config = self.translator.translate(self._configuration)
