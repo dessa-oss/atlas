d6c5edad94c0309cc780d5949f9b7ad4953234a5
pairing <developers@dessa.com>
2020-01-09 11:31:09 -0500

    [CZ CALVIN] Removed BucketPipelineListing from the SDK

--- aws_utils/src/foundations_aws/aws_pipeline_archive_listing.py
+++ aws_utils/src/foundations_aws/aws_pipeline_archive_listing.py
@@ -11,7 +11,7 @@ class AWSPipelineArchiveListing(object):
     def __init__(self, bucket):
         from foundations_aws.aws_bucket import AWSBucket
         from foundations.prefixed_bucket import PrefixedBucket
-        from foundations.bucket_pipeline_listing import BucketPipelineListing
+        from foundations_contrib.bucket_pipeline_listing import BucketPipelineListing
 
         self._listing = BucketPipelineListing(
             PrefixedBucket, 'pipeline_archives', AWSBucket, bucket)
