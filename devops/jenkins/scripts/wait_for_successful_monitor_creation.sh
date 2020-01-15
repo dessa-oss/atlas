451bd0305aa3b90b5490a9a21904e831a3f67677
pairing <dessa-devops@dessa.com>
2020-01-15 09:42:02 -0500

    [KD CALVIN] Changed waiting for monitor creation to wait for login

--- devops/jenkins/scripts/wait_for_successful_monitor_creation.sh
+++ devops/jenkins/scripts/wait_for_successful_monitor_creation.sh
@@ -1,7 +1,21 @@
 attempt_counter=0
 max_attempts=$1
 
-foundations login http://localhost:5558 -u test -p test
+echo "Attempting to login to foundations."
+until $(foundations login http://localhost:5558 -u test -p test >> /dev/null); do
+  if [ ${attempt_counter} -eq ${max_attempts} ];then
+    echo "Max attempts reached"
+    exit 1
+  fi
+
+  printf '.'
+  attempt_counter=$(($attempt_counter+1))
+  sleep 2
+done
+
+attempt_counter=0
+
+echo "Attempting to create a monitor."
 until $(foundations monitor create test main.py >> /dev/null); do
   if [ ${attempt_counter} -eq ${max_attempts} ];then
     echo "Max attempts reached"
