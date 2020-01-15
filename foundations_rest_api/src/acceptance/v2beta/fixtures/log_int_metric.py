682c7c7c26d96c4a4e2048b23b864ce7b40fb44c
pairing <developers@dessa.com>
2020-01-15 14:54:48 -0500

    [AM CZ] configuration files for foundations submit

---
+++ foundations_rest_api/src/acceptance/v2beta/fixtures/log_int_metric.py
@@ -0,0 +1,9 @@
+"""
+Copyright (C) DeepLearning Financial Technologies Inc. - All Rights Reserved
+Unauthorized copying, distribution, reproduction, publication, use of this file, via any medium is strictly prohibited
+Proprietary and confidential
+Written by Susan Davis <s.davis@dessa.com>, 12 2018
+"""
+
+import foundations
+foundations.log_metric('int_metric', '5' * 5000)
