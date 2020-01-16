54650a3871b1497b9ef49c4f9eb0b0c2ae0e8ecd
Cindy Zhao <cindyzhao@Cindys-MacBook-Pro.local>
2020-01-16 10:23:30 -0500

    [AM CZ] ensubst redis host in tracker configurations

--- foundations_rest_api/src/foundations_rest_api/config/job_submission_envsubst.sh
+++ foundations_rest_api/src/foundations_rest_api/config/job_submission_envsubst.sh
@@ -4,5 +4,5 @@ envsubst '$REMOTE_FOUNDATIONS_HOME $LOCAL_DOCKER_SCHEDULER_HOST $REDIS_PORT' < a
 envsubst '$FOUNDATIONS_HOME $REDIS_HOST' < acceptance/v2beta/fixtures/foundations_home/config/local_docker_scheduler/worker_config/execution/default.config.envsubst.yaml > acceptance/v2beta/fixtures/foundations_home/config/local_docker_scheduler/worker_config/execution/default.config.yaml
 envsubst '$FOUNDATIONS_HOME $LOCAL_DOCKER_SCHEDULER_HOST $REDIS_PORT' < acceptance/v2beta/fixtures/foundations_home/config/execution/default.config.envsubst.yaml > acceptance/v2beta/fixtures/foundations_home/config/execution/default.config.yaml
 
-envsubst '$REDIS_HOST' < acceptance/v2beta/fixtures/foundations_home/config/local_docker_scheduler/database.config.envsubst.yaml > acceptance/v2beta/fixtures/foundations_home/config/local_docker_scheduler/database.config.yaml
-envsubst '$REDIS_HOST' < acceptance/v2beta/fixtures/foundations_home/config/local_docker_scheduler/tracker_client_plugins.envsubst.yaml > acceptance/v2beta/fixtures/foundations_home/config/local_docker_scheduler/tracker_client_plugins.yaml
+envsubst '$REDIS_HOST $REDIS_PORT' < acceptance/v2beta/fixtures/foundations_home/config/local_docker_scheduler/database.config.envsubst.yaml > acceptance/v2beta/fixtures/foundations_home/config/local_docker_scheduler/database.config.yaml
+envsubst '$REDIS_HOST $REDIS_PORT' < acceptance/v2beta/fixtures/foundations_home/config/local_docker_scheduler/tracker_client_plugins.envsubst.yaml > acceptance/v2beta/fixtures/foundations_home/config/local_docker_scheduler/tracker_client_plugins.yaml
