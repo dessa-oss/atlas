04c5be5032f250e0e8440356cd89907bf12b8a7f
amackillop <austin.mackillop@gmail.com>
2020-01-17 15:36:03 -0500

    [AM CZ NN] Check if log folder exists before setting file handle

--- devops/startup_atlas_api.py
+++ devops/startup_atlas_api.py
@@ -9,6 +9,7 @@ try:
     from foundations_local_docker_scheduler_plugin.job_deployment import JobDeployment
     import logging
     import os
+    import os.path as path
 
     foundations_home = os.path.abspath(os.path.expanduser(os.environ.get('FOUNDATIONS_HOME', '~/.foundations')))
     redis_url = f"redis://{os.environ.get('REDIS_HOST', 'redis')}:{os.environ.get('REDIS_PORT', 6379)}"
@@ -25,6 +26,8 @@ try:
     root_logger = logging.getLogger()
     root_logger.setLevel(logging.DEBUG)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    if not path.exists(f'{foundations_home}/logs'):
+        os.mkdir(f'{foundations_home}/logs')
     file_handler = logging.FileHandler(f'{foundations_home}/logs/atlas_rest_api.log')
     file_handler.setLevel(logging.DEBUG)
     file_handler.setFormatter(formatter)
