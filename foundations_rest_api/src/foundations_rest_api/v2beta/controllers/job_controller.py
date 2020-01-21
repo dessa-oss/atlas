c09522bd70241331728a67a14367a883a5413e30
pippinlee <pippinblee@gmail.com>
2020-01-21 10:15:31 -0500

    [NN PL] remove kubernetes based tests

--- foundations_rest_api/src/foundations_rest_api/v2beta/controllers/job_controller.py
+++ foundations_rest_api/src/foundations_rest_api/v2beta/controllers/job_controller.py
@@ -11,6 +11,7 @@ from foundations_rest_api.utils.api_resource import api_resource
 @api_resource('/api/v2beta/projects/<string:project_name>/job_listing/<string:job_id>')
 class JobController(object):
 
+    # TODO: add unit tests as previous ones were k8s based
     def delete(self):
         from foundations_core_rest_api_components.response import Response
         from foundations_core_rest_api_components.lazy_result import LazyResult
