b4987bf8ead46ded417ba70f7e3876b694b29733
Nekhil Nagia <nekhilnagia16@gmail.com>
2020-01-22 11:56:57 -0500

    [NN] Formatting for tests

--- foundations_orbit_sdk/src/test/contract_validators/test_domain_checker.py
+++ foundations_orbit_sdk/src/test/contract_validators/test_domain_checker.py
@@ -12,30 +12,34 @@ import numpy as np
 import pandas as pd
 
 class TestDomainChecker(Spec):
+
+    @set_up
+    def set_up(self):
+        self.domain_checker = DomainChecker()
+
     def test_validate_with_no_columns_configured_returns_empty_results(self):
         import pandas
 
         empty_dataframe = pandas.DataFrame({})
         expected_result = {}
-        self.assertEqual(expected_result, DomainChecker().validate(empty_dataframe))
+        self.assertEqual(expected_result, self.domain_checker.validate(empty_dataframe))
 
     def test_domain_checker_passes_when_configured_and_reference_dataframe_used_when_validating(self):
-        domain_checker = DomainChecker()
         column_name = self.faker.word()
-        domain_checker.configure(attributes=[column_name])
+        self.domain_checker.configure(attributes=[column_name])
 
         df = self._generate_dataframe([column_name], int, 100)
-
-        domain_checker.calculate_stats_from_dataframe(df)
+        self.domain_checker.calculate_stats_from_dataframe(df)
 
         expected_result = {column_name: {'status': 'passed'}}
-
-        self.assertEqual(expected_result, domain_checker.validate(df))
+        self.assertEqual(expected_result, self.domain_checker.validate(df))
+    
+    def test_domain_checker_fails_when_column_configured_but_domain_out_of_range(self):
+        pass
 
     def _generate_dataframe(self, column_names, dtype, length):
         import pandas, numpy
         data = {}
-        # numpy.random.seed = 6379
 
         if dtype == int:
             for column in column_names:
