15a6a002039b561169e3c78e5b90c1566bc96162
pippinlee <pippinblee@gmail.com>
2020-01-22 16:11:02 -0500

    [CZ PL] remove safe inspect and tests #1155

--- foundations_internal/src/test/__init__.py
+++ foundations_internal/src/test/__init__.py
@@ -9,7 +9,6 @@ from test.test_cache import TestCache
 from test.test_deployment_manager import TestDeploymentManager
 from test.test_foundations_context import TestFoundationsContext
 from test.test_pipeline_context import TestPipelineContext
-from test.test_safe_inspect import TestSafeInspect
 from test.test_module_manager import TestModuleManager
 from test.test_foundations_serializer import TestFoundationsSerializer
 from test.test_job_resources import TestJobResources
