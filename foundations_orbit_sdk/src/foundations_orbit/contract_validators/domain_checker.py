712940c7d21ce4053aedf221ca92ce7ec89eda95
calvinchoi <c.choi@dessa.com>
2020-01-22 15:06:23 -0500

    [CALVIN NN] Implementing more domain checker

--- foundations_orbit_sdk/src/foundations_orbit/contract_validators/domain_checker.py
+++ foundations_orbit_sdk/src/foundations_orbit/contract_validators/domain_checker.py
@@ -7,13 +7,42 @@ Written by Susan Davis <s.davis@dessa.com>, 01 2020
 
 
 class DomainChecker:
+    def __init__(self):
+        self._unique_values = None
+
     def validate(self, dataframe_to_validate):
-        if len(dataframe_to_validate) != 0:
-            return {dataframe_to_validate.columns[0]: {'status': 'passed'}}
-        return {}
+        summary = {
+            'healthy': 0,
+            'critical': 0,
+            'warning': 0
+        }
+        details_by_attribute = []
+
+        for column in dataframe_to_validate.columns:
+            detail = { 'attribute_name': column }
+            in_domain_mask = dataframe_to_validate[column].isin(self._unique_values[column])
+            column_test_passed = in_domain_mask.all()
+
+            if column_test_passed:
+                summary['healthy'] += 1
+                detail['validation_outcome'] = 'healthy'
+            else:
+                summary['critical'] += 1
+                detail['validation_outcome'] = 'critical'
+                detail['values_out_of_bounds'] = list(dataframe_to_validate[column].mask(in_domain_mask).dropna().unique())
+                detail['percentage_out_of_bounds'] = (~in_domain_mask).sum() / len(in_domain_mask)
+
+            details_by_attribute.append(detail)
+
+        return {
+            'summary': summary,
+            'details_by_attribute': details_by_attribute
+        }
 
     def calculate_stats_from_dataframe(self, reference_dataframe):
-        pass
+        import pandas as pd
+
+        self._unique_values = reference_dataframe.apply(pd.unique)
 
     def configure(self, attributes=[]):
         pass
--- foundations_orbit_sdk/src/test/contract_validators/test_domain_checker.py
+++ foundations_orbit_sdk/src/test/contract_validators/test_domain_checker.py
@@ -18,31 +18,65 @@ class TestDomainChecker(Spec):
         self.domain_checker = DomainChecker()
 
     def test_validate_with_no_columns_configured_returns_empty_results(self):
-        import pandas
-
-        empty_dataframe = pandas.DataFrame({})
-        expected_result = {}
+        empty_dataframe = pd.DataFrame({})
+        expected_result = {
+            'summary': {
+                'healthy': 0,
+                'critical': 0,
+                'warning': 0
+            },
+            'details_by_attribute': []
+        }
         self.assertEqual(expected_result, self.domain_checker.validate(empty_dataframe))
 
     def test_domain_checker_passes_when_configured_and_reference_dataframe_used_when_validating(self):
         column_name = self.faker.word()
         self.domain_checker.configure(attributes=[column_name])
 
-        df = self._generate_dataframe([column_name], int, 100)
+        df = self._generate_dataframe([column_name], int)
         self.domain_checker.calculate_stats_from_dataframe(df)
 
-        expected_result = {column_name: {'status': 'passed'}}
+        expected_result = {
+            'summary': {
+                'healthy': 1,
+                'critical': 0,
+                'warning': 0
+            },
+            'details_by_attribute': [{
+                'attribute_name': column_name,
+                'validation_outcome': 'healthy'
+            }]
+        }
         self.assertEqual(expected_result, self.domain_checker.validate(df))
-    
-    def test_domain_checker_fails_when_column_configured_but_domain_out_of_range(self):
-        pass
 
-    def _generate_dataframe(self, column_names, dtype, length):
-        import pandas, numpy
+    def test_domain_checker_fails_when_column_configured_but_domain_out_of_range(self):
+        column_name = self.faker.word()
+        self.domain_checker.configure(attributes=[column_name])
+
+        ref_df = self._generate_dataframe([column_name], int, min=1, max=5)
+        cur_df = self._generate_dataframe([column_name], int, min=6, max=10)
+        self.domain_checker.calculate_stats_from_dataframe(ref_df)
+
+        expected_result = {
+            'summary': {
+                'healthy': 0,
+                'critical': 1,
+                'warning': 0
+            },
+            'details_by_attribute': [{
+                'attribute_name': column_name,
+                'validation_outcome': 'critical',
+                'values_out_of_bounds': list(cur_df[column_name]),
+                'percentage_out_of_bounds': 1
+            }]
+        }
+        self.assertEqual(expected_result, self.domain_checker.validate(cur_df))
+
+    def _generate_dataframe(self, column_names, dtype, min=-10, max=10):
         data = {}
 
         if dtype == int:
             for column in column_names:
-                data[column] = np.random.randint(-100, 100, length)
+                data[column] = list(range(min, max))
 
-        return pandas.DataFrame(data)
+        return pd.DataFrame(data)
