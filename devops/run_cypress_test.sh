67ef366829cb4162c6bae34663b0290eafc6e804
kyledef <kyle.e.defreitas@gmail.com>
2020-01-26 01:05:42 -0500

    [KD] #1078 added ta check for redis-cli before running cypress tests

--- devops/run_cypress_test.sh
+++ devops/run_cypress_test.sh
@@ -6,6 +6,13 @@ source ./devops/set_environment_for_docker_scheduler.sh
 cwd=`pwd`
 F9S_ENV_TYPE=$1
 
+echo "checking if redis-cli is installed and available ....."
+redis-cli --version >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+  echo "redis-cli is not available. The cli tool is required to clear redis between tests"
+  exit 1
+fi
+
 if [[ $F9S_ENV_TYPE == "atlas" ]]; then
     UI_FOLDER="foundations_ui"
     FIXTURE_FOLDER="atlas_scheduler"
