cd84984cf644dac40cff16dc999773e796c43e75
kyledef <kyle.e.defreitas@gmail.com>
2020-01-27 22:17:31 -0500

    [KD] split the coverage between unit and acceptance tests

--- run_coverage_tests.sh
+++ run_coverage_tests.sh
@@ -1,40 +1,34 @@
 #!/bin/bash
 
-# function get_coverage_report_for_module {
-#     module_directory_to_add="$1"
-#     module_directory="$2"
-#     if [ -d "${module_directory_to_add}/test" ]; then
-#         cd ${module_directory_to_add}
-#         coverage erase && coverage run --source=${module_directory} -m unittest test
-#     fi
-#     if [ -d "${module_directory_to_add}/integration" ]; then
-#         coverage run --source=${module_director} -m unittest integration
-#     fi
-#     if [ -d "${module_directory_to_add}/test" ] || [ -d "${module_directory_to_add}/inegration" ]; then
-#         coverage html -i && \
-#         mkdir -p ${cwd}/coverage_results && \
-#         cp .coverage ${cwd}/coverage_results/.coverage_${module_directory}
-#     fi
-# }
-
 function get_coverage_report_for_module {
-    module_directory_to_add="$1"
-    module_directory="$2"
-    if [ -d "${module_directory_to_add}/test" ]; then
-        cd ${module_directory_to_add}
-        coverage erase && coverage run --source=${module_directory} -m unittest test
+    module_directory="$1"
+    real_module_name="$2"
+    coverage_type="$3"
+
+    if [ -d "${module_directory}/test" ] && { [ "${coverage_type}" == "unittest" ] || [ "${coverage_type}" == "all" ]; }; then
+        echo "Running coverage for unit tests"
+        cd "${module_directory}" || exit && \
+            coverage run --source="${real_module_name}" -m unittest test
     fi
-    if [ -d "${module_directory_to_add}/integration" ]; then
-        coverage run --source=${module_director} -m unittest integration
+    if [ -d "${module_directory}/integration" ]  && { [ "${coverage_type}" == "integration" ] || [ "${coverage_type}" == "all" ]; }; then
+        echo "Running coverage for integration tests"
+        cd "${module_directory}" || exit && \
+            coverage run --source="${real_module_name}" -m unittest integration
     fi
-    if [ -d "${module_directory_to_add}/test" ] || [ -d "${module_directory_to_add}/inegration" ]; then
+    if [ -d "${module_directory}/test" ] || [ -d "${module_directory}/integration" ]; then
         coverage html -i && \
-        mkdir -p ${cwd}/coverage_results && \
-        cp .coverage ${cwd}/coverage_results/.coverage_${module_directory}
+        mkdir -p "${cwd}/coverage_results" && \
+        cp .coverage "${cwd}/coverage_results/.coverage_${real_module_name}"
     fi
 }
 
-cwd=`pwd`
+cwd=$(pwd)
+
+if [ -z "$1" ]; then
+  coverage_type="all"
+else
+  coverage_type=$1
+fi
 
 export TZ=EST
 
@@ -62,7 +56,7 @@ do
         real_module_name="foundations_aws"
     fi
     
-    get_coverage_report_for_module "${cwd}/${module_directory}/src" ${real_module_name}
+    get_coverage_report_for_module "${cwd}/${module_directory}/src" ${real_module_name} "${coverage_type}"
 done
 
 cd ${cwd}/coverage_results && \
