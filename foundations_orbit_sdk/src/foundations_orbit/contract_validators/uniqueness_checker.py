de331cbfe9ca2271ed1262d34c2065bbc79069ad
Nekhil Nagia <nekhilnagia16@gmail.com>
2020-01-29 12:06:53 -0500

    [Calvin NN] #849 | Added hypothesis tests for uniqueness

--- foundations_orbit_sdk/src/foundations_orbit/contract_validators/uniqueness_checker.py
+++ foundations_orbit_sdk/src/foundations_orbit/contract_validators/uniqueness_checker.py
@@ -8,8 +8,25 @@ Written by Calvin Choi <c.choi@dessa.com>, 01 2020
 class UniquenessChecker:
     
     def validate(self, dataframe_to_validate):
-        
+        summary = {
+            'healthy': 0,
+            'critical': 0,
+            'warning': 0
+        }
+        details_by_attribute = []
+
+        for column in dataframe_to_validate.columns:
+            is_unique = len(dataframe_to_validate[column].unique()) == len(dataframe_to_validate[column])
+            column_test_passed = is_unique
+
+            if column_test_passed:
+                detail = { 'attribute_name': column }
+                summary['healthy'] += 1
+                detail['validation_outcome'] = 'healthy'
+
+                details_by_attribute.append(detail)
+
         return {
-            'summary': {},
-            'details_by_attribute': []
+            'summary': summary,
+            'details_by_attribute': details_by_attribute
         }
--- foundations_orbit_sdk/src/test/contract_validators/test_uniqueness_checker.py
+++ foundations_orbit_sdk/src/test/contract_validators/test_uniqueness_checker.py
@@ -9,6 +9,17 @@ import numpy as np
 import pandas as pd
 from foundations_spec import *
 from foundations_orbit.contract_validators.uniqueness_checker import UniquenessChecker
+import string
+from hypothesis import given, assume, example, settings
+import hypothesis.strategies as st
+from hypothesis.extra.pandas import column, data_frames
+
+@st.composite
+def dataframes(draw, *strategies: st.SearchStrategy) -> st.SearchStrategy:
+    names = draw(st.lists(st.text(min_size=1), unique=True, min_size=1))
+    cols = [column(name, elements=draw(st.sampled_from(strategies)), unique=True) for name in names]
+    return draw(data_frames(cols))
+
 
 class TestUniquenessChecker(Spec):
 
@@ -21,9 +32,68 @@ class TestUniquenessChecker(Spec):
 
     def test_uniqueness_checker_validate_with_empty_dataframe_returns_boilerplate_result(self):
         expected = {
-            'summary': {},
+            'summary': {
+                'healthy': 0,
+                'critical': 0,
+                'warning': 0
+            },
             'details_by_attribute': []
         }
         self.assertEqual(expected, self.uniqueness_checker.validate(pd.DataFrame()))
 
 
+    def test_uniqueness_checker_validate_with_one_column_passes(self):
+        expected_result = {
+            'summary': self._generate_summary_dictionary(healthy=1),
+            'details_by_attribute': [{
+                'attribute_name': self.column_name,
+                'validation_outcome': 'healthy'
+            }]
+        }
+
+        self.assertEqual(expected_result, self.uniqueness_checker.validate(self._generate_unique_dataframe([self.column_name], int)))
+
+    @given(dataframes(st.booleans()))
+    def test_uniqueness_checker_using_hypothesis_bools(self, df):
+        self._hypothesis_run_validation_against_same_dataframe(df)
+
+    @given(dataframes(st.text(alphabet=string.ascii_lowercase)))
+    def test_uniqueness_checker_using_hypothesis_strings(self, df):
+        self._hypothesis_run_validation_against_same_dataframe(df)
+
+    @given(dataframes(st.integers()))
+    def test_uniqueness_checker_using_hypothesis_numbers(self, df):
+        self._hypothesis_run_validation_against_same_dataframe(df)
+
+    def _hypothesis_run_validation_against_same_dataframe(self, df):
+        assume(not df.empty)
+
+        uniqueness_checker = UniquenessChecker()
+        with self.assert_does_not_raise():
+            # uniqueness_checker.configure(attributes=list(df.columns))
+            actual_result = uniqueness_checker.validate(df)
+
+            expected_result = {
+                'summary': self._generate_summary_dictionary(healthy=len(df.columns)),
+                'details_by_attribute': [{
+                    'attribute_name': column_name,
+                    'validation_outcome': 'healthy'
+                } for column_name in df.columns]
+            }
+
+            self.assertEqual(expected_result, actual_result)
+
+    def _generate_unique_dataframe(self, column_names, dtype, min=-10, max=10):
+        data = {}
+        for column in column_names:
+            if dtype == int:
+                data[column] = list(range(min, max))
+
+        return pd.DataFrame(data)
+    
+    def _generate_summary_dictionary(self, healthy=0, critical=0, warning=0):
+        return {
+                'healthy': healthy,
+                'critical': critical,
+                'warning': warning
+            }
