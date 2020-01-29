07c501d8d8154126273a5ee1361a79f1e64dbecf
calvinchoi <c.choi@dessa.com>
2020-01-29 11:09:11 -0500

    [CALVIN NN] #1169 | Added fail fast to atlas cypress tests

--- foundations_ui/cypress/support/index.js
+++ foundations_ui/cypress/support/index.js
@@ -1,8 +1,24 @@
-before(() => {
+before(function() {
   cy.exec('python cypress/fixtures/atlas_scheduler/envsubst.py');
+
+  if (Cypress.env('FAIL_FAST')) {
+    cy.task('shouldSkip').then(value => {
+      if (value) {
+        this.skip();
+      }
+    });
+  }
 });
 
-beforeEach(() => {
+beforeEach(function() {
+  if (Cypress.env('FAIL_FAST')) {
+    cy.task('shouldSkip').then(value => {
+      if (value) {
+        this.skip();
+      }
+    });
+  }
+
   const guiHost = Cypress.env('GUI_HOST');
   const proxyPort = Cypress.env('PROXY_PORT');
 
@@ -14,4 +30,10 @@ beforeEach(() => {
       cy.setCookie('atlas_access_token', response.body.access_token);
       cy.setCookie('atlas_refresh_token', response.body.refresh_token);
     });
+});
+
+afterEach(function() {
+  if (this.currentTest.state === 'failed') {
+    return cy.task('shouldSkip', true);
+  }
 });
