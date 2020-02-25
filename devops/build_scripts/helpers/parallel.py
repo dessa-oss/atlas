e2cdc165b65863153aa37043a01cf3c771e13911
Kyle De Freitas <k.defreitas@dessa.com>
2020-02-25 08:23:35 -0500

    [KD] removed copyrights from devops folders

--- devops/build_scripts/helpers/parallel.py
+++ devops/build_scripts/helpers/parallel.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-
-# Copyright (C) DeepLearning Financial Technologies Inc. - All Rights Reserved
-# Unauthorized copying, distribution, reproduction, publication, use of this file, via any medium is strictly prohibited
-# Proprietary and confidential
-# Written by Susan Davis <s.davis@dessa.com>, 12 2019
 
 def _command_process(process_string):
     import subprocess
