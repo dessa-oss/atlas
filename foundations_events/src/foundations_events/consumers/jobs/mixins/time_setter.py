194ec7dd91691fde99ae23ad74846a809a3813de
Kyle De Freitas <k.defreitas@dessa.com>
2020-02-25 08:29:34 -0500

    [KD] removed copyright from missed files

--- foundations_events/src/foundations_events/consumers/jobs/mixins/time_setter.py
+++ foundations_events/src/foundations_events/consumers/jobs/mixins/time_setter.py
@@ -1,9 +1,3 @@
-"""
-Unauthorized copying, distribution, reproduction, publication, use of this file, via any medium is strictly prohibited
-Proprietary and confidential
-Written by Thomas Rogers <t.rogers@dessa.com>, 06 2018
-Copyright (C) DeepLearning Financial Technologies Inc. - All Rights Reserved
-"""
 
 from foundations_events.consumers.jobs.mixins.property_setter import PropertySetter
 
