9e77c3ba3bb1c68a99ae574a352d5d595c7786f8
Kyle De Freitas <k.defreitas@dessa.com>
2020-02-29 23:46:34 -0500

    moved docker files for rest api and gui into their respective folders

--- devops/download_gui_images.sh
+++ devops/download_gui_images.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-build_version=`./docker_image_version.sh`
+build_version=`python get_version.py`
 
 docker pull docker.shehanigans.net/foundations-rest-api:${build_version} && \
     docker pull docker.shehanigans.net/foundations-gui:${build_version}
