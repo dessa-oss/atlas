0cd777dd94fe041522b10104bc35394552ae8463
Kyle De Freitas <kdefreitas@squareup.com>
2020-03-03 16:09:32 -0500

    [KD SR] removed the requirements from atlas-server

--- atlas_server/setup.py
+++ atlas_server/setup.py
@@ -15,9 +15,6 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
-    requirements = f.read().splitlines()
-
 setup(
     name="foundations-atlas",
     use_scm_version = { 'local_scheme': 'dirty-tag' },
@@ -30,6 +27,5 @@ setup(
     packages=find_packages('src'),
     package_dir={'': 'src'},
     scripts=['atlas-server', 'atlas-server.cmd'],
-    python_requires='>=3.6',
-    install_requires=requirements
+    python_requires='>=3.6'
 )
