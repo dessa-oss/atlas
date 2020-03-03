893b1ccf8066c5d1b95daa85fe8396fc9014f944
Kyle De Freitas <kdefreitas@squareup.com>
2020-03-03 14:24:40 -0500

    [KD] removed unused script files

--- ci_install_requirements.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-pip install ansible==2.8.2 boto==2.49.0 boto3==1.9.86 botocore==1.13.42 wget==3.2
-pip install -i https://$NEXUS_USER:$NEXUS_PASSWORD@$NEXUS_PYPI_PATH --extra-index-url https://pypi.org/simple -U -r requirements_test.txt
--- devops/download_gui_images.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-build_version=`python get_version.py`
-
-docker pull docker.shehanigans.net/foundations-rest-api:${build_version} && \
-    docker pull docker.shehanigans.net/foundations-gui:${build_version}
