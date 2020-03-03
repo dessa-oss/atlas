939fa872fd9c9ae90fd55dba3abb91e3a0757efa
Nekhil Nagia <nekhilnagia16@gmail.com>
2020-03-03 16:12:54 -0500

    [NN] changed all references of docker.shehanigans.net/atlas-ce -> docker.shehanigans.net/atlas in the scripts

--- archive_server/build_image.sh
+++ archive_server/build_image.sh
@@ -2,7 +2,7 @@
 
 pip_version=`python get_version.py`
 export build_version=`echo $pip_version | sed 's/+/_/g'`
-export NEXUS_DOCKER_REGISTRY=${NEXUS_DOCKER_REGISTRY:-docker.shehanigans.net/atlas-ce}
+export NEXUS_DOCKER_REGISTRY=${NEXUS_DOCKER_REGISTRY:-docker.shehanigans.net/atlas}
 
 docker build --network=host -t $NEXUS_DOCKER_REGISTRY/archive_server:$build_version . \
     && docker tag $NEXUS_DOCKER_REGISTRY/archive_server:$build_version $NEXUS_DOCKER_REGISTRY/archive_server:latest
--- archive_server/push_image.sh
+++ archive_server/push_image.sh
@@ -2,7 +2,7 @@
 
 pip_version=`python get_version.py`
 export build_version=`echo $pip_version | sed 's/+/_/g'`
-export NEXUS_DOCKER_REGISTRY=${NEXUS_DOCKER_REGISTRY:-docker.shehanigans.net/atlas-ce}
+export NEXUS_DOCKER_REGISTRY=${NEXUS_DOCKER_REGISTRY:-docker.shehanigans.net/atlas}
 
 docker push $NEXUS_DOCKER_REGISTRY/archive_server:$build_version \
     && docker push $NEXUS_DOCKER_REGISTRY/archive_server:latest
