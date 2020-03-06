465cb8bf7de7bf930c9fc3133a3862c25425646f
Kyle De Freitas <kdefreitas@squareup.com>
2020-03-06 01:09:53 -0500

    updated path for building rest api image

--- atlas/foundations_rest_api/build_image.sh
+++ atlas/foundations_rest_api/build_image.sh
@@ -4,24 +4,23 @@ docker_image="foundations-rest-api"
 docker_registry=${DOCKER_REGISTRY:-docker.shehanigans.net}
 pip_build_version=`python get_version.py`
 docker_build_version=$(echo $pip_build_version | sed 's/+/_/g')
-curr=$(pwd)
+curr="$(pwd)/atlas"
 
 image_tag=${docker_registry}/${docker_image}:${docker_build_version}
 image_tag_latest=${docker_registry}/${docker_image}:latest
 
-rm -rf tmp/pip_wheels && \
-    mkdir -p tmp/pip_wheels && \
-    cp dist/* tmp/pip_wheels && \
+rm -rf "${curr}/tmp/pip_wheels" && \
+    mkdir -p "${curr}/tmp/pip_wheels" && \
+    cp "${curr}/dist/*" "${curr}/tmp/pip_wheels" && \
     docker build \
         -t ${image_tag} \
         --network=host \
-        --file foundations_rest_api/Dockerfile \
+        --file ${curr}/foundations_rest_api/Dockerfile \
         --build-arg main_file=run_api_server.py \
-        . &&
+        ${curr} && \
 
     docker tag \
         ${image_tag} \
-        ${image_tag_latest} &&
+        ${image_tag_latest} && \
 
-echo "Successfully built image tagged to ${docker_build_version} and latest" ||
-    echo "Failed to built image"
+echo "Successfully built image tagged to ${docker_build_version} and latest"
