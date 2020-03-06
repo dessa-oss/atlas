6dc55bb020b5a41c3f2316a09719ed992f4d3ffc
Shahzad Raza <raza.shahzad@gmail.com>
2020-03-05 22:26:36 -0500

    Collapsed atlas folder structure (#22)
    * [KD SR] Collapsed all foundations modules into atlas directory

--- activate_dev_env.sh
+++ activate_dev_env.sh
@@ -5,19 +5,19 @@ add_path () {
 }
 
 cwd=`pwd`
-add_path "$cwd/foundations_spec/src" && \
-    add_path "$cwd/foundations_events/src" && \
-    add_path "$cwd/foundations_internal/src" && \
-    add_path "$cwd/foundations_core_cli/src" && \
-    add_path "$cwd/foundations_atlas_cli/src" && \
-    add_path "$cwd/foundations_contrib/src" && \
-    add_path "$cwd/foundations_sdk/src" && \
-    add_path "$cwd/foundations_authentication/src" && \
-    add_path "$cwd/ssh_utils/src" && \
-    add_path "$cwd/gcp_utils/src" && \
-    add_path "$cwd/aws_utils/src" && \
-    add_path "$cwd/foundations_local_docker_scheduler_plugin/src" && \
-    add_path "$cwd/foundations_rest_api/src" && \
-    add_path "$cwd/foundations_core_rest_api_components/src" && \
+add_path "$cwd/atlas/foundations_spec/src" && \
+    add_path "$cwd/atlas/foundations_events/src" && \
+    add_path "$cwd/atlas/foundations_internal/src" && \
+    add_path "$cwd/atlas/foundations_core_cli/src" && \
+    add_path "$cwd/atlas/foundations_atlas_cli/src" && \
+    add_path "$cwd/atlas/foundations_contrib/src" && \
+    add_path "$cwd/atlas/foundations_sdk/src" && \
+    add_path "$cwd/atlas/foundations_authentication/src" && \
+    add_path "$cwd/atlas/ssh_utils/src" && \
+    add_path "$cwd/atlas/gcp_utils/src" && \
+    add_path "$cwd/atlas/aws_utils/src" && \
+    add_path "$cwd/atlas/foundations_local_docker_scheduler_plugin/src" && \
+    add_path "$cwd/atlas/foundations_rest_api/src" && \
+    add_path "$cwd/atlas/foundations_core_rest_api_components/src" && \
 
 source "${cwd}/devops/set_environment_for_dev.sh"
--- run_acceptance_tests.sh
+++ run_acceptance_tests.sh
@@ -9,14 +9,14 @@ echo "Waiting for Atlas GUI to start at http://localhost:3000"
 ./devops/wait_for_url.sh "http://localhost:3000" 80
 
 
-cd "$CWD/testing" || exit
+cd "$CWD/atlas/testing" || exit
 
 for acceptance_directory in $(echo *acceptance)
 do
     python -Wi -m unittest -f -v $acceptance_directory || exit 1
 done
 
-cd "$CWD/foundations_rest_api/src" || exit
+cd "$CWD/atlas/foundations_rest_api/src" || exit
 python -Wi -m unittest -f -v acceptance || exit 1
 
 cd $CWD
