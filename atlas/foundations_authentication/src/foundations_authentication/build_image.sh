2fa3718d323c2d00de7c4bb1fc9cd940047078ca
Kyle De Freitas <kdefreitas@squareup.com>
2020-03-07 22:10:47 -0500

    [KD] split up build and push for auth server docker image

--- atlas/foundations_authentication/src/foundations_authentication/build_image.sh
+++ atlas/foundations_authentication/src/foundations_authentication/build_image.sh
@@ -2,12 +2,12 @@
 
 # NB: File is expected to be executed from root of project
 
-docker_image="foundations-gui"
+docker_image="auth-server"
 docker_registry=${DOCKER_REGISTRY:-docker.shehanigans.net}
 pip_build_version=`python get_version.py`
 docker_build_version=$(echo $pip_build_version | sed 's/+/_/g')
 
-image_dir="$(pwd)/foundations_ui"
+image_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 image_tag=${docker_registry}/${docker_image}:${docker_build_version}
 image_tag_latest=${docker_registry}/${docker_image}:latest
@@ -21,4 +21,4 @@ docker tag \
     ${image_tag} \
     ${image_tag_latest} &&
 
-echo "Successfully built image tagged to ${docker_build_version} and latest"
+echo "Successfully built image ${docker_image} tagged to ${docker_build_version} and latest"
