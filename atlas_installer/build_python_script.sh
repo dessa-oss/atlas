beea367586ec925e5b10f35cf31fae301eccbc51
Nekhil Nagia <nekhilnagia16@gmail.com>
2020-03-09 13:50:17 -0400

    [NN] minor change to get pipeline green

--- atlas_installer/build_python_script.sh
+++ atlas_installer/build_python_script.sh
@@ -4,7 +4,7 @@ source build_tools/get.sh
 
 mkdir -p dist \
     && echo "Building python script..." \
-    && sed "s/There is no version information available./$(get_version)/g" \
+    && sed "s/There is no version information available./$(get_version  ../../)/g" \
         atlas_installer.py > dist/atlas_installer_before_license.py \
     && tar -zcvf licenses.tgz licenses \
     && sed "s/This is the license tar byte string used by the builder do not remove/$(python print_license_tar_bye_string.py)/g" \
