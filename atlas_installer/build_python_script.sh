a389e9f8a2ad897eb932c97f5228baa0125ab872
Nekhil Nagia <nekhilnagia16@gmail.com>
2020-03-09 14:58:17 -0400

    [NN] remove licenses generation for atlas_installer

--- atlas_installer/build_python_script.sh
+++ atlas_installer/build_python_script.sh
@@ -5,11 +5,7 @@ source build_tools/get.sh
 mkdir -p dist \
     && echo "Building python script..." \
     && sed "s/There is no version information available./$(get_version  ../../)/g" \
-        atlas_installer.py > dist/atlas_installer_before_license.py \
-    && tar -zcvf licenses.tgz licenses \
-    && sed "s/This is the license tar byte string used by the builder do not remove/$(python print_license_tar_bye_string.py)/g" \
-        dist/atlas_installer_before_license.py > dist/atlas_installer.py \
-    && rm licenses.tgz dist/atlas_installer_before_license.py \
+        atlas_installer.py > dist/atlas_installer.py \
     && echo "Finished python script"
 
 du -hs dist/*
