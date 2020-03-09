f46527d312b55ba8d7bc22c4b14c472be8c5ccd4
Austin Mackillop <amackillop@squareup.com>
2020-03-09 08:38:39 -0400

    [AM] fix test that doesn't respect custom foundations home

--- atlas/foundations_contrib/src/test/test_log_manager.py
+++ atlas/foundations_contrib/src/test/test_log_manager.py
@@ -32,8 +32,10 @@ class TestLogManager(Spec):
 
     @let
     def file_log_path(self):
+        import os
         import os.path
-        return os.path.expanduser('~/.foundations/logs/system.log')
+        FOUNDATIONS_HOME = os.getenv('FOUNDATIONS_HOME', '~/.foundations')
+        return os.path.expanduser(f'{FOUNDATIONS_HOME}/logs/system.log')
 
     def test_root_logger_has_no_level_set(self):
         self._setup_logger()
