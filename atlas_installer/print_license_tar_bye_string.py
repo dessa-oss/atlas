2b4e971c2156ce8beb41ecde46a2a065c3173d08
Kyle E DeFreitas <kyledef@users.noreply.github.com>
2020-03-10 10:23:42 -0400

    [KD] fixed the building of the installer based on group changes (#35)
    Fixed atlas installer

--- atlas_installer/print_license_tar_bye_string.py
+++ atlas_installer/print_license_tar_bye_string.py
@@ -1,3 +1,8 @@
-with open("licenses.tgz", "rb") as f:
+import sys
+
+tar_file = sys.argv[1]
+
+
+with open(tar_file, "rb") as f:
     b = f.read()
     print(int.from_bytes(b, byteorder='little', signed=True), len(b))
