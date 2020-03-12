325633c0330a316c6b38260dc6cf4bad7445cef4
Zichen Zhao <cindyz@squareup.com>
2020-03-12 13:54:53 -0400

    [CZ] remove push_image scripts from public repo

--- tensorboard/docker/tensorboard_rest_api/push_image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-pip_version=`python get_version.py`
-export build_version=`echo $pip_version | sed 's/+/_/g'`
-export NEXUS_DOCKER_REGISTRY=${NEXUS_DOCKER_REGISTRY:-docker.shehanigans.net/atlas}
-
-docker push $NEXUS_DOCKER_REGISTRY/tensorboard-rest-api:$build_version \
-    && docker push $NEXUS_DOCKER_REGISTRY/tensorboard-rest-api:latest
