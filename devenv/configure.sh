c1ea0f8671d6279d77905ef6ed3049f330e609b4
amackillop <austin.mackillop@gmail.com>
2020-05-12 17:04:25 -0400

    add .docker if missing, add docker-compose

--- devenv/configure.sh
+++ devenv/configure.sh
@@ -4,3 +4,7 @@ for file in $(find .foundations -type f -name "*.config"); do \
     envsubst < $file > $file.yaml
 done
 
+# .docker is missing in github actions runner
+if [ ! -d "$HOME/.docker"]; then
+    mkdir $HOME/.docker
+fi
